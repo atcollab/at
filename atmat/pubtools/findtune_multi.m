8150337ed5af8e8ce42a67f1ef7e0274fb7b21e7
Boaz Nash <boaz.nash@esrf.fr>
2010-04-13 19:44:01 +0000

    add another tune finding algorithm

---
+++ atmat/pubtools/findtune_multi.m
@@ -0,0 +1,75 @@
+function [tune,spectrum]=findtune(pos,method)
+
+if nargin < 2, method=3; end
+
+nturns=size(pos,1);
+nparts=size(pos,2);
+nt2=fix(nturns/2);
+
+posm=mean(pos);
+wrong=~isfinite(posm);
+
+switch method
+case 1
+methname='highest peak';
+pos2=pos-posm(ones(nturns,1),:);
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:)));
+tune=(rmax-1)/nturns;
+
+case 2
+methname='interpolation';
+pos2=pos-posm(ones(nturns,1),:);
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:)));
+rmax=rmax+(rmax==1);
+kmax=sub2ind([nturns nparts],rmax,1:nparts);
+back=(spectrum(kmax-1) > spectrum(kmax+1));
+k1=kmax-back;
+k2=k1+1;
+v1=abs(spectrum(k1));
+v2=abs(spectrum(k2));
+tune=(rmax-back-1 +(v2./(v1+v2)))/nturns;
+
+case 3
+methname='window + interp.';
+w=hann_window(nturns);
+pos2=(pos-posm(ones(nturns,1),:)).*w(:,ones(1,nparts));
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:)));
+rmax=rmax+(rmax==1);
+kmax=sub2ind([nturns nparts],rmax,1:nparts);
+back=(spectrum(kmax-1) > spectrum(kmax+1));
+k1=kmax-back;
+k2=k1+1;
+v1=abs(spectrum(k1));
+v2=abs(spectrum(k2));
+tune=(rmax-back-1 +((2*v2-v1)./(v1+v2)))/nturns;
+%tune2=(rmax-back-1)/nturns + asin(phi(v1,v2,cos(2*pi/nturns))*sin(2*pi/nturns))/2/pi;
+%disp(['method 4 tune: ' num2str(mean2(tune2')) ' (rms: ' num2str(std2(tune2')) ')']);
+
+case 4
+methname='naff';
+pos2=pos-posm(ones(nturns,1),:);
+[tune,amplitude,phase]=specfran(pos2);
+tune=abs(tune);
+
+end
+tune(wrong)=NaN;
+errmax=2.5*std2(tune');
+keep=(abs(tune-mean2(tune'))<=errmax);
+reject=find(~(keep | wrong));
+for bpm=reject
+    [bname,kdx]=srbpmname(bpm);
+    disp(['reject ' bname ' (' num2str(kdx) ')']);
+end
+disp(sprintf('%20s tune:%g (rms:%g)',methname, mean2(tune(keep)'),std2(tune(keep)')));
+
+function vv=phi(a,b,c)
+d1=c*(a+b);
+delt=d1.*d1 - 2*a.*b.*(2*c*c-c-1).*a.*a - b.*b - 2*a.*b*c;
+vv=(-(a+b*c).*(a-b) + b.*sqrt(delt))./(a.*a + b.*b  +2*a.*b*c);
+
+
+function w=hann_window(n)
+w=0.5*(1-cos(2*pi*(0:n-1)'/n));
--- atmat/pubtools/nuampls.m
+++ atmat/pubtools/nuampls.m
@@ -13,13 +13,15 @@ function [nux,nuz]=atnuampl(ring,ampl,xz,plt)
 % amplitude.
 
 if nargin < 3, xz=1; end
+if nargin < 4, plt=0; end
 siza=size(ampl);
 nampl=prod(siza);
-p0=repmat(0.00001*[1;0;1;0;0;0], 1,nampl);
+p0=repmat(0.00001*[1;0;1;0;-2000;0], 1,nampl);
 p0(xz,:)=p0(xz,:)+ampl(:)';
 p1=ringpass(ring,p0,128);
-x1=reshape(p1(1,:),nampl,128)';
+x1=reshape(p1(1,:)-i*p1(2,:),nampl,128)';
 nux=reshape(findtune(reshape(p1(1,:),nampl,[])',3),siza);
+%nux=reshape(findtune(reshape(p1(1,:),nampl,[])'),siza);
 nuz=reshape(findtune(reshape(p1(3,:),nampl,[])',3),siza);
 %plot((ampl.*ampl)',[nux-nux(1);nuz-nuz(1)]','o-');
 if (plt==1)
