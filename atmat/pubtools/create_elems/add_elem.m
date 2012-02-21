e790c891104754148a170e988735764aecd13244
Boaz Nash <boaz.nash@esrf.fr>
2012-02-21 17:57:27 +0000

    add_elem for inserting element into lattice

---
+++ atmat/pubtools/create_elems/add_elem.m
@@ -0,0 +1,7 @@
+function [ ring2 ] = add_elem( ring1, ElemData )
+%add_elem adds an element to the end of a ring and outputs the new one
+%   Detailed explanation goes here
+
+ ring2={ring1{:},ElemData};
+end
+
