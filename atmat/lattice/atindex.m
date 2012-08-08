3d380c60512c80bff75b9a0073f9d288383ef09c
Laurent Farvacque <laurent.farvacque@esrf.fr>
2012-08-08 07:21:29 +0000

    Simplified ATINDEX

--- atmat/lattice/atindex.m
+++ atmat/lattice/atindex.m
@@ -1,32 +1,30 @@
-function indexstruct = atindex(LATTICE,varargin);
+function indexstruct = atindex(lattice,varargin)
 %ATINDEX extracts the information about element families and
 % indexes from AT lattice
 % ATI = ATINDEX(LATTICE)
 %  returns a srtucture with fields named after element family
 %  containing an array of their AT indexes;
-%  
+%
 %   ATI.QF = [...]
 %   ATI.QD = [...];
 %   ...
 % See also FINDCELLS
-indexstruct = struct([]);
-fieldnames = {};
-indexes = {};
-for i = 1:length(LATTICE);
-    
-    if isfield(LATTICE{i},'FamName') & ~isempty(LATTICE{i}.FamName);
-        m = find(strcmp(fieldnames,LATTICE{i}.FamName));
-        if isempty(m)
-            fieldnames{end+1} = LATTICE{i}.FamName;
-            indexes{end+1}{1} = i;
-        else
-            indexes{m}{end+1}=i; 
+
+indexstruct=struct();
+for i = 1:length(lattice);
+    if isfield(lattice{i},'FamName') && ~isempty(lattice{i}.FamName)
+        famname=lattice{i}.FamName;
+        try
+            a.(famname)=0; %#ok<STRNU>
+        catch %#ok<CTCH>
+            famname=['x' famname]; %#ok<AGROW>
         end
+    else
+        famname='unnamed';
+    end
+    if isfield(indexstruct,famname)
+        indexstruct.(famname)(end+1)=i;
+    else
+        indexstruct.(famname)(1)=i;
     end
 end
-
-for m = 1:length(indexes)
-    indexes{m}=cell2mat(indexes{m});
-end
-ARGS = reshape([fieldnames;indexes],1,[]);
-indexstruct = struct(ARGS{:});
