c66b087850ab317671eb78c626c87c4a032aec9f
Boaz Nash <boaz.nash@esrf.fr>
2012-08-14 16:02:58 +0000

    add tool ataddmpolecomppoly.m to add a multipole component to an existing PolynomA or PolynomB

---
+++ atmat/lattice/ataddmpolecomppoly.m
@@ -0,0 +1,28 @@
+function [PolynomOut] = ataddmpolecomppoly(Polynom,refindex,newindex,strength,radius)
+%ataddmpolecomppoly adds a multipole component to an existing polynomial,
+%scaling it by the reference multipole value and a radius to set the length
+%scale
+%[PolynomOut] = ATADDMPOLECOMPPOLY(Polynom,index,strength,radius)
+%
+%Polynom = given field polynomial that we want to add a component to
+%refindex = index of magnet: 1 for dipole, 2 for quadrupole
+%newindex: index of Multipole to add
+%strength: strength of the multipole component at the given radius
+%radius: reference radius for defining the absolute strength
+% B^(N)_(n) = radius^(n-N)*b_n/b_N
+%
+%See also: attiltelem, atshiftelem, atmodelem
+if (Polynom(refindex)==0)
+    error('reference polynomial value is 0');
+else
+    
+    if(newindex > length(Polynom))
+        PolynomOut=[Polynom zeros(1,newindex-length(Polynom))];
+    else
+        PolynomOut=Polynom;
+    end
+    
+    
+    val=power(radius,newindex-refindex)*strength/Polynom(refindex);
+    PolynomOut(newindex)=PolynomOut(newindex)+val;
+end
