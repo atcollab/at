c00755f119648d830eaedb02bbe2c36bc64dbe86
Boaz Nash <boaz.nash@esrf.fr>
2012-08-22 20:52:15 +0000

    add Energy field to atrfcavity element

--- atmat/lattice/element_creation/atrfcavity.m
+++ atmat/lattice/element_creation/atrfcavity.m
@@ -1,5 +1,5 @@
-function Elem=atrfcavity(fname,L,V,F,H,method)
-%ATRFCAVITY(FAMNAME,LENGTH,VOLTAGE,FREQUENCY,HARMONICNUMBER,PASSMETHOD)
+function Elem=atrfcavity(fname,L,V,F,H,E,method)
+%ATRFCAVITY(FAMNAME,LENGTH,VOLTAGE,FREQUENCY,HARMONICNUMBER,ENERGY,PASSMETHOD)
 %	creates an rfcavity element
 %
 %		FamName			family name
@@ -7,12 +7,13 @@ function Elem=atrfcavity(fname,L,V,F,H,method)
 %		Voltage			peak voltage (V)
 %		Frequency		RF frequency [Hz] 
 % 		HarmNumber		Harmonic Number
+%       Energy          Energy in eV          
 %		PassMethod		name of the function on disk to use for tracking
 %
 %See also: ATDRIFT, ATSEXTUPOLE, ATSBEND, ATRBEND
 %          ATMULTIPOLE, ATTHINMULTIPOLE, ATMARKER, ATCORRECTOR
 
-if nargin < 6, method='CavityPass'; end
+if nargin < 7, method='CavityPass'; end
 
 Elem.FamName = fname;  % add check for identical family names
 Elem.Length = L;
@@ -21,4 +22,5 @@ Elem.Frequency = F;
 Elem.HarmNumber = H; %is this required?  not a required arg in pass method
 Elem.PhaseLag = 0;
 Elem.PassMethod=method;
+Elem.Energy=E;
 Elem.Class='RFCavity';
