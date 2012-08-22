4b75782e764c3b2295d9e9b811ca3e28f548e091
Boaz Nash <boaz.nash@esrf.fr>
2012-08-22 19:27:58 +0000

    add atrfcavity basic RF cavity element and make existing rfcavity use this function

---
+++ atmat/lattice/element_creation/atrfcavity.m
@@ -0,0 +1,23 @@
+function Elem=atrfcavity(fname,L,V,F,H,method)
+%ATRFCAVITY(FAMNAME,LENGTH,VOLTAGE,FREQUENCY,HARMONICNUMBER,PASSMETHOD)
+%	creates an rfcavity element
+%
+%		FamName			family name
+%		Length			length[m]
+%		Voltage			peak voltage (V)
+%		Frequency		RF frequency [Hz] 
+% 		HarmNumber		Harmonic Number
+%		PassMethod		name of the function on disk to use for tracking
+%
+%See also: ATDRIFT, ATSEXTUPOLE, ATSBEND, ATRBEND
+%          ATMULTIPOLE, ATTHINMULTIPOLE, ATMARKER, ATCORRECTOR
+
+if nargin < 6, method='CavityPass'; end
+
+Elem.FamName = fname;  % add check for identical family names
+Elem.Length = L;
+Elem.Voltage = V;
+Elem.Frequency = F;
+Elem.HarmNumber = H; %is this required?  not a required arg in pass method
+Elem.PhaseLag = 0;
+Elem.PassMethod=method;
