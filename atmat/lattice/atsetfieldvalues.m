a3e2010c8ccc850a6d6963d73c1fd15f0395a6f6
Laurent Farvacque <laurent.farvacque@esrf.fr>
2013-01-09 12:14:43 +0000

    Added new argument type as a structure whose field names select element family names

--- atmat/lattice/atsetfieldvalues.m
+++ atmat/lattice/atsetfieldvalues.m
@@ -1,10 +1,23 @@
-function ring = atsetfieldvalues(ring,index,varargin)
+function newring = atsetfieldvalues(ring,varargin)
 %ATSETFIELDVALUES sets the field values of MATLAB cell array of structures
 %
 %   Note that the calling syntax must be in the form of assignment:
 %   RING = ATSETFIELDVALUES(RING,...)
 %   MATLAB does not modify variables that only appear on the right
 %   hand side as arguments.
+%
+%NEWRING=ATSETFIELDVALUES(RING,INDEX,....,VALUES)
+%   In this mode, the function will set values on the elements of RING
+%   specified by INDEX, given as a list of indices or as a logical mask
+%
+%NEWRING=ATSETFIELDVALUES(RING,....,VALUESTRUCT)
+%   In this mode, the function will set values on the elements of RING
+%   whose family names are given by the field names of VALUESTRUCT
+%
+%NEWRING=ATSETFIELDVALUES(RING,RINGINDEX,....,VALUESTRUCT)
+%   As in the previous mode, the function will set values on the elements
+%   of RING whose family names are given by the field names of VALUESTRUCT.
+%   But RINGINDEX is provided to avoid multiple computation.
 %
 % Numeric data
 % ---------------------------------------------------------
@@ -38,7 +51,7 @@ function ring = atsetfieldvalues(ring,index,varargin)
 %
 % Each cell of VALUES is affected to the specified field of each selected
 % element of RING.The length of VALUES must be either length(INDEX) or 1
-% (the value will be repeated for each element). 
+% (the value will be repeated for each element).
 %
 % See also ATGETFIELDVALUES ATGETCELLS SETCELLSTRUCT FINDCELLS
 
@@ -46,28 +59,47 @@ if ~iscell(ring) || isempty(ring) || ~isstruct(ring{1})
     error('The first argument must be a non-empty cell array of structures')
 end
 
-if islogical(index)
-    index=find(index);
-end
-
-if ischar(varargin{end})            % Character array
-    values=cellstr(varargin{end});
-elseif isnumeric(varargin{end})     % Numeric data
-    [r c]=size(varargin{end}); %#ok<ASGLU>
-    if c==1
-        values=num2cell(varargin{end});
+if islogical(varargin{1})
+    newring=doset(ring,find(varargin{1}),varargin{2:end});
+elseif isnumeric(varargin{1})
+    newring=doset(ring,varargin{:});
+elseif isstruct(varargin{end})
+    if isstruct(varargin{1})
+        ringidx=varargin{1};
+        ib=2;
     else
-        values=squeeze(num2cell(varargin{end},1:ndims(varargin{end})-1));
+        ringidx=atindex(ring);
+        ib=1;
     end
-else                                % Cell array of strings
-    values=varargin{end};
+    newring=ring;
+    for fn=fieldnames(varargin{end})'
+        fname=char(fn);
+        newring=doset(newring,ringidx.(fname),varargin{ib:end-1},varargin{end}.(fname));
+    end
 end
 
-nvals = numel(index);
-if length(values)==1
-    values=values(ones(nvals,1));
-end
-for idv=1:nvals
-    idx=index(idv);
-    ring{idx}=setfield(ring{idx},varargin{1:end-1},values{idv}); %#ok<SFLD>
+    function ring=doset(ring,index,varargin)
+        if ischar(varargin{end})            % Character array
+            values=cellstr(varargin{end});
+        elseif isnumeric(varargin{end})     % Numeric data
+            [r,c]=size(varargin{end}); %#ok<ASGLU>
+            if c==1
+                values=num2cell(varargin{end});
+            else
+                values=squeeze(num2cell(varargin{end},1:ndims(varargin{end})-1));
+            end
+        else                                % Cell array of strings
+            values=varargin{end};
+        end
+        
+        nvals = numel(index);
+        if length(values)==1
+            values=values(ones(nvals,1));
+        end
+        for idv=1:nvals
+            idx=index(idv);
+            ring{idx}=setfield(ring{idx},varargin{1:end-1},values{idv}); %#ok<SFLD>
+        end
+    end
+
 end
