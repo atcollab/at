deda3725423bf6ab8a3c488ab20e3fcd7690efc3
Simone Liuzzo <simone.liuzzo@esrf.fr>
2013-03-31 01:10:26 +0000

    creates also conditioned variables. allows input of indexes in place of element names.

--- atmat/pubtools/atmatch/atVariableBuilder.m
+++ atmat/pubtools/atmatch/atVariableBuilder.m
@@ -7,6 +7,13 @@ function variable=atVariableBuilder(r,familyname,fieldtochange,LowLim,HighLim)
 % familyname={'QD','SF','Dipole1',...}
 % fieldtochange={{'PolynomB',{1,2}},{'PolynomB',{1,3}},{'BendingAngle',{1}},...}
 % 
+% if fieldtochange is a a cell array of one element, the same fieldtochange
+% will be applied to all familynames.
+% 
+% familyname={@(r,v)function(r,v,...),'SF','Dipole1',...}
+% fieldtochange={init_v_values,{'PolynomB',{1,3}},{'BendingAngle',{1}},...}
+% 
+% 
 % use to create standard variables that change AT lattice structure fields.
 % 
 % to create more sophisticated variables, please use directly the variable
@@ -26,33 +33,44 @@ function variable=atVariableBuilder(r,familyname,fieldtochange,LowLim,HighLim)
 % Providing the Indx as an input rether than recalculating it, is faster.
 % see example on matching.
 % 
-% ex : v=atVariableBuilder(r,familyname,fieldtochange)
+% ex : v=atVariableBuilder(r,{'quad','sext','dip'},{{'PolynomB',{1,2}},{'PolynomB',{1,3}},{'BendingAngle'}})
+% ex : v=atVariableBuilder(r,{'Q1','Q2','Q3'},{{'PolynomB',{1,2}}})
 % ex : v=atVariableBuilder(r,familyname,fieldtochange,LowLim,HighLim)
-% ex : variable=atVariableBuilder(RING,...
-%                                 {'QD','B'},...
-%                                 {{'PolynomB',{1,2}},...
-%                                  {'BendingAngle',{1,1}}
-%                                 })
+% ex : v=atVariableBuilder(r,{@(r,~)matchchrom(r,fam1,fam2)},{[]})
 %                                 
 
 % history of changes
 % created 21-03-2013
+% update 29-03-2013 create many variables with the same parameter field.
+% update 30-03-2013 create function variables.
+
 
 variable=[];
-
-for nstru=1:length(familyname)
+if length(fieldtochange)==1
+    fieldtochange=repmat(fieldtochange,1,length(familyname));
+end
     
+for nstru=1:length(familyname)
     if nargin<4
         LowLim=[];
         HighLim=[];
     end
     
-    var=struct('Indx',findcells(r,'FamName',familyname{nstru}),...
-               'Parameter',{fieldtochange{nstru}},...
-               'LowLim',LowLim,...
-               'HighLim',HighLim...
-               );
-    
+    if isa(familyname{nstru},'function_handle') || isa(familyname{nstru},'numeric')
+        var=struct('Indx',familyname{nstru},...
+            'Parameter',{fieldtochange{nstru}},...
+            'LowLim',LowLim,...
+            'HighLim',HighLim...
+            );
+    else
+        var=struct('Indx',findcells(r,'FamName',familyname{nstru}),...
+            'Parameter',{fieldtochange{nstru}},...
+            'LowLim',LowLim,...
+            'HighLim',HighLim...
+            );
+        
+    end
+
     variable=[variable, var]; %#ok<*AGROW>
 end
 
