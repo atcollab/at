36b3d8258ab826e7f1bb3730d90f0e1e43bae0e9
Boaz Nash <boaz.nash@esrf.fr>
2013-07-18 14:12:30 +0000

    add function to compute dispersion function from the etatoids computed via the one turn map.  Seems to agree at some points, and not at others.  more work needed

---
+++ atmat/pubtools/get_dispersion_from_etaoids.m
@@ -0,0 +1,16 @@
+function disp = get_dispersion_from_etaoids(ring,refpts)
+%get_dispersion_from_etaoids computes dispersion functions (x,px,y,py) at refpts
+%using the etaoids (E. Forest terminology) which are computed from the one turn map
+
+for j=1:length(refpts)
+    k=refpts(j);
+    if k==1
+        newring=ring;
+    else
+        newring=[ring(k:end);ring(1:k-1)];
+    end
+    m66=findm66(newring);
+    A=amat(m66);
+    [H1,H2,H3]=find_etaoids(A);
+    disp(:,j) = H3*[0 0 0 0 1 0]';
+end
