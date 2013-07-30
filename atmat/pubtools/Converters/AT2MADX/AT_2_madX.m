eee361358321312eb34d3487ce8c3b6f10a347e4
Simone Liuzzo <simone.liuzzo@esrf.fr>
2013-07-30 22:28:20 +0000

    AT_2_MADX: imporved help, added Octupoles, added class, added multipoles,sbend dipoles, first line element has no comma.

--- atmat/pubtools/Converters/AT2MADX/AT_2_madX.m
+++ atmat/pubtools/Converters/AT2MADX/AT_2_madX.m
@@ -1,9 +1,23 @@
 function AT_2_madX(AT_ring,linename)
+% function AT_2_madX(AT_ring,linename)
 % this functions converts the AT lattice AT_ring in madX format.
 % 
-% Beta codes are assumed, please modify to suite your needs.
+% a MADX LINE is generated.
+% 
+% file ['' linename '_lattice.madx'] is generated contiaining the lattice
+% elements definitions and the LINE. no other comands introduced
 %
-% last update begin 2012
+% to test in MADX run (replace linename with apropriate name)
+% madx < linename_lattice.madx 
+% 
+% to test with twiss and plot add following lines at the end of file:
+% beam;
+% use, period=linename;
+% twiss;
+% plot, haxis=s, vaxis1=betx,bety, vaxis2=dx;
+% 
+
+outfile=['' linename '_lattice.madx'];
 
 outfile='madXelemdef.elem';
 elelat=['!!\n!!  madX lattice elements: ' linename '\n!!  Created: ' datestr(now) '\n!!\n!!\n\n'];
@@ -20,18 +34,25 @@ format='%8.10f';
 for i=1:length(families)
    el= AT_ring{ind_first_oc_ring(i)};
    if isfield(el,'BetaCode')
-    switch el.('BetaCode')
-        case 'DI' % dipole
+       type=el.BetaCode;
+   elseif isfield(el,'Class')
+       type=el.Class;
+   else
+       type='Marker';
+   end
+       
+    switch type
+        case {'DI','Dipole','Bend'} % dipole
             di=[el.('FamName')   ': '...
-                ' RBEND,L=' num2str(el.('Length'),format) ', '...
+                ' SBEND,L=' num2str(el.('Length'),format) ', '...
                 ' ANGLE = ' num2str(el.('BendingAngle'),format) ', '...
                 ' K1 = ' num2str(el.('PolynomB')(2),format) ', '...
-                ' E1= ' num2str(0*el.('EntranceAngle'),format) ', '...
-                ' E2= ' num2str(0*el.('ExitAngle'),format) '; '...
+                ' E1= ' num2str(el.('EntranceAngle'),format) ', '...
+                ' E2= ' num2str(el.('ExitAngle'),format) '; '...
                ];
            
             elelat=[elelat di '\n']; %#ok<*AGROW>
-        case 'QP' % quadrupole
+        case {'QP','Quadrupole'} % quadrupole
             
             qp=[el.('FamName')   ': '...
                 ' QUADRUPOLE,  L= ' num2str(el.('Length'),format)  ','...
@@ -39,43 +60,42 @@ for i=1:length(families)
                 ];
             
             elelat=[elelat qp '\n'];
-        case 'SX' % sextupole
+        case {'SX','Sextupole'} % sextupole
             sx=[el.('FamName')   ': '...
-                ' KSEXT,  L= ' num2str(el.('Length'),format)  ', '...
-                ' K2= ' num2str(el.('PolynomB')(3),format) ' ;'...
+                ' SEXTUPOLE,  L= ' num2str(el.('Length'),format)  ', '...
+                ' K2= ' num2str(el.('PolynomB')(3)*2,format) ' ;'...
                 ];
             elelat=[elelat sx '\n'];
-        case 'MP' % bpm
-            % el.('PolynomB')
-            ord=find(el.('PolynomB'));
-            if isempty(ord)
-                ord=length(el.('PolynomB'));
-            end
-                mp=[el.('FamName')   ': '...
-                    ' MULT,  L= ' num2str(el.('Length'),format)  ', '...
-                    'ORDER= ' num2str(ord(1)-1) ', &\n' ... 
-                   'KNL= ' num2str(el.('PolynomB')(ord(1)),format) ' ;'...
- %                   'KNL= ' num2str(factorial(ord(1)-1)*el.('PolynomB')(ord(1)),format) ', \n'...
-                %' N_KICKS= ' num2str(ceil(el.('Length')*nkickperL+1),'%d') ' \n'...
+        case {'OC','Octupole'} % sextupole
+            sx=[el.('FamName')   ': '...
+                ' OCTUPOLE,  L= ' num2str(el.('Length'),format)  ', '...
+                ' K3= ' num2str(el.('PolynomB')(4)*6,format) ' ;'...
+                ];
+            elelat=[elelat sx '\n'];
+        case {'MP','Multipole'} % multipole
+            mp=[el.('FamName')   ': '...
+                ' MULTIPOLE,  LRAD= ' num2str(el.('Length'),format)  ', '...
+                'KNL:= {' num2str(el.('PolynomB').*(factorial(0:length(el.('PolynomB'))-1)),[format ', ']) '} ,'...
+                'KSL:= {' num2str(el.('PolynomA').*(factorial(0:length(el.('PolynomA'))-1)),[format ', ']) '};'...
                 ];
             elelat=[elelat mp '\n'];
-        case 'PU' % bpm
+        case {'PU','Monitor'} % bpm
             pu=[el.('FamName') ' :MONITOR' ' ;'...
                 ];
             elelat=[elelat pu '\n'];
-        case 'MRK'% marker
+        case {'MRK','Marker'}% marker
             mrk=[el.('FamName') ' :MARKER' ' ;'...
                 ];
             elelat=[elelat mrk '\n'];
-        case 'KI' % kicker
+        case {'KI','Corrector'} % kicker
             ki=[el.('FamName') ' :KICKER' ' ;'...
                 ];
             
             elelat=[elelat ki '\n'];
-        case 'DR' % drift
+        case {'DR','Drift'} % drift
             dr=[el.('FamName') ' : DRIFT, L= ' num2str(el.('Length'),format) ' ;'];
             elelat=[elelat dr '\n']; 	
-         case 'CAV' % drift
+         case {'CAV','RFCavity'} % drift
             rfc=[el.('FamName') ' : RFCavity, L=' num2str(el.('Length'),format)...
                 ',VOLT='  num2str(el.('Voltage'),format) ' ;'...
                 ', freq=' num2str(el.('Frequency'),format) '' ' ;'];
@@ -84,7 +104,7 @@ for i=1:length(families)
            
               otherwise
     end
-   end
+  
 end
 
 
@@ -95,9 +115,11 @@ elelat=[elelat linename ' : LINE = (' ' '];
 %% define lattice line
 % loop all elements
 for i=1:length(AT_ring)
-    
- elelat=[elelat ',' AT_ring{i}.('FamName') ' '];
-
+    if i~=1
+        elelat=[elelat ',' AT_ring{i}.('FamName') '\n '];
+    else
+        elelat=[elelat ' ' AT_ring{i}.('FamName') '\n '];
+    end
 end
 
 elelat=[elelat ') ;'];
