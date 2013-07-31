cf2631ab82eaa77254f36beccf2b4a3dcd9ff2d0
Boaz Nash <boaz.nash@esrf.fr>
2013-07-31 12:10:14 +0000

    update atringparam: use FamName for structure entry.  can use as ring name.

--- atmat/lattice/element_creation/atringparam.m
+++ atmat/lattice/element_creation/atringparam.m
@@ -1,14 +1,15 @@
-function Elem=atringparam(rname,Energy,per)
+function Elem=atringparam(fname,Energy,per)
 %ATRINGPARAM(rname,E0,per)
 %	creates a RingParameter Element which should go at the beginning of the ring
 %
-%RNAME		name of Ring
+%FNAME		Family name which may be used as name of Ring
 %Energy     Energy of electrons
+%PER        Periodicity of the ring (=1 if ring is already expanded)
 %
 %See also: ATDRIFT, ATQUADRUPOLE, ATSEXTUPOLE, ATSBEND, ATRBEND
 %          ATMULTIPOLE, ATTHINMULTIPOLE
 
-Elem.RingName=rname;
+Elem.FamName=fname;
 Elem.Energy=Energy;
 Elem.Periodicity=per;
 Elem.Length=0;
