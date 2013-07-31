378e5f2bee086f9412a70fafbb4c590d9664ed70
Boaz Nash <boaz.nash@esrf.fr>
2013-07-31 14:53:16 +0000

    remove add_elem function since its functionality included in splitdrift function

--- atmat/pubtools/create_elems/add_elem.m
+++
@@ -1,8 +0,0 @@
-function [ ring2 ] = add_elem( ring, Elem, index )
-%[ ring2 ] = add_elem( ring, Elem, index )
-%add_elem adds an element to a ring at position index and outputs a new one
-ringpart1 = ring(1:index);
-ringpart2 = ring(index+1:end);
-
-% ring2={ringpart1{:}; Elem; ringpart2{:}};
- ring2=[ringpart1;{Elem};ringpart2];
