4db217431f58241afca5145a8e13247b2acb5e44
Laurent Farvacque <laurent.farvacque@esrf.fr>
2013-08-01 13:37:44 +0000

    Rewritten for efficiency

--- atmat/lattice/atgetfieldvalues.m
+++ atmat/lattice/atgetfieldvalues.m
@@ -1,70 +1,61 @@
-function values = atgetfieldvalues(ring,index,varargin)
+function values = atgetfieldvalues(ring,varargin)
 %ATGETFIELDVALUES retrieves the field values AT cell array of elements
 %
+% VALUES = ATGETFIELDVALUES(RING,'field') extracts the values of
+% the field 'field' in all the elements of RING
+%
 % VALUES = ATGETFIELDVALUES(RING,INDEX,'field') extracts the values of
-% the field 'field' in the elements of RING specified by INDEX
+% the field 'field' in the elements of RING selected by INDEX
 %
-% IF RING{I}.FIELD is a string, VALUES is a length(INDEX) x 1 cell array of
-% strings
+% if RING{I}.FIELD is a numeric scalar
+%    VALUES is a length(INDEX) x 1 array
+% otherwise
+%    VALUES is a length(INDEX) x 1 cell array
 %
-% IF RING{I}.FIELD is numeric, VALUES is a multi-dimensional array such
-% that values(:,:,I) is equal to RING{I}.FIELD
 %
 % More generally ATGETFIELDVALUES(RING,INDEX,subs1,subs2,...) will call
 %  GETFIELD(RING{I},subs1,subs2,...) for I in INDEX
 %
-% Example:
-% V=ATGETFIELDVALUES(RING,1:10,'PolynomB') is a 1xNx10 array
-% such that V(:,:,I)=RING{INDEX(I)}.PolynomB for I=1:10
+% Examples:
 %
-% V=ATGETFIELDVALUES(RING,1:10,'PolynomB',{1,2}) is a 1x1x10 array
-% such that V(1,1,I)=RING{INDEX(I)},PolynomB(1,2)
+% V=ATGETFIELDVALUES(RING,1:10,'PolynomB') is a 10x1  cell array
+% such that V{I}=RING{I}.PolynomB for I=1:10
+%
+% V=ATGETFIELDVALUES(RING(1:10),'PolynomB',{1,2}) is a 10x1 array
+% such that V(I)=RING{I},PolynomB(1,2)
 %
 %
 % See also ATSETFIELDVALUES ATGETCELLS GETCELLSTRUCT FINDCELLS
 
-if ~iscell(ring) || isempty(ring) || ~isstruct(ring{1})
-    error('The first argument must be a non-empty cell array of structures')
+if islogical(varargin{1}) || isnumeric(varargin{1})
+    values=atgetfield(ring(varargin{1}),varargin{2:end});
+else
+    values=atgetfield(ring,varargin{:});
 end
 
-if islogical(index)
-    index=find(index);
-end
-
-nvals = numel(index);
-
-vals=cell(nvals,1);
-vnum=true;
-classnam='double';
-%vchar=true;
-dims=[0 0];
-for idv=1:nvals
-    try
-        vv=getfield(ring{index(idv)},varargin{:});
-        vals{idv}=vv;
-        if isnumeric(vv)
-%            vchar=false;
-            dims=max([dims;size(vv)]);
-            classnam=class(vv);
-        else
-            vnum=false;
+    function values = atgetfield(line,varargin)        
+        [values,isnum,isscal,isok]=cellfun(@scan,line,'UniformOutput',false);
+        isok=cat(1,isok{:});
+        if all(cat(1,isscal{isok}))
+            if all(cat(1,isnum{isok}))
+                values(~isok)={NaN};
+                values=cat(1,values{:});
+            end
         end
         
-    catch %#ok<*CTCH>
-    end
-end
-
-if vnum
-    values = NaN([dims nvals],classnam);
-    for idv = 1:nvals
-        try
-            [vl,vc]=size(vals{idv});
-            values(1:vl,1:vc,idv) = vals{idv};
-        catch
+        function [val,isnum,isscal,isok]=scan(el)
+            try
+                val=getfield(el,varargin{:});
+                isnum=isnumeric(val);
+                isscal=isscalar(val);
+                isok=true;
+            catch
+                val=[];
+                isnum=false;
+                isscal=false;
+                isok=false;
+            end
         end
     end
-else
-    values=vals;
+
 end
-
-
--- atmat/lattice/getcellstruct.m
+++ atmat/lattice/getcellstruct.m
@@ -15,39 +15,5 @@ function values = getcellstruct(CELLARRAY,field,index,varargin)
 %
 % See also ATGETFIELDVALUES SETCELLSTRUCT FINDCELLS
 
-if(~iscell(CELLARRAY) || ~isstruct(CELLARRAY{1}) || isempty(CELLARRAY))
-    error('The first argument must be a non-empty cell array of structures')
+values=atgetfieldvalues(CELLARRAY(index),field,varargin);
 end
-% Chechk if the second argument is a string
-if(~ischar(field))
-    error('The second argument ''field'' must be a character string')
-end
-
-% cell array 'mn' here is used as comma separated list
-% to pass as an argument to getfield.
-if nargin > 3
-    mn=varargin;
-else
-    mn={1};
-end
-
-selcells=CELLARRAY(index);
-NV = length(selcells);
-if NV==0
-    values=zeros(NV,1);
-elseif isnumeric(selcells{1}.(field))
-    values = zeros(NV,1,class(selcells{1}.(field)));
-    for I = 1:NV
-        values(I) = getfield(selcells{I},field,mn);
-    end
-elseif ischar(selcells{1}.(field))
-    values = cell(NV,1);
-    for I = 1:NV
-        values{I} = selcells{I}.(field);
-    end
-else
-    error('The field data must be numeric or character array')
-end
-
-
-
