c66dbcdd806962fc401cf351293075b3eb88d30e
Laurent Farvacque <laurent.farvacque@esrf.fr>
2013-09-25 14:58:48 +0000

    Plots multipoles

--- atmat/pubtools/atplot/atplotsyn.m
+++ atmat/pubtools/atplot/atplotsyn.m
@@ -7,42 +7,39 @@ ylim=get(ax,'YLim');
 
 sl=findspos(ring(:,1),1:size(ring,1)+1);
 ll=diff(sl);
-b3=atgetfieldvalues(ring(:,1),'PolynomB',{3});
-b2=atgetfieldvalues(ring(:,1),'PolynomB',{2});
 
-dipoles=atgetcells(ring(:,1),'BendingAngle',@(elem,bangle) bangle~=0);
-l=ll(dipoles);
-s=sl(dipoles);
-lplot=zeros(5,length(l));
-lplot(3:4,:)=[l;l];
-xplot=s(ones(5,1),:)+lplot;
-yplot=zeros(5,length(l));
-yplot(2:3,:)=0.05*ylim(2);
-%p1=patch(xplot,yplot,[0.5 0.5 1],'FaceAlpha',0.2);
+[dipoles,qpoles,spoles,mpoles]=cellfun(@eltype,ring);
+
+ampl=0.05*ylim(2);
+[xplot,yplot]=setxpl(sl(dipoles),ll(dipoles),[0;0;1;1;0],[0;ampl;ampl;0;0]);
 p1=patch(xplot,yplot,[0.5 0.5 1]);
 
-qpoles=isfinite(b2) & b2~=0 & ~dipoles;
-strength=b2(qpoles)';
-l=ll(qpoles);
-s=sl(qpoles);
-lplot=zeros(6,length(l));
-lplot(3:5,:)=[0.5*l;l;l];
-xplot=s(ones(6,1),:)+lplot;
-yplot=zeros(6,length(l));
-yplot(2:4,:)=0.05*ylim(2);
-yplot(3,:)=yplot(3,:)+0.02*ylim(2)*sign(strength);
+ampl=0.05*ylim(2);
+foc=0.02*ylim(2)*sign(atgetfieldvalues(ring(qpoles),'PolynomB',{2}))';
+[xplot,yplot]=setxpl(sl(qpoles),ll(qpoles),[0;0;0.5;1;1;0],[0;ampl;ampl;ampl;0;0]);
+yplot(3,:)=yplot(3,:)+foc;
 p2=patch(xplot,yplot,[1 0.5 0.5]);
 
-spoles=isfinite(b3) & b3~=0 & ~dipoles & ~qpoles;
-strength=b3(spoles)';
-l=ll(spoles);
-s=sl(spoles);
-lplot=zeros(6,length(l));
-lplot(3:5,:)=[0.5*l;l;l];
-xplot=s(ones(6,1),:)+lplot;
-yplot=zeros(6,length(l));
-yplot(2:4,:)=0.03*ylim(2);
-yplot(3,:)=yplot(3,:)+0.01*ylim(2)*sign(strength);
+ampl=0.04*ylim(2);
+foc=0.01*ylim(2)*sign(atgetfieldvalues(ring(spoles),'PolynomB',{3}))';
+[xplot,yplot]=setxpl(sl(spoles),ll(spoles),[0;0;0.33;0.66;1;1;0],[0;ampl;ampl;ampl;ampl;0;0]);
+yplot(3:4,:)=yplot(3:4,:)+foc([1;1],:);
 p3=patch(xplot,yplot,[0.5 1 0.5]);
-pp=[p1;p2;p3];
+
+ampl=0.03*ylim(2);
+[xplot,yplot]=setxpl(sl(mpoles),ll(mpoles),[0;0;1;1;0],[0;ampl;ampl;0;0]);
+p4=patch(xplot,yplot,[0 0.5 0]);
+
+pp=[p1;p2;p3;p4];
+
+    function [xpl,ypl]=setxpl(s,l,xmotif,ymotif)
+        nm=length(xmotif);
+        xpl=s(ones(nm,1),:)+l(ones(nm,1),:).*xmotif(:,ones(1,length(s),1));
+        ypl=ymotif(:,ones(1,length(s),1));
+    end
+
+    function varargout=eltype(elem)
+        tst=strcmp(atguessclass(elem),{'Bend','Quadrupole','Sextupole','Multipole'});
+        varargout=num2cell(tst);
+    end
 end
