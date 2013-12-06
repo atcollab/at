0313d717f74622be46cb6d3a178eba99e54419fa
Boaz Nash <boaz.nash@esrf.fr>
2013-12-06 21:08:26 +0000

    add functions to plot projection of beam on x-y axis at different places around the ring

---
+++ atmat/pubtools/local_lattice_params/atmakeXYProjectionEllipse.m
@@ -0,0 +1,22 @@
+function [x,y] =atmakeXYProjectionEllipse( sigxx,sigyy,sigxy )
+%atmakeXYProjectionEllipse( sigxx,sigyy,sigxy ) gives points to plot the contour ellipses
+%given the second spatial moments <x^2>, <y^2>, and <xy> 
+
+%we solve for effective Twiss parameters and emittance.
+%Note that since this is x-y space, this is just a computational tool
+%This emittance is not constant around ring.
+
+%first compute epsilon
+eps=sqrt(sigxx.*sigyy-sigxy.^2);
+%now compute beta,gamma,alpha
+beta=sigxx./eps;
+gamma=sigyy./eps;
+alpha=-sigxy./eps;
+
+t=linspace(0,2*pi);
+xh=sqrt(eps)*cos(t);
+yh=sqrt(eps)*sin(t);
+
+x=sqrt(beta)*xh;
+y=(-alpha/sqrt(beta))*xh+(1/sqrt(beta))*yh;
+
---
+++ atmat/pubtools/local_lattice_params/plotContours.m
@@ -0,0 +1,18 @@
+function h=plotContours(ring,indices)
+%h=plotdriftcontours(ring)
+%load S25ERRseed1.mat
+
+[dat,prm]=atx(ring);
+sig=cat(3,dat.beam66);
+
+sigxx=squeeze(sig(1,1,:));
+sigxy=squeeze(sig(1,3,:));
+sigyy=squeeze(sig(3,3,:));
+
+%dr278=findcells(ring,'FamName','DR_278');
+
+hold on
+for j=indices
+    [x,y]=atmakeXYProjectionEllipse(sigxx(j),sigyy(j),sigxy(j));
+    plot(x,y)
+end
