eacc6cf8691f9946668103a1628b6cabe2e48a68
Laurent Farvacque <laurent.farvacque@esrf.fr>
2014-02-12 14:34:03 +0000

    Support for MATLAB R2014a

--- atintegrators/mexpassmethod.m
+++ atintegrators/mexpassmethod.m
@@ -7,45 +7,55 @@ function mexpassmethod(PASSMETHODS, varargin)
 %  'all' - option automatically detects all C files matching *Pass.c pattern
 %
 % The second argument is a list of options passed to the 'mex' script
-% 
+%
 % Examples: mexpassmethod('DriftPass','-v')
 %           mexpassmethod('all','-argcheck')
 %           mexpassmethod({'DriftPass','BendLinearPass'})
 %
-% Note:  MEXPASSMETHOD automatically determines the host 
-% platform and costructs -D<PLATFORM> option to feed to the 
-% mex script. All pass-methods #incude elempass.h header file
+% Note:  MEXPASSMETHOD automatically determines the host
+% platform and costructs -D<PLATFORM> option to feed to the
+% mex script. All pass-methods #include elempass.h header file
 % which uses #if defined(PLATFORM) directive to select
 % between platform-specific branches
 %
 % See also: file:elempass.h
 
-pdir=fileparts(mfilename('fullpath'));
-PLATFORMOPTION = ['-D',computer,' '];
-if nargin >= 2, PLATFORMOPTION = varargin{1}; end
+PLATFORMOPTION=[varargin{:}];
 
 %Additional platform-specific options for mex
-switch computer
-case 'SOL2'
-    PLATFORMOPTION = [PLATFORMOPTION,'LDFLAGS=''-G -mt -M ',fullfile(pdir,'mexFunctionSOL2.map'),''' '];
-case 'SOL64'
-    PLATFORMOPTION = [PLATFORMOPTION,'LDFLAGS=''-G -mt -M ',fullfile(pdir,'mexFunctionSOL2.map'),''' '];
-case 'GLNX86'
-    PLATFORMOPTION = [PLATFORMOPTION,'LDFLAGS=''-pthread -shared -m32 -Wl,--no-undefined -Wl,--version-script,',fullfile(pdir,'mexFunctionGLNX86.map'),''' '];  
-case 'GLNXA64'
-    PLATFORMOPTION = [PLATFORMOPTION,'LDFLAGS=''-pthread -shared -Wl,--no-undefined -Wl,--version-script,',fullfile(pdir,'mexFunctionGLNX86.map'),''' '];  
-case 'MAC'
-    PLATFORMOPTION = [PLATFORMOPTION 'LDFLAGS=''-bundle -undefined dynamic_lookup -Wl,-exported_symbols_list,' fullfile(pdir,'mexFunctionMAC.map') ''' '];  
-case 'MACI'
-    PLATFORMOPTION = [PLATFORMOPTION 'LDFLAGS=''-bundle -undefined error -arch i386  -mmacosx-version-min=10.5 -Wl,-exported_symbols_list,' fullfile(pdir,'mexFunctionMAC.map') ''' '];  
-case 'MACI64'
-    PLATFORMOPTION = [PLATFORMOPTION 'LDFLAGS=''-bundle -undefined error -arch x86_64 -mmacosx-version-min=10.5 -Wl,-exported_symbols_list,' fullfile(pdir,'mexFunctionMAC.map') ''' '];  
+if ~ispc()
+    pdir=fileparts(mfilename('fullpath'));
+    switch computer
+        case {'SOL2','SOL64'}
+            exportarg='-M';
+            map='mexFunctionSOL2.map';
+            ldflags=['-G -mt ',exportarg,' ' fullfile(pdir,map)];
+        case 'GLNX86'
+            exportarg='-Wl,--version-script,';
+            map='mexFunctionGLNX86.map';
+            ldflags=['-pthread -shared -m32 -Wl,--no-undefined ',exportarg,fullfile(pdir,map)];
+        case 'GLNXA64'
+            exportarg='-Wl,--version-script,';
+            map='mexFunctionGLNX86.map';
+            ldflags=['-pthread -shared -Wl,--no-undefined ',exportarg,fullfile(pdir,map)];
+        case {'MAC','MACI','MACI64'}
+            exportarg='-Wl,-exported_symbols_list,';
+            map='mexFunctionMAC.map';
+    end
+    
+    if ~exist('verLessThan') || verLessThan('matlab','7.6') %#ok<EXIST>
+        PLATFORMOPTION=[PLATFORMOPTION ' LDFLAGS=''',ldflags,''' '];
+    elseif verLessThan('matlab','8.3')
+        ldf=regexprep(mex.getCompilerConfigurations('C').Details.LinkerFlags,['(' exportarg '\s?)([^\s,]+)'],['$1',fullfile(pdir,map)]);
+        PLATFORMOPTION=[PLATFORMOPTION,' LDFLAGS=''',strrep(ldf,'$','\$'),''' '];
+    else
+        PLATFORMOPTION=[PLATFORMOPTION,' LINKEXPORT=''',exportarg,fullfile(pdir,map),''' '];
+    end
 end
-
 
 if ischar(PASSMETHODS) % one file name - convert to a cell array
     if strcmpi(PASSMETHODS,'all')
-        % Find all files matching '*Pass.c' wildcard   
+        % Find all files matching '*Pass.c' wildcard
         D = dir('*Pass.c');
         PASSMETHODS = cell(size(D));
         for i = 1:length(D)
@@ -57,21 +67,15 @@ if ischar(PASSMETHODS) % one file name - convert to a cell array
 end
 
 for i = 1:length(PASSMETHODS)
-
-        PM = PASSMETHODS{i};
-        evalin('base',['clear ',PM]);
+    PM = PASSMETHODS{i};
+    evalin('base',['clear ',PM]);
+    if exist(fullfile(pwd,[PM '.c']),'file')
         MEXSTRING = ['mex ',PLATFORMOPTION,PM,'.c'];
-        
-        %message = sprintf('%s\n',MEXSTRING);
-        %disp(message);
-        
-        if exist([pwd,'\',PM,'.c'],'file') | exist ([pwd,'/',PM,'.c'],'file') 
-            disp(MEXSTRING);
-            evalin('base',MEXSTRING);
-        else 
-            disp([PM,'.c',' - NOT FOUND! SKIP']); 
-        end
-        
+        disp(MEXSTRING);
+        evalin('base',MEXSTRING);
+    else
+        disp([PM,'.c',' - NOT FOUND! SKIP']);
+    end
 end
 
 
