9265517af0778983ad4dbb8dd3fa0a1000dd78f6
Laurent Farvacque <laurent.farvacque@esrf.fr>
2014-08-13 14:35:27 +0000

    allow 2x2 and 4x4 matrices

--- atmat/atphysics/amat.m
+++ atmat/atphysics/amat.m
@@ -1,5 +1,10 @@
-function a=amat(m66)
-%find A matrix from one turn map matrix
+function a=amat(transmat)
+%find A matrix from one turn map matrix T such that:
+%
+%           [Rotx  0    0  ]
+%inv(A)*T*A=[ 0   Rotz  0  ]
+%           [ 0    0   Rots]
+%
 %Order it so that it is close to the order of x,y,z
 %also ensure that positive modes are before negative so that
 %one has proper symplecticity
@@ -9,41 +14,44 @@ function a=amat(m66)
 
 persistent Vxyz jm
 if isempty(Vxyz)
-    jm=jmat(3);
-    Vxyz=1/sqrt(2)*[...
-        -1i -1 0 0 0 0;...
-        1 1i 0 0 0 0;...
-        0 0 -1i -1 0 0;...
-        0 0 1 1i 0 0;...
-        0 0 0 0 -1i -1;...
-        0 0 0 0 1 1i;...
-        ];
+    Vxyz={...
+        1/sqrt(2)*[-1i -1;1 1i;],...
+        1/sqrt(2)*[-1i -1 0 0;1 1i 0 0;0 0 -1i -1;0 0 1 1i;],...
+        1/sqrt(2)*[-1i -1 0 0 0 0;1 1i 0 0 0 0;0 0 -1i -1 0 0;...
+        0 0 1 1i 0 0; 0 0 0 0 -1i -1;0 0 0 0 1 1i;]...
+        };
+    jm={jmat(1),jmat(2),jmat(3)};
 end
 
-[V,~]=eig(m66);
+nv=size(transmat,1);
+dms=nv/2;
+idx=reshape(1:nv,2,dms);
+select=idx(1,:);
+
+[V,~]=eig(transmat);
 
 %compute norms of each:
-Vp=V'*jm;
-n=-0.5*1i*sum(Vp.'.*V);
+Vp=V'*jm{dms};
+n=-0.5i*sum(Vp.'.*V);
 
 %put positive modes before negative modes (swap columns if first n in pair
 %is negative)
-order=[1 1 3 3 5 5]+(n<0);
+order=reshape(idx([1 1],:),1,nv) + (n<0);
 V=V(:,order);
 n=n(order);
 
 %now normalize each vector
-Vn=V./repmat(sqrt(abs(n)),6,1);
+Vn=V./repmat(sqrt(abs(n)),nv,1);
 
 %find the vecs that project most onto x,y,z, and reorder
-nn=0.5*abs(sqrt(-1i*Vn'*jm*Vxyz));
-[~,ind]=max(nn([1 3 5],[1 3 5]));
+nn=0.5*abs(sqrt(-1i*Vn'*jm{dms}*Vxyz{dms}));
+[~,ind]=max(nn(select,select));
 
 %reorder pairs
 V_ordered=Vn(:,2*ind-1);
 
 %build a matrix
-a=reshape([real(V_ordered);imag(V_ordered)],6,6);
+a=reshape([real(V_ordered);imag(V_ordered)],nv,nv);
 
 end
 
