8283a59806e1eb78d572aeb0648a578a085a351b
Laurent Farvacque <laurent.farvacque@esrf.fr>
2014-09-16 09:13:22 +0000

    Add an optional "DPP" argument for off-momentum calculation

--- atmat/atphysics/mcf.m
+++ atmat/atphysics/mcf.m
@@ -1,13 +1,17 @@
-function a = mcf(RING)
+function a = mcf(RING,dp0)
 %MCF momentum compaction factor
 % MCF(RING) calculates momentum compaction factor of RING
-dP = 0.000001;
-fp0 = findorbit4(RING,0);
-fp = findorbit4(RING,dP);
+%
+% MCF(RING,DPP) computes the momentum compaction for off-momentum DPP
+
+if nargin < 2, dp0=0; end
+ddp = 0.000001;
+fp0 = findorbit4(RING,dp0);
+fp = findorbit4(RING,dp0+ddp);
 % Build initial condition vector that starts
 % on the fixed point
 X0dP = fp;
-X0dP(5) = dP;
+X0dP(5) = ddp;
 X0dP(6) = 0;
 
 X0 = [fp0;0;0];
@@ -16,5 +20,5 @@ X0 = [fp0;0;0];
 T = ringpass(RING,[X0 X0dP]);
 % Calculate alpha
 RingLength = findspos(RING,length(RING)+1);
-a = (T(6,2)-T(6,1))/(dP*RingLength);
+a = (T(6,2)-T(6,1))/(ddp*RingLength);
 
