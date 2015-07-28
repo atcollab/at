4bd8f090376479dd137ffdb1eb3a12270bbe33c0
Laurent Farvacque <laurent.farvacque@esrf.fr>
2015-07-28 14:46:35 +0000

    Moved plClosedOrbit to the plotfunctions directory

--- atmat/pubtools/plotfunctions/plClosedOrbit.m
+++ atmat/pubtools/plotfunctions/plClosedOrbit.m
@@ -1,15 +1,24 @@
-function plotdata=plClosedOrbit(lindata,ring,dpp,varargin) %#ok<INUSD>
-%DEFAULTPLOT    Default plotting function for ATPLOT
+function varargout=plClosedOrbit(varargin)
+%PLCLOSEDORBIT    Plot H and V closed orbits
 %
-%Plots beta-functions on left axis and dispersion on right axis
+%Helper function for atplot: plot
+%- H and V closed orbits on left axis
+%- Dispersion on right axis
 
-CoD=cat(2,lindata.ClosedOrbit);
-plotdata(1).values=[CoD(1,:)' CoD(3,:)'];
-plotdata(1).labels={'x','z'};
-plotdata(1).axislabel='y_{co} and x_{co} [m]';
-dispersion=cat(2,lindata.Dispersion)';
-plotdata(2).values=dispersion(:,3);
-plotdata(2).labels={'\eta_y'};
-plotdata(2).axislabel='vertical dispersion [m]';
-
+if nargout == 1
+    lindata=varargin{1};
+    CoD=cat(2,lindata.ClosedOrbit)';
+    plotdata(1).values=CoD(:,[1 3]);
+    plotdata(1).labels={'x','z'};
+    plotdata(1).axislabel='y_{co} and x_{co} [m]';
+    dispersion=cat(2,lindata.Dispersion)';
+    plotdata(2).values=dispersion(:,3);
+    plotdata(2).labels={'\eta_y'};
+    plotdata(2).axislabel='vertical dispersion [m]';
+    varargout={plotdata};
+else
+    refpts=1:length(varargin{1})+1;
+    [lindata,tune,chrom]=atlinopt(varargin{1:2},refpts); %#ok<ASGLU>
+    varargout={cat(1,lindata.SPos),plClosedOrbit(lindata,varargin{1:2})};
+end
 end
