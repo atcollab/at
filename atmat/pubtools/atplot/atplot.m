a04efcdacf8e429a0856253da213dd574550f1af
Laurent Farvacque <laurent.farvacque@esrf.fr>
2015-07-28 14:35:46 +0000

    added general function "atbaseplot"

--- atmat/pubtools/atplot/atplot.m
+++ atmat/pubtools/atplot/atplot.m
@@ -1,4 +1,4 @@
-function curve = atplot(varargin)
+function curve = atplot(varargin) %#ok<FNDEF>
 %ATPLOT Plots optical functions
 %
 %ATPLOT                 Plots THERING in the current axes
@@ -12,21 +12,23 @@ function curve = atplot(varargin)
 %ATPLOT(...,[SMIN SMAX])  Zoom on the specified range
 %
 %ATPLOT(...,'OptionName',OptionValue,...) Available options:
-%	'labels',REFPTS             Display the selected element names
-%	'leftargs',{properties}     properties set on the left axis
-%	'rightargs',{properties}    properties set on the right axis
+%   'comment',true|false        Prints lattice information (default:true)
+%   'synopt',true|false         Plots the lattice elements
+%   'labels',REFPTS             Display the names of selected element names
+%   'leftargs',{properties}     properties set on the left axis
+%   'rightargs',{properties}    properties set on the right axis
 %
 %ATPLOT(...,@PLOTFUNCTION,args...)
 %	Allows for a user supplied function providing the values to be plotted
 %   PLOTFUNCTION must be of form:
 %   PLOTDATA=PLOTFUNCTION(LINDATA,RING,DPP,args...)
 %
-%       PLOTDATA is a structure array,
-%       PLOTDATA(1) describes data for the left axis
-%           PLOTDATA(1).VALUES: data to be plotted length(ring)+1 X nbcurve
-%           PLOTDATA(1).LABELS: curve labela, cell array, 1 X nbcurve
+%       PLOTDATA: structure array,
+%         PLOTDATA(1) describes data for the left (main) axis
+%           PLOTDATA(1).VALUES: data to be plotted, length(ring)+1 x nbcurve
+%           PLOTDATA(1).LABELS: curve labels, cell array, 1 x nbcurve
 %           PLOTDATA(1).AXISLABEL: string
-%       PLOTDATA(2) optional, describes data for the left axis
+%         PLOTDATA(2) optional, describes data for the right (secondary) axis
 %
 %   The default function displayed below as an example plots beta functions
 %	and dispersion
@@ -43,143 +45,57 @@ function curve = atplot(varargin)
 % end
 %
 %CURVE=ATPLOT(...) Returns handles to some objects:
+%   CURVE.PERIODICIY ring periodicity
+%   CURVE.LENGTH    structure length
+%   CURVE.DPP       deltap/p
 %   CURVE.LEFT      Handles to the left axis plots
 %   CURVE.RIGHT     Handles to the right axis plots
-%   CURVE.LATTICE	Handles to the Element patches: structure with fields
+%   CURVE.LATTICE   Handles to the Element patches: structure with fields
 %          Dipole,Quadrupole,Sextupole,Multipole,BPM,Label
-%   CURVE.COMMENT	Handles to the Comment text
+%   CURVE.COMMENT   Handles to the Comment text
 %
-%See also: atplot>defaultplot
-global THERING
+%
+%ATPLOT calls the more general ATBASEPLOT function, which uses a slightly
+%different syntax.
+%
+%See also: atbaseplot
 
-npts=400;
-narg=1;
-plotfun=@defaultplot;
-% Select axes for the plot
-if narg<=length(varargin) && isscalar(varargin{narg}) && ishandle(varargin{narg});
-    ax=varargin{narg};
-    narg=narg+1;
-else
-    ax=gca;
+funcarg=find(cellfun(@(arg) isa(arg,'function_handle'),varargin),1);
+if isempty(funcarg)
+    funcarg=nargin+1;
 end
-% Select the lattice
-if narg<=length(varargin) && iscell(varargin{narg});
-    [elt0,nperiods,ring0]=get1cell(varargin{narg});
-    narg=narg+1;
-else
-    [elt0,nperiods,ring0]=get1cell(THERING);
+resarg=find(cellfun(@(arg) ischar(arg),varargin(1:funcarg-1)),1);
+if isempty(resarg)
+    resarg=funcarg;
 end
+options=varargin(resarg:funcarg-1);
+[comment,options]=getoption(options,'comment',true);
 
-% Select the momentum deviation
-if narg<=length(varargin) && isscalar(varargin{narg}) && isnumeric(varargin{narg});
-    dpp=varargin{narg};
-    narg=narg+1;
-else
-    dpp=0;
-end
-s0=findspos(ring0,1:elt0+1);
-srange=s0([1 end]);     %default plot range
-el1=1;
-el2=elt0+1;
+lindata=[];
 
-plotarg=nargin+1;
-synarg=nargin+1;
-for iarg=narg:nargin
-    % explicit plot range
-    if isnumeric(varargin{iarg}) && (numel(varargin{iarg})==2)
-        srange=varargin{iarg};
-        els=find(srange(1)>s0,1,'last');
-        if ~isempty(els), el1=els; end
-        els=find(s0>srange(2),1,'first');
-        if ~isempty(els), el2=els; end
-    elseif isa(varargin{iarg},'function_handle')
-        plotfun=varargin{iarg};
-        plotarg=iarg+1;
-        break
-    else
-        synarg=iarg;
-        break
-    end
+curve=atbaseplot(varargin{1:resarg-1},@ringplot,varargin(funcarg:end),...
+    options{:});
+
+if (comment)
+    set(get(curve.left(1),'Parent'),'Position',[.13 .11 .775 .775]);
+    tuneper=lindata(end).mu/2/pi;
+    tunes=curve.periodicity*tuneper;
+    circumference=curve.periodicity*curve.length;
+    if curve.periodicity > 1, plural='s'; else plural=''; end
+    line1=sprintf('\\nu_x=%8.3f      \\deltap/p=%.3f%i %s',tunes(1),curve.dpp);
+    line2=sprintf('\\nu_z=%8.3f      %2i %s, C=%10.3f',tunes(2),...
+        curve.periodicity,['period' plural],circumference);
+    curve.comment=text(-0.14,1.12,{line1;line2},'Units','normalized',...
+        'VerticalAlignment','top','FontSize',12);
 end
 
-rsrc=(varargin(synarg:end));
-[leftargs,rsrc]=getoption(rsrc,'leftargs',{});
-[rightargs,rsrc]=getoption(rsrc,'rightargs',{});
-%ring=[ring0((1:el1-1)');atslice(ring0(el1:el2-1),250);ring0((el2:elt0)')];
-%ring=[ring0(1:el1-1,1);atslice(ring0(el1:el2-1,1),400);ring0(el2:elt0,1)];
-elmlength=findspos(ring0(el1:el2-1,1),el2-el1+1)/npts;
-ring=ring0(1:el1-1,1);
-cellfun(@splitel,ring0(el1:el2-1,1));
-ring=[ring;ring0(el2:elt0,1)];
-elt=length(ring);
-plrange=el1:el2+elt-elt0;
-[lindata,tuneper,chrom]=atlinopt(ring,dpp,1:elt+1); %#ok<ASGLU>
-s=cat(1,lindata.SPos);
-
-set(ax,'Position',[.13 .11 .775 .775]);
-outp=plotfun(lindata,ring,dpp,varargin{plotarg:nargin});
-if numel(outp) >= 2
-    [ax2,h1,h2]=plotyy(ax,s(plrange),outp(1).values(plrange,:),s(plrange),outp(2).values(plrange,:));
-    set(ax2(1),leftargs{:},'FontSize',12);
-    set(ax2(2),rightargs{:},'XTick',[],'FontSize',12);
-    ylabel(ax2(1),outp(1).axislabel);
-    ylabel(ax2(2),outp(2).axislabel);
-elseif numel(outp) == 1
-    h1=plot(ax,s(plrange),outp(1).values(plrange,:));
-    h2=[];
-    ax2=ax;
-    set(ax2(1),leftargs{:},'FontSize',12);
-    ylabel(ax2(1),outp(1).axislabel);
-else
-    h1=[];
-    h2=[];
-    set(ax,leftargs{:},'YLim',[0 1]);
-    ax2=ax;
-end
-set(ax2,'XLim',srange);
-curve.left=h1;
-curve.right=h2;
-[curve.lattice]=atplotsyn(ax2(1),ring0,rsrc{:});  % Plot lattice elements
-lts=get(ax2(1),'Children');                 % Put them in the background
-nsy=length(lts)-length(h1);
-set(ax2(1),'Children',lts([nsy+1:end 1:nsy]));
-xlabel(ax2(1),'s [m]');                     % Add labels
-if ~isempty([h1;h2])
-    set([h1;h2],'LineWidth',1);
-    labels=[outp.labels];
-    legend([h1;h2],labels{:});
-    %labels=[outp.labels synlabels];
-    %legend([h1;h2;curve.lattice],labels{:});
-end
-grid on
-% axes(ax);
-linkaxes([ax2(1) ax2(2)],'x');% allows zoom on both right and left plots
-tuneper=lindata(end).mu/2/pi;
-tunes=nperiods*tuneper;
-circ=nperiods*s0(end);
-if nperiods > 1, plural='s'; else plural=''; end
-line1=sprintf('\\nu_x=%8.3f      \\deltap/p=%.3f%i %s',tunes(1),dpp);
-line2=sprintf('\\nu_z=%8.3f      %2i %s, C=%10.3f',tunes(2),nperiods,...
-    ['period' plural],circ);
-curve.comment=text(-0.14,1.12,{line1;line2},'Units','normalized',...
-    'VerticalAlignment','top','FontSize',12);
-
-    function splitel(elem)
-        if isfield(elem,'Length') && elem.Length > 0
-            nslices=ceil(elem.Length/elmlength);
-            newelems=atdivelem(elem,ones(1,nslices)./nslices);
+    function [s,plotdata]=ringplot(ring,dpp,plotfun,varargin)
+        [lindata,tune,chrom]=atlinopt(ring,dpp,1:length(ring)+1); %#ok<ASGLU>
+        s=cat(1,lindata.SPos);
+        if nargin < 3
+            plotdata=defaultplot(lindata,ring,dpp);
         else
-            newelems={elem};
-        end
-        ring=[ring;newelems];
-    end
-
-    function [cellsize,np,cell]=get1cell(ring)
-        [cellsize,np]=size(ring);
-        cell=ring(:,1);
-        params=atgetcells(cell,'Class','RingParam');
-        if any(params)
-            np=ring{find(params,1)}.Periodicity;
+            plotdata=plotfun(lindata,ring,dpp,varargin{:});
         end
     end
 end
