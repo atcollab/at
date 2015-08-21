e915f94533f4ae0692f92d3bcf6896280de9de2c
Laurent Farvacque <laurent.farvacque@esrf.fr>
2015-08-21 14:52:30 +0000

    Added "findtune" and "atnuampl"

---
+++ atmat/atphysics/atnuampl.m
@@ -0,0 +1,69 @@
+function varargout=atnuampl(ring,ampl,xz,varargin)
+%ATNUAMPL	computes tune shift with amplitude
+%[NUX,NUZ]=ATNUAMPL(RING,AMPLITUDE)
+%[NUX,NUZ]=ATNUAMPL(RING,AMPLITUDE,1)
+%
+%	Computes tunes for the specified horizontal amplitudes
+%
+%[NUX,NUZ]=ATNUAMPL(RING,AMPLITUDE,3)
+%
+%	Computes tunes for the specified vertical amplitudes
+%
+%ATNUAMPL(...)
+%   Plots the computed tunes in the current axes
+%
+%ATNUAMPL(...,Name,Value)
+%   Uses additional options specified by one or more Name,Value pairs.
+%   Possible values are:
+%       nturns: specify the number of turns for tracking (default 256)
+%       method: specify the method for tune determination
+%               1: Highest peak in fft
+%               2: Interpolation on fft results
+%               3: Windowing + interpolation (default)
+%               4: NAFF
+%   Other options are transmitted to the plot function
+
+
+lab={'x^2','p_x^2','z^2','p_z^2'};
+if nargin < 3, xz=1; end
+if ~isempty(varargin) && isnumeric(varargin{1})
+    orbit=varargin{1};
+    varargin(1)=[];
+else
+    warning off MATLAB:singularMatrix
+    orbit=findorbit6(ring);
+    warning on MATLAB:singularMatrix
+    if ~all(isfinite(orbit))
+        orbit=zeros(6,1);
+        orbit(1:5)=findsyncorbit(ring,0);
+    end
+end
+[nturns,varargin]=getoption(varargin,'nturns',256);
+[method,varargin]=getoption(varargin,'method',3);
+
+[~,nbper]=atenergy(ring);
+[lindata,fractune0]=atlinopt(ring,0,1:length(ring)+1);
+tune0=nbper*lindata(end).mu/2/pi;
+offs=[nbper -nbper];
+siza=size(ampl);
+nampl=prod(siza);
+p0=repmat(0.00003*[1;0;1;0;0;0], 1,nampl); % 30 microns minimum amplitude
+p0(xz,:)=max(p0(xz,:),ampl(:)');
+p0=p0+orbit(:,ones(1,nampl));
+p1=ringpass(ring,p0,nturns)-orbit(:,ones(1,nampl*nturns));
+tunetrack=[findtune(reshape(p1(1,:),nampl,nturns)',method);...
+    findtune(reshape(p1(3,:),nampl,nturns)',method)]';
+[~,k]=min([fractune0-tunetrack(1,:); 1-fractune0-tunetrack(1,:)]);
+np=offs(k);
+offset=round(tune0-np.*tunetrack(1,:));
+tunetrack=np(ones(nampl,1),:).*tunetrack + offset(ones(nampl,1),:);
+if nargout > 0
+    varargout={reshape(tunetrack(:,1),siza),reshape(tunetrack(:,2),siza)};
+else
+    inttunes=floor(tune0);
+    plot((ampl.*ampl)',tunetrack-inttunes(ones(nampl,1),:),'o-',varargin{:});
+    legend('\nu_x','\nu_z');
+    xlabel(lab{xz});
+    ylabel('\nu');
+    grid on
+end
---
+++ atmat/atphysics/findtune.m
@@ -0,0 +1,81 @@
+function [tune,spectrum]=findtune(pos,method)
+%FINDTUNE   get the tune value from turn by tun positions
+%
+%TUNE=FINDTUNE(POS,METHOD)
+%
+%POS:       Tune-by-turn particle position
+%METHOD:    Method for tune determination:
+%               1: Highest peak in fft
+%               2: Interpolation on fft results
+%               3: Windowing + interpolation
+%
+%[TUNE,SPECTRUM]=FINDTUNE(...) Also returns the fft
+
+
+if nargin < 2, method=3; end
+
+nturns=size(pos,1);
+nparts=size(pos,2);
+nt2=fix(nturns/2);
+
+posm=mean(pos);
+wrong=~isfinite(posm);
+
+switch method
+case 1
+methname='highest peak';
+pos2=pos-posm(ones(nturns,1),:);
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:))); %#ok<ASGLU>
+tune=(rmax-1)/nturns;
+
+case 2
+methname='interpolation';
+pos2=pos-posm(ones(nturns,1),:);
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:))); %#ok<ASGLU>
+rmax=rmax+(rmax==1);
+kmax=sub2ind([nturns nparts],rmax,1:nparts);
+back=(spectrum(kmax-1) > spectrum(kmax+1));
+k1=kmax-back;
+k2=k1+1;
+v1=abs(spectrum(k1));
+v2=abs(spectrum(k2));
+tune=(rmax-back-1 +(v2./(v1+v2)))/nturns;
+
+case 3
+methname='window + interp.';
+w=hann_window(nturns);
+pos2=(pos-posm(ones(nturns,1),:)).*w(:,ones(1,nparts));
+spectrum=fft(pos2);
+[vmax,rmax]=max(abs(spectrum(1:nt2,:))); %#ok<ASGLU>
+rmax=rmax+(rmax==1);
+kmax=sub2ind([nturns nparts],rmax,1:nparts);
+back=(spectrum(kmax-1) > spectrum(kmax+1));
+k1=kmax-back;
+k2=k1+1;
+v1=abs(spectrum(k1));
+v2=abs(spectrum(k2));
+tune=(rmax-back-1 +((2*v2-v1)./(v1+v2)))/nturns;
+%tune2=(rmax-back-1)/nturns + asin(phi(v1,v2,cos(2*pi/nturns))*sin(2*pi/nturns))/2/pi;
+%disp(['method 4 tune: ' num2str(mean2(tune2')) ' (rms: ' num2str(std2(tune2')) ')']);
+
+end
+tune(wrong)=NaN;
+errmax=2.5*std2(tune');
+keep=(abs(tune-mean2(tune'))<=errmax);
+reject=find(~(keep | wrong));
+for bpm=reject
+    [bname,kdx]=sr.bpmname(bpm);
+    disp(['reject ' bname ' (' num2str(kdx) ')']);
+end
+fprintf('%20s tune:%g (rms:%g)\n',methname, mean2(tune(keep)'),std2(tune(keep)'));
+
+function vv=phi(a,b,c) %#ok<DEFNU>
+d1=c*(a+b);
+delt=d1.*d1 - 2*a.*b.*(2*c*c-c-1).*a.*a - b.*b - 2*a.*b*c;
+vv=(-(a+b*c).*(a-b) + b.*sqrt(delt))./(a.*a + b.*b  +2*a.*b*c);
+
+
+function w=hann_window(n)
+w=0.5*(1-cos(2*pi*(0:n-1)'/n));
