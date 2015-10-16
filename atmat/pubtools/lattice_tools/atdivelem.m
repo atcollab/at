6e40cdfc046f1f5316e02a78df21f3d898fa7f46
Laurent Farvacque <laurent.farvacque@esrf.fr>
2015-10-16 08:57:27 +0000

    Corrected a bug where T1,T2,R1,R2,FringeInt1,FringeInt2 where not correctly set when splitting an element

--- atmat/pubtools/lattice_tools/atdivelem.m
+++ atmat/pubtools/lattice_tools/atdivelem.m
@@ -19,6 +19,8 @@ function line = atdivelem(elem,frac)
 % See also ATINSERTELEMS ATSLICE ATSPLITELEM
 
 line=atsetfieldvalues(repmat({elem},length(frac),1),'Length',elem.Length*frac(:));
+line(2:end)=cellfun(@(elem) rmfield(elem,{'T1','R1'}),line(2:end),'UniformOutput',false);
+line(1:end-1)=cellfun(@(elem) rmfield(elem,{'T2','R2'}),line(1:end-1),'UniformOutput',false);
 if isfield(elem,'BendingAngle')
     line=atsetfieldvalues(line,'BendingAngle',elem.BendingAngle*frac(:)/sum(frac));
 end
@@ -31,6 +33,16 @@ if isfield(elem,'BendingAngle')
     drexitangle=zeros(size(line));
     drexitangle(end)=elem.ExitAngle;
     line=atsetfieldvalues(line,'ExitAngle',drexitangle);
+end
+if isfield(elem,'FringeInt1')
+    fringe1=zeros(size(line));
+    fringe1(1)=elem.FringeInt1;
+    line=atsetfieldvalues(line,'FringeInt1',fringe1);
+end
+if isfield(elem,'FringeInt2')
+    fringe2=zeros(size(line));
+    fringe2(end)=elem.FringeInt2;
+    line=atsetfieldvalues(line,'FringeInt1',fringe2);
 end
 
 end
