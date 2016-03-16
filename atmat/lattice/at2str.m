38e5942d957ec15ac7319cc6c1a4063be800ee92
Laurent Farvacque <laurent.farvacque@esrf.fr>
2016-03-16 16:26:53 +0000

    More robust when processing incomplete elements

--- atmat/lattice/at2str.m
+++ atmat/lattice/at2str.m
@@ -14,49 +14,51 @@ switch atclass
         create=@atquadrupole;
         args={elem.Length,elem.PolynomB(2)};
         [options,args]=doptions(elem,create,{'Length','K'},{'PolynomB','PolynomA'},args);
-        if ~any(abs(elem.PolynomA)>0)
-            options=rmfield(options,'PolynomA');
+        if ~any(abs(getfield2(elem,'PolynomA',0))>0)
+            options=rmfield2(options,{'PolynomA'});
         end
-        anom=abs(elem.PolynomB)~=0;
+        anom=abs(getfield2(elem,'PolynomB',0))~=0;
         anom(2)=false;
         if ~any(anom)
-            options=rmfield(options,'PolynomB');
+            options=rmfield2(options,{'PolynomB'});
         end
     case 'Sextupole'
         create=@atsextupole;
         args={elem.Length,elem.PolynomB(3)};
         [options,args]=doptions(elem,create,{'Length'},{'PolynomB','PolynomA'},args);
-        if ~any(abs(elem.PolynomA)>0)
-            options=rmfield(options,'PolynomA');
+        if ~any(abs(getfield2(elem,'PolynomA',0))>0)
+            options=rmfield2(options,{'PolynomA'});
         end
-        anom=abs(elem.PolynomB)~=0;
+        anom=abs(getfield2(elem,'PolynomB',0))~=0;
         anom(3)=false;
         if ~any(anom)
-            options=rmfield(options,'PolynomB');
+            options=rmfield2(options,{'PolynomB'});
         end
     case 'Bend'
         args={elem.Length,elem.BendingAngle,elem.PolynomB(2)};
         [options,args]=doptions(elem,@atsbend,{'Length','BendingAngle','K'},...
             {'EntranceAngle','ExitAngle','PolynomB','PolynomA'},args);
-        if all([elem.EntranceAngle elem.ExitAngle] == 0)
-            create=@atsbend;
-            options=rmfield(options,{'EntranceAngle','ExitAngle'});
-        else
+        angle1=getfield2(elem,'EntranceAngle',0);
+        angle2=getfield2(elem,'ExitAngle',0);
+        if all([angle1 angle2] == 0.5*elem.BendingAngle)
             create=@atrbend;
-            if elem.EntranceAngle == 0.5*elem.BendingAngle
-                options=rmfield(options,'EntranceAngle');
+            options=rmfield2(options,{'EntranceAngle','ExitAngle'});
+        else
+            create=@atsbend;
+            if angle1 == 0
+                options=rmfield2(options,{'EntranceAngle'});
             end
-            if elem.ExitAngle == 0.5*elem.BendingAngle
-                options=rmfield(options,'ExitAngle');
+            if angle2 == 0
+                options=rmfield2(options,{'ExitAngle'});
             end
         end
-        if ~any(abs(elem.PolynomA)>0)
-            options=rmfield(options,'PolynomA');
+        if ~any(abs(getfield2(elem,'PolynomA',0))>0)
+            options=rmfield2(options,{'PolynomA'});
         end
-        anom=abs(elem.PolynomB)~=0;
+        anom=abs(getfield2(elem,'PolynomB',0))~=0;
         anom(2)=false;
         if ~any(anom)
-            options=rmfield(options,'PolynomB');
+            options=rmfield2(options,{'PolynomB'});
         end
     case 'Corrector'
         create=@atcorrector;
@@ -101,6 +103,19 @@ fmt=['%15s(%s' repmat(',%s',1,length(varg)-1) ')'];
 strargs=cellfun(@(arg) mat2str(reshape(arg,[],size(arg,2))),varg,'UniformOutput',false);
 elstr=sprintf(fmt,func2str(create),strargs{:});
 
+    function s2 = rmfield2(s1,fieldnames)
+        ok=cellfun(@(fn) isfield(s1,fn), fieldnames);
+        s2=rmfield(s1,fieldnames(ok));
+    end
+
+    function result = getfield2(s,varargin)
+        try
+            result=getfield(s,varargin{1:end-1});
+        catch
+            result=varargin{end};
+        end
+    end
+
     function [opts,args]=doptions(elem,create,argn,dontcheck,args)
         if nargin<4, dontcheck={}; end
         if nargin<3, argn={}; end
@@ -111,11 +126,11 @@ elstr=sprintf(fmt,func2str(create),strargs{:});
             args=[args,{elem.PassMethod}];
         end
         argnames=[{'FamName'},argn,{'PassMethod'}];
-        defel=rmfield(defelem,[argnames,dontcheck]);    % Keep only optional fields
+        defel=rmfield(defelem,[argnames,dontcheck]);	% Keep only optional fields
         fnames=fieldnames(defel)';
         fvalues=struct2cell(defel)';
         ok=isfield(elem,fnames);                        % Check for default values
         ok(ok)=cellfun(@(fname,fval) isequal(elem.(fname),fval),fnames(ok),fvalues(ok));
-        opts=rmfield(elem,[argnames,fnames(ok)]);       % Keep only non-default values
+        opts=rmfield2(elem,[argnames,fnames(ok)]);      % Keep only non-default values
     end
 end
