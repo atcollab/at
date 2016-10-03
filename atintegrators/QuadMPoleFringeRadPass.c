e53bec7684e39b007e32696ee98f75e29771d647
Nicola Carmignani <nicola.carmignani@esrf.rf>
2016-10-03 08:46:39 +0000

    three radiation pass methods have now the track function and they are compatible with python at

--- atintegrators/QuadMPoleFringeRadPass.c
+++ atintegrators/QuadMPoleFringeRadPass.c
@@ -1,4 +1,4 @@
-#include "at.h"
+#include "atelem.c"
 #include "atlalib.c"
 #include "driftkickrad.c"	/* drift6.c, strthinkickrad.c */
 #include "quadfringe.c"		/* QuadFringePassP, QuadFringePassN */
@@ -8,6 +8,24 @@
 #define KICK1     1.351207191959657328
 #define KICK2    -1.702414383919314656
 
+struct elem
+{
+    double Length;
+    double *PolynomA;
+    double *PolynomB;
+    int MaxOrder;
+    int NumIntSteps;
+    double Energy;
+    /* Optional fields */
+    double *R1;
+    double *R2;
+    double *T1;
+    double *T2;
+    double *RApertures;
+    double *EApertures;
+    double *fringeIntM0;
+    double *fringeIntP0;
+};
 
 void QuadMPoleFringeRadPass(double *r, double le, const double *A, const double *B,
         double E0, int max_order, int num_int_steps,
@@ -132,130 +150,77 @@ void QuadMPoleFringeRadPass(double *r, double le, const double *A, const double 
     }
 }
 
-#ifdef MATLAB_MEX_FILE
-
-#include "elempass.h"
-#include "mxutils.c"
-
-ExportMode int* passFunction(const mxArray *ElemData, int *FieldNumbers,
-        double *r_in, int num_particles, int mode)
-#define NUM_FIELDS_2_REMEMBER 14
+#if defined(MATLAB_MEX_FILE) || defined(PYAT)
+ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
+        double *r_in, int num_particles, struct parameters *Param)
 {
-    double *A , *B;
-    double  *pr1, *pr2, *pt1, *pt2, *RApertures, *EApertures, *fringeIntM0, *fringeIntP0;
-    int max_order, num_int_steps;
-    double le, E0;
-    
-    switch(mode) {
-        case MAKE_LOCAL_COPY: 	/* Find field numbers first
-         * Save a list of field number in an array
-         * and make returnptr point to that array
-         */
-            /* Allocate memory for integer array of
-             * field numbers for faster futurereference
-             */
-            FieldNumbers = (int *) mxCalloc(NUM_FIELDS_2_REMEMBER, sizeof(int));
-            
-            /*  Populate */
-            
-            FieldNumbers[0] = GetRequiredFieldNumber(ElemData, "PolynomA");
-            FieldNumbers[1] = GetRequiredFieldNumber(ElemData, "PolynomB");
-            FieldNumbers[2] = GetRequiredFieldNumber(ElemData, "MaxOrder");
-            FieldNumbers[3] = GetRequiredFieldNumber(ElemData, "NumIntSteps");
-            FieldNumbers[4] = GetRequiredFieldNumber(ElemData, "Length");
-            FieldNumbers[5] = GetRequiredFieldNumber(ElemData, "Energy");
-            
-            FieldNumbers[6] = mxGetFieldNumber(ElemData,"R1");
-            FieldNumbers[7] = mxGetFieldNumber(ElemData,"R2");
-            FieldNumbers[8] = mxGetFieldNumber(ElemData,"T1");
-            FieldNumbers[9] = mxGetFieldNumber(ElemData,"T2");
-            FieldNumbers[10] = mxGetFieldNumber(ElemData,"RApertures");
-            FieldNumbers[11] = mxGetFieldNumber(ElemData,"EApertures");
-            FieldNumbers[12] = mxGetFieldNumber(ElemData,"fringeIntM0");
-            FieldNumbers[13] = mxGetFieldNumber(ElemData,"fringeIntP0");
-            
-            /* Fall through next section... */
-            
-        case	USE_LOCAL_COPY:	/* Get fields from MATLAB using field numbers
-         * The second argument pointer to the array of field
-         * numbers is previously created with
-         * QuadMPoleFringeRadPass(..., MAKE_LOCAL_COPY)
-         */
-            A = mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[0]));
-            B = mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[1]));
-            max_order = (int)mxGetScalar(mxGetFieldByNumber(ElemData, 0, FieldNumbers[2]));
-            num_int_steps = (int)mxGetScalar(mxGetFieldByNumber(ElemData, 0, FieldNumbers[3]));
-            le = mxGetScalar(mxGetFieldByNumber(ElemData,0,FieldNumbers[4]));
-            E0 = mxGetScalar(mxGetFieldByNumber(ElemData,0,FieldNumbers[5]));
-            
-            /* Optional fields */
-            pr1 = (FieldNumbers[6] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[6])) : NULL;
-            pr2 = (FieldNumbers[7] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[7])) : NULL;
-            pt1 = (FieldNumbers[8] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[8])) : NULL;
-            pt2 = (FieldNumbers[9] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[9])) : NULL;
-            RApertures = (FieldNumbers[10] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[10])) : NULL;
-            EApertures = (FieldNumbers[11] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[11])) : NULL;
-            fringeIntM0 = (FieldNumbers[12] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[12])) : NULL;
-            fringeIntP0 = (FieldNumbers[13] >= 0) ? mxGetPr(mxGetFieldByNumber(ElemData, 0, FieldNumbers[13])) : NULL;
-            break;
-            
-        default:
-            mexErrMsgTxt("No match for calling mode in function QuadMPoleFringeRadPass\n");
-            
+    if (!Elem) {
+        double Length=atGetDouble(ElemData,"Length"); check_error();
+        double *PolynomA=atGetDoubleArray(ElemData,"PolynomA"); check_error();
+        double *PolynomB=atGetDoubleArray(ElemData,"PolynomB"); check_error();
+        int MaxOrder=atGetLong(ElemData,"MaxOrder"); check_error();
+        int NumIntSteps=atGetLong(ElemData,"NumIntSteps"); check_error();
+        double Energy=atGetDouble(ElemData,"Energy"); check_error();
+        /*optional fields*/
+        double *R1=atGetOptionalDoubleArray(ElemData,"R1"); check_error();
+        double *R2=atGetOptionalDoubleArray(ElemData,"R2"); check_error();
+        double *T1=atGetOptionalDoubleArray(ElemData,"T1"); check_error();
+        double *T2=atGetOptionalDoubleArray(ElemData,"T2"); check_error();
+        double *EApertures=atGetOptionalDoubleArray(ElemData,"EApertures"); check_error();
+        double *RApertures=atGetOptionalDoubleArray(ElemData,"RApertures"); check_error();
+        double *fringeIntM0=atGetOptionalDoubleArray(ElemData,"fringeIntM0"); check_error();
+        double *fringeIntP0=atGetOptionalDoubleArray(ElemData,"fringeIntP0"); check_error();
+        Elem = (struct elem*)atMalloc(sizeof(struct elem));
+        Elem->Length=Length;
+        Elem->PolynomA=PolynomA;
+        Elem->PolynomB=PolynomB;
+        Elem->MaxOrder=MaxOrder;
+        Elem->NumIntSteps=NumIntSteps;
+        Elem->Energy=Energy;
+        /*optional fields*/
+        Elem->R1=R1;
+        Elem->R2=R2;
+        Elem->T1=T1;
+        Elem->T2=T2;
+        Elem->EApertures=EApertures;
+        Elem->RApertures=RApertures;
+        Elem->fringeIntM0=fringeIntM0;
+        Elem->fringeIntP0=fringeIntP0;
     }
-    
-    QuadMPoleFringeRadPass(r_in, le, A, B, E0, max_order, num_int_steps,
-            pt1, pt2, pr1, pr2, fringeIntM0, fringeIntP0, RApertures, EApertures, num_particles);
-    return FieldNumbers;
+    QuadMPoleFringeRadPass(r_in,Elem->Length,Elem->PolynomA,Elem->PolynomB,
+            Elem->Energy,Elem->MaxOrder,Elem->NumIntSteps,Elem->T1,Elem->T2,Elem->R1,Elem->R2,
+            Elem->fringeIntM0,Elem->fringeIntP0,Elem->RApertures,Elem->EApertures,num_particles);
+    return Elem;
 }
+#endif /*defined(MATLAB_MEX_FILE) || defined(PYAT)*/
 
+#if defined(MATLAB_MEX_FILE)
 void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-    if (nrhs==2) {
+    if (nrhs == 2 ) {
         double *r_in;
-        double *pr1, *pr2, *pt1, *pt2, *RApertures, *EApertures, *fringeIntM0, *fringeIntP0;
-        mxArray *tmpmxptr;
-        
-        double *A = mxGetPr(GetRequiredField(prhs[0], "PolynomA"));
-        double *B = mxGetPr(GetRequiredField(prhs[0], "PolynomB"));
-        int max_order = (int) mxGetScalar(GetRequiredField(prhs[0], "MaxOrder"));
-        int num_int_steps = (int) mxGetScalar(GetRequiredField(prhs[0], "NumIntSteps"));
-        double le = mxGetScalar(GetRequiredField(prhs[0], "Length"));
-        double E0 = mxGetScalar(GetRequiredField(prhs[0], "Energy"));
+        const mxArray *ElemData = prhs[0];
         int num_particles = mxGetN(prhs[1]);
-        if (mxGetM(prhs[1]) != 6) mexErrMsgIdAndTxt("AT:WrongArg","Second argument must be a 6 x N matrix");
-        
-        
-        /* Optional arguments */
-        tmpmxptr = mxGetField(prhs[0],0,"R1");
-        pr1 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"R2");
-        pr2 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"T1");
-        pt1 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"T2");
-        pt2 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"RApertures");
-        RApertures = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"EApertures");
-        EApertures = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"fringeIntM0");
-        fringeIntM0 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
-        tmpmxptr = mxGetField(prhs[0],0,"fringeIntP0");
-        fringeIntP0 = tmpmxptr ? mxGetPr(tmpmxptr) : NULL;
-        
+        double Length=atGetDouble(ElemData,"Length"); check_error();
+        double *PolynomA=atGetDoubleArray(ElemData,"PolynomA"); check_error();
+        double *PolynomB=atGetDoubleArray(ElemData,"PolynomB"); check_error();
+        int MaxOrder=atGetLong(ElemData,"MaxOrder"); check_error();
+        int NumIntSteps=atGetLong(ElemData,"NumIntSteps"); check_error();
+        double Energy=atGetDouble(ElemData,"Energy"); check_error();
+        /*optional fields*/
+        double *R1=atGetOptionalDoubleArray(ElemData,"R1"); check_error();
+        double *R2=atGetOptionalDoubleArray(ElemData,"R2"); check_error();
+        double *T1=atGetOptionalDoubleArray(ElemData,"T1"); check_error();
+        double *T2=atGetOptionalDoubleArray(ElemData,"T2"); check_error();
+        double *EApertures=atGetOptionalDoubleArray(ElemData,"EApertures"); check_error();
+        double *RApertures=atGetOptionalDoubleArray(ElemData,"RApertures"); check_error();
+        double *fringeIntP0=atGetOptionalDoubleArray(ElemData,"fringeIntP0"); check_error();
+        double *fringeIntM0=atGetOptionalDoubleArray(ElemData,"fringeIntM0"); check_error();
         /* ALLOCATE memory for the output array of the same size as the input  */
         plhs[0] = mxDuplicateArray(prhs[1]);
         r_in = mxGetPr(plhs[0]);
-        QuadMPoleFringeRadPass(r_in, le, A, B, E0, max_order, num_int_steps,
-                pt1, pt2, pr1, pr2, fringeIntM0, fringeIntP0, RApertures, EApertures, num_particles);
+        QuadMPoleFringeRadPass(r_in,Length,PolynomA,PolynomB,Energy,MaxOrder,NumIntSteps,
+                T1,T2,R1,R2,fringeIntM0,fringeIntP0,RApertures,EApertures,num_particles);
     }
     else if (nrhs == 0) {
         /* list of required fields */
@@ -266,7 +231,6 @@ void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         mxSetCell(plhs[0],3,mxCreateString("MaxOrder"));
         mxSetCell(plhs[0],4,mxCreateString("NumIntSteps"));
         mxSetCell(plhs[0],5,mxCreateString("Energy"));
-        
         if (nlhs>1) {
             /* list of optional fields */
             plhs[1] = mxCreateCellMatrix(8,1);
@@ -284,4 +248,4 @@ void mexFunction(	int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         mexErrMsgIdAndTxt("AT:WrongArg","Needs 0 or 2 arguments");
     }
 }
-#endif
+#endif /* MATLAB_MEX_FILE */
