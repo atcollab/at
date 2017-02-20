8a127aed0d0ce3f41554c5d2b750b07118777390
Laurent Farvacque <laurent.farvacque@esrf.fr>
2017-02-20 16:52:33 +0000

    Use THERING if no argument is given

--- atmat/lattice/atindex.m
+++ atmat/lattice/atindex.m
@@ -1,30 +1,48 @@
-function indexstruct = atindex(lattice,varargin)
+function indexstruct = atindex(lattice)
 %ATINDEX extracts the information about element families and
 % indexes from AT lattice
-% ATI = ATINDEX(LATTICE)
+%
+%ATI=ATINDEX(LATTICE)
 %  returns a srtucture with fields named after element family
 %  containing an array of their AT indexes;
 %
 %   ATI.QF = [...]
 %   ATI.QD = [...];
 %   ...
-% See also FINDCELLS
+%
+%ATI=ATINDEX() Uses the global variable THERING
+%
+% See also ATGETCELLS
 
-indexstruct=struct();
-for i = 1:length(lattice);
-    if isfield(lattice{i},'FamName') && ~isempty(lattice{i}.FamName)
-        famname=lattice{i}.FamName;
-        try
-            a.(famname)=0; %#ok<STRNU>
-        catch %#ok<CTCH>
-            famname=['x' famname]; %#ok<AGROW>
+if nargin < 1
+    indexstruct=ati(THERING);
+else
+    indexstruct=ati(lattice);
+end
+
+    function ids=ati(lattice)
+        ids=struct();
+        for i=1:length(lattice);
+            try
+                famname=lattice{i}.FamName;
+                try
+                    a.(famname)=0;
+                catch %#ok<CTCH>
+                    try
+                        famname=['x' famname]; %#ok<AGROW>
+                        a.(famname)=0;
+                    catch
+                        famname='badname';
+                    end
+                end
+            catch
+                famname='unnamed';
+            end
+            if isfield(ids,famname)
+                ids.(famname)(end+1)=i;
+            else
+                ids.(famname)(1)=i;
+            end
         end
-    else
-        famname='unnamed';
-    end
-    if isfield(indexstruct,famname)
-        indexstruct.(famname)(end+1)=i;
-    else
-        indexstruct.(famname)(1)=i;
     end
 end
