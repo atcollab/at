55ae24de932f365d279a9bed7f6dea0d75d798c2
Laurent Nadolski <laurent.nadolski@synchrotron-soleil.fr>
2017-02-23 23:12:29 +0000

    Add header and more generic way blockdiagonal

--- atmat/atphysics/jmat.m
+++ atmat/atphysics/jmat.m
@@ -1,12 +1,22 @@
-function mat=jmat(n)
+function mat=jmat(dim)
+% JMAT - Computes antisymmetric Matrix [O 1; -1 0]
+% 
+%  INPUTS
+%    1. dim - 1,2,3 Dimension of the sqare matrix
+%
+%  OUPUTS
+%    2. mat - Antisymmetric block Matrix [O 1; -1 0] 
+% 
+%  See also symplectify
 
-if(n==1)
+S2 = [0 1; -1 0];
+
+if(dim==1)
     mat=[0 1;-1 0];
-end
-if(n==2)
-    mat=[0 1 0 0;-1 0 0 0;0 0 0 1;0 0 -1 0];
-end
-if(n==3)
-    mat=[0 1 0 0 0 0;-1 0 0 0 0 0;0 0 0 1 0 0;0 0 -1 0 0 0;0 0 0 0 0 1;
-    0 0 0 0 -1 0];
+elseif(dim==2)
+    mat = blkdiag(S2,S2);
+elseif(dim==3)
+    mat = blkdiag(S2,S2,S2);
+else
+    Error('Dim is 1, 2 or 3')
 end
