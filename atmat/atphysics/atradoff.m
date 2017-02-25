c2033bf309f1657a7b1dbe923a1c6d422faa7ec5
Laurent Nadolski <laurent.nadolski@synchrotron-soleil.fr>
2017-02-25 17:41:19 +0000

    Modified header and variable names

--- atmat/atphysics/atradoff.m
+++ atmat/atphysics/atradoff.m
@@ -1,65 +1,69 @@
-function [ring,radelems,cavities]=atradoff(ring1,varargin)
-%ATRADOFF			switches radiation off
+function [ring2,radelemIndex,cavitiesIndex]=atradoff(ring1,varargin)
+%ATRADOFF  switches radiation and cavity off
 %
-%RING2=ATRADOFF(RING,CAVIPASS,BENDPASS,QUADPASS)
-%   Changes passmethods to turn off radiation
-%   damping. 
+%   [RING2,RADINDEX,CAVINDEX] = ATRADOFF(RING,CAVIPASS,BENDPASS,QUADPASS)
+%    Changes passmethods to turn off radiation damping. 
 %
-%RING:		initial AT structure
-%CAVIPASS:	pass method for cavities (default IdentityPass)
-%           '' makes no change,
-%BENDPASS:	pass method for bending magnets. Special values:
-%           '' makes no change,
-%           'auto' wille substitute 'RadPass' with 'Pass' in any method
-%           (default: 'auto')
-%QUADPASS:	pass method for quadrupoles
-%           '' makes no change,
-%           'auto' wille substitute 'RadPass' with 'Pass' in any method
-%           (default: '')
+%  INPUTS
+%    1. RING      initial AT structure
+%    2. CAVIPASS  pass method for cavities (default IdentityPass)
+%                 '' makes no change,
+%    3. BENDPASS  pass method for bending magnets. Special values:
+%                 '' makes no change,
+%                 'auto' wille substitute 'RadPass' with 'Pass' in any method
+%                 (default: 'auto')
+%    4. QUADPASS  pass method for quadrupoles
+%                 '' makes no change,
+%                 'auto' wille substitute 'RadPass' with 'Pass' in any method
+%                 (default: '')
 %
-%[RING2,RADINDEX,CAVINDEX]=ATRADOFF(...)
-%           returns the index of radiative elements and cavities
+%   OUPUTS
+%     1. RING2     Output ring
+%     2. RADINDEX  Indices of elements with radiation
+%     3. CAVINDEX  Indices of cavities
+%
+%  See also ATRADON, ATCAVITYON, ATCAVITYOFF
 
 [cavipass,bendpass,quadpass]=parseargs({'IdentityPass','auto',''},varargin);
 
-ring=ring1;
+ring2=ring1;
 
 if ~isempty(cavipass)
-    cavities=atgetcells(ring,'Frequency');
-    if ~any(cavities)
+    cavitiesIndex=atgetcells(ring2,'Frequency');
+    if ~any(cavitiesIndex)
         warning('AT:atradon:NoCavity', 'No cavity found in the structure');
     end
-    ring(cavities)=changepass(ring(cavities),cavipass);
+    ring2(cavitiesIndex)=changepass(ring2(cavitiesIndex),cavipass);
 else
-    cavities=false(size(ring));
+    cavitiesIndex=false(size(ring2));
 end
 
 if ~isempty(bendpass)
     isdipole=@(elem,bangle) bangle~=0;
-    dipoles=atgetcells(ring,'BendingAngle',isdipole);
+    dipoles=atgetcells(ring2,'BendingAngle',isdipole);
     if sum(dipoles) <= 0
         warning('AT:atradon:NoBend', 'No dipole in the structure');
     end
-    ring(dipoles)=changepass(ring(dipoles),bendpass);
+    ring2(dipoles)=changepass(ring2(dipoles),bendpass);
 else
-    dipoles=false(size(ring));
+    dipoles=false(size(ring2));
 end
 
 if ~isempty(quadpass)
     isquadrupole=@(elem,polyb) length(polyb) >= 2 && polyb(2)~=0;
-    quadrupoles=atgetcells(ring,'PolynomB',isquadrupole) & ~dipoles;
+    quadrupoles=atgetcells(ring2,'PolynomB',isquadrupole) & ~dipoles;
     if sum(quadrupoles) <= 0
         warning('AT:atradon:NoQuad', 'No quadrupole in the structure');
     end
-    ring(quadrupoles)=changepass(ring(quadrupoles),quadpass);
+    ring2(quadrupoles)=changepass(ring2(quadrupoles),quadpass);
 else
-    quadrupoles=false(size(ring));
+    quadrupoles=false(size(ring2));
 end
 
-radelems=dipoles|quadrupoles;
+radelemIndex=dipoles|quadrupoles;
 
-disp(['Cavities located at position ' num2str(find(cavities)')]);
-disp([num2str(sum(radelems)) ' elements with radiation switched off']);
+disp(['Cavities located at position ' num2str(find(cavitiesIndex)')]);
+disp([num2str(sum(radelemIndex)) ' elements with radiation switched off']);
 
     function newline=changepass(line,newpass)
     if strcmp(newpass,'auto')
--- atmat/atphysics/atradon.m
+++ atmat/atphysics/atradon.m
@@ -1,28 +1,33 @@
-function [ring,radelems,cavities,energy]=atradon(ring1,varargin)
-%ATRADON			switches RF and radiation on
+function [ring,radelemIndex,cavitiesIndex,energy]=atradon(ring1,varargin)
+%ATRADON switches RF and radiation on
 %
-%RING2=ATRADON(RING,CAVIPASS,BENDPASS,QUADPASS)
-%   Changes passmethods to get RF cavity acceleration and radiation
-%   damping. ATRADON also sets the "Energy" field on the modified elements,
-%   looking for the machine energy in:
+%  [RING2,RADINDEX,CAVINDEX,ENERGY] = ATRADON(RING,CAVIPASS,BENDPASS,QUADPASS)
+%    Changes passmethods to get RF cavity acceleration and radiation
+%    damping. ATRADON also sets the "Energy" field on the modified elements,
+%    looking for the machine energy in:
 %       1) 1st 'RingParam' element
 %       2) 1st 'RFCavity' element
 %       3) field "E0" of the global variable "GLOBVAL"
+%  INPUTS
+%    1. RING	   initial AT structure
+%    2. CAVIPASS   pass method for cavities (default ThinCavityPass)
+%                  '' makes no change,
+%    3. BENDPASS   pass method for bending magnets. Special values:
+%                  '' makes no change,
+%                  'auto' will substitute 'Pass' with 'RadPass' in any method
+%                  (default: 'auto')
+%    4. QUADPASS   pass method for quadrupoles
+%                  '' makes no change,
+%                  'auto' will substitute 'Pass' with 'RadPass' in any method
+%                  (default: '')
 %
-%RING:		initial AT structure
-%CAVIPASS:	pass method for cavities (default ThinCavityPass)
-%           '' makes no change,
-%BENDPASS:	pass method for bending magnets. Special values:
-%           '' makes no change,
-%           'auto' wille substitute 'Pass' with 'RadPass' in any method
-%           (default: 'auto')
-%QUADPASS:	pass method for quadrupoles
-%           '' makes no change,
-%           'auto' wille substitute 'Pass' with 'RadPass' in any method
-%           (default: '')
+%   OUPUTS
+%     1. RING2     Output ring
+%     2. RADINDEX  Indices of elements with radiation
+%     3. CAVINDEX  Indices of cavities
 %
-%[RING2,RADINDEX,CAVINDEX,ENERGY]=ATRADON(...)
-%           returns the index of radiative elements and cavities + energy
+%  See also ATRADOFF, ATCAVITYON, ATCAVITYOFF
+
 
 [cavipass,bendpass,quadpass]=parseargs({'CavityPass','auto',''},varargin);
 
@@ -30,12 +35,12 @@ ring=ring1;
 
 energy=atenergy(ring);
 if ~isempty(cavipass)
-    cavities=atgetcells(ring,'Frequency');
-    if any(cavities)
-        ring(cavities)=changepass(ring(cavities),cavipass,energy);
+    cavitiesIndex=atgetcells(ring,'Frequency');
+    if any(cavitiesIndex)
+        ring(cavitiesIndex)=changepass(ring(cavitiesIndex),cavipass,energy);
     end
 else
-    cavities=false(size(ring));
+    cavitiesIndex=false(size(ring));
 end
 
 if ~isempty(bendpass)
@@ -58,14 +63,14 @@ else
     quadrupoles=false(size(ring));
 end
 
-radelems=dipoles|quadrupoles;
+radelemIndex=dipoles|quadrupoles;
 
-if any(cavities)
-    atdisplay(1,['Cavities located at position ' num2str(find(cavities)')]);
+if any(cavitiesIndex)
+    atdisplay(1,['Cavities located at position ' num2str(find(cavitiesIndex)')]);
 else
     atdisplay(1,'No cavity');
 end
-atdisplay(1,[num2str(sum(radelems)) ' elements switched to include radiation']);
+atdisplay(1,[num2str(sum(radelemIndex)) ' elements switched to include radiation']);
 
     function newline=changepass(line,newpass,nrj)
     if strcmp(newpass,'auto')
