ed37836760a940ace333a22573ba839497a6bed5
Laurent Nadolski <laurent.nadolski@synchrotron-soleil.fr>
2017-02-25 18:25:54 +0000

    Header + shaping of text

--- atmat/lattice/element_creation/private/decodeatargs.m
+++ atmat/lattice/element_creation/private/decodeatargs.m
@@ -1,10 +1,29 @@
 function [rsrc,varargout] = decodeatargs(default_values,args)
-%EXTRACTRES separates arguments and resources
-%[RSRC,ARGS]=EXTRACTRES(DEFARGS,ARGLIST)
-%   DEFARGS must have length >= 2
+%DECODEATARGS separates arguments and resources
+%
+%  [RSRC,ARGS]=decodeatargs(DEFARGS,ARGLIST)
+%
+%  INPUTS
+%    1. DEFARGS - Values per default if not specify for mandatory
+%                 argument
+%    2. ARGS    - Other arugments
+%
+%  OUPUTS
+%    1. rsrc      - Mandatory arguments
+%    2. varargout - Optional arguments
+%
+%  NOTES
+%    1. DEFARGS must have length >= 2
+%
+%  See also getoption, getflag
 
-nopass=@(arg) ischar(arg) && ~isempty(arg) && isempty(regexp(arg,'.*Pass$','once'));
-chararg=find(cellfun(nopass,[args {'x'}]),1);
-rsrc=args(chararg:end);
-varargout=parseargs(default_values,args(1:chararg-1));
+% function to get the first string which is after the PassMethod
+nopass    = @(arg) ischar(arg) && ~isempty(arg) && isempty(regexp(arg,'.*Pass$','once'));
+% get index of first optional char Argument
+chararg   = find(cellfun(nopass,[args {'x'}]),1); 
+% get all optional argument (ressources)
+rsrc      = args(chararg:end);
+% output mandatory argument
+varargout = parseargs(default_values,args(1:chararg-1));
+
 end
