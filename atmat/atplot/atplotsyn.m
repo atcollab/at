0dccc4d639b96b255aba22e2e6665847690a8118
Laurent Farvacque <laurent.farvacque@esrf.fr>
2017-02-28 17:29:28 +0000

    Restore compatibility with old matlab versions

--- atmat/atplot/atplotsyn.m
+++ atmat/atplot/atplotsyn.m
@@ -16,7 +16,7 @@ xlim=get(ax,'XLim');
 slim=diff(xlim);
 tlim=diff(get(ax,'YLim'));
 nlines=length(get(ax,'Children'));
-%axes(ax);
+axes(ax);  % Compatibility with old matlab versions
 
 sl=findspos(ring(:,1),1:size(ring,1)+1);
 ll=diff(sl);
@@ -30,7 +30,8 @@ labs=labs(sok);
 ampl=0.05*tlim;     % Dipoles
 [xplot,yplot]=setxpl(sl(dipoles),ll(dipoles),[0;0;1;1;0],ampl*[0;1;1;0;0]);
 %[xplot,yplot]=setxpl(sl(dipoles),ll(dipoles),[0;0;1;1;0],ampl*[-1;1;1;-1;-1]);
-p1=patch(ax,xplot,yplot,[0.5 0.5 1],'DisplayName','Dipoles');
+p1=patch(xplot,yplot,[0.5 0.5 1],'DisplayName','Dipoles');
+%p1=patch(ax,xplot,yplot,[0.5 0.5 1],'DisplayName','Dipoles');
 
 ampl=0.05*tlim;     % Quadrupoles
 foc=reshape(0.4*ampl*sign(atgetfieldvalues(rok(qpoles),'PolynomB',{2})),1,[]);
@@ -38,7 +39,8 @@ foc=reshape(0.4*ampl*sign(atgetfieldvalues(rok(qpoles),'PolynomB',{2})),1,[]);
 yplot(3,:)=yplot(3,:)+foc;
 %[xplot,yplot]=setxpl(sl(qpoles),ll(qpoles),[0;0;0.5;1;1;0.5;0],ampl*[-1;1;1;1;-1;-1;-1]);
 %yplot(6,:)=yplot(6,:)-foc;
-p2=patch(ax,xplot,yplot,[1 0.5 0.5],'DisplayName','Quadrupoles');
+p2=patch(xplot,yplot,[1 0.5 0.5],'DisplayName','Quadrupoles');
+%p2=patch(ax,xplot,yplot,[1 0.5 0.5],'DisplayName','Quadrupoles');
 
 ampl=0.04*tlim;     % Sextupoles
 foc=reshape(0.25*ampl*sign(atgetfieldvalues(rok(spoles),'PolynomB',{3})),1,[]);
@@ -46,17 +48,20 @@ foc=reshape(0.25*ampl*sign(atgetfieldvalues(rok(spoles),'PolynomB',{3})),1,[]);
 yplot(3:4,:)=yplot(3:4,:)+foc([1;1],:);
 %[xplot,yplot]=setxpl(sl(spoles),ll(spoles),[0;0;0.33;0.66;1;1;0.66;0.33;0],ampl*[-1;1;1;1;1;-1;-1;-1;-1]);
 %yplot(7:8,:)=yplot(7:8,:)-foc([1;1],:);
-p3=patch(ax,xplot,yplot,[0.5 1 0.5],'DisplayName','Sextupoles');
+p3=patch(xplot,yplot,[0.5 1 0.5],'DisplayName','Sextupoles');
+%p3=patch(ax,xplot,yplot,[0.5 1 0.5],'DisplayName','Sextupoles');
 
 ampl=0.03*tlim;     % Other multipoles
 [xplot,yplot]=setxpl(sl(mpoles),ll(mpoles),[0;0;1;1;0],ampl*[0;1;1;0;0]);
 %[xplot,yplot]=setxpl(sl(mpoles),ll(mpoles),[0;0;1;1;0],ampl*[-1;1;1;-1;-1]);
-p4=patch(ax,xplot,yplot,[0 0.5 0]);
+p4=patch(xplot,yplot,[0 0.5 0]);
+%p4=patch(ax,xplot,yplot,[0 0.5 0]);
 
 ampl=0.015*tlim;    % BPMs
 amplx=0.005*slim;
 [xplot,yplot]=setxpl(sl(bpms),ones(1,sum(bpms)),[-amplx;0;amplx;0;-amplx],[0;-ampl;0;ampl;0]);
-p5=patch(ax,xplot,yplot,[0 0 0],'clipping','off');
+p5=patch(xplot,yplot,[0 0 0],'clipping','off');
+%p5=patch(ax,xplot,yplot,[0 0 0],'clipping','off');
 
 if any(labs)
     slabs=sl(labs)+0.5*ll(labs);
