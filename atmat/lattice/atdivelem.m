cc3e6f8607fc739807874d7ac18429d746675ebc
Laurent Farvacque <laurent.farvacque@esrf.fr>
2017-03-14 09:30:11 +0000

    Correctly handle fringe fields in element division

--- atmat/lattice/atdivelem.m
+++ atmat/lattice/atdivelem.m
@@ -4,7 +4,7 @@ function line = atdivelem(elem,frac)
 %ELEM:  Element to be divided
 %FRAC:  Length of each piece, as a fraction of the total length
 %
-%LINE:  Cell array 
+%LINE:  Cell array
 %
 % The sum of frac elements does not need to be 1, however for bending
 % magnets, the length will be divided according to FRAC, but the total
@@ -19,56 +19,49 @@ function line = atdivelem(elem,frac)
 % See also ATINSERTELEMS ATSLICE ATSPLITELEM
 
 line=atsetfieldvalues(repmat({elem},length(frac),1),'Length',elem.Length*frac(:));
-if isfield(elem,'T1')
-    line(2:end)=cellfun(@(elem) rmfield(elem,'T1'),line(2:end),'UniformOutput',false);
-end
-if isfield(elem,'R1')
-    line(2:end)=cellfun(@(elem) rmfield(elem,'R1'),line(2:end),'UniformOutput',false);
-end
-if isfield(elem,'T2')
-    line(1:end-1)=cellfun(@(elem) rmfield(elem,'T2'),line(1:end-1),'UniformOutput',false);
-end
-if isfield(elem,'R2')
-    line(1:end-1)=cellfun(@(elem) rmfield(elem,'R2'),line(1:end-1),'UniformOutput',false);
-end
-if isfield(elem,'BendingAngle')
-    line=atsetfieldvalues(line,'BendingAngle',elem.BendingAngle*frac(:)/sum(frac));
-end
-if isfield(elem,'EntranceAngle')
-    drentrangle=zeros(size(line));
-    drentrangle(1)=elem.EntranceAngle;
-    line=atsetfieldvalues(line,'EntranceAngle',drentrangle);
-end
-if isfield(elem,'ExitAngle')
-    drexitangle=zeros(size(line));
-    drexitangle(end)=elem.ExitAngle;
-    line=atsetfieldvalues(line,'ExitAngle',drexitangle);
-end
-if isfield(elem,'FringeInt1')
-    fringe1=zeros(size(line));
-    fringe1(1)=elem.FringeInt1;
-    line=atsetfieldvalues(line,'FringeInt1',fringe1);
-end
-if isfield(elem,'FringeInt2')
-    fringe2=zeros(size(line));
-    fringe2(end)=elem.FringeInt2;
-    line=atsetfieldvalues(line,'FringeInt2',fringe2);
-end
-
-if isfield(elem,'FullGap')
-    fringe2=zeros(size(line));
-    fringe2([1,end])=elem.FullGap;
-    line=atsetfieldvalues(line,'FullGap',fringe2);
-end
-if isfield(elem,'EdgeEffect1')
-    fringe2=zeros(size(line));
-    fringe2(1)=elem.EdgeEffect1;
-    line=atsetfieldvalues(line,'EdgeEffect1',fringe2);
-end
-if isfield(elem,'EdgeEffect2')
-    fringe2=zeros(size(line));
-    fringe2(end)=elem.EdgeEffect2;
-    line=atsetfieldvalues(line,'EdgeEffect2',fringe2);
+if length(frac)>1
+    if isfield(elem,'T1')
+        line(2:end)=cellfun(@(elem) rmfield(elem,'T1'),line(2:end),'UniformOutput',false);
+    end
+    if isfield(elem,'R1')
+        line(2:end)=cellfun(@(elem) rmfield(elem,'R1'),line(2:end),'UniformOutput',false);
+    end
+    if isfield(elem,'T2')
+        line(1:end-1)=cellfun(@(elem) rmfield(elem,'T2'),line(1:end-1),'UniformOutput',false);
+    end
+    if isfield(elem,'R2')
+        line(1:end-1)=cellfun(@(elem) rmfield(elem,'R2'),line(1:end-1),'UniformOutput',false);
+    end
+    if isfield(elem,'BendingAngle')
+        % Correct bending angles
+        line=atsetfieldvalues(line,'BendingAngle',elem.BendingAngle*frac(:)/sum(frac));
+        % Keep entrance angle only in first slice
+        if isfield(elem,'EntranceAngle')
+            drentrangle=zeros(size(line));
+            drentrangle(1)=elem.EntranceAngle;
+            line=atsetfieldvalues(line,'EntranceAngle',drentrangle);
+        end
+        % Keep exit angle only on last slice
+        if isfield(elem,'ExitAngle')
+            drexitangle=zeros(size(line));
+            drexitangle(end)=elem.ExitAngle;
+            line=atsetfieldvalues(line,'ExitAngle',drexitangle);
+        end
+        % Suppress dipole fringe effects at intermediate points
+        line(2:end)=atsetfieldvalues(line(2:end),'FringeBendEntrance',0);
+        line(1:end-1)=atsetfieldvalues(line(1:end-1),'FringeBendExit',0);
+    end
+    % Suppress quadrupole fringe effects at intermediate points
+    if isfield(elem,'FringeQuadEntrance')
+        fringe1=zeros(size(line));
+        fringe1(1)=elem.FringeQuadEntrance;
+        line=atsetfieldvalues(line,'FringeQuadEntrance',fringe1);
+    end
+    if isfield(elem,'FringeQuadExit')
+        fringe2=zeros(size(line));
+        fringe2(end)=elem.FringeQuadExit;
+        line=atsetfieldvalues(line,'FringeQuadExit',fringe2);
+    end
 end
 
 end
