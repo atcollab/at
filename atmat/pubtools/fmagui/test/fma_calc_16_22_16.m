34fd6a8aecd748cde55352c061dca9ddd20a96c4
Boaz Nash <boaz.nash@esrf.fr>
2017-03-16 10:40:01 +0000

    remove pubtools fmagui directory

--- atmat/pubtools/fmagui/test/fma_calc_16_22_16.m
+++
@@ -1,6 +0,0 @@
-function varargout = fma_calc(fma,handles)
-
-disp('In fma_calc and updating a gui handle/object');
-set(handles.comments,'String','This has been set from fma_calc');
-
-varargout{1} = 1;
--- atmat/pubtools/fmagui/test/runscript.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo.
-echo Starting MATLAB and running Frequency Map Analysis...
-echo Input file : 
-echo        %%CD%%aspsr_v1_fma_input.dat
-echo Matlab error logs saved to : 
-echo        %%CD%%aspsr_v1_fma_input_matlaberrors.log
-echo.
-matlab -nosplash -nodesktop -nojvm -logfile %%CD%%aspsr_v1_fma_input_matlaberrors.log -r "fma = fma_lib('analysis','%%CD%%aspsr_v1_fma_input.dat'); plot_emailresults(fma); exit;"
