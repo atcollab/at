76e827e7fdeb9e703421add7bb98c42d9c4af594
Nicola Carmignani <nicola.carmignani@esrf.rf>
2017-05-30 12:02:22 +0000

    added two old functions for dipole fringe fields: edge and edge_fringe for compatibility with old dipole passmethods

--- atintegrators/atphyslib.c
+++ atintegrators/atphyslib.c
@@ -132,3 +132,21 @@ static void edge_fringe2B(double* r, double inv_rho, double edge_angle, double f
     r[3] += -r2*fy + 2*T414*r0*r[3]+2*T413*r0*r2+2*T423*r1*r2 ;
     
 }
+
+static void edge(double* r, double inv_rho, double edge_angle)
+{	/* Edge focusing in dipoles with hard-edge field */
+    double psi = inv_rho*tan(edge_angle);
+
+    r[1]+=r[0]*psi;
+	r[3]-=r[2]*psi;
+}
+
+
+static void edge_fringe(double* r, double inv_rho, double edge_angle, double fint, double gap)
+{   /* Edge focusing in dipoles with fringe field */
+    double fx = inv_rho*tan(edge_angle);
+    double psi_bar = edge_angle-inv_rho*gap*fint*(1+sin(edge_angle)*sin(edge_angle))/cos(edge_angle)/(1+r[4]);
+    double fy = inv_rho*tan(psi_bar);
+    r[1]+=r[0]*fx;
+    r[3]-=r[2]*fy;    
+}
