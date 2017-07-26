d5d2a98d4b6b9621db0bd9595dc5b45cf761a358
Boaz Nash <boaz.nash@esrf.fr>
2017-07-26 14:27:46 +0000

    simplify and improve atfastring demo

--- atmat/atdemos/atfastring/testatfastring.m
+++ atmat/atdemos/atfastring/testatfastring.m
@@ -1,27 +1,42 @@
-ring=soleil;
-indcav=findcells(ring,'Class','RFCavity');
-cav=ring(indcav(1));
-ring(indcav(:))=[];
-ring=[cav;ring];
+%Demo of using atfastring for fast tracking
+%load esrf ring
+ring=esrf;
 
+% indcav=findcells(ring,'Class','RFCavity');
+% cav=ring(indcav(1));
+% ring(indcav(:))=[];
+% ring=[cav;ring];
+
+% set RF cavity
 ring=atsetcavity(ring,8e6,0,992);
 
+%Now, create fastring and fastringrad from ring.
 [fastring,fastringrad]=atfastring(ring);
-fastringLin=fastring;
-fastringLin(3)=[];
-m66=fastringLin{2}.M66;
-MS=symplectify(m66);
-fastringLin{2}.M66=MS;
 
-z0=[0,0,0,0,0.03,0]';
-zz0=[1e-5;0;1e-5;0;1e-3;0];
 
-z1Lin=ringpass(fastringLin,zz0,1e7);
+%Set an initial condition for tracking
+z0=[1e-5,0,0,0,1e-3,0]';
 
-z1=ringpass(ring,z0,100);
-z1fast=ringpass(fastring,z0,100);
-z1fastrad=ringpass(fastringrad,z0,10000);
+%Now, track with full ring and with fast ring and time the computation.
+tic
+z1=ringpass(ring,z0,500);
+toc
+
+tic
+z1fast=ringpass(fastring,z0,500);
+toc
+
+
+% Now compare the horizontal and transverse tracking results.
+plot(z1(1,:),'r');
+hold on
+plot(z1fast(1,:),'b');
 
 figure
+plot(z1(5,:),'r');
 hold on
-plot(z1fastrad(5,:),'-k');
+plot(z1fast(5,:),'b');
+
+% figure
+% hold on
+% plot(z1fastrad(5,:),'-k');
