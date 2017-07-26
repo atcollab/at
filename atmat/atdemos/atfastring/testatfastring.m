f8e25d1dc4f42673f73b0cd8e0ea730f7730a1bc
Boaz Nash <boaz.nash@esrf.fr>
2017-07-26 15:10:29 +0000

    compute and display tunes and chromaticities for full versus fast rings

--- atmat/atdemos/atfastring/testatfastring.m
+++ atmat/atdemos/atfastring/testatfastring.m
@@ -1,4 +1,13 @@
-%Demo of using atfastring for fast tracking
+% Demo of using atfastring for fast tracking
+% The ESRF lattice is loaded, cavity is set, and then the fast ring is
+% created.
+% An electron is tracked through the full lattice and fast ring and
+% the tracking time and tracking results are compared. 
+
+clear all
+clc
+echo on
+
 %load esrf ring
 ring=esrf;
 
@@ -27,15 +36,29 @@ z1fast=ringpass(fastring,z0,500);
 toc
 
 
+% Check tunes and chromaticity:
+[p,t,c]=atlinopt(ring,0,1);
+[pf,tf,cf]=atlinopt(fastring,0,1);
+
+t
+tf
+c
+cf
+
 % Now compare the horizontal and transverse tracking results.
-plot(z1(1,:),'r');
-hold on
-plot(z1fast(1,:),'b');
+plot(1:length(z1),z1(1,:),'r',1:length(z1fast),z1fast(1,:),'b');
+legend('full ring','fast ring');
+xlabel('turns');
+ylabel('x (m)');
 
 figure
 plot(z1(5,:),'r');
 hold on
 plot(z1fast(5,:),'b');
+legend('full ring','fast ring');
+xlabel('turns');
+ylabel('delta');
+
 
 % figure
 % hold on
