4d47bc9201b8a01bdbb25659bc57db30cfba31ae
Boaz Nash <boaz.nash@esrf.fr>
2017-08-25 09:50:28 +0000

    remove FitQuadrupoleLength atmatch example which was incomplete

--- atmat/atdemos/atmatchExamples/FitQuadrupoleLength/VaryMagLength.m
+++
@@ -1,9 +0,0 @@
-function r=VaryMagLength(r,DL,indP,indM)
-
-ndp=length(indP);
-ndm=length(indM);
-
-r=atsetfieldvalues(r,indP,'Length',atgetfieldvalues(r,indP,'Length')+DL/ndp);
-r=atsetfieldvalues(r,indM,'Length',atgetfieldvalues(r,indM,'Length')-DL/ndm);
-
-return
--- atmat/atdemos/atmatchExamples/FitQuadrupoleLength/compK1.m
+++
@@ -1,8 +0,0 @@
-function dif=compK1(arc,indQ1,indQ2,varargin)
-
-KQ1=atgetfieldvalues(arc,indQ1,'PolynomB',{1,2});
-KQ2=atgetfieldvalues(arc,indQ2,'PolynomB',{1,2});
-
-dif=KQ2(1)-KQ1(1);
-
-return
