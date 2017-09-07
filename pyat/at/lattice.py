1537a078d9c304eca1d87b2e82d8cebe7fa89630
Laurent Farvacque <laurent.farvacque@esrf.fr>
2017-09-07 15:36:12 +0200

    Give the possibility to use either integer or boolean refpts arrays.

--- pyat/at/lattice.py
+++ pyat/at/lattice.py
@@ -5,37 +5,34 @@ A lattice as understood by pyAT is any sequence of elements.  These functions
 are useful for working with these sequences.
 """
 import numpy
+import collections
 
 
-def get_refpts(refpts, n_elements, append_last=False):
-    """
-    If using refpts in a call to at.atpass(), it must be a numpy array of type
-    uint32.  This function creates such an array.
-    """
-    if refpts is None:
-        refpts = [n_elements]
-    int_array = numpy.array(refpts, dtype=numpy.uint32)
-    if numpy.any(numpy.diff(int_array) < 0) or numpy.any(int_array > n_elements):
-        raise ValueError('refpts must be an ascending array: {} {}'.
-                         format(int_array, n_elements))
-    if append_last and refpts[-1] != n_elements:
-        int_array = numpy.append(int_array, [n_elements])
-    return numpy.array(int_array, dtype=numpy.uint32)
+def uint32_refpts(refpts, n_elements):
+    if isinstance(refpts, numpy.ndarray) and refpts.dtype == bool:
+        urefpts = numpy.array(numpy.flatnonzero(refpts), dtype=numpy.uint32)
+    else:
+        if not isinstance(refpts, (collections.Sequence, numpy.ndarray)):
+            refpts = [refpts]
+        if numpy.any(numpy.diff(numpy.array(refpts)) < 0) or (refpts[-1] > n_elements):
+            raise ValueError('refpts must be ascending and less or equal to {}'.format(n_elements))
+        urefpts = numpy.asarray(refpts, dtype=numpy.uint32)
+    return urefpts
 
 
-def get_s_pos(ring, refpts=None):
+def bool_refpts(refpts, n_elements):
+    if isinstance(refpts, numpy.ndarray) and refpts.dtype == bool:
+        return refpts
+    else:
+        brefpts = numpy.zeros(n_elements+1, dtype=bool)
+        brefpts[refpts] = True
+        return brefpts
+
+
+def get_s_pos(ring, refpts):
     """
     Return a numpy array corresponding to the s position of the specified
     elements.
     """
-    refpts = get_refpts(refpts, len(ring), append_last=True)
-    total = 0
-    s_pos = numpy.zeros(len(refpts))
-    j = 1
-    for i, element in enumerate(ring):
-        total += element.Length
-        if i in refpts:
-            s_pos[j] = total
-            j += 1
-    s_pos[-1] = total
-    return s_pos
+    s_pos = numpy.concatenate(([0.0], numpy.cumsum([getattr(el, 'Length', 0.0) for el in ring])))
+    return s_pos[refpts]
