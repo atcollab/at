449117e9011b343bf41033ff57ac36e1fc1bdc9f
Will Rogers <wahrogers@gmail.com>
2017-09-09 16:41:23 +0100

    Fix tests in test_basic_elements.py for new array ordering.

--- pyat/test/test_basic_elements.py
+++ pyat/test/test_basic_elements.py
@@ -9,17 +9,18 @@ def test_correct_dimensions_does_not_raise_error(rin):
     atpass(l, rin, 1)
     rin = numpy.zeros((6,))
     atpass(l, rin, 1)
-    rin = numpy.zeros((2,6))
+    rin = numpy.array(numpy.zeros((6, 2), order='F'))
+    atpass(l, rin, 1)
 
 
 @pytest.mark.parametrize("dipole_class", (elements.Dipole, elements.Bend))
 def test_dipole(rin, dipole_class):
     b = dipole_class('dipole', 1.0, 0.1, EntranceAngle=0.05, ExitAngle=0.05)
     l = [b]
-    rin[0,0] = 1e-6
+    rin[0, 0] = 1e-6
     rin_orig = numpy.copy(rin)
     atpass(l, rin, 1)
-    rin_expected = numpy.array([1e-6, 0, 0, 0, 0, 1e-7]).reshape((1,6))
+    rin_expected = numpy.array([1e-6, 0, 0, 0, 0, 1e-7]).reshape((6, 1))
     numpy.testing.assert_almost_equal(rin_orig, rin_expected)
 
 
@@ -37,8 +38,8 @@ def test_aperture_inside_limits(rin):
     a = elements.Aperture('aperture', [-1e-3, 1e-3, -1e-4, 1e-4])
     assert a.Length == 0
     lattice = [a]
-    rin[0][0] = 1e-5
-    rin[0][2] = -1e-5
+    rin[0, 0] = 1e-5
+    rin[2, 0] = -1e-5
     rin_orig = numpy.array(rin, copy=True)
     atpass(lattice, rin, 1)
     numpy.testing.assert_equal(rin, rin_orig)
@@ -48,18 +49,18 @@ def test_aperture_outside_limits(rin):
     a = elements.Aperture('aperture', [-1e-3, 1e-3, -1e-4, 1e-4])
     assert a.Length == 0
     lattice = [a]
-    rin[0][0] = 1e-2
-    rin[0][2] = -1e-2
+    rin[0, 0] = 1e-2
+    rin[2, 0] = -1e-2
     atpass(lattice, rin, 1)
-    assert numpy.isinf(rin[0][5])
-    assert rin[0][2] == -1e-2  # Only the 6th coordinate is marked as infinity
+    assert numpy.isinf(rin[5, 0])
+    assert rin[2, 0] == -1e-2  # Only the 6th coordinate is marked as infinity
 
 
 def test_drift_offset(rin):
     d = elements.Drift('drift', 1)
     lattice = [d]
-    rin[0][0] = 1e-6
-    rin[0][2] = 2e-6
+    rin[0, 0] = 1e-6
+    rin[2, 0] = 2e-6
     rin_orig = numpy.array(rin, copy=True)
     atpass(lattice, rin, 1)
     numpy.testing.assert_equal(rin, rin_orig)
@@ -69,11 +70,11 @@ def test_drift_divergence(rin):
     d = elements.Drift('drift', 1.0)
     assert d.Length == 1
     lattice = [d]
-    rin[0][1] = 1e-6
-    rin[0][3] = -2e-6
+    rin[1, 0] = 1e-6
+    rin[3, 0] = -2e-6
     atpass(lattice, rin, 1)
     # results from Matlab
-    rin_expected = numpy.array([1e-6, 1e-6, -2e-6, -2e-6, 0, 2.5e-12]).reshape(1,6)
+    rin_expected = numpy.array([1e-6, 1e-6, -2e-6, -2e-6, 0, 2.5e-12]).reshape(6, 1)
     numpy.testing.assert_equal(rin, rin_expected)
 
 
@@ -81,19 +82,19 @@ def test_drift_two_particles(rin):
     d = elements.Drift('drift', 1.0)
     assert d.Length == 1
     lattice = [d]
-    two_rin = numpy.concatenate((rin, rin), axis=0)
+    two_rin = numpy.array(numpy.concatenate((rin, rin), axis=1), order='F')
     # particle one is offset
-    two_rin[0][0] = 1e-6
-    two_rin[0][2] = 2e-6
+    two_rin[0, 0] = 1e-6
+    two_rin[2, 0] = 2e-6
     # particle two has divergence
-    two_rin[1][1] = 1e-6
-    two_rin[1][3] = -2e-6
+    two_rin[1, 1] = 1e-6
+    two_rin[3, 1] = -2e-6
     two_rin_orig = numpy.array(two_rin, copy=True)
     atpass(lattice, two_rin, 1)
     # results from Matlab
-    p1_expected = numpy.array(two_rin_orig[0,:]).reshape(1,6)
-    p2_expected = numpy.array([1e-6, 1e-6, -2e-6, -2e-6, 0, 2.5e-12]).reshape(1,6)
-    two_rin_expected = numpy.concatenate((p1_expected, p2_expected), axis=0)
+    p1_expected = numpy.array(two_rin_orig[:, 0]).reshape(6, 1)
+    p2_expected = numpy.array([1e-6, 1e-6, -2e-6, -2e-6, 0, 2.5e-12]).reshape(6, 1)
+    two_rin_expected = numpy.concatenate((p1_expected, p2_expected), axis=1)
     numpy.testing.assert_equal(two_rin, two_rin_expected)
 
 
@@ -107,7 +108,7 @@ def test_quad(rin):
                             0,
                             0,
                             0,
-                            0.000000010330489]).reshape(1, 6) * 1e-6
+                            0.000000010330489]).reshape(6, 1) * 1e-6
     numpy.testing.assert_allclose(rin, expected)
 
 
