f41e57f567f1ddae43ea2fc30a26a4d762300e63
Simone Liuzzo <simone.liuzzo@esrf.fr>
2017-12-04 11:26:12 +0100

    FieldIntegral/testerr.m : no plots, modified figures

--- atmat/pubtools/LatticeTuningFunctions/errors/FieldIntegral/testerr.m
+++ atmat/pubtools/LatticeTuningFunctions/errors/FieldIntegral/testerr.m
@@ -1,12 +1,13 @@
 clear all
 close all
-addpath('/mntdirect/_machfs/liuzzo/CODE/LatticeTuningFunctions/errors')
+addpath('../../errors')
+addpath('../../../../../machine_data')
 
 % load lattice
-load ../../ESRFLattice.mat
+ring=esrf;
 
 % get indexes
-indm=indBPM;
+indm=find(atgetcells(ring,'Class','Monitor'));
 indq=find(atgetcells(ring,'Class','Quadrupole')); % girders are defined by GS and GE markers (start and end of girder)
 
 rerr=atsetrandomerrors(...
@@ -14,12 +15,16 @@ rerr=atsetrandomerrors(...
     indq,...
     indm,...
     1,...
-    1e-1,...
+    1e-3,...
     2.5,...
     'dpb2');
 
-figure('units','normalized','position',[0.1 0.4 0.65 0.35])
-atplot(rerr,[0,100],'comment',[],@plPolynomBComp);
-saveas(gca,'FieldIntegral.fig')
-export_fig('FieldIntegral.jpg','-r300')
+figure('units','normalized','position',[0.1 0.4 0.65 0.45])
+atplot(rerr,'comment',[],@plPolynomBComp);
+figure('units','normalized','position',[0.1 0.4 0.65 0.45])
+atplot(ring);
+figure('units','normalized','position',[0.1 0.4 0.65 0.45])
+atplot(rerr);
+%saveas(gca,'FieldIntegral.fig')
+%export_fig('FieldIntegral.jpg','-r300')
 
