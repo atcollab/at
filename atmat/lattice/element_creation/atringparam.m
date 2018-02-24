640a22c9448e039a922bcff34a2d2c61ad514c4d
Laurent S. NADOLSKI <nadolski@synchrotron-soleil.fr>
2018-02-24 09:29:23 +0100

    Format help

--- atmat/lattice/element_creation/atringparam.m
+++ atmat/lattice/element_creation/atringparam.m
@@ -1,12 +1,17 @@
 function elem=atringparam(fname,varargin)
-%ATRINGPARAM(FAMNAME,E0,NBPERIODS)
-%	creates a RingParameter Element which should go at the beginning of the ring
+%ATRINGPARAM - creates a RingParameter Element which should go at the beginning of the ring
 %
-%FAMNAME	Family name which may be used as name of Ring
-%E0         Energy of electrons
-%NBPERIODS	Periodicity of the ring (1 if ring is already expanded)
+%  ATRINGPARAM(FAMNAME,E0,NBPERIODS)
+%	
+%  INPUTS
+%    1. FAMNAME	  Family name which may be used as name of Ring
+%    2. E0        Energy of electrons
+%    3. NBPERIODS Periodicity of the ring (1 if ring is already expanded)
 %
-%See also: ATDRIFT, ATQUADRUPOLE, ATSEXTUPOLE, ATSBEND, ATRBEND
+%  OUTPUS
+%    1. elem - RingParam class elem
+%
+%  See also ATDRIFT, ATQUADRUPOLE, ATSEXTUPOLE, ATSBEND, ATRBEND
 %          ATMULTIPOLE, ATTHINMULTIPOLE
 
 [rsrc,energy,nbper]=decodeatargs({6E9,1},varargin);
