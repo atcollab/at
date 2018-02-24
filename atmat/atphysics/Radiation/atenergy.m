1ccf41232a83feed3021bbbdd2f5a9f8a4220e18
Laurent S. NADOLSKI <nadolski@synchrotron-soleil.fr>
2018-02-24 07:28:15 +0100

    Improve header and comments

--- atmat/atphysics/Radiation/atenergy.m
+++ atmat/atphysics/Radiation/atenergy.m
@@ -1,29 +1,48 @@
 function [energy,nbper,voltage,harmnumber,U0]=atenergy(ring)
-%ENERGY=ATENERGY(RING) Gets the RING energy
+%ATENERGY - Gets the lattice energy
+%
+%  ENERGY=ATENERGY(RING) Gets the RING energy
 %   ATENERGY looks for the machine energy in:
 %       1) the 1st 'RingParam' element
 %       2) the 1st 'RFCavity' element
 %       3) the field "E0" of the global variable "GLOBVAL"
+%  
+%  INPUTS
+%    1. ring  Ring structure
 %
-%[ENERGY,PERIODS]=ATENERGY(RING) also outputs the number of periods
+%  OUPUTS
+%    1. energy     Ring energy in eV
+%    2. nbper      Number of periods to make 2pi phase advance
+%    3. voltage    Total RF voltage  
+%    4. harmnumber Harmonic number
+%    5. U0         Energy loss per turn in eV
 %
-%[ENERGY,PERIODS,VOLTAGE,HARMNUMBER]=ATENERGY(RING) also outputs the harmonic number
+%  NOTES
+%    1. Cavities in the ring must have the Class RFCavity.
+%    2. Check for 2 pi phase advance if more than 2 ouput arguments
 %
-%[ENERGY,PERIODS,VOLTAGE,HARMNUMBER,U0]=ATENERGY(RING) also outputs total losses in eV
+%  EXAMPLES
+%    1. [ENERGY,PERIODS]=atenergy(ring) also outputs the number of periods
+%    2. [ENERGY,PERIODS,VOLTAGE,HARMNUMBER]=atenergy(ring) also outputs the harmonic number
+%    3. [ENERGY,PERIODS,VOLTAGE,HARMNUMBER,U0]=atenergy(ring) also outputs total losses in eV
+%
+%  See also atgetU0 atsetcavity atenergy
 
 global GLOBVAL
+TWO_PI_ERROR = 1.e-4;
+ 
+params   = atgetcells(ring(:,1),'Class','RingParam');
+cavities = atgetcells(ring(:,1),'Frequency');
+dipoles  = atgetcells(ring(:,1),'BendingAngle');
+theta    = atgetfieldvalues(ring(dipoles),'BendingAngle');
 
-params=atgetcells(ring(:,1),'Class','RingParam');
-cavities=atgetcells(ring(:,1),'Frequency');
-dipoles=atgetcells(ring(:,1),'BendingAngle');
-theta=atgetfieldvalues(ring(dipoles),'BendingAngle');
-if any(params)
+if any(params) % case RingParam is defined in the lattice
     parmelem=ring{find(params,1)};
     energy=parmelem.Energy;
     if nargout >= 2
         nbper=parmelem.Periodicity;
     end
-else
+else % else look for energy in cavity or GLOBVAL
     if any(cavities) && isfield(ring{find(cavities,1)},'Energy')
         energy=ring{find(cavities,1)}.Energy;
     elseif isfield(GLOBVAL,'E0')
@@ -32,24 +51,25 @@ else
         error('AT:NoEnergy',...
             'Energy not defined (searched in ''RingParam'',''RFCavity'',GLOBVAL.E0)');
     end
-    if nargout >= 2
+    if nargout >= 2 % get number of periods
         if size(ring,2) > 1
             nbper=size(ring,2);
-        else
+        else % Guess number of periods based on the total bending angle
             nbp=2*pi/sum(theta);
             nbper=round(nbp);
             if ~isfinite(nbp)
                 warning('AT:WrongNumberOfCells','No bending in the cell, ncells set to 1');
                 nbper=1;
-            elseif abs(nbp-nbper) > 1.e-4
+            elseif abs(nbp-nbper) > TWO_PI_ERROR
                 warning('AT:WrongNumberOfCells','non integer number of cells: ncells = %g -> %g',nbp,nbper);
             end
         end
     end
 end
 
-if nargout >= 3
-    if any(cavities)
+% Get total cavity voltage and harmonic number
+if nargout >= 3 
+    if any(cavities) % sum over cavity number
         voltage=nbper*sum(atgetfieldvalues(ring(cavities),'Voltage'));
         harmnumber=nbper*atgetfieldvalues(ring(find(cavities,1)),'HarmNumber');
     elseif nargout >= 5
@@ -59,7 +79,9 @@ if nargout >= 3
         error('AT:NoCavity','No cavity element in the ring');
     end
 end
-if nargout >= 5
+
+% get energy loss per turn
+if nargout >= 5 
     % Losses = Cgamma/2/pi*EGeV^4*I2
     cgamma=4e9*pi*PhysConstant.classical_electron_radius.value/3/...
         PhysConstant.electron_mass_energy_equivalent_in_MeV.value^3; % [m/GeV^3]
