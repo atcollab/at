0db63a18ef36cd0601ae3b0d1cdb4b7286d6132f
Laurent S. NADOLSKI <nadolski@synchrotron-soleil.fr>
2018-02-27 07:38:47 +0100

    Change the code for back compatibility with previous version of Matlab Improve help part and add a subdirectory

--- atmat/athelp.m
+++ atmat/athelp.m
@@ -1,5 +1,34 @@
 function athelp
 %ATHELP generates the list of Accelerator Toolbox functions
+%
+%  EXAMPLES
+%  1. athelp: full help.
+%  2. for selected help, use help directory where directory is
+%      help atintegrators
+%      help atmat
+%      help atdemos
+%      help atgui
+%      help atmatch
+%      help atphysics
+%      help linearoptics
+%      help longitudinaldynamics
+%      help nonlineardynamics
+%      help atplot
+%      help plotfunctions
+%      help lattice
+%      help element_creation
+%      help pubtools
+%      help survey
+%      help lattice_tools
+%      help LatticeTuningFunctions
+%      help machine_date
+%      help tuneandchromaticity
+%      help touschekpiwinski
+%      help radiation
+%      help parametersummaryfunctions
+%           
+%
+%  See also help
 
 % Generate help file (Contents) use command atupdateContents
 
@@ -7,7 +36,12 @@ ATROOT = atroot;
 
 DIR_old = pwd;
 cd(fileparts(ATROOT))
-doc(cd);
+
+%for comaptbility with previous Matlab version 2013b for example
+%folder should not include full path
+[~, folder] = fileparts(cd);
+doc(folder);
+
 cd(DIR_old)
 
 %help(fileparts(ATROOT));
