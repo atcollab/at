8fa0e22b22e22ba476a5e1b88fd35e84f77aeb54
Will Rogers <wahrogers@gmail.com>
2018-03-22 11:29:06 +0000

    WIP adapting ExactHamiltonianPass for Python.

--- atintegrators/ExactHamiltonianPass.cc
+++ atintegrators/ExactHamiltonianPass.cc
@@ -1,106 +1,44 @@
-#include "mex.h"
-extern "C"
-{
-#include "elempass.h"
-}
+#include "atelem.c"
 #include <string.h>
 #include "atlalib.c"
 
 #define AT_MODE
 #include "track.cc"
 
-/* field metadata */
-
-static const char * FieldNames[] =
+struct elem
 {
-    "PolynomA",
-    "PolynomB",
-    "MaxOrder", 
-    "NumIntSteps",
-    "Length",
-    "BendingAngle",
-    "Type",
-    "gK",
-    "MultipoleFringe",
-    "R1", 
-    "R2",
-    "T1",
-    "T2"
+    double Length;
+    double BendingAngle;
+    double *PolynomA;
+    double *PolynomB;
+    int MaxOrder;
+    int NumIntSteps;
+    int Type;
+    /* Optional fields */
+    double gK;
+    int MultipoleFringe;
+    double *R1;
+    double *R2;
+    double *T1;
+    double *T2;
 };
 
 static const char * FirstOptionalName = "gK";
 
 #define NUM_FIELDS_2_REMEMBER (sizeof(FieldNames) / sizeof(FieldNames[0]))
 
-static int Lookup_Field(const char * name, const mxArray *ElemData, int optional)
+void ExactHamiltonianPass(double *r_in, double le,
+                     double *A, double *B,
+                     const double *T1, const double *T2,
+                     const double *R1, const double *R2,
+                     int max_order, int num_int_steps,
+                     double phi, int type,
+                     double gK, int multipole_fringe,
+                     int num_particles)
 {
-    char buffer[1024];
-    int fnum = mxGetFieldNumber(ElemData, name);
-    if(fnum < 0 && optional == 0)
-    {
-        sprintf(buffer, 
-                "Required field '%s' was not found in the element data structure", name);
-        mexErrMsgTxt(buffer);
-    }
-    return fnum;
-}
-
-static double * Get_Pr(const mxArray *ElemData, int *FieldNumbers, int n)
-{
-    if(FieldNumbers[n] < 0)
-        return NULL;
-    else 
-        return mxGetPr(mxGetFieldByNumber(ElemData,0,FieldNumbers[n]));
-}
-
-static double Get_Scalar(const mxArray *ElemData, int *FieldNumbers, int n)
-{
-    if(FieldNumbers[n] < 0)
-        return 0;
-    else 
-        return mxGetScalar(mxGetFieldByNumber(ElemData,0,FieldNumbers[n]));
-}
-
-static void Lookup_Fields(const mxArray *ElemData, int * Index)
-{
-    int n;
-    int optional = 0;
-    for(n = 0; n < NUM_FIELDS_2_REMEMBER; n++)
-    {
-        const char * name = FieldNames[n];
-        if(strcmp(name, FirstOptionalName) == 0)
-        {
-            optional = 1;
-        }
-        Index[n] = Lookup_Field(name, ElemData, optional);
-    }
-}
-
-static void BendPass(double * r, int num_particles, const mxArray * ElemData, int * FieldNumbers)
-{
-    int c, n, p;
+    int c, n;
     double * r6;
 
-    int f = 0;
-    
-    /* oh the old rtti problem again */
-
-    double * A           =      Get_Pr     (ElemData, FieldNumbers, f++);
-    double * B           =      Get_Pr     (ElemData, FieldNumbers, f++);
-    int max_order        = (int)Get_Scalar (ElemData, FieldNumbers, f++);
-    int num_int_steps    = (int)Get_Scalar (ElemData, FieldNumbers, f++);
-    double le            =      Get_Scalar (ElemData, FieldNumbers, f++);
-    double phi           =      Get_Scalar (ElemData, FieldNumbers, f++);
-    int type             = (int)Get_Scalar (ElemData, FieldNumbers, f++);
-
-    double gK            =      Get_Scalar (ElemData, FieldNumbers, f++);
-    int multipole_fringe = (int)Get_Scalar (ElemData, FieldNumbers, f++);
-
-    double * R1          =      Get_Pr     (ElemData, FieldNumbers, f++);
-    double * R2          =      Get_Pr     (ElemData, FieldNumbers, f++);
-    double * T1          =      Get_Pr     (ElemData, FieldNumbers, f++);
-    double * T2          =      Get_Pr     (ElemData, FieldNumbers, f++);
-    
     // copy AT structure into pass method structure
 
     element e = {0};
@@ -121,8 +59,8 @@ static void BendPass(double * r, int num_particles, const mxArray * ElemData, in
 
     for(c = 0; c<num_particles; c++)
     {
-        r6 = r+c*6;	
-        if(!mxIsNaN(r6[0]))
+        r6 = r_in+c*6;	
+        if(!atIsNaN(r6[0]))
         {   
             /* misalignment at entrance */
             if(T1)
@@ -141,30 +79,57 @@ static void BendPass(double * r, int num_particles, const mxArray * ElemData, in
     }
 }
 
-ExportMode int * passFunction(const mxArray *ElemData, int *FieldNumbers,
-                              double *r_in, int num_particles, int mode)
+#if defined(MATLAB_MEX_FILE) || defined(PYAT)
+
+ExportMode struct elem *trackFunction(const atElem *ElemData, struct elem *Elem,
+                               double *r_in, int num_particles,
+                               struct parameters *Param)
 
 {
-    int n;
-    if(mode == MAKE_LOCAL_COPY)
-    {
-        /* Allocate memory for integer array of 
-           field numbers for faster future reference
+    double le, bending_angle;
+    double *polynom_a, *polynom_b;
+    long max_order, num_int_steps, type, multipole_fringe;
+    double *R1, *R2, *T1, *T2;
+    double phi, gK;
+    le = atGetDouble(ElemData,"Length"); check_error();
+    bending_angle = atGetDouble(ElemData,"BendingAngle"); check_error();
+    polynom_a = atGetDoubleArray(ElemData,"PolynomA"); check_error();
+    polynom_b = atGetDoubleArray(ElemData,"PolynomB"); check_error();
+    max_order = atGetLong(ElemData, "MaxOrder"); check_error();
+    num_int_steps = atGetLong(ElemData, "NumIntSteps"); check_error();
+    type = atGetLong(ElemData, "Type"); check_error();
+    multipole_fringe = atGetLong(ElemData, "Type"); check_error();
+    phi = atGetDouble(ElemData,"Phi"); check_error();
+    gK = atGetDouble(ElemData,"gK"); check_error();
+    R1=atGetOptionalDoubleArray(ElemData,"R1"); check_error();
+    R2=atGetOptionalDoubleArray(ElemData,"R2"); check_error();
+    T1=atGetOptionalDoubleArray(ElemData,"T1"); check_error();
+    T2=atGetOptionalDoubleArray(ElemData,"T2"); check_error();
+    ExactHamiltonianPass(r_in, le, polynom_a, polynom_b, T1, T2, R1, R2, max_order,
+             num_int_steps, phi, type, gK, multipole_fringe, num_particles);
+    Elem = (struct elem*)atMalloc(sizeof(struct elem));
+    Elem->Length=le;
+    Elem->BendingAngle=bending_angle;
+    Elem->PolynomA=polynom_a;
+    Elem->PolynomB=polynom_b;
+    Elem->MaxOrder=max_order;
+    Elem->NumIntSteps=num_int_steps;
+    /*optional fields*/
+    Elem->R1=R1;
+    Elem->R2=R2;
+    Elem->T1=T1;
+    Elem->T2=T2;
 
-           JR pretty uncool, what about the memory leak?
-           that's why my AT crashes
-        */
-        FieldNumbers = (int*)mxCalloc(NUM_FIELDS_2_REMEMBER,sizeof(int));
-        Lookup_Fields(ElemData, FieldNumbers);
-     }
-        
-    if(mode == MAKE_LOCAL_COPY || mode == USE_LOCAL_COPY)
-    {
-        BendPass(r_in, num_particles, ElemData, FieldNumbers);
-    }
-    
-    return FieldNumbers;
+    return Elem;
 }
+
+MODULE_DEF(ExactHamiltonianPass) /* Dummy module initialisation */
+
+#endif /* defined(PYAT) || defined(MATLAB_MEX_FILE) */
+
+
+
+#if defined(MATLAB_MEX_FILE)
 
 /* generic show required and optional fields */
 static void ShowFields(int nlhs, mxArray **plhs)
@@ -204,7 +169,6 @@ static void ShowFields(int nlhs, mxArray **plhs)
     }
 }
 
-
 /* generic mexFunction */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
@@ -220,10 +184,24 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         Lookup_Fields(ElemData, FieldNumbers);
 	plhs[0] = mxDuplicateArray(prhs[1]);
 	r_in = mxGetPr(plhs[0]);
-        BendPass(r_in, n, ElemData, FieldNumbers);
+        le = atGetDouble(ElemData,"Length"); check_error();
+        PolynomA=atGetDoubleArray(ElemData,"PolynomA"); check_error();
+        PolynomB=atGetDoubleArray(ElemData,"PolynomB"); check_error();
+        max_order = atGetInt(ElemData, "MaxOrder"); check_error();
+        num_int_steps = atGetInt(ElemData, "NumIntSteps"); check_error();
+        phi = atGetDouble(ElemData,"Phi"); check_error();
+        gK = atGetDouble(ElemData,"gK"); check_error();
+        multipole_fringe = atGetLong(ElemData, "Type"); check_error();
+        R1=atGetOptionalDoubleArray(ElemData,"R1"); check_error();
+        R2=atGetOptionalDoubleArray(ElemData,"R2"); check_error();
+        T1=atGetOptionalDoubleArray(ElemData,"T1"); check_error();
+        T2=atGetOptionalDoubleArray(ElemData,"T2"); check_error();
+        ExactHamiltonianPass(r_in, le, polynom_a, polynom_b, T1, T2, R1, R2, max_order,
+                 num_int_steps, phi, type, gK, multipole_fringe, num_particles)
     }
     else
     {
         ShowFields(nlhs, plhs);
     }
 }
+#endif
