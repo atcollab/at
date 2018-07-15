33d820d29f8e54efee6bb96f8bcdd3d282003d09
Laurent Farvacque <laurent.farvacque@gmail.com>
2018-07-15 10:42:31 +0200

    Corrected bug in dimensions of kick tables

--- atintegrators/IdTablePass.c
+++ atintegrators/IdTablePass.c
@@ -18,10 +18,10 @@ struct elem {
     double Length;
     double *xkick;
     double *ykick;
-    double *xtable;
-    double *ytable;
-    int n_map;
-    int m_map;
+    double *x_map;
+    double *y_map;
+    int nx_map;
+    int ny_map;
     int Nslice;
     /* Optional fields */
     double *xkick1;
@@ -32,48 +32,45 @@ struct elem {
     double *T2;
 };
 
-double *GLOBAL_x, *GLOBAL_y;
-int GLOBAL_m,GLOBAL_n;
+double *GLOBAL_x_map, *GLOBAL_y_map;
+int GLOBAL_nx_map,GLOBAL_ny_map;
 
 static double get_kick(double *r6, double *ktable)
 {
     double f;
     /*cubic interpolation*/
-    /*splin2(GLOBAL_y,GLOBAL_x,GLOBAL_xkick,GLOBAL_xkick2,GLOBAL_n,GLOBAL_m,y,x,&f);*/
+    /*splin2(GLOBAL_y_map,GLOBAL_x_map,GLOBAL_xkick,GLOBAL_xkick2,GLOBAL_n,GLOBAL_nx,y,x,&f);*/
     
     /*biliniar interpolation*/
 #ifdef MATLAB_MEX_FILE
-    /* Transpose coordinates because kick-table is FORTRAN-ordered */
-    linint(GLOBAL_y, GLOBAL_x, ktable, GLOBAL_m, GLOBAL_n, r6[2], r6[0], &f);
+    /* Transpose coordinates because the kick-table is FORTRAN-ordered */
+    linint(GLOBAL_y_map, GLOBAL_x_map, ktable, GLOBAL_ny_map, GLOBAL_nx_map, r6[2], r6[0], &f);
 #else
-    /* Assume kick-table is C-ordered */
-    linint(GLOBAL_x, GLOBAL_y, ktable, GLOBAL_m, GLOBAL_n, r6[0], r6[2], &f);
+    /* Assume the kick-table is C-ordered */
+    linint(GLOBAL_x_map, GLOBAL_y_map, ktable, GLOBAL_nx_map, GLOBAL_ny_map, r6[0], r6[2], &f);
 #endif
     return f;
  }
 
 void IdKickMapModelPass(double *r, double le, double *xkick1, double *ykick1,
-        double *xkick, double *ykick, double *x, double *y,int n,int m, int Nslice,
+        double *xkick, double *ykick, double *x_map, double *y_map,int nx_map,int ny_map, int Nslice,
         double *T1, double *T2, double *R1, double *R2, int num_particles)
 {
     double *r6, deltaxp, deltayp, limitsptr[4];
-    int c;
+    int c, ns;
     double L1 = le/(2*Nslice);
     
     /*Act as AperturePass*/
-    limitsptr[0]=x[0];
-    limitsptr[1]=x[n-1];
-    limitsptr[2]=y[0];
-    limitsptr[3]=y[m-1];
+    limitsptr[0]=x_map[0];
+    limitsptr[1]=x_map[nx_map-1];
+    limitsptr[2]=y_map[0];
+    limitsptr[3]=y_map[ny_map-1];
     
-    /*globalize*/
-    
-    /* For cubic interpolation only*/
-    
-    GLOBAL_x=x;
-    GLOBAL_y=y;
-    GLOBAL_m=m; /* y used as colums*/
-    GLOBAL_n=n; /* x used as rows*/
+    /*globalize*/    
+    GLOBAL_x_map=x_map;
+    GLOBAL_y_map=y_map;
+    GLOBAL_nx_map=nx_map;
+    GLOBAL_ny_map=ny_map;
     
     for (c=0; c<num_particles; c++) {
         r6 = r+c*6;
@@ -82,9 +79,9 @@ void IdKickMapModelPass(double *r, double le, double *xkick1, double *ykick1,
             if (T1) ATaddvv(r6,T1);
             if (R1) ATmultmv(r6,R1);
             /* Check physical apertures at the entrance of the magnet */
-              checkiflostRectangularAp(r6, limitsptr);
+            checkiflostRectangularAp(r6, limitsptr);
             /*Tracking in the main body*/
-            for (m=0; m<Nslice; m++) { /* Loop over slices*/
+            for (ns=0; ns<Nslice; ns++) { /* Loop over slices*/
                 ATdrift6(r6,L1);
                 if (!atIsNaN(r6[0])&&!atIsNaN(r6[2])) {
                     /*The kick from IDs varies quadratically, not linearly, with energy.   */
@@ -109,15 +106,15 @@ ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
         double *r_in, int num_particles, struct parameters *Param)
 {
     if (!Elem) {
-        int Nslice, n_map,m_map;
-        double Length, *xkick, *ykick, *xtable, *ytable;
+        int Nslice, ny_map,nx_map;
+        double Length, *xkick, *ykick, *x_map, *y_map;
         double *xkick1, *ykick1;
         double *R1, *R2, *T1, *T2;
         Length=atGetDouble(ElemData,"Length"); check_error();
-        xkick=atGetDoubleArray(ElemData,"xkick"); check_error();
+        xkick=atGetDoubleArraySz(ElemData,"xkick", &nx_map, &ny_map); check_error();
         ykick=atGetDoubleArray(ElemData,"ykick"); check_error();
-        xtable=atGetDoubleArraySz(ElemData,"xtable", &m_map, &n_map); check_error();
-        ytable=atGetDoubleArray(ElemData,"ytable"); check_error();
+        x_map=atGetDoubleArray(ElemData,"xtable"); check_error();
+        y_map=atGetDoubleArray(ElemData,"ytable"); check_error();
         Nslice=atGetLong(ElemData,"Nslice"); check_error();
         /*optional fields*/
         xkick1=atGetOptionalDoubleArray(ElemData,"xkick1"); check_error();
@@ -130,10 +127,10 @@ ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
         Elem->Length=Length;
         Elem->xkick=xkick;
         Elem->ykick=ykick;
-        Elem->xtable=xtable;
-        Elem->ytable=ytable;
-        Elem->n_map=n_map;
-        Elem->m_map=m_map;
+        Elem->x_map=x_map;
+        Elem->y_map=y_map;
+        Elem->nx_map=nx_map;
+        Elem->ny_map=ny_map;
         Elem->Nslice=Nslice;
         Elem->xkick1=xkick1;
         Elem->ykick1=ykick1;
@@ -143,7 +140,7 @@ ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
         Elem->T2=T2;
     }
     IdKickMapModelPass(r_in, Elem->Length, Elem->xkick1, Elem->ykick1,
-            Elem->xkick, Elem->ykick, Elem->xtable, Elem->ytable, Elem->n_map, Elem->m_map,Elem->Nslice,
+            Elem->xkick, Elem->ykick, Elem->x_map, Elem->y_map, Elem->nx_map, Elem->ny_map,Elem->Nslice,
             Elem->T1, Elem->T2, Elem->R1, Elem->R2, num_particles);
     return Elem;
 }
@@ -155,18 +152,18 @@ MODULE_DEF(IdTablePass)        /* Dummy module initialisation */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     if (nrhs == 2) {
-        int Nslice, n_map,m_map;
-        double Length, *xkick, *ykick, *xtable, *ytable;
+        int Nslice, ny_map, nx_map;
+        double Length, *xkick, *ykick, *x_map, *y_map;
         double *xkick1, *ykick1;
         double *R1, *R2, *T1, *T2;
         double *r_in;
         const mxArray *ElemData = prhs[0];
         int num_particles = mxGetN(prhs[1]);
         Length=atGetDouble(ElemData,"Length"); check_error();
-        xkick=atGetDoubleArray(ElemData,"xkick"); check_error();
+        xkick=atGetDoubleArraySz(ElemData,"xkick", &nx_map, &ny_map); check_error();
         ykick=atGetDoubleArray(ElemData,"ykick"); check_error();
-        xtable=atGetDoubleArraySz(ElemData,"xtable", &m_map, &n_map); check_error();
-        ytable=atGetDoubleArray(ElemData,"ytable"); check_error();
+        x_map=atGetDoubleArray(ElemData,"xtable"); check_error();
+        y_map=atGetDoubleArray(ElemData,"ytable"); check_error();
         Nslice=atGetLong(ElemData,"Nslice"); check_error();
         /*optional fields*/
         xkick1=atGetOptionalDoubleArray(ElemData,"xkick1"); check_error();
@@ -178,8 +175,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         /* ALLOCATE memory for the output array of the same size as the input  */
         plhs[0] = mxDuplicateArray(prhs[1]);
         r_in = mxGetPr(plhs[0]);
-        IdKickMapModelPass(r_in, Length, xkick1, ykick1, xkick, ykick, xtable, ytable,
-                n_map, m_map, Nslice, T1, T2, R1, R2, num_particles);
+        IdKickMapModelPass(r_in, Length, xkick1, ykick1, xkick, ykick, x_map, y_map,
+                nx_map, ny_map, Nslice, T1, T2, R1, R2, num_particles);
     }
     else if (nrhs == 0) {
         /* return list of required fields */
