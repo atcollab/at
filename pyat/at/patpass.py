80e547644930cb0bac5df5010f48db9bafa5c466
Laurent Farvacque <laurent.farvacque@esrf.fr>
2018-08-25 20:42:56 +0200

    ignore output as long as refpts is not handled

--- pyat/at/patpass.py
+++ pyat/at/patpass.py
@@ -2,20 +2,22 @@
 Simple parallelisation of atpass() using multiprocessing.
 """
 import multiprocessing
-from at import atpass
+# noinspection PyUnresolvedReferences
+from .atpass import atpass
+from .lattice import uint32_refpts
 import numpy
 
 
 def _atpass_one(args):
-    ring, rin, turns = args
-    ans = atpass(ring, rin, turns)
-    return rin
+    ring, rin, turns, refpts = args
+    ans = atpass(ring, rin, turns, refpts)
+    return ans
 
 
-def _patpass(ring, rin, nturns, pool_size):
+def _patpass(ring, rin, nturns, refpts, pool_size):
     rout = numpy.zeros((rin.shape[0], rin.shape[1] * nturns))
     pool = multiprocessing.Pool(pool_size)
-    args = [(ring, rin[:, i], nturns) for i in range(rin.shape[1])]
+    args = [(ring, rin[:, i], nturns, refpts) for i in range(rin.shape[1])]
     results = pool.map(_atpass_one, args)
     for i, res in enumerate(results):
         # Fold the results back into the same shape as atpass.
@@ -33,6 +35,8 @@ def patpass(ring, rin, nturns, reuse=True, refpts=None, pool_size=None):
         raise ValueError('patpass does not support altering lattices')
     if refpts is not None:
         raise ValueError('patpass does not yet support refpts')
+    refpts = len(ring)
+    refs = uint32_refpts(refpts, len(ring))
     if pool_size is None:
         pool_size = multiprocessing.cpu_count()
-    return _patpass(ring, rin, nturns, pool_size)
+    return _patpass(ring, rin, nturns, refs, pool_size)
