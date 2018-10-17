d0bcb39ab54452bd1d3ea99f07257db9f8cae793
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-10-17 16:16:39 +0100

    Removed use of indexes, updated variable names and error raising syntax.

--- pyat/at/load_mat.py
+++ pyat/at/load_mat.py
@@ -21,31 +21,30 @@ PASSMETHOD_MAPPING = {'CorrectorPass': 'Corrector', 'Matrix66Pass': 'M66',
                       'ThinMPolePass': 'ThinMultipole', 'DriftPass': 'Drift'}
 
 
-def hasattrs(element_kwargs, *attributes):
+def hasattrs(kwargs, *attributes):
     """Check if the element would have the specified attribute(s), i.e. if they
         are in kwargws; allows checking for multiple attributes in one go.
     """
     results = []
     for attribute in attributes:
         try:
-            element_kwargs[attribute]
+            kwargs[attribute]
             results.append(True)
         except KeyError:
             results.append(False)
     return any(results)
 
 
-def find_class_name(elem_kwargs):
+def find_class_name(kwargs):
     try:
-        class_name = elem_kwargs.pop('Class')
+        class_name = kwargs.pop('Class')
         class_name = CLASS_MAPPING.get(class_name, class_name)
         if class_name in CLASSES:
             return class_name
         else:
-            raise AttributeError("Invalid Class {0} on element {1}."
-                                 .format(class_name, elem_kwargs['Index']))
+            raise AttributeError("Class {0} does not exist.".format(class_name))
     except KeyError:
-        fam_name = elem_kwargs.get('FamName')
+        fam_name = kwargs.get('FamName')
         if fam_name in CLASSES:
             return fam_name
         elif fam_name.lower() == 'ap':
@@ -55,40 +54,39 @@ def find_class_name(elem_kwargs):
         elif fam_name.lower() == 'bpm':
             return 'Monitor'
         else:
-            pass_method = elem_kwargs.get('PassMethod')
+            pass_method = kwargs.get('PassMethod')
             class_from_pass = PASSMETHOD_MAPPING.get(pass_method)
             if class_from_pass != None:
                 return class_from_pass
-            else:  # I could add some additional logic in here.
-                if hasattrs(elem_kwargs, 'FullGap', 'FringeInt1', 'FringeInt2',
-                            'gK', 'EntranceAngle', 'ExitAngle'):
+            else:
+                if hasattrs(kwargs, 'FullGap', 'FringeInt1', 'FringeInt2', 'gK',
+                            'EntranceAngle', 'ExitAngle'):
                     return 'Bend'
-                elif hasattrs(elem_kwargs, 'Frequency', 'HarmNumber',
-                              'PhaseLag'):
+                elif hasattrs(kwargs, 'Frequency', 'HarmNumber', 'PhaseLag'):
                     return 'RFCavity'
-                elif hasattrs(elem_kwargs, 'KickAngle'):
+                elif hasattrs(kwargs, 'KickAngle'):
                     return 'Corrector'
-                elif hasattrs(elem_kwargs, 'Periodicity'):
+                elif hasattrs(kwargs, 'Periodicity'):
                     return 'RingParam'
-                elif hasattrs(elem_kwargs, 'Limits'):
+                elif hasattrs(kwargs, 'Limits'):
                     return 'Aperture'
-                elif hasattrs(elem_kwargs, 'M66'):
+                elif hasattrs(kwargs, 'M66'):
                     return 'M66'
-                elif hasattrs(elem_kwargs, 'GCR'):
+                elif hasattrs(kwargs, 'GCR'):
                     return 'Monitor'
-                elif hasattrs(elem_kwargs, 'K'):
+                elif hasattrs(kwargs, 'K'):
                     return 'Quadrupole'
-                elif hasattrs(elem_kwargs, 'PolynomB'):
+                elif hasattrs(kwargs, 'PolynomB'):
                     try:
-                        PolynomB = numpy.array(elem_kwargs['PolynomB'],
+                        PolynomB = numpy.array(kwargs['PolynomB'],
                                                dtype=numpy.float64)
                         loworder = numpy.where(PolynomB != 0.0)[0][0]
                     except IndexError:
-                        if (elem_kwargs.get('PassMethod') ==
+                        if (kwargs.get('PassMethod') ==
                             'StrMPoleSymplectic4Pass'):
                             return 'Multipole'
-                        elif hasattrs(elem_kwargs, 'BendingAngle'):
-                            if float(elem_kwargs['BendingAngle']) == 0.0:
+                        elif hasattrs(kwargs, 'BendingAngle'):
+                            if float(kwargs['BendingAngle']) == 0.0:
                                 return 'Drift'
                             else:
                                 return 'Bend'
@@ -96,21 +94,21 @@ def find_class_name(elem_kwargs):
                             return 'Drift'
                     if loworder == 1:
                         return 'Quadrupole'
-                    elif (loworder == 2) or (float(elem_kwargs['PolynomA'][1])
-                                             != 0.0):  # could be skew quad?
+                    elif (loworder == 2) or (float(kwargs['PolynomA'][1]) !=
+                                             0.0):  # could be skew quad?
                         return 'Sextupole'
-                    elif (loworder == 3) or (float(elem_kwargs['PolynomA'][3])
-                                             != 0.0):
+                    elif (loworder == 3) or (float(kwargs['PolynomA'][3]) !=
+                                             0.0):
                         return 'Octupole'
                     else:
-                        if hasattrs(elem_kwargs, 'Length'):
+                        if hasattrs(kwargs, 'Length'):
                             return 'Multipole'
                         else:
                             return 'ThinMultipole'
-                elif hasattrs(elem_kwargs, 'BendingAngle'):
+                elif hasattrs(kwargs, 'BendingAngle'):
                     return 'Dipole'
-                elif hasattrs(elem_kwargs, 'Length'):
-                    if float(elem_kwargs['Length']) != 0.0:
+                elif hasattrs(kwargs, 'Length'):
+                    if float(kwargs['Length']) != 0.0:
                         return 'Drift'
                     else:
                         return 'Marker'
@@ -118,29 +116,25 @@ def find_class_name(elem_kwargs):
                     return 'Marker'
 
 
-def sanitise_class(element_kwargs):
-    pass_method = element_kwargs.get('PassMethod')
+def sanitise_class(kwargs):
+    pass_method = kwargs.get('PassMethod')
     if pass_method != None:
         pass_to_class = PASSMETHOD_MAPPING.get(pass_method)
-        if (pass_method == 'IdentityPass') and (element_kwargs['Class'].lower()
-                                                == 'drift'):
-            element_kwargs['Class'] = 'Monitor'
+        if (pass_method == 'IdentityPass') and (kwargs['Class'].lower() ==
+                                                'drift'):
+            kwargs['Class'] = 'Monitor'
         elif pass_to_class != None:
-            if pass_to_class.lower() != element_kwargs['Class'].lower():
-                raise AttributeError("On element {0}, Class {1} is not "
-                                     "compatible with PassMethod {2}:{3}."
-                                     .format(element_kwargs['Index'],
-                                             element_kwargs['Class'],
-                                             pass_method))
+            if pass_to_class.lower() != kwargs['Class'].lower():
+                raise AttributeError("PassMethod {0} is not compatible with "
+                                     "Class {1}.".format(pass_method,
+                                                         kwargs['Class']))
         else:
-            if element_kwargs['Class'].lower() in ['marker', 'monitor', 'drift',
+            if kwargs['Class'].lower() in ['marker', 'monitor', 'drift',
                                                    'ringparam']:
                 if pass_method not in ['DriftPass', 'IdentityPass']:
-                    raise AttributeError("On element {0}, Class {1} is not "
-                                         "compatible with PassMethod {2}."
-                                         .format(element_kwargs['Index'],
-                                                 element_kwargs['Class'],
-                                                 pass_method))
+                    raise AttributeError("PassMethod {0} is not compatible with"
+                                         " Class {1}.".format(pass_method,
+                                                              kwargs['Class']))
 
 
 def load_element(index, element_array):
@@ -162,8 +156,8 @@ def load_element(index, element_array):
 
     cl = getattr(elements, kwargs['Class'])
     if cl == None:
-        raise AttributeError("Class {0}, on element {1}, does not exist."
-                             .format(kwargs['Class'], index))
+        raise AttributeError("Class {0} does not exist."
+                             .format(kwargs['Class']))
     # Remove mandatory attributes from the keyword arguments.
     args = [kwargs.pop(attr) for attr in cl.REQUIRED_ATTRIBUTES]
     element = cl(*args, **kwargs)
@@ -177,5 +171,8 @@ def load(filename, key='RING'):
     py_ring = []
     element_arrays = m[key].flat
     for i in range(len(element_arrays)):
-        py_ring.append(load_element(i, element_arrays[i][0, 0]))
+        try:
+            py_ring.append(load_element(i, element_arrays[i][0, 0]))
+        except AttributeError as error_message:
+            raise AttributeError('On element {0}, {1}'.format(i, error_message))
     return py_ring
