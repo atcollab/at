b7f96edba4a2d9240a58bfb14c230d3e2ac28ecd
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-11-20 19:39:57 +0000

    Remove accidentally committed twiss_transfer and rename test_load_mat.

--- pyat/test_matlab/twiss_transfer.m
+++
@@ -1,9 +0,0 @@
-function [ t ] = twiss_transfer(RING,DP,refpts)
-twiss = twissring(RING,DP,refpts);
-Orbit = [twiss.ClosedOrbit];
-M44 = reshape([twiss.M44], 4, 4, []);
-Alpha = reshape([twiss.alpha], 2, []);
-Beta = reshape([twiss.beta], 2, []);
-Mu = reshape([twiss.mu], 2, []);
-t = struct('O', Orbit, 'M', M44, 'A', Alpha, 'B', Beta, 'U', Mu);
-end
