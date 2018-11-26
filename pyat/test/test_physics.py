df0ada573c606cb77150ce8dd4e78a239d97a9d2
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-11-26 12:34:23 +0000

    Use atol for almost all assert_allclose array comparisons.

--- pyat/test/test_physics.py
+++ pyat/test/test_physics.py
@@ -36,7 +36,7 @@ def test_find_orbit4(ring):
 def test_find_orbit4_finds_zeros_if_dp_zero(ring):
     orbit4, _ = physics.find_orbit4(ring, 0)
     expected = numpy.zeros((6,))
-    numpy.testing.assert_allclose(orbit4, expected)
+    numpy.testing.assert_allclose(orbit4, expected, atol=1e-7)
 
 
 def test_find_orbit4_result_unchanged_by_atpass(ring):
@@ -134,7 +134,7 @@ def test_find_m44_no_refpts(ring):
 def test_get_twiss(ring, refpts):
     twiss0, tune, chrom, twiss = physics.get_twiss(ring, DP, refpts,
                                                    get_chrom=True)
-    numpy.testing.assert_allclose(twiss['s_pos'][-1], 56.209377216)
+    numpy.testing.assert_allclose(twiss['s_pos'][-1], 56.209377216, atol=1e-9)
     numpy.testing.assert_allclose(twiss['closed_orbit'][0][:5],
                                   [1.0916359e-7, 0, 0, 0, DP], atol=1e-12)
     numpy.testing.assert_allclose(twiss['m44'][-1, :, :], M44_MATLAB,
@@ -159,7 +159,7 @@ def test_linopt(ring, refpts):
                                                     get_chrom=True)
     numpy.testing.assert_allclose(tune, [0.365529, 0.493713], rtol=1e-5)
     numpy.testing.assert_allclose(chrom, [-0.309037, -0.441859], rtol=1e-5)
-    numpy.testing.assert_allclose(lindata['s_pos'][-1], 56.209377216)
+    numpy.testing.assert_allclose(lindata['s_pos'][-1], 56.209377216, atol=1e-9)
     numpy.testing.assert_allclose(lindata['closed_orbit'][-1][:5],
                                   [1.091636e-7, 1.276757e-15, 4.238871e-33,
                                    1.117703e-33, DP], atol=1e-12)
@@ -171,8 +171,9 @@ def test_linopt(ring, refpts):
                 [6.577482e-33, 8.75482e-32, -5.949696e-3, -0.99922]]
     numpy.testing.assert_allclose(lindata['m44'][-1], expected, rtol=1e-5,
                                   atol=1e-7)
-    numpy.testing.assert_allclose(lindata['alpha'][-1],
-                                  [-1.32787e-7, 1.85909e-7], rtol=1e-5)
+    numpy.testing.assert_allclose(lindata['alpha'][-1], [-1.32787e-7,
+                                                         1.85909e-7],
+                                  rtol=1e-5, atol=1e-7)
     numpy.testing.assert_almost_equal(lindata['beta'][-1], [2.98719, 6.638115],
                                       decimal=4)
     numpy.testing.assert_almost_equal(lindata['mu'][-1], [2.296687, 3.102088],
@@ -180,13 +181,13 @@ def test_linopt(ring, refpts):
     numpy.testing.assert_almost_equal(lindata['gamma'][-1], 1)
     numpy.testing.assert_allclose(lindata['A'][-1],
                                   [[-0.6638, 2.23415],
-                                   [-0.25037, -0.6638]], rtol=1e-5)
+                                   [-0.25037, -0.6638]], rtol=1e-5, atol=1e-7)
     numpy.testing.assert_allclose(lindata['B'][-1],
                                   [[-0.99922, 0.262171],
-                                   [-0.00595, -0.99922]], rtol=1e-4)
-    numpy.testing.assert_allclose(lindata['C'][-1],
-                                  [[-9.87933e-32, -1.65044e-30],
-                                   [-2.44501e-32, -2.91703e-31]], rtol=1e-5)
+                                   [-0.00595, -0.99922]], rtol=1e-4, atol=1e-7)
+    numpy.testing.assert_allclose(lindata['C'][-1], [[-9.87933e-32, -1.65044e-30],
+                                                     [-2.44501e-32, -2.91703e-31]],
+                                  rtol=1e-5, atol=1e-7)
 
 
 @pytest.mark.parametrize('refpts', ([145], [1, 2, 3, 145]))
@@ -194,7 +195,7 @@ def test_linopt_uncoupled(ring, refpts):
     lindata0, tune, chrom, lindata = physics.linopt(ring, DP, refpts,
                                                     coupled=False)
     numpy.testing.assert_allclose(tune, [0.365529, 0.493713], rtol=1e-5)
-    numpy.testing.assert_allclose(lindata['s_pos'][-1], 56.209377216)
+    numpy.testing.assert_allclose(lindata['s_pos'][-1], 56.209377216, atol=1e-9)
     numpy.testing.assert_allclose(lindata['closed_orbit'][-1][:5],
                                   [1.091636e-7, 1.276757e-15, 4.238871e-33,
                                    1.117703e-33, DP], atol=1e-12)
@@ -203,8 +204,9 @@ def test_linopt_uncoupled(ring, refpts):
                     [6.577482e-33, 8.75482e-32, -5.949696e-3, -0.99922]]
     numpy.testing.assert_allclose(lindata['m44'][-1], expected_m44, rtol=1e-5,
                                   atol=1e-7)
-    numpy.testing.assert_allclose(lindata['alpha'][-1],
-                                  [-1.32787e-7, 1.85909e-7], rtol=1e-5)
+    numpy.testing.assert_allclose(lindata['alpha'][-1], [-1.32787e-7,
+                                                         1.85909e-7],
+                                  rtol=1e-5, atol=1e-7)
     numpy.testing.assert_almost_equal(lindata['beta'][-1], [2.98719, 6.638115],
                                       decimal=4)
     numpy.testing.assert_almost_equal(lindata['mu'][-1], [2.296687, 3.102088],
@@ -212,12 +214,12 @@ def test_linopt_uncoupled(ring, refpts):
     numpy.testing.assert_almost_equal(lindata['gamma'][-1], 1)
     numpy.testing.assert_allclose(lindata['A'][-1],
                                   [[-0.6638, 2.23415],
-                                   [-0.25037, -0.6638]], rtol=1e-5)
+                                   [-0.25037, -0.6638]], rtol=1e-5, atol=1e-7)
     numpy.testing.assert_allclose(lindata['B'][-1],
                                   [[-0.99922, 0.262171],
-                                   [-0.00595, -0.99922]], rtol=1e-4)
-    numpy.testing.assert_allclose(lindata['C'][-1], [[0., 0.],
-                                                     [0., 0.,]], rtol=1e-5)
+                                   [-0.00595, -0.99922]], rtol=1e-4, atol=1e-7)
+    numpy.testing.assert_allclose(lindata['C'][-1], [[0., 0.], [0., 0.,]],
+                                  rtol=1e-5, atol=1e-7)
 
 
 def test_linopt_no_refpts(ring):
