a0ffcd8339664a1896002646e4fb9af421ab408e
Simone Liuzzo <simone.liuzzo@esrf.fr>
2019-02-04 13:25:50 +0100

    bug: PolynomB instead polynomA for vertical steerers

--- atmat/pubtools/LatticeTuningFunctions/correction/table/atsetcorrectiontable.m
+++ atmat/pubtools/LatticeTuningFunctions/correction/table/atsetcorrectiontable.m
@@ -12,22 +12,31 @@ function rcor = atsetcorrectiontable(ring,cortab)
 
 
 % create correctors table
-allind = 1:length(ring);
+allind = atgetcells(ring,'PolynomB');%1:length(ring);
 
 Lengths = atgetfieldvalues(ring,allind,'Length');
 
 % values for lattice with errors no correction
 ch0  = atgetfieldvalues(ring,allind,'PolynomB',{1,1});
-cv0  = atgetfieldvalues(ring,allind,'PolynomB',{1,1});
+cv0  = atgetfieldvalues(ring,allind,'PolynomA',{1,1});
 cq0  = atgetfieldvalues(ring,allind,'PolynomB',{1,2});
 csk0 = atgetfieldvalues(ring,allind,'PolynomA',{1,2});
 cs0  = atgetfieldvalues(ring,allind,'PolynomB',{1,3});
 co0  = atgetfieldvalues(ring,allind,'PolynomB',{1,4});
 
+ch0(isnan(ch0))=0;
+cv0(isnan(cv0))=0;
+cq0(isnan(cq0))=0;
+csk0(isnan(csk0))=0;
+cs0(isnan(cs0))=0;
+co0(isnan(co0))=0;
+
 % values for lattice with errors and correction
 rcor = ring;
+cortab=cortab(allind,:);
+
 rcor  = atsetfieldvalues(rcor,allind,'PolynomB',{1,1},ch0  + cortab.KL0n./Lengths);
-rcor  = atsetfieldvalues(rcor,allind,'PolynomB',{1,1},cv0  + cortab.KL0s./Lengths);
+rcor  = atsetfieldvalues(rcor,allind,'PolynomA',{1,1},cv0  + cortab.KL0s./Lengths);
 rcor  = atsetfieldvalues(rcor,allind,'PolynomB',{1,2},cq0  + cortab.KL1n./Lengths);
 rcor  = atsetfieldvalues(rcor,allind,'PolynomA',{1,2},csk0 + cortab.KL1s./Lengths);
 rcor  = atsetfieldvalues(rcor,allind,'PolynomB',{1,3},cs0  + cortab.KL2n./Lengths);
