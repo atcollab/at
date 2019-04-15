84fd54104af391b2a40e7f5fe932e12f840b88ce
ncarmignani <n.carmignani@gmail.com>
2019-04-15 15:26:10 +0200

    added an option to atsetshift to add a relative shift

--- atmat/lattice/atsetshift.m
+++ atmat/lattice/atsetshift.m
@@ -10,16 +10,20 @@ function ring=atsetshift(varargin)
 %	X  ->  X-DX
 %   Y  ->  Y-DY
 %
-% ATSETSHIFT(ELEMINDEX, DX, DY) Uses the global variable THERING
+% RING=ATSETSHIFT(RING, ELEMINDEX, DX, DY, 'RelativeShift')
+% adds the shifts DX and DY to the the previous misalignments 
+% of the elements 
 %
+% ATSETSHIFT(ELEMINDEX, DX, DY) Uses the global variable THERING
+% 
 % See also: ATSETTILT
 
+[RelativeShift,varargin]=getflag(varargin,'RelativeShift');
 global THERING
 if ~iscell(varargin{1})
     THERING=atsetshift(THERING,varargin{:});
 else
     [ring,idx,dx,dy]=deal(varargin{:});
-    
     if length(dx) == 1
         dx=dx*ones(size(idx));
     elseif length(dx) ~= length(idx)
@@ -31,7 +35,13 @@ else
         error('AT:length','Vector lengths are incompatible: %i/%i.',length(idx),length(dy))
     end
     
-    for i = 1:length(idx)
-        ring{idx(i)}=atshiftelem(ring{idx(i)},dx(i),dy(i));
+    if RelativeShift
+        for i = 1:length(idx)
+            ring{idx(i)}=atshiftelem(ring{idx(i)},dx(i),dy(i),'RelativeShift');
+        end
+    else
+        for i = 1:length(idx)
+            ring{idx(i)}=atshiftelem(ring{idx(i)},dx(i),dy(i));
+        end
     end
 end
--- atmat/lattice/atshiftelem.m
+++ atmat/lattice/atshiftelem.m
@@ -1,13 +1,23 @@
-function elem = atshiftelem(elem,dx,dz)
+function elem = atshiftelem(elem,dx,dy,varargin)
 %ATSHIFTELEM set new displacement parameters
 %NEWELEM=ATSHIFTELEM(OLDELEM,DX,DZ)
 %
 %DX:	horizontal element displacement
-%DZ:	Vertical element displacement
+%DY:	Vertical element displacement
+%
+%
+%NEWELEM=ATSHIFTELEM(OLDELEM,DX,DY,'RelativeShift')
+%the shift is added to the previous misalignment of the element
 %
 %See also: attiltelem, atmodelem
 
-disp=[dx;0;dz;0;0;0];
-elem.T1=-disp;
-elem.T2=disp;
+RelativeShift=getflag(varargin,'RelativeShift');
+disp=[dx;0;dy;0;0;0];
+if RelativeShift && isfield(elem,'T1')
+    elem.T1=elem.T1-disp;
+    elem.T2=elem.T2+disp;
+else
+    elem.T1=-disp;
+    elem.T2=disp;
+end
 end
