23b3194dc9ac39ace523da9107607ac70794eb1f
Laurent Farvacque <laurent.farvacque@gmail.com>
2019-05-28 17:58:32 +0200

    Generalized 'load_lattice' function

--- pyat/at/load/__init__.py
+++ pyat/at/load/__init__.py
@@ -1,6 +1,8 @@
 """
-Import of AT lattice from different formats:
+Import/export AT lattice from/to different formats:
 - .mat files
+- .m files
 """
-
+from .allfiles import *
 from .matfile import *
+from .reprfile import *
