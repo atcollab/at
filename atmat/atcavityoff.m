e56094e36fc893254b9287c71df3651437810d81
Laurent Farvacque <laurent.farvacque@gmail.com>
2020-07-28 10:28:40 +0200

    atcavityon/off calls atradon/off

--- atmat/atcavityoff.m
+++ atmat/atcavityoff.m
@@ -2,8 +2,7 @@ function [ring, ATCavityIndex]=atcavityoff(ring)
 %ATCAVITYOFF switches RF cavities off
 %
 %  [RING2, CAVITIESINDEX] = ATCAVITYOFF(RING)
-%    Changes passmethods to turn off radiation
-%   damping.
+%    Changes cavity passmethods to turn off acceleration
 %
 %  INPUTS:
 %  1. RING      initial AT structure
@@ -14,29 +13,8 @@ function [ring, ATCavityIndex]=atcavityoff(ring)
 %
 %  See also ATCAVITYON, ATRADON, ATRADOFF
 
-%
 %% Written by Laurent S. Nadolski
 
-% Look for cavities
-ATCavityIndex = findcells(ring, 'Frequency');
-
-% Return if no cavity found
-if isempty(ATCavityIndex)
-    atdisplay(1,'AT:atcavityoff: No cavities were found in the lattice.');
-    return
-end
-
-% Make column vector 
-ATCavityIndex =ATCavityIndex(:)';
-
-% Loops over cavity elements
-for iCavity =ATCavityIndex        
-    % Based on cavity length, decide of the passmethod to switch off the cavities
-    if ring{iCavity}.Length == 0
-        ring{iCavity}.PassMethod = 'IdentityPass';
-    else
-        ring{iCavity}.PassMethod = 'DriftPass';
-    end
-end
+[ring,~,ATCavityIndex]=atradoff(ring,'auto','','','','');
 
 end
