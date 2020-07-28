0cd96311747e5739fd40d248a9b49190089e6612
Laurent Farvacque <laurent.farvacque@gmail.com>
2020-07-28 10:28:40 +0200

    atcavityon/off calls atradon/off

--- atmat/atcavityon.m
+++ atmat/atcavityon.m
@@ -2,8 +2,7 @@ function [ring, ATCavityIndex]=atcavityon(ring,cavityPass)
 %ATRADON switches RF cavities on
 %
 %  [RING2,CAVINDEX]=ATCAVITYON(RING,CAVITYPASS)
-%    Changes passmethods to get RF cavity acceleration and radiation
-%    damping. 
+%    Changes cavity passmethods to get RF acceleration
 %
 %  INPUTS
 %  1. RING	     	initial AT structure
@@ -11,47 +10,16 @@ function [ring, ATCavityIndex]=atcavityon(ring,cavityPass)
 %
 %  OUPUTS
 %  1. RING2          output ring with cavities off
-%  2. CAVITIESINDEX  indices of radiative elements and cavities
-%  3. ENERGY         energy
+%  2. CAVITIESINDEX  indices of cavities
 %
-%  See also ATRADOFF, ATRADON, ATCAVITYOFF
+%  See also ATCAVITYOFF, ATRADON, ATRADOFF
 
-%
 %% Written by Laurent S. Nadolski
 
-if nargin == 1 
-    cavityPass = 'On';
+if nargin <= 1
+    cavityPass='CavityPass';
 end
 
-% Look for cavity location
-ATCavityIndex = findcells(ring, 'Frequency');
-
-% Return if no cavity found
-if isempty(ATCavityIndex)
-    atdisplay(1,'AT:atcavityon: No cavities were found in the lattice.');
-    return
-end
-
-% Make column vector 
-ATCavityIndex =ATCavityIndex(:)';
-
-% Loops over cavity elements
-for iCavity =ATCavityIndex
-    
-    if size(cavityPass,1) == 1
-        CavityString = deblank(cavityPass);
-    elseif size(cavityPass,1) == length(ATCavityIndex)
-        CavityString = deblank(cavityPass(iCavity,:));
-    else
-        error('AT:atcavityoff: Number of rows in the input string must be 1 row or equal to the number of cavities.');
-    end
-    
-    % Set cavity pass method    
-    if strcmpi(CavityString,'On')
-        ring{iCavity}.PassMethod = 'CavityPass';        
-    else % Custom passmethod
-        ring{iCavity}.PassMethod = CavityString;
-    end
-end
+[ring,~,ATCavityIndex,~]=atradon(ring,cavityPass,'','','','');
 
 end
