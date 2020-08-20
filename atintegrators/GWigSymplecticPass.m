247fd1824a82f59be9c7b6f796af47d00dd0f708
Will Rogers <will.rogers@diamond.ac.uk>
2020-08-20 11:32:21 +0100

    Add notes for GWigSymplectic pass methods.

---
+++ atintegrators/GWigSymplecticPass.m
@@ -0,0 +1,5 @@
+% GWigSymplecticRadPass.m Help file for GWigSymplecticRadPass.c
+%  GWigSymplecticRadPass.c - generic symplectic integrator for wigglers
+%  including radiation
+% 
+%see also: GWigSymplecticRadPass.c, GWigSymplecticPass.m  
---
+++ atintegrators/GWigSymplecticRadPass.m
@@ -0,0 +1,4 @@
+% GWigSymplecticPass.m Help file for GWigSymplecticPass.c
+%  GWigSymplecticPass.c - generic symplectic integrator for wigglers
+% 
+%see also: GWigSymplecticPass.c, GWigSymplecticRadPass.m  
