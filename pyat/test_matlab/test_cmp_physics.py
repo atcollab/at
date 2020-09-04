feef53fdbe68e2a12b3d6caa41ff67419c952530
Simon White <swhite@esrf.fr>
2020-09-04 10:39:11 +0200

    added matlab comparison for fastring

--- pyat/test_matlab/test_cmp_physics.py
+++ pyat/test_matlab/test_cmp_physics.py
@@ -156,9 +156,39 @@ def test_quantdiff(engine, ml_lattice, py_lattice):
     radring,ind = engine.pyproxy('atradon',ml_lattice,nargout=2)
     dmat_ml=engine.pyproxy('quantumDiff',radring,ind);  
     # Comparison  
-    numpy.testing.assert_allclose(dmat,dmat_ml)
-    
+    numpy.testing.assert_allclose(dmat,dmat_ml, rtol = 1.0e-8, atol = 1.0e-20)
 
+@pytest.mark.parametrize('ml_lattice, py_lattice',
+                         [(pytest.lazy_fixture('ml_hmba'),
+                           pytest.lazy_fixture('py_hmba'))])
+def test_fastring(engine, ml_lattice, py_lattice):
+    ml_lattice[0]['Periodicity']=1.0
+    #Python call
+    ring, ringrad = physics.fastring.fast_ring(py_lattice)
+    #Matlab call
+    ring_ml, ringrad_ml = engine.pyproxy('atfastring',ml_lattice,nargout=2)
+    #Comparison
+    for r,rml,idq in zip([ring,ringrad],[ring_ml,ringrad_ml],[3,4]):
+        numpy.testing.assert_allclose(r[0].Frequency,rml[1]['Frequency'],rtol = 1.0e-20)
+        numpy.testing.assert_allclose(r[0].Voltage,rml[1]['Voltage'],rtol = 1.0e-20)
+        numpy.testing.assert_allclose(r[1].I2,rml[2]['I2'],rtol = 1.0e-20)
+        numpy.testing.assert_allclose(r[1].Length,rml[2]['Length'],rtol = 1.0e-20)
+        numpy.testing.assert_allclose(r[1].M66,rml[2]['M66'],atol = 1.0e-7)
+        numpy.testing.assert_allclose(r[1].T1,numpy.squeeze(rml[2]['T1']),rtol = 1.0e-8, atol = 1.0e-11)
+        numpy.testing.assert_allclose(r[1].T2,numpy.squeeze(rml[2]['T2']),rtol = 1.0e-8, atol = 1.0e-11)
+        numpy.testing.assert_allclose(r[idq-1].A1,rml[idq]['A1'],rtol = 0.02)
+        numpy.testing.assert_allclose(r[idq-1].A2,rml[idq]['A2'],rtol = 0.02)
+        numpy.testing.assert_allclose(r[idq-1].A3,rml[idq]['A3'],rtol = 0.02)
+        numpy.testing.assert_allclose(r[idq-1].Alphax,rml[idq]['Alphax'],rtol = 1.0e-15)
+        numpy.testing.assert_allclose(r[idq-1].Alphay,rml[idq]['Alphay'],rtol = 1.0e-15)
+        numpy.testing.assert_allclose(r[idq-1].Betax,rml[idq]['Betax'],rtol = 1.0e-15)
+        numpy.testing.assert_allclose(r[idq-1].Betay,rml[idq]['Betay'],rtol = 1.0e-15)
+        numpy.testing.assert_allclose(r[idq-1].Qpx,rml[idq]['Qpx'],rtol = 1.0e-5)
+        numpy.testing.assert_allclose(r[idq-1].Qpy,rml[idq]['Qpy'],rtol = 1.0e-5)
+        numpy.testing.assert_allclose(r[idq-1].T1,numpy.squeeze(rml[idq]['T1']),rtol = 1.0e-8, atol = 1.0e-11)
+        numpy.testing.assert_allclose(r[idq-1].T2,numpy.squeeze(rml[idq]['T2']),rtol = 1.0e-8, atol = 1.0e-11)
+    ml_lattice[0]['Periodicity']=32.0
+    
 
 @pytest.mark.parametrize('dp', (0.00, 0.01, -0.01))
 @pytest.mark.parametrize('ml_lattice, py_lattice',
