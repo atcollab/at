7e44961959a2589138523cd2143889252297bf64
Ahmad Mash'al <a-mashal@ipm.ir>
2020-09-20 01:21:05 +0430

    Add files via upload

--- atmat/atphysics/ParameterSummaryFunctions/RadIntegrals.m
+++ atmat/atphysics/ParameterSummaryFunctions/RadIntegrals.m
@@ -81,7 +81,25 @@ for i=1:nHarmV
 end
 BT=@(x) sqrt((Bx(x).^2)+(By(x).^2));
 end   
-
+%------------- Dispersion Through Wiggler ----------------
+ring{Wigidx(1),1}.Nstep=20;  %increase integration precision
+ring{Wigidx(1),1}.Nmeth=4;
+WIGELEM=ring(Wigidx(1));
+d=1e-6;
+Oplus =[ d*Dis0(1);  d*Dis0(2);  d*Dis0(3);  d*Dis0(4);  d;0];
+Ominus=[-d*Dis0(1); -d*Dis0(2); -d*Dis0(3); -d*Dis0(4); -d;0];
+linepass(WIGELEM,Oplus,1:2);
+ORBp = importdata('Diswig.DAT');
+sp=ORBp(:,5)';
+ORBp=ORBp(:,1:4);
+linepass(WIGELEM,Ominus,1:2);
+ORBm = importdata('Diswig.DAT');
+ORBm=ORBm(:,1:4);
+DisW=(ORBp-ORBm)/d/2;
+Dsx=DisW(:,1)';
+Dspx=DisW(:,2)';
+Dsy=DisW(:,3)';
+Dspy=DisW(:,4)';
 %------------- Radiation Integrals------------------------
 E0=GLOBVAL.E0/1e9;
 rho2=@(x) (BT(x).^2)*((0.2998/E0)^2);
@@ -89,66 +107,53 @@ I2=integral(rho2,0,LT);
 rho3=@(x) abs(BT(x).^3)*((0.2998/E0)^3);
 I3=integral(rho3,0,LT);
 
-
 switch WMode
     case 'Horz'
         RHOy=@(x) By(x)*0.2998/E0;
-        [Ds,Dsp,sp]= Dispwig (RHOy,Lw,Dis0(1));
-        Ds=Ds';
-        Dsp=Dsp';
-        fI1=Ds.*RHOy(sp);
+        fI1=Dsx.*RHOy(sp);
         I1=Npole*trapz(sp,fI1);
-        fI4=Ds.*By(sp).*(By(sp).^2)*((0.2998/E0)^3);
+        fI4=Dsx.*By(sp).*(By(sp).^2)*((0.2998/E0)^3);
         I4=Npole*trapz(sp,fI4);
         alphax=Alph(1)*ones(1,length(sp));
         betax=Bta(1)*ones(1,length(sp));
         gammax=(1+alphax.^2)./betax;
-        H=gammax.*(Ds.^2)+2*alphax.*(Dsp.^2)+betax.*(Ds.*Dsp);
+        H=gammax.*(Dsx.^2)+2*alphax.*(Dspx.^2)+betax.*(Dsx.*Dspx);
         fI5=H.*abs(By(sp).^3)*((0.2998/E0)^3);
         I5=Npole*trapz(sp,fI5);
     case 'Vert'
         RHOx=@(x) Bx(x)*0.2998/E0;
-        [Ds,Dsp,sp]= Dispwig (RHOx,Lw,Dis0(2));
-        Ds=Ds';
-        Dsp=Dsp';
-        fI1=Ds.*RHOx(sp);
+        fI1=Dsy.*RHOx(sp);
         I1=Npole*trapz(sp,fI1);
-        fI4=Ds.*Bx(sp).*(Bx(sp).^2)*((0.2998/E0)^3);
+        fI4=Dsy.*Bx(sp).*(Bx(sp).^2)*((0.2998/E0)^3);
         I4=Npole*trapz(sp,fI4);
         alphax=Alph(2)*ones(1,length(sp));
         betax=Bta(2)*ones(1,length(sp));
         gammax=(1+alphax.^2)./betax;
-        H=gammax.*(Ds.^2)+2*alphax.*(Dsp.^2)+betax.*(Ds.*Dsp);
+        H=gammax.*(Dsy.^2)+2*alphax.*(Dspy.^2)+betax.*(Dsy.*Dspy);
         fI5=H.*abs(Bx(sp).^3)*((0.2998/E0)^3);
         I5=Npole*trapz(sp,fI5);
     case 'Ellip'
         RHOy=@(x) By(x)*0.2998/E0;
-        [Ds,Dsp,sp]= Dispwig (RHOy,Lw,Dis0(1));
-        Ds=Ds';
-        Dsp=Dsp';
-        fI1=Ds.*RHOy(sp);
+        fI1=Dsx.*RHOy(sp);
         I1y=Npole*trapz(sp,fI1);
-        fI4=Ds.*By(sp).*(By(sp).^2)*((0.2998/E0)^3);
+        fI4=Dsx.*By(sp).*(By(sp).^2)*((0.2998/E0)^3);
         I4y=Npole*trapz(sp,fI4);
         alphax=Alph(1)*ones(1,length(sp));
         betax=Bta(1)*ones(1,length(sp));
         gammax=(1+alphax.^2)./betax;
-        H=gammax.*(Ds.^2)+2*alphax.*(Dsp.^2)+betax.*(Ds.*Dsp);
+        H=gammax.*(Dsx.^2)+2*alphax.*(Dspx.^2)+betax.*(Dsx.*Dspx);
         fI5=H.*abs(By(sp).^3)*((0.2998/E0)^3);
         I5y=Npole*trapz(sp,fI5);
         
         RHOx=@(x) Bx(x)*0.2998/E0;
-        [Ds,Dsp,sp]= Dispwig (RHOx,Lw,Dis0(2));
-        Ds=Ds';
-        Dsp=Dsp';
-        fI1=Ds.*RHOx(sp);
+        fI1=Dsy.*RHOx(sp);
         I1x=Npole*trapz(sp,fI1);
-        fI4=Ds.*Bx(sp).*(Bx(sp).^2)*((0.2998/E0)^3);
+        fI4=Dsy.*Bx(sp).*(Bx(sp).^2)*((0.2998/E0)^3);
         I4x=Npole*trapz(sp,fI4);
         alphax=Alph(2)*ones(1,length(sp));
         betax=Bta(2)*ones(1,length(sp));
         gammax=(1+alphax.^2)./betax;
-        H=gammax.*(Ds.^2)+2*alphax.*(Dsp.^2)+betax.*(Ds.*Dsp);
+        H=gammax.*(Dsy.^2)+2*alphax.*(Dspy.^2)+betax.*(Dsy.*Dspy);
         fI5=H.*abs(Bx(sp).^3)*((0.2998/E0)^3);
         I5x=Npole*trapz(sp,fI5);
         
