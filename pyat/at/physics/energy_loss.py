d34170bd612caaaee7b6fb24a8a92b23c77dd622
Simon White <swhite@esrf.fr>
2021-05-01 16:42:45 +0200

    check sign of the derivative voltage and preserve phase difference between cavities

--- pyat/at/physics/energy_loss.py
+++ pyat/at/physics/energy_loss.py
@@ -5,12 +5,13 @@ from math import sqrt, pi
 import numpy
 from scipy.optimize import least_squares
 from at.lattice import Lattice, Dipole, Wiggler, RFCavity
-from at.lattice import check_radiation, set_cavity, AtError
-from at.lattice import checktype, get_refpts
+from at.lattice import check_radiation, AtError
+from at.lattice import checktype, get_refpts, set_value_refpts
 from at.tracking import lattice_pass
 from at.physics import clight, Cgamma, e_mass
 
-__all__ = ['get_energy_loss', 'set_cavity_phase', 'ELossMethod']
+__all__ = ['get_energy_loss', 'set_cavity_phase', 'ELossMethod',
+           'get_timelag_fromU0']
 
 
 class ELossMethod(Enum):
@@ -83,6 +84,55 @@ def get_energy_loss(ring, method=ELossMethod.INTEGRAL):
         raise AtError('Invalid method: {}'.format(method))
 
 
+def get_timelag_fromU0(ring, method=ELossMethod.INTEGRAL, cavpts=None):
+    """
+    Get the TimeLag attribute of RF cavities based on frequency,
+    voltage and energy loss per turn, so that the synchronous phase is zero.
+    An error occurs if all cavities do not have the same frequency.
+    Used in set_cavity_phase()
+
+    PARAMETERS
+        ring        lattice description
+
+    KEYWORDS
+        method=ELossMethod.INTEGRAL
+                            method for energy loss computation.
+                            See "get_energy_loss".
+        cavpts=None         Cavity location. If None, use all cavities.
+                            This allows to ignore harmonic cavities.
+    RETURN
+        timelag
+    """
+    u0 = get_energy_loss(ring, method=method)
+    try:
+        rfv = ring.get_rf_voltage(cavpts=cavpts)
+        freq = ring.get_rf_frequency(cavpts=cavpts)
+    except AtError:
+        if cavpts is None:
+            cavpts = get_refpts(ring, RFCavity)
+        freq = numpy.array([cav.Frequency for cav in ring.select(cavpts)])
+        rfv = numpy.array([cav.Voltage for cav in ring.select(cavpts)])
+        tl0 = numpy.array([cav.TimeLag for cav in ring.select(cavpts)])
+        if u0 > numpy.sum(rfv):
+            raise AtError('Not enough RF voltage: unstable ring')
+        ctmax = 1/numpy.amin(freq)*clight/2
+        eq = lambda x: numpy.sum(rfv*numpy.sin(2*pi*freq*(x)/clight))-u0
+        deq = lambda x: numpy.sum(rfv*numpy.cos(2*pi*freq*(x)/clight))
+        zero_diff = least_squares(deq, ctmax/2, bounds=(0, ctmax)).x[0]
+        if numpy.sign(deq(zero_diff-1.0e-6))>0:
+            phis = least_squares(eq, zero_diff/2, 
+                                 bounds=(0, zero_diff)).x[0]
+        else:
+            phis = least_squares(eq, (ctmax-zero_diff)/2,
+                                 bounds=(zero_cross,ctmax)).x[0]
+        timelag = phis + tl0- tl0[numpy.argmin(freq)]      
+    else:
+        if u0 > rfv:
+            raise AtError('Not enough RF voltage: unstable ring')
+        timelag = clight/(2*pi*freq) * numpy.arcsin(u0/rfv)*np.ones(len(cavpts))
+    return timelag  
+
+
 def set_cavity_phase(ring, method=ELossMethod.INTEGRAL,
                      refpts=None, cavpts=None, copy=False):
     """
@@ -106,26 +156,11 @@ def set_cavity_phase(ring, method=ELossMethod.INTEGRAL,
     if cavpts is None and refpts is not None:
         warn(FutureWarning('You should use "cavpts" instead of "refpts"'))
         cavpts = refpts
-    u0 = get_energy_loss(ring, method=method)
-    try:
-        rfv = ring.get_rf_voltage(cavpts=cavpts)
-        freq = ring.get_rf_frequency(cavpts=cavpts)
-    except AtError:
-        if cavpts is None:
-            cavpts = get_refpts(ring, RFCavity)
-        freq = numpy.array([cav.Frequency for cav in ring.select(cavpts)])
-        rfv = numpy.array([cav.Voltage for cav in ring.select(cavpts)])
-        if u0 > numpy.sum(rfv):
-            raise AtError('Not enough RF voltage: unstable ring')
-        ctmax = 1/numpy.amin(freq)*clight
-        eq = lambda x: numpy.sum(rfv*numpy.sin(2*pi*freq*x/clight))-u0
-        tl = least_squares(eq, 0, bounds=(-ctmax, ctmax)).x[0]
-        set_cavity(ring, TimeLag=tl, cavpts=cavpts, copy=copy)
-    else:
-        if u0 > rfv:
-            raise AtError('Not enough RF voltage: unstable ring')
-        timelag = clight / (2*pi*freq) * numpy.arcsin(u0/rfv)
-        set_cavity(ring, TimeLag=timelag, cavpts=cavpts, copy=copy)
+    elif cavpts is None:
+        cavpts = get_refpts(ring,RFCavity)
+    timelag = get_timelag_fromU0(ring, method=method, cavpts=cavpts)
+    print(len(timelag),len(cavpts))
+    set_value_refpts(ring, cavpts, 'TimeLag', timelag, copy=copy)
     print("\nThis function modifies the time reference\n"
           "This should be avoided, you have been warned!\n",
           file=sys.stderr)
