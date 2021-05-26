449f389c24af8db1d8fa3b553d2aa7db050dfbaa
Laurent Farvacque <laurent.farvacque@esrf.fr>
2021-05-26 19:08:51 +0200

    Rebased on master

--- atmat/atphysics/Orbit/findorbit.m
+++ atmat/atphysics/Orbit/findorbit.m
@@ -1,14 +1,59 @@
-function orbit  = findorbit(RING,D, varargin)
-%FINDORBIT Alias to the orbit search functions: (findorbit4, findsyncorbit,
-%          findorbit4 ...) 
-%  depending on the number of parameters, their types and argument options
-%  It can also return additional parameters.
-%  
-%  NOTES
-%  1. Temporary version of 7/18/00 - defaults to FINDORBIT4
+function [orbs,orbitin]  = findorbit(ring,varargin)
+%FINDORBIT find the closed orbit
+%
+%Depending on the lattice, FINDORBIT will:
+% - use findorbit6 if radiation is ON,
+% - use findsyncorbit if radiation is OFF and ct is specified,
+% - use findorbit4 otherwise
+%
+%[ORBIT,FIXEDPOINT]=FINDORBIT(RING,REFPTS)
+%
+% ORBIT:        6xNrefs, closed orbit at the selected reference points
+% FIXEDPOINT:   6x1 closed orbit at the entrance of the lattice
+%
+%[...]=FINDORBIT(RING,...,'dp',DP) Specify the momentum deviation when
+%   radiation is OFF (default: 0)
+%
+%[...]=FINDORBIT(RING,...,'dct',DCT) Specify the path lengthening when
+%   radiation is OFF (default: 0)
+%
+%[...]=FINDORBIT(RING,...,'orbit',ORBIT) Specify the orbit at the entrance
+%   of the ring, if known. FINDORBIT will then transfer it to the reference points
+%
+%[...]=FINDORBIT(RING,...,'guess',GUESS) The search will start with GUESS as
+%   initial conditions
+%
+%For other keywords, see the underlying functions.
 %
 % See also FINDORBIT4, FINDSYNCORBIT, FINDORBIT6.
 
-orbit = findorbit4(RING, D, varargin{:});
+[orbitin,varargs]=getoption(varargin,'orbit',[]);
+[refpts,varargs]=getargs(varargs,[],'check',@(arg) isnumeric(arg) || islogical(arg));
+if isempty(orbitin)
+    if check_radiation(ring)    % Radiation ON: 6D orbit
+        dp=getoption(varargs,'dp',NaN);
+        ct=getoption(varargs,'dct',NaN);
+        if isfinite(dp) || isfinite(ct)
+            warning('AT:linopt','In 6D, "dp" and "dct" are ignored');
+        end
+        orbitin=x_orbit6(ring,varargs{:});
+    else                        % Radiation OFF: 4D orbit
+        [~,varargs]=getoption(varargs,'DPStep');   % Take it out because findorbit4 does not accept it
+        [dp,varargs]=getoption(varargs,'dp',0);
+        orbitin=xorbit_dp(ring,dp,varargs{:});
+    end
+    args={'KeepLattice'};
+else
+    args={};
+end
 
-return
+if islogical(refpts)
+    refpts=find(refpts);
+end
+if isempty(refpts)
+    % return only the fixed point at the entrance of RING{1}
+    orbs=orbitin;
+else
+    orbs=linepass(RING,orbitin,refpts,args{:});
+end
+end
---
+++ atmat/atphysics/Orbit/xorbit_dp.m
@@ -0,0 +1,39 @@
+function Ri = xorbit_dp(ring,dp,varargin)
+%XORBIT_DP  Private function used by findorbit4
+%ORBIT=XORBIT_DP(RING,DP)   Closed orbit for fixed off-momentum.
+[dct,varargs]=getoption(varargin,'dct',NaN);
+if isnan(dct)
+    [XYStep,varargs]=getoption(varargs,'XYStep');	% Step size for numerical differentiation
+    [dps,varargs]=getoption(varargs,'OrbConvergence');	% Convergence threshold
+    [max_iterations,varargs]=getoption(varargs,'OrbMaxIter');	% Max. iterations
+    [Ri,varargs]=getoption(varargs,'guess',zeros(6,1));
+    [Ri,varargs]=getargs(varargs,Ri,'check',@(arg) isnumeric(arg) && isequal(size(arg),[6,1])); %#ok<ASGLU>
+    
+    % Set the momentum component of Ri to the specified dP
+    Ri(5) = dp;
+    scaling=XYStep*[1 1 1 1];
+    D = [diag(scaling) zeros(4,1);zeros(2,5)];
+    
+    change=Inf;
+    itercount = 0;
+    args={};
+    while (change > dps) && (itercount < max_iterations)
+        RMATi = Ri(:,ones(1,5)) + D;
+        RMATf = linepass(ring,RMATi,args{:});
+        Rf = RMATf(:,end);
+        % compute the transverse part of the Jacobian
+        J4 = (RMATf(1:4,1:4)-RMATf(1:4,5))./scaling;
+        Ri_next = Ri +  [(eye(4) - J4)\(Rf(1:4)-Ri(1:4)); 0; 0];
+        change = norm(Ri_next - Ri);
+        Ri = Ri_next;
+        itercount = itercount+1;
+        args={'KeepLattice'};
+    end
+    
+    if itercount == max_iterations
+        warning('Maximum number of iterations reached. Possible non-convergence')
+    end
+else
+    Ri=xorbit_ct(ring,dct,varargs{:});
+end
+end
