7905e32782a857ff0ee55d7d1255faa2863ffad2
Simon White <swhite@esrf.fr>
2021-06-22 10:00:49 +0200

    simpler version

--- pyat/at/tracking/patpass.py
+++ pyat/at/tracking/patpass.py
@@ -1,9 +1,9 @@
 """
 Simple parallelisation of atpass() using multiprocessing.
 """
+from warnings import warn
 import multiprocessing
 from at.tracking import atpass
-from at.lattice import uint32_refpts
 from sys import platform
 import numpy
 
@@ -11,30 +11,34 @@ import numpy
 __all__ = ['patpass']
 
 
+globring = None
+
+
 def _atpass_one(args):
-    if len(args)==3:
-        return atpass(ringg, *args)
-    else:
-        return atpass(*args)
+    return atpass(globring, *args)
 
 
-def _patpass(r_in, nturns, refpts, pool_size, ring=None):
-    pool = multiprocessing.Pool(pool_size)
-    if ring is None:
-        args = [(r_in[:, i], nturns, refpts) for i in range(r_in.shape[1])]
+def _atpass(ring, r_in, nturns, refs, pool_size=None, globvar=True):
+    if platform.startswith('linux') and globvar:
+        global globring
+        globring = ring
+        args = [(r_in[:, i], nturns, refs) for i in range(r_in.shape[1])]
+        with multiprocessing.Pool(pool_size) as pool:
+            results = pool.map(_atpass_one, args)
+        globring = None
     else:
-        args = [(ring, r_in[:, i], nturns, refpts) for i in range(r_in.shape[1])]
-    results = pool.map(_atpass_one, args)
-    pool.close()
-    pool.join()
+        args = [(ring, r_in[:, i], nturns, refs) for i in range(r_in.shape[1])]
+        with multiprocessing.Pool(pool_size) as pool:
+            results = pool.starmap(atpass, args)
     return numpy.concatenate(results, axis=1)
 
 
-def patpass(ring, r_in, nturns, refpts=None, pool_size=None, **kwargs):
+def patpass(ring, r_in, nturns=1, refpts=None, pool_size=None, globvar=True):
     """
     Simple parallel implementation of atpass().  If more than one particle
     is supplied, use multiprocessing to run each particle in a separate
-    process.
+    process. In case a single particle is provided or the ring contains
+    ImpedanceTablePass element, atpass is returned
 
     INPUT:
         ring            lattice description
@@ -50,22 +54,17 @@ def patpass(ring, r_in, nturns, refpts=None, pool_size=None, **kwargs):
                            len(ring)+1, where selected elements are True.
                         Defaults to None, meaning no refpts, equivelent to
                         passing an empty array for calculation purposes.
+        pool_size       number of processes, if None the min(npart,nproc) is used
+        globvar         For linux machines speed-up is achieved by defining a global
+                        ring variable, this can be disabled using globvar=False
     """
     if refpts is None:
         refpts = len(ring)
-    refs = uint32_refpts(refpts, len(ring))
-    if len(numpy.atleast_1d(r_in[0]))>1:
+    refs = ring.uint32_refpts(refpts)
+    pm_ok = [e.PassMethod=='ImpedanceTablePass' for e in ring]
+    if len(numpy.atleast_1d(r_in[0]))>1 or any(pm_ok):
         if pool_size is None:
             pool_size = min(len(r_in[0]),multiprocessing.cpu_count())
-        if platform == "linux" or platform == "linux2":
-            global ringg
-            ringg = ring
-            results = _patpass(r_in, nturns, refs, pool_size)
-            r_in[:] = numpy.squeeze(results[:,:,-1,-1])
-            del ringg
-        else:
-            results = _patpass(r_in, nturns, refs, pool_size,ring=ring) 
-            r_in[:] = numpy.squeeze(results[:,:,-1,-1])   
+        return _atpass(ring, r_in, nturns, refs, pool_size=pool_size, globvar=globvar)
     else:
-            results = atpass(ring, r_in, nturns, refs) 
-    return results
+        return atpass(ring, r_in, nturns, refs)
