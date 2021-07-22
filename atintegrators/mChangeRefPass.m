3d268925805790845974b26e1b1fd20fa80c9a66
Laurent Farvacque <laurent.farvacque@esrf.fr>
2021-07-22 08:37:25 +0200

    Added ChangeRefPass

---
+++ atintegrators/mChangeRefPass.m
@@ -0,0 +1,14 @@
+function [Out1, Out2]  = mChangeRefPass(Elem,Rin)
+%MCHANGEREFPASS Change the reference energy by scaling
+
+if nargin
+    scaling=Elem.Scaling;
+    Out1 = Rin;
+    Out1(2,:)=Rin(2,:)/scaling;
+    Out1(4,:)=Rin(4,:)/scaling;
+    Out1(5,:)=(Rin(5,:) + (1.0-scaling))/scaling;
+%     Out1(5,:)=(Rin(5,:) + (1.0-scaling));
+else % If called with no input args - return lists of required and optional fields
+    Out1 = {'Scaling'};
+    Out2 = {};
+end
