416b9d3f798900c12b148c1e4e1137c4cfc6cbd1
Laurent Farvacque <laurent.farvacque@esrf.fr>
2021-08-01 20:16:49 +0200

    Matlab help

--- atmat/at.m
+++ atmat/at.m
@@ -1,9 +1,3 @@
-%This is the Accelerator Toolbox particle tracking code.
-%Accelerator Toolbox was originally created by Andrei Terebilo.
-%Development is now continued by a multi-laboratory collaboration,
-%atcollab.
+%Accelerator Toolbox particle tracking code.
 %
-%The integrators used for tracking particles may be compiled with
-%the command atmexall.
-%For getting started, one may look at the examples in atmat/atdemos.
-%Example lattice files are located in machine_data.
+%See also: atmat
