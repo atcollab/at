2bdc32f937f48ed0bf7d0be520e1a2065978c592
Simon White <swhite@esrf.fr>
2021-08-12 10:05:26 +0200

    use get)tune from linear.py

--- pyat/at/matching/globalfit.py
+++ pyat/at/matching/globalfit.py
@@ -4,19 +4,18 @@ such as the tune and the chromaticity
 """
 import numpy
 from at.lattice import set_value_refpts
-from at.physics import linopt
+from at.physics import get_tune, get_chrom
+from at.matching import matching 
 
 __all__ = ['fit_tune', 'fit_chrom']
 
 
 def _get_tune(ring, dp=0):
-    _, tune, _, _ = linopt(ring, dp=dp)
-    return tune
+    return get_tune(ring, dp=0)[0:2]
 
 
 def _get_chrom(ring, dp=0):
-    _, _, chrom, _ = linopt(ring, dp=dp, get_chrom=True)
-    return chrom
+    return get_chrom(ring, dp=0)[0:2]
 
 
 def _fit_tune_chrom(ring, index, func, refpts1, refpts2, newval, tol=1.0e-12,
@@ -34,24 +33,31 @@ def _fit_tune_chrom(ring, index, func, refpts1, refpts2, newval, tol=1.0e-12,
         data = numpy.subtract(datap, datan)/(2*delta)
         return data
 
+    def _fit(ring, index, func, refpts1, refpts2, newval, J, dp=0):
+        val = func(ring, dp=dp)
+        dk = numpy.linalg.solve(J, numpy.subtract(newval, val))
+        set_value_refpts(ring, refpts1, 'PolynomB', dk[0], index=index,
+                         increment=True)
+        set_value_refpts(ring, refpts2, 'PolynomB', dk[1], index=index,
+                         increment=True)
+        val = func(ring, dp=dp)
+        sumsq = numpy.sum(numpy.square(numpy.subtract(val, newval)))
+        return sumsq
+
     delta = 1e-6*10**(index)
-    val = func(ring, dp=dp)
     dq1 = _get_resp(ring, index, func, refpts1, 'PolynomB', delta, dp=dp)
     dq2 = _get_resp(ring, index, func, refpts2, 'PolynomB', delta, dp=dp)
     J = [[dq1[0], dq2[0]], [dq1[1], dq2[1]]]
-    dk = numpy.linalg.solve(J, numpy.subtract(newval, val))
-    set_value_refpts(ring, refpts1, 'PolynomB', dk[0], index=index,
-                     increment=True)
-    set_value_refpts(ring, refpts2, 'PolynomB', dk[1], index=index,
-                     increment=True)
-
-    val = func(ring, dp=dp)
-    sumsq = numpy.sum(numpy.square(numpy.subtract(val, newval)))
-    if sumsq > tol and niter > 0:
-        _fit_tune_chrom(ring, index, func, refpts1, refpts2, newval, tol=tol,
-                        dp=dp, niter=niter-1)
-    else:
-        return
+    
+    n=0
+    sumsq = tol+1
+    print('Initial value', func(ring, dp=dp))
+    while sumsq > tol and n < niter:
+        sumsq= _fit(ring, index, func, refpts1, refpts2, newval, J, dp=dp)
+        print('iter#',n,'Res.',sumsq)
+        n +=1
+    print('Final value', func(ring, dp=dp),'\n')
+    return
 
 
 def fit_tune(ring, refpts1, refpts2, newval, tol=1.0e-12, dp=0, niter=3):
@@ -70,6 +76,7 @@ def fit_tune(ring, refpts1, refpts2, newval, tol=1.0e-12, dp=0, niter=3):
     Typical usage:
     at.matching.fit_tune(ring, refpts1, refpts2, [0.1,0.25])
     """
+    print('\nFitting Tune...')
     _fit_tune_chrom(ring, 1, _get_tune, refpts1, refpts2, newval, tol=tol,
                     dp=dp, niter=niter)
 
@@ -91,5 +98,6 @@ def fit_chrom(ring, refpts1, refpts2, newval, tol=1.0e-12, dp=0, niter=3):
     Typical usage:
     at.matching.fit_chrom(ring, refpts1, refpts2, [10,5])
     """
+    print('\nFitting Chromaticity...')
     _fit_tune_chrom(ring, 2, _get_chrom, refpts1, refpts2, newval, tol=tol,
                     dp=dp, niter=niter)
