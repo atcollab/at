2cdd26bdf62a9815652d2efeeeaf91cb8042ddfd
liuzzo <simone.liuzzo@esrf.fr>
2021-09-23 08:13:42 +0200

    some reorganization of the example script

--- pyat/examples/example_dynamic_aperture.py
+++ pyat/examples/example_dynamic_aperture.py
@@ -21,6 +21,65 @@ sr_ring.set_rf_harmonic_number(992)
 sr_ring.set_rf_voltage(6.0e6)
 sr_ring.radiation_on()
 
+# Dynamic aperture on-energy
+t = time.time()
+print('On-energy DA')
+h0, v0, da_, search = da.dynamic_aperture(sr_ring,
+                    n_turns=N,
+                    n_radii=14,
+                    file_name_save=folder_data + '/on_en_da',
+                    num_recursions=5)
+daplot.plot_dynamic_aperture(h0, v0, da_, search, file_name_save=folder_data + '/on_en_da')
+
+elapsed = time.time() - t
+print('On-energy DA took {s:2.1f}s'.format(s=elapsed))
+
+"""  FOLLOWING WORKS ONLY IN UNIX.
+print('On-energy DA grid (parallel computation)')
+h0, v0, da_, _ = da.dynamic_aperture(sr_ring,
+                    n_turns=N,
+                    n_radii=21,
+                    n_theta=21,
+                    grid_mode='grid',  # grid mode also allows parallel computation
+                    file_name_save=folder_data + '/on_en_da_grid',
+                    num_recursions=5)
+# daplot.plot_dynamic_aperture(h0, v0, da_, search, file_name_save=folder_data + '/on_en_da_grid')
+
+elapsed = time.time() - t
+print('On-energy DA grid (parallel) took {s:2.1f}s'.format(s=elapsed))
+
+"""
+
+
+# max hor. position vs momenutm deviation (off-energy DA)
+t = time.time()
+print('Off-energy DA')
+maxneg0, dpp, da_ = da.off_energy_dynamic_aperture(sr_ring,
+                               n_turns=N,
+                               deltaps=np.linspace(-0.1, 0.1, 21),
+                               file_name_save=folder_data + '/off_en_da',
+                               inject_from_inside=True,
+                               num_recursions=5)
+
+daplot.plot_off_energy_dynamic_aperture(maxneg0, dpp, da_, file_name_save=folder_data + '/off_en_da')
+
+elapsed = time.time() - t
+print('Off-energy DA took {s:2.1f}s'.format(s=elapsed))
+
+
+#  Local Momentum Acceptance
+t = time.time()
+print('Momentum acceptance')
+mom, s, da_ = da.momentum_acceptance(sr_ring, n_turns=N,
+                       file_name_save=folder_data + '/mom_acc',
+                       ref_pts=range(0, 125, 5), num_recursions=2)
+daplot.plot_momentum_acceptance(mom, s, da_, file_name_save=folder_data + '/mom_acc')
+
+elapsed = time.time() - t
+print('Momentum acceptance took {s:2.1f}s'.format(s=elapsed))
+
+
+# x - xp Acceptance
 t = time.time()
 
 print('x-xp acceptance, using class directly')
@@ -38,9 +97,10 @@ elapsed = time.time() - t
 print('x-xp Acceptance took {s:2.1f}s'.format(s=elapsed))
 
 
+# generic multidimentsional Acceptance
 print('5D acceptance, using class directly')
 DA = da.Acceptance6D(sr_ring,
-                  mode='6D', start_index=555, n_turns=N, dp=-0.0)
+                  mode='6D', start_index=1, n_turns=N, dp=-0.0)
 DA.n_points['ct'] = 1 # do not scan ct coordinate
 DA.n_points['delta'] = 1 # do not scan ct coordinate
 DA.n_points['x'] = 9 # do not scan ct coordinate
@@ -61,60 +121,4 @@ print(DA.survived)
 elapsed = time.time() - t
 daplot.plot6d(DA,file_name_save=folder_data + '/Acceptance_5D')
 print('6D Acceptance took {s:2.1f}s'.format(s=elapsed))
-
-
-t = time.time()
-print('On-energy DA')
-h0, v0, da_, search = da.dynamic_aperture(sr_ring,
-                    n_turns=N,
-                    n_radii=14,
-                    file_name_save=folder_data + '/on_en_da',
-                    num_recursions=5)
-daplot.plot_dynamic_aperture(h0, v0, da_, search, file_name_save=folder_data + '/on_en_da')
-
-elapsed = time.time() - t
-print('On-energy DA took {s:2.1f}s'.format(s=elapsed))
-
-"""
-print('On-energy DA grid (parallel computation)')
-h0, v0, da_, _ = da.dynamic_aperture(sr_ring,
-                    n_turns=N,
-                    n_radii=21,
-                    n_theta=21,
-                    grid_mode='grid',  # grid mode also allows parallel computation
-                    file_name_save=folder_data + '/on_en_da_grid',
-                    num_recursions=5)
-# daplot.plot_dynamic_aperture(h0, v0, da_, search, file_name_save=folder_data + '/on_en_da_grid')
-
-elapsed = time.time() - t
-print('On-energy DA grid (parallel) took {s:2.1f}s'.format(s=elapsed))
-
-"""
-
-
-# off-en DA
-t = time.time()
-print('Off-energy DA')
-maxneg0, dpp, da_ = da.off_energy_dynamic_aperture(sr_ring,
-                               n_turns=N,
-                               deltaps=np.linspace(-0.1, 0.1, 21),
-                               file_name_save=folder_data + '/off_en_da',
-                               inject_from_inside=True,
-                               num_recursions=5)
-
-daplot.plot_off_energy_dynamic_aperture(maxneg0, dpp, da_, file_name_save=folder_data + '/off_en_da')
-
-elapsed = time.time() - t
-print('Off-energy DA took {s:2.1f}s'.format(s=elapsed))
-
-# MA
-t = time.time()
-print('Momentum acceptance')
-mom, s, da_ = da.momentum_acceptance(sr_ring, n_turns=N,
-                       file_name_save=folder_data + '/mom_acc',
-                       ref_pts=range(0, 125, 5), num_recursions=2)
-daplot.plot_momentum_acceptance(mom, s, da_, file_name_save=folder_data + '/mom_acc')
-
-elapsed = time.time() - t
-print('Momentum acceptance took {s:2.1f}s'.format(s=elapsed))
 
