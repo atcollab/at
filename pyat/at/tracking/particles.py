3ab1fbc862d1a9e25bd726d49d22e390054d0708
Lee Carver <carver@rnice9-0308.esrf.fr>
2021-09-29 14:51:06 +0200

    Better implementation. Much clearer outline. Help updated

--- pyat/at/tracking/particles.py
+++ pyat/at/tracking/particles.py
@@ -7,8 +7,34 @@ from at.lattice.lattice_object import Lattice
 
 __all__ = ['beam', 'sigma_matrix']
 
+def _generate_uncorrelated_sigma_matrix(bx, ax, emitx, by, ay, emity, blength, espread):
+    sig_matrix_long = numpy.array([
+                         [espread*espread, 0],
+                         [0, blength*blength]
+                                  ])
 
-def sigma_matrix(ring=None, twiss_in=None, emitx=0, emity=0, blength=0, espread=0):
+    sig_matrix_x = emitx * numpy.array([
+                              [bx, -ax],
+                              [-ax, (1 + ax * ax)/bx]
+                                      ])
+
+    sig_matrix_y = emity * numpy.array([
+                              [by, -ay],
+                              [-ay, (1 + ay * ay)/by]
+                                       ])
+
+    sig_matrix = numpy.block([
+                    [sig_matrix_x,
+                        numpy.zeros((2, 4))],
+                    [numpy.zeros((2, 2)),
+                        sig_matrix_y,
+                        numpy.zeros((2, 2))],
+                    [numpy.zeros((2, 4)),
+                        sig_matrix_long]
+                            ])
+    return sig_matrix
+
+def sigma_matrix(ring=None, twiss_in=None, emitx=None, emity=None, blength=None, espread=None):
     """
     Calculate the correlation matrix to be used for particle generation
 
@@ -23,22 +49,38 @@ def sigma_matrix(ring=None, twiss_in=None, emitx=0, emity=0, blength=0, espread=
     OUTPUT
         sigma_matrix    6x6 correlation matrix
 
-    """
+    If the lattice object is provided with no other arguments, ohmi_envelope is used 
+    to compute the correlated sigma matrix.
 
+    If the lattice object and emittances and longitudinal parameters are provided, then
+    the 2x2 uncorrelated matrices are computed for each plane (x,y,z) using the initial
+    optics computed from ring.get_optics, and are combined together into the 6x6 matrix.
+
+    If the twiss_in is provided alongside the emittances and longitudinal parameters, then
+    the 2x2 uncorrelated matrices are computed for each plane and combined into the 6x6
+    matrix. 
+
+    """
+    flag = emitx or emity or blength or espread
+    if flag:
+        assert emitx, 'emitx must be defined'
+        assert emity, 'emity must be defined'
+        assert blength, 'blength must be defined'
+        assert espread, 'espread must be defined'
 
     if ring:
         if isinstance(ring, Lattice):
-            ring = ring.radiation_on(copy=True)
-            emit0, beamdata, emit = ohmi_envelope(ring, refpts=[0])
-            sig_matrix = emit.r66[0]
-
-            if emity:
+            if flag:
+                print ('Generating uncorrelated sigma matrix by combining 2x2 matrices computed from initial optics conditions')
                 ld0, bd, ld = ring.get_optics()
-                sig_matrix_y = emity * numpy.array([
-                                          [ld0.beta[1], -ld0.alpha[1]],
-                                          [-ld0.alpha[1], (1 + ld0.alpha[1]**2)/ld0.beta[1]]
-                                                   ])
-                sig_matrix[2:4, 2:4] = sig_matrix_y
+                bx, by = ld0.beta
+                ax, ay = ld0.alpha
+                sig_matrix = _generate_uncorrelated_sigma_matrix(bx, ax, emitx, by, ay, emity, blength, espread)
+            else:
+                print ('Generating correlated sigma matrix using ohmi envelope')
+                ring = ring.radiation_on(copy=True)
+                emit0, beamdata, emit = ohmi_envelope(ring, refpts=[0])
+                sig_matrix = emit.r66[0]
         else:
             raise AttributeError('Input must be lattice object')
 
@@ -50,32 +92,9 @@ def sigma_matrix(ring=None, twiss_in=None, emitx=0, emity=0, blength=0, espread=
         if (blength and not espread) or (espread and not blength):
             raise AttributeError('Both blength and espread have to be provided')
 
+        print ('Generating uncorrelated sigma matrix by combining 2x2 matrices computed from parameters in twiss_in')
+        sig_matrix = _generate_uncorrelated_sigma_matrix(bx, ax, emitx, by, ay, emity, blength, espread)
 
-
-        sig_matrix_long = numpy.array([
-                             [espread*espread, 0],
-                             [0, blength*blength]
-                                      ])
-
-        sig_matrix_x = emitx * numpy.array([
-                                  [bx, -ax],
-                                  [-ax, (1 + ax * ax)/bx]
-                                          ])
-
-        sig_matrix_y = emity * numpy.array([
-                                  [by, -ay],
-                                  [-ay, (1 + ay * ay)/by]
-                                           ])
-
-        sig_matrix = numpy.block([
-                        [sig_matrix_x,
-                            numpy.zeros((2, 4))],
-                        [numpy.zeros((2, 2)),
-                            sig_matrix_y,
-                            numpy.zeros((2, 2))],
-                        [numpy.zeros((2, 4)),
-                            sig_matrix_long]
-                                ])
     else:
         raise AttributeError('A lattice or twiss_in must be provided')
     return sig_matrix
