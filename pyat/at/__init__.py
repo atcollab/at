9abe69ff2912c53acf2fa8ef64a822ed3afa0386
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-01-19 10:27:29 +0100

    Updated packaging

--- pyat/at/__init__.py
+++ pyat/at/__init__.py
@@ -1,7 +1,16 @@
 """Python port of the Accelerator Toolbox"""
 
-# Make all functions visible in the at namespace:
 import sys
+if sys.version_info.minor < 8:
+    from importlib_metadata import version, PackageNotFoundError
+else:
+    from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version('accelerator-toolbox')
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+# from ._version import version as __version__
+# Make all functions visible in the at namespace:
 from .lattice import *
 from .tracking import *
 from .physics import *
