1861392f77d4889c2a99b6eb2286aec035637203
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-01-25 20:39:01 +0100

    Python packaging (#346)
    * Updated packaging
    
    * use extras_require for optional dependencies
    
    * Add python 3.10 in tests
    
    * Add python 3.10 in tests
    
    * Ensure a c99 compliant C compiler
    
    * separate C anc C++ extensions

--- pyat/setup.py
+++ pyat/setup.py
@@ -1,14 +1,12 @@
 import glob
-from io import open
 import os
 from os.path import abspath, basename, dirname, exists, join, splitext
 import sys
 import shutil
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 
 # Numpy build dependency defined in pyproject.toml.
 import numpy
-
 
 
 def select_omp():
@@ -30,15 +28,15 @@ here = abspath(dirname(__file__))
 macros = [('PYAT', None)]
 with_openMP = False
 
-cflags = []
-
+cflags = ['-std=c99']
+cppflags = []
 
 mpi = os.environ.get('MPI', None)
 if mpi is None:
     mpi_macros = []
     mpi_includes = []
 else:
-    mpi_macros = [('MPI',None)]
+    mpi_macros = [('MPI', None)]
     os.environ["CC"] = 'mpicc'
     try:
         import mpi4py
@@ -81,51 +79,54 @@ else:
 if not sys.platform.startswith('win32'):
     cflags += ['-Wno-unused-function']
 
-
-# Get the long description from the README file
-with open(join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
 # It is easier to copy the integrator files into a directory inside pyat
 # for packaging. However, we cannot always rely on this directory being
 # inside a clone of at and having the integrator sources available, and
 # this file is executed each time any setup.py command is run.
 # It appears that only copying the files when they are available is
 # sufficient.
-at_source = abspath(join(here, 'at.c'))
 integrator_src_orig = abspath(join(here, '..', 'atintegrators'))
-integrator_src = abspath(join(here, 'integrator-src'))
-diffmatrix_source = abspath(
-    join(here, '..', 'atmat', 'atphysics', 'Radiation')
-)
+integrator_src = 'integrator-src'
+diffmatrix_orig = abspath(join(here, '..', 'atmat', 'atphysics', 'Radiation'))
 
 if exists(integrator_src_orig):
     # Copy files into pyat for distribution.
     source_files = glob.glob(join(integrator_src_orig, '*.[ch]'))
     source_files.extend(glob.glob(join(integrator_src_orig, '*.cc')))
-    source_files.extend(
-        glob.glob(join(diffmatrix_source, 'findmpoleraddiffmatrix.c'))
-    )
+    source_files.append(join(diffmatrix_orig, 'findmpoleraddiffmatrix.c'))
     if not exists(integrator_src):
         os.makedirs(integrator_src)
     for f in source_files:
         shutil.copy2(f, integrator_src)
 
-pass_methods = glob.glob(join(integrator_src, '*Pass.c'))
-pass_methods.extend(glob.glob(join(integrator_src, '*Pass.cc')))
-diffmatrix_method = join(integrator_src, 'findmpoleraddiffmatrix.c')
+c_pass_methods = glob.glob(join(integrator_src, '*Pass.c'))
+cpp_pass_methods = glob.glob(join(integrator_src, '*Pass.cc'))
+diffmatrix_source = join(integrator_src, 'findmpoleraddiffmatrix.c')
+at_source = 'at.c'
 
 
-def integrator_ext(pass_method):
+def c_integrator_ext(pass_method):
     name, _ = splitext(basename(pass_method))
     name = ".".join(('at', 'integrators', name))
     return Extension(
         name=name,
         sources=[pass_method],
-        include_dirs=[numpy.get_include(), mpi_includes, integrator_src, diffmatrix_source],
+        include_dirs=[numpy.get_include(), mpi_includes, integrator_src],
         define_macros=macros + omp_macros + mpi_macros,
         extra_compile_args=cflags + omp_cflags,
+        extra_link_args=omp_lflags
+    )
+
+
+def cpp_integrator_ext(pass_method):
+    name, _ = splitext(basename(pass_method))
+    name = ".".join(('at', 'integrators', name))
+    return Extension(
+        name=name,
+        sources=[pass_method],
+        include_dirs=[numpy.get_include(), mpi_includes, integrator_src],
+        define_macros=macros + omp_macros + mpi_macros,
+        extra_compile_args=cppflags + omp_cflags,
         extra_link_args=omp_lflags
     )
 
@@ -134,40 +135,21 @@ at = Extension(
     'at.tracking.atpass',
     sources=[at_source],
     define_macros=macros + omp_macros + mpi_macros,
-    include_dirs=[numpy.get_include(), integrator_src, diffmatrix_source],
+    include_dirs=[numpy.get_include(), integrator_src],
     extra_compile_args=cflags + omp_cflags,
     extra_link_args=omp_lflags
 )
 
 diffmatrix = Extension(
     name='at.physics.diffmatrix',
-    sources=[diffmatrix_method],
-    include_dirs=[numpy.get_include(), integrator_src, diffmatrix_source],
+    sources=[diffmatrix_source],
+    include_dirs=[numpy.get_include(), integrator_src],
     define_macros=macros,
     extra_compile_args=cflags
 )
 
 setup(
-    name='accelerator-toolbox',
-    version='0.2.1',
-    description='Accelerator Toolbox',
-    long_description=long_description,
-    author='The AT collaboration',
-    author_email='atcollab-general@lists.sourceforge.net',
-    url='https://github.com/atcollab/at',
-    # Numpy 1.16.6 is the oldest version that builds with Python 3.9.
-    install_requires=['numpy>=1.16.6', 'scipy>=0.16'],
-    packages=find_packages(),
-    ext_modules=[at, diffmatrix] + [integrator_ext(pm) for pm in pass_methods],
-    zip_safe=False,
-    python_requires='>=3.6.0',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ]
+    ext_modules=[at, diffmatrix] +
+                [c_integrator_ext(pm) for pm in c_pass_methods] +
+                [cpp_integrator_ext(pm) for pm in cpp_pass_methods],
 )
