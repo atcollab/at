912b0360b2f721116128efde21d2fba985438e46
Simon White <swhite@esrf.fr>
2022-02-15 17:30:11 +0100

    fix pep in integrators

--- pyat/at/integrators/pyDriftPass.py
+++ pyat/at/integrators/pyDriftPass.py
@@ -5,7 +5,7 @@ This is an example of a python passmethod, here the Drift
 passmethod. This is a simplified version that does not check
 aperture.
 
-The python passmethods has to be in the python path or in 
+The python passmethods has to be in the python path or in
 at.integratorsand contain a function trackFunction()
 
 The element creation is as follows:
@@ -19,19 +19,24 @@ In case a python passmethod and a c passmethod with the same name
 are found, the c passmethod is used
 '''
 
+
 def drift6(r, L):
     p_norm = 1/(1+r[4])
-    NormL  = L*p_norm   
-    r[0]+= NormL*r[1] 
-    r[2]+= NormL*r[3]
-    r[5]+= NormL*p_norm*(r[1]*r[1]+r[3]*r[3])/2
-   
+    NormL = L*p_norm
+    r[0] += NormL*r[1]
+    r[2] += NormL*r[3]
+    r[5] += NormL*p_norm*(r[1]*r[1]+r[3]*r[3])/2
 
-def trackFunction(rin,elem=None):  
-    l = elem.Length
+
+def trackFunction(rin, elem=None):
+    le = elem.Length
     for rtmp in numpy.atleast_2d(rin.T):
-        if hasattr(elem,'T1'): rtmp += elem.T1
-        if hasattr(elem,'R1'): rtmp[:] = numpy.dot(elem.R1,rtmp)
-        drift6(rtmp, l)
-        if hasattr(elem,'R2'): rtmp[:] = numpy.dot(elem.R2,rtmp)
-        if hasattr(elem,'T2'): rtmp += elem.T2
+        if hasattr(elem, 'T1'):
+            rtmp += elem.T1
+        if hasattr(elem, 'R1'):
+            rtmp[:] = numpy.dot(elem.R1, rtmp)
+        drift6(rtmp, le)
+        if hasattr(elem, 'R2'):
+            rtmp[:] = numpy.dot(elem.R2, rtmp)
+        if hasattr(elem, 'T2'):
+            rtmp += elem.T2
--- pyat/at/integrators/pyIdentityPass.py
+++ pyat/at/integrators/pyIdentityPass.py
@@ -4,7 +4,7 @@ import numpy
 This is an example of a python passmethod, here the Identity
 passmethod.
 
-The python passmethods has to be in the python path or in 
+The python passmethods has to be in the python path or in
 at.integratorsand contain a function trackFunction()
 
 The element creation is as follows:
@@ -22,8 +22,10 @@ In case a pyhton passmethod and a c passmethod with the same name
 are found, the c passmethod is used
 '''
 
+
 _id = numpy.identity(6)
 
-def trackFunction(rin,elem=None):
+
+def trackFunction(rin, elem=None):
     for rtmp in numpy.atleast_2d(rin.T):
         rtmp[:] = numpy.dot(_id, rtmp)
