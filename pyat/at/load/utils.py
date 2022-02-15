bd0aca14ac15aaa275957447d7190fe25589224e
Simon White <swhite@esrf.fr>
2022-02-15 17:43:47 +0100

    fix pep in load

--- pyat/at/load/utils.py
+++ pyat/at/load/utils.py
@@ -30,8 +30,8 @@ class RingParam(elt.Element):
     """Private class for Matlab RingParam element"""
     REQUIRED_ATTRIBUTES = elt.Element.REQUIRED_ATTRIBUTES + ['Energy',
                                                              'Periodicity']
-    _conversions = dict(elt.Element._conversions, Energy=float, Periodicity=int,
-                        Particle=_particle)
+    _conversions = dict(elt.Element._conversions, Energy=float,
+                        Periodicity=int, Particle=_particle)
 
     def __init__(self, family_name, energy, periodicity=1, **kwargs):
         kwargs.setdefault('Energy', energy)
@@ -355,7 +355,8 @@ def element_to_m(elem):
     if 'PassMethod' in kwds:
         argstrs.append(convert(kwds.pop('PassMethod')))
     argstrs += [', '.join((repr(k), convert(v))) for k, v in kwds.items()]
-    return '{0:>15}({1});...'.format(m_name(elem.__class__), ', '.join(argstrs))
+    return '{0:>15}({1});...'.format(m_name(elem.__class__),
+                                     ', '.join(argstrs))
 
 
 # Kept for compatibility but should be deprecated:
