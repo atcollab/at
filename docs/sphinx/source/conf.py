ad47ca86c869a4c53d8e6c42119b09fd988215be
Simon White <swhite@esrf.fr>
2022-05-11 18:01:18 +0200

    update

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./../../../pyat/at/'))
-
+#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./../../../pyat/at/acceptance'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
