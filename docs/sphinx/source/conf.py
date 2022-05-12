a2aa1de2074e3b6c84d3440d7e9cbb37554e813f
swhite2401 <swhite@esrf.fr>
2022-05-12 14:30:26 +0200

    add logo

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -51,6 +51,7 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'images/company_logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
