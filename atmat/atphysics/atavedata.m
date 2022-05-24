8cca13ff7e8b11c779587ef0a48b78bafb710825
Zeus <zeus@cells.es>
2022-05-24 20:58:25 +0200

    atavedata modified to avoid large errors when the lattice has errors(polynom(2) and displacement errors in sextupoles, rotated quadrupoles). Also dipole edge focusing effects have been included.

--- atmat/atphysics/atavedata.m
+++ atmat/atphysics/atavedata.m
@@ -13,6 +13,7 @@ function [lindata,avebeta,avemu,avedisp,nu,xsi]=atavedata(ring,dpp,refpts,vararg
 %[LINDATA,AVEBETA,AVEMU,AVEDISP,TUNES,CHROMS]=ATAVEDATA(RING,DPP,REFPTS,ORBITIN)
 %    does not search for closed orbit. instead ORBITIN is used
 
+
 lr=length(ring)+1;
 if islogical(refpts)
     refs=[refpts(:);false(lr-length(refpts),1)];
@@ -28,6 +29,9 @@ lindata=lind(refs(needed)); %refpts
 avebeta=cat(1,lindata.beta); %refpts
 avemu=cat(1,lindata.mu); %refpts
 avedisp=cat(2,lindata.Dispersion)'; %refpts
+ClosedOrbit=cat(2,lindata.ClosedOrbit)'; %refpts
+%plot(avebeta);hold all;
+
 
 if any(long)
     initial=[long(needed(1:end-1));false]; %needed
@@ -40,44 +44,71 @@ if any(long)
     alpha0=cat(1,lind(initial).alpha); %long
     mu0=avemu(lg,:); %long
     disp0=avedisp(lg,:); %long
+    ClosedOrbit0=ClosedOrbit(lg,:); %long
     
-    beta1=cat(1,lind(final).beta); %long
-    alpha1=cat(1,lind(final).alpha); %long
+    %beta1=cat(1,lind(final).beta); %long
+    %alpha1=cat(1,lind(final).alpha); %long
     mu1=cat(1,lind(final).mu); %long
     disp1=cat(2,lind(final).Dispersion)'; %long
+    ClosedOrbit1=cat(2,lind(final).ClosedOrbit)'; %long
     
     L2=[L L]; %long
-    avebeta(lg,:)=betadrift(beta0,beta1,alpha0,L2);
+    avebeta(lg,:)=betadrift(beta0,alpha0,L2);
     avemu(lg,:)=0.5*(mu0+mu1);
     avedisp(lg,[1 3])=(disp1(:,[1 3])+disp0(:,[1 3]))*0.5;
-    
-    foc=atgetcells(ring(long),'PolynomB',@(el,polb) length(polb)>=2 && polb(2)~=0); %long
+
+    foc=atgetcells(ring(long),'PolynomB',@(el,polb) length(polb)>=2 && polb(2)~=0||polb(3)~=0); %long
     if any(foc)
-        qp=false(size(long));
-        qp(long)=foc;
+        qp=false(size(lg));
+        qp(lg)=foc;
         K=zeros(size(L)); %long
-        K(foc)=atgetfieldvalues(ring(qp),'PolynomB',{2});
+        q=1e-12+atgetfieldvalues(ring(refpts(qp)),'PolynomB',{2});
+        m=atgetfieldvalues(ring(refpts(qp)),'PolynomB',{3});
+        R11=atgetfieldvalues(ring(refpts(qp)),'R2',{1,1});
+        dx=(atgetfieldvalues(ring(refpts(qp)),'T2',{1})-atgetfieldvalues(ring(refpts(qp)),'T1',{1}))/2;
+        ba=atgetfieldvalues(ring(refpts(qp)),'BendingAngle');
+        ba(isnan(ba))=0;
+        irho=ba./L(foc);
+        e1=atgetfieldvalues(ring(refpts(qp)),'EntranceAngle');
+        Fint=atgetfieldvalues(ring(refpts(qp)),'Fint');
+        gap=atgetfieldvalues(ring(refpts(qp)),'gap');
+        e1(isnan(e1))=0;
+        Fint(isnan(Fint))=0;
+        gap(isnan(gap))=0;
+        d_csi=ba.*gap.*Fint.*(1+sin(e1).^2)./cos(e1)./L(foc);
+        Cp=[ba.*tan(e1)./L(foc) -ba.*tan(e1-d_csi)./L(foc)];
+        for ii=1:2
+            alpha0(foc,ii)=alpha0(foc,ii)-beta0(foc,ii).*Cp(:,ii);
+        end
+        R11(isnan(R11))=1;
+        irho(isnan(irho))=0;
+        m(isnan(m))=0;
+        dx(isnan(dx))=0;
+
+        dx0=(ClosedOrbit0(foc,1)+ClosedOrbit1(foc,1))/2;
+        K(foc)=q.*R11+2*m.*(dx0-dx);
         K2=[K -K]; %long
-        sel=false(size(avebeta,1)); %refpts
+        K2(foc,1)=K2(foc,1)+irho.^2;
+        sel=false(size(avebeta,1),1); %refpts
         sel(lg)=foc;
-        avebeta(sel,:)=betafoc(beta1(foc,:),alpha0(foc,:),alpha1(foc,:),K2(foc,:),L2(foc,:));
-        avedisp(sel,[1 3])=dispfoc(disp0(foc,[2 4]),disp1(foc,[2 4]),K2(foc,:),L2(foc,:));
+        avebeta(sel,:)=betafoc(beta0(foc,:),alpha0(foc,:),K2(foc,:),L2(foc,:));
+        avedisp(sel,[1 3])=dispfoc(disp0(foc,[1 3]),disp0(foc,[2 4]),K2(foc,:),L2(foc,:));
     end
     avedisp(lg,[2 4])=(disp1(:,[1 3])-disp0(:,[1 3]))./L2;
 end
 
-    function avebeta=betadrift(beta0,beta1,alpha0,L)
+    function avebeta=betadrift(beta0,alpha0,L)
         gamma0=(alpha0.*alpha0+1)./beta0;
-        avebeta=0.5*(beta0+beta1)-gamma0.*L.*L/6;
+        avebeta=beta0-alpha0.*L+gamma0.*L.*L/3;
     end
 
-    function avebeta=betafoc(beta1,alpha0,alpha1,K,L)
-        gamma1=(alpha1.*alpha1+1)./beta1;
-        avebeta=0.5*((gamma1+K.*beta1).*L+alpha1-alpha0)./K./L;
+    function avebeta=betafoc(beta0,alpha0,K,L)
+        gamma0=(alpha0.*alpha0+1)./beta0;
+        avebeta=((beta0+gamma0./K).*L+(beta0-gamma0./K).*sin(2.0*sqrt(K).*L)./sqrt(K)/2.0+(cos(2.0*sqrt(K).*L)-1.0).*alpha0./K)./L/2.0;
     end
 
-    function avedisp=dispfoc(dispp0,dispp1,K,L)
-        avedisp=(dispp0-dispp1)./K./L;
+    function avedisp=dispfoc(disp0,dispp0,K,L)
+        avedisp=(disp0.*(sin(sqrt(K).*L)./sqrt(K))+dispp0.*(1-cos(sqrt(K).*L))./K)./L;
     end
-
+%plot(avebeta);hold all;
 end
