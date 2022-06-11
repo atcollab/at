83d393f36a70179047049a4b2bad2b1ae24e5dbc
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-06-11 18:22:47 +0200

    pin Sphinx < 5

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["README.rst"]
+exclude_patterns = ["README.rst", "**/*.so"]
 autodoc_default_options = {
         # Make sure that any autodoc declarations show the right members
         "members": True,
