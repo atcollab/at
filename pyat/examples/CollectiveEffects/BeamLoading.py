0345c1d8106be1e46026d3ae6a83933773646f23
Simon White <swhite@esrf.fr>
2022-06-22 17:04:36 +0200

    fix example

--- pyat/examples/CollectiveEffects/BeamLoading.py
+++ pyat/examples/CollectiveEffects/BeamLoading.py
@@ -71,42 +71,7 @@ print('ResFreq', fres0, bl_elem.ResFrequency)
 print('Vcav:', vcav0, bl_elem.Vcav)
 print('Vgen:', vgen0, bl_elem.Vgen) 
 print('Qs:', qs_anal, qs_anal_ss, qs_dp, qs_dz)
-for p, Ib in enumerate(allCurrents):
-    bl_elem.Current = Ib
-    qsa = get_anal_qs(qs0,Ib,volt,rshunt,phil,u0)
-    print('\n')
-    print('Current : ',Ib)
-    print('Generator voltage: ', fring.rf_voltage)
-    print('Resonator frequency: ', fring[-1].ResFrequency)
-    print('Detuning angle: ', fring.get_elements(at.RFCavity)[0].PhaseLag/numpy.pi*180)
-    print('Synch. tune: ', qsa)   
-    part = numpy.squeeze(at.lattice_pass(fring, part, nturns=Nturns))
-    tbt_dp = numpy.mean(part[4,:,:], axis=0)
-    tbt_s = numpy.mean(part[5,:,:], axis=0)
-    try:
-        qs_dp = harmonic_analysis.get_tunes_harmonic(tbt_dp,'laskar', num_harmonics=20, fmin=1e-5, fmax=0.1)
-        qs_s = harmonic_analysis.get_tunes_harmonic(tbt_s,'laskar', num_harmonics=20, fmin=1e-5, fmax=0.1)
-    except ValueError:
-        qs_dp = 0
-        qs_s = 0
-    allQs[p] = (qs_dp+qs_s)/2
-    allQsa[p] = qsa
-    part = part[:,:,-1]
 
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax1.plot(allCurrents, allQs, marker='x', linestyle='dashed', color='k', label='Tracking')
-ax1.plot(allCurrents, allQsa, marker='x', linestyle='dashed', color='r', label='Analytic')
-ax1.set_xlabel('Ib [mA]')
-ax1.set_ylabel('Qs')
-ax1.legend()
-ax2 = fig.add_subplot(212)
-ax2.plot(tbt_dp, color='k', label='dp')
-ax2.plot(tbt_s, color='r', label='ct')
-ax2.set_xlabel('Turn #')
-ax2.set_ylabel('dp/ ct [m]')
-ax2.legend()
-plt.show()
 
 
 
