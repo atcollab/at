951ed6a015329e4f060d8deec28cee51d9ecc3cb
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-06-23 16:14:28 +0200

    Handle python 3.6

--- pyat/at/__init__.py
+++ pyat/at/__init__.py
@@ -1,6 +1,12 @@
 """Python port of the Accelerator Toolbox"""
 
-from ._version import __version__, __version_tuple__
+try:
+    # setuptools_scm >= 7 for python >= 3.7
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    # setuptools_scm < 7 for python 3.6
+    from ._version import version as __version__
+    from ._version import version_tuple as __version_tuple__
 # Make all functions visible in the at namespace:
 from .lattice import *
 from .tracking import *
