b4faa3369415e6b2c4f146f85c7ba284a757813f
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-07-03 20:58:28 +0200

    typo

--- atmat/attests/githubsetup.m
+++ atmat/attests/githubsetup.m
@@ -2,9 +2,9 @@ function githubsetup(execmode)
 %GITHUBSETUP    Setup Matlab for AT tests in GitHib Actions
 
 savepath('pathdef.m');
-atmexall -c_only;
+atmexall;
 if ispc
-    pyenv("Version",'3.9','ExecutionMode', execmode))
+    pyenv("Version",'3.9','ExecutionMode', execmode);
 else
     pyenv('Version',fullfile(getenv('pythonLocation'),'bin','python'),...
       'ExecutionMode', execmode);
