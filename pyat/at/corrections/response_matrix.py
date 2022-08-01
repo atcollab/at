e8731a206c3aeeb7d9f5d7b7f3a28643fba77e0f
Simon White <swhite@esrf.fr>
2022-08-01 15:58:41 +0200

    update

--- pyat/at/corrections/response_matrix.py
+++ pyat/at/corrections/response_matrix.py
@@ -13,22 +13,20 @@ class RMMode(Enum):
     PLUS = 1
     MINUS = 2
     PLUSMINUS = 3
-    
-_mode2range = {RMMode.PLUS : numpy.array([1]), 
-               RMMode.MINUS : numpy.array([-1]),
-               RMMode.PLUSMINUS : numpy.array([-1, 1])}
 
     
 class RMVariable(Variable):
-    def __init__(setfun, getfun, delta, mode=RMMode.PLUSMINUS, name=''):
-        self.delta = delta*_mode2range[mode]          
-        super(RMVariable, self).__init__(setfun, getfun, name=name)
+    def __init__(self, setfun, getfun, delta, name=''):
+        self.delta = delta          
+        super(RMVariable, self).__init__(setfun, getfun,
+                                         name=name)
         
         
 class RMElementVariable(ElementVariable):
-    def __init__(refpts, attrname, delta, index=None, mode=RMMode.PLUSMINUS, name=''):  
-        self.delta = delta*_mode2range[mode]        
-        super(RMElementVariable, self).__init__(refpts, attrname, index=index, name=name) 
+    def __init__(self, refpts, attrname, delta, index=None, name=''):  
+        self.delta = delta        
+        super(RMElementVariable, self).__init__(refpts, attrname,
+                                                index=index, name=name) 
         
         
 class RMObservable(Constraints):
@@ -49,13 +47,41 @@ class RMLinoptObservable(LinoptConstraints):
 
 class ResponseMatrix(object):
 
-    def __init__(variables, observables):
-        self.variables = variables
-        self.observables = observables
+    def __init__(self, variables, observables, mode=RMMode.PLUSMINUS):
+        self.variables = numpy.atleast_1d(variables)
+        self.observables = numpy.atleast_1d(observables)
+        self.mode = mode
+        self.fullrm = None
         
-    def compute(ring):
-        var0 = [var.get(ring) for var in variables] 
-        print(var0)
+    def compute(self,ring):
+        var0 = [var.get(ring) for var in self.variables] 
+        val0 = [obs.values(ring) for obs in self.observables]       
+        rv = []     
+        for var, v0 in zip(self.variables, var0):
+            if self.mode == RMMode.PLUSMINUS:
+                var.set(ring, v0 + var.delta)  
+                op = numpy.squeeze([obs.values(ring) for obs in self.observables])
+                var.set(ring, v0 - var.delta)  
+                om = numpy.squeeze([obs.values(ring) for obs in self.observables])
+                rv.append((om-op)/2/var.delta)
+            elif self.mode == RMMode.PLUS:
+                var.set(ring, v0 + var.delta)  
+                op = numpy.squeeze([obs.values(ring) for obs in self.observables])
+                rv.append((val0-op)/var.delta)
+            else:
+                var.set(ring, v0 - var.delta)  
+                om = numpy.squeeze([obs.values(ring) for obs in self.observables])
+                rv.append((om-val0)/var.delta)    
+            var.set(ring, v0)
+        self.fullrm = numpy.array(rv)
+        
+     def get_reduced_rm(self, mask_var, mask_obs, mask_refpts):
+        hasrefpts = [hasattr(obs, 'refpts'
+         
+        
+     
+
+                  
         
         
     
