ddc4d9766bcba5730c8b2c538e65ecd4392b26e6
Laurent Farvacque <laurent.farvacque@esrf.fr>
2022-09-10 09:35:49 +0200

    Document atenable__6d and atdisable_6d

--- atmat/atphysics/Radiation/atdisable_6d.m
+++ atmat/atphysics/Radiation/atdisable_6d.m
@@ -1,6 +1,60 @@
 function [newring,radelemIndex,cavitiesIndex] = atdisable_6d(ring,varargin)
+%ATDISABLE_6D  switches radiation and cavity off
+%
+% [NEWRING,RADINDEX,CAVINDEX] = ATDISABLE_6D(RING,CAVIPASS,BENDPASS,QUADPASS)
+%    Changes passmethods to turn off cavities, radiation damping and all
+%    elements acting on the particle momentum.
+%
+% The default is to turn everything OFF., 
+%
+%  INPUTS:
+%  1. RING      initial AT structure
+%  2. CAVIPASS  pass method for cavities
+%               '' makes no change,
+%               'auto' sets'IdentityPass' or 'DriftPass' depending of cavity length)
+%               anything else is used as the new PassMethod.
+%  3. BENDPASS  pass method for bending magnets
+%               '' makes no change,
+%               'auto' substitutes 'RadPass' with 'Pass' in any method
+%               anything else is used as the new PassMethod.
+%  4. QUADPASS  pass method for quadrupoles
+%               '' makes no change,
+%               'auto' substitutes 'RadPass' with 'Pass' in any method
+%               anything else is used as the new PassMethod.
+%
+%  [...] = ATDISABLE_6D(...[,keyword,value]...)
+%   The following keywords trigger the processing of the following elements:
+%
+%   'allpass'       Defines the default pass method for all elements not
+%                   explicitly specified. Replaces the following default
+%                   values.
+%   'cavipass'      pass method for RF cavities. Default 'auto'
+%   'bendpass'      pass method for bending magnets. Default 'auto'
+%   'quadpass'      pass method for quadrupoles. Default 'auto'
+%   'sextupass'     pass method for sextupoles. Default 'auto'
+%   'octupass'      pass method for bending magnets. Default 'auto'
+%   'wigglerpass'	pass method for wigglers. Default 'auto'
+%   'quantdiffpass' pass method for quantum diffusion. Default 'auto'
+%
+%   OUPUTS:
+%   1. NEWRING   Output ring
+%   2. RADINDEX  Indices of elements with radiation
+%   3. CAVINDEX  Indices of active cavities
+%
+%  EXAMPLES:
+%
+%>> ringrad=atdisable_6d(ring);
+%   Turns off all elements acting on momentum.
+%
+%>> ringrad=atdisable_6d(ring,'auto','allpass','');
+%   Turns cavities off and leaves everything else unchanged.
+%
+%>> ringrad=atdisable_6d(ring,'allpass','auto','cavipass','');
+%   Turns off everything except RF cavities.
+%
+%  See also ATENABLE_6D, CHECK_6D, ATCAVITYON, ATCAVITYOFF
 
-% Process the input arguments
+% Process the keyword arguments
 [allpass,varargs]=getoption(varargin,'allpass',[]);
 [octupass,varargs]=getoption(varargs,'octupass',default_pass('auto'));
 [sextupass,varargs]=getoption(varargs,'sextupass',default_pass('auto'));
@@ -9,6 +63,7 @@ function [newring,radelemIndex,cavitiesIndex] = atdisable_6d(ring,varargin)
 [bendpass,varargs]=getoption(varargs,'bendpass',default_pass('auto'));
 [cavipass,varargs]=getoption(varargs,'cavipass',default_pass('auto'));
 [quantdiffpass,varargs]=getoption(varargs,'quantdiffpass',default_pass('auto'));
+% Process the positional arguments
 [cavipass,bendpass,quadpass]=getargs(varargs,cavipass,bendpass,quadpass);
 
 % Build the modification table
@@ -50,14 +105,12 @@ end
             % 'auto' multipole modification
             elem.PassMethod=strrep(elem.PassMethod,'RadPass','Pass');
             if isfield(elem,'Energy'), elem=rmfield(elem,'Energy'); end
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
         
         function elem=fixelem(elem)
             % Explicit multipole modification
             elem.PassMethod=newpass;
             if isfield(elem,'Energy'), elem=rmfield(elem,'Energy'); end
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
     end
 
@@ -75,7 +128,6 @@ end
         function elem=modelem(elem)
             % Default element modification
             elem.PassMethod=newpass;
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
     end
 
@@ -96,13 +148,11 @@ end
             else
                 elem.PassMethod='IdentityPass';
             end
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
         
         function elem=fixelem(elem)
             % Explicit RF modification
             elem.PassMethod=newpass;
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
     end                
 
--- atmat/atphysics/Radiation/atenable_6d.m
+++ atmat/atphysics/Radiation/atenable_6d.m
@@ -1,6 +1,61 @@
 function [newring,radelemIndex,cavitiesIndex,energy] = atenable_6d(ring,varargin)
+%ATENABLE_6D switches RF and radiation on
+%
+%[NEWRING,RADINDEX,CAVINDEX] = ATENABLE_6D(RING,CAVIPASS,BENDPASS,QUADPASS)
+%    Changes passmethods to get RF cavity acceleration and radiation
+%    damping.
+%
+% The default is to turn cavities ON and set radiation in dipoles,
+% quadrupoles and wigglers.
+%
+%  INPUTS:
+%  1. RING      initial AT structure
+%  2. CAVIPASS	pass method for cavities
+%               '' makes no change,
+%               'auto' set 'RFCavityPass',
+%               anything else is used as the new PassMethod.
+%  3. BENDPASS	pass method for bending magnets
+%               '' makes no change,
+%               'auto' substitutes 'Pass' with 'RadPass' in any method,
+%               anything else is used as the new PassMethod.
+%  4. QUADPASS	pass method for quadrupoles
+%               '' makes no change,
+%               'auto' substitutes 'Pass' with 'RadPass' in any method,
+%               anything else is used as the new PassMethod.
+%
+%  [...] = ATENABLE_6D(...,keyword,value)
+%   The following keywords trigger the processing of the following elements:
+%
+%   'allpass'       Defines the default pass method for all elements not
+%                   explicitly specified. Replaces the following default
+%                   values.
+%   'cavipass'      pass method for RF cavities. Default 'auto'
+%   'bendpass'      pass method for bending magnets. Default 'auto'
+%   'quadpass'      pass method for quadrupoles. Default 'auto'
+%   'sextupass'     pass method for sextupoles. Default ''
+%   'octupass'      pass method for octupoles. Default ''
+%   'wigglerpass'   pass method for wigglers. Default 'auto'
+%   'quantdiffpass' pass method for quantum radiation. default 'auto'
+%
+%  OUPUTS:
+%  1. NEWRING   Output ring
+%  2. RADINDEX  Indices of elements with radiation
+%  3. CAVINDEX  Indices of active cavities
+%
+%  EXAMPLES:
+%
+%>> ringrad=atenable_6d(ring);
+%   Turns cavities on and sets radiation in bending magnets, quadrupoles and wigglers (default)
+%
+%>> ringrad=atenable_6d(ring,'auto','allpass','');
+%   Turns cavities on and leaves everything else unchanged
+%
+%>> ringrad=atenable_6d(ring,'allpass','','bendpass','auto');
+%   Turns on radiation in bending magnets and leaves everything else unchanged
+%
+%  See also ATDISABLE_6D, CHECK_6D, ATCAVITYON, ATCAVITYOFF
 
-% Process the input arguments
+% Process the keyword arguments
 [allpass,varargs]=getoption(varargin,'allpass',[]);
 [octupass,varargs]=getoption(varargs,'octupass',default_pass(''));
 [sextupass,varargs]=getoption(varargs,'sextupass',default_pass(''));
@@ -9,6 +64,7 @@ function [newring,radelemIndex,cavitiesIndex,energy] = atenable_6d(ring,varargin
 [bendpass,varargs]=getoption(varargs,'bendpass',default_pass('auto'));
 [cavipass,varargs]=getoption(varargs,'cavipass',default_pass('auto'));
 [quantdiffpass,varargs]=getoption(varargs,'quantdiffpass',default_pass('auto'));
+% Process the positional arguments
 [cavipass,bendpass,quadpass]=getargs(varargs,cavipass,bendpass,quadpass);
 
 energy=atenergy(ring);
@@ -54,14 +110,12 @@ end
                 elem.PassMethod=strrep(elem.PassMethod,'Pass','RadPass');
                 elem.Energy=energy;
             end
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
         
         function elem=fixelem(elem)
             % Explicit multipole modification
             elem.PassMethod=newpass;
             elem.Energy=energy;
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
     end
 
@@ -79,7 +133,6 @@ end
         function elem=modelem(elem)
             % Default element modification
             elem.PassMethod=newpass;
-            fprintf('%10s: %s\n', elem.FamName, elem.PassMethod);
         end
     end
 
