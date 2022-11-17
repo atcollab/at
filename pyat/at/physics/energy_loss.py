15fc6e2f0bc23e8969dc9f9f3621f3be3e17c77e
Simon White <swhite@esrf.fr>
2022-11-17 09:02:09 +0100

    remove quantum diffusion and collective fomr u0 calculation

--- pyat/at/physics/energy_loss.py
+++ pyat/at/physics/energy_loss.py
@@ -6,6 +6,7 @@ import numpy
 from scipy.optimize import least_squares
 from at.lattice import Lattice, Dipole, Wiggler, RFCavity, Refpts
 from at.lattice import check_radiation, AtError, AtWarning
+from at.lattice import QuantumDiffusion, Collective
 from at.lattice import checktype, set_value_refpts, get_cells, refpts_len
 from at.constants import clight, Cgamma
 from at.tracking import lattice_pass
@@ -68,7 +69,7 @@ def get_energy_loss(ring: Lattice,
     def tracking(ring):
         """Losses from tracking
         """
-        ringtmp = ring.disable_6d(RFCavity, copy=True)
+        ringtmp = ring.disable_6d(RFCavity, QuantumDiffusion, Collective, copy=True)
         o6 = numpy.squeeze(lattice_pass(ringtmp, numpy.zeros(6),
                            refpts=len(ringtmp)))
         if numpy.isnan(o6[0]):
