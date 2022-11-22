051a5827227b730e4af4c077698f0205d907846f
Simon White <swhite@esrf.fr>
2022-11-22 15:53:24 +0100

    use analytical ct drift

--- pyat/at/lattice/lattice_object.py
+++ pyat/at/lattice/lattice_object.py
@@ -630,8 +630,8 @@ class Lattice(list):
     @property
     def bunch_spos(self) -> numpy.ndarray:
         """Bunch position around the ring [m]"""
-        o6, _ = self.find_orbit()
-        bs = (self.circumference+o6[5])/len(self._fillpattern)
+        frev = self.rf_frequency / self.harmonic_number
+        bs = self.beta*clight/frev/len(self._fillpattern)
         allpos = bs*numpy.arange(len(self._fillpattern))
         return allpos[self._fillpattern > 0]
 
