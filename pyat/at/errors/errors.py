236334ce75ec9ed119e3f6852d4942ef1a848e3e
swhite2401 <simon.white@esrf.fr>
2022-12-05 14:14:11 +0100

    simplify

--- pyat/at/errors/errors.py
+++ pyat/at/errors/errors.py
@@ -1,7 +1,7 @@
 import numpy
 from typing import Callable
 import functools
-from at.lattice import refpts_iterator, Lattice
+from at.lattice import refpts_iterator, Lattice, AtError
 from at.lattice import shift_elem, rotate_elem
 from at.physics import find_orbit, get_optics
 from at.tracking import lattice_pass
@@ -37,15 +37,17 @@ def assign_errors(ring, key, truncation=None, seed=None, **kwargs):
             rv = _truncated_randn(vals.shape, sigma=vals,
                                   truncation=truncation, seed=seed)
             [setattr(e, attr, v) for e, v in zip(elements, rv)]
-            
+
 
 def _rotmat(theta):
     cs = numpy.cos(theta)
     sn = numpy.sin(theta)
     return numpy.array([[cs, -sn], [sn, cs]])
-    
+
 
 def _apply_bpm_orbit_error(ring, orbit, refpts):
+    if refpts is None:
+        return orbit
     for e, o6 in zip(refpts_iterator(ring, refpts), orbit):
         if hasattr(e, 'BPMGain'):
             o6[[0, 2]] *= e.BPMGain
@@ -54,9 +56,11 @@ def _apply_bpm_orbit_error(ring, orbit, refpts):
         if hasattr(e, 'BPMTilt'):
             o6[[0, 2]] = _rotmat(e.BPMTilt) @ o6[[0, 2]]
     return orbit
-    
-    
+
+
 def _apply_bpm_track_error(ring, trajectory, refpts):
+    if refpts is None:
+        return trajectory
     for traj in trajectory.T:
         for e, o6 in zip(refpts_iterator(ring, refpts), traj):
             if hasattr(e, 'BPMGain'):
@@ -67,8 +71,8 @@ def _apply_bpm_track_error(ring, trajectory, refpts):
                 o6[:, [0, 2]] = [_rotmat(e.BPMTilt) @ o2 
                                  for o2 in o6[:, [0, 2]]]
     return trajectory
-    
-    
+
+
 def _apply_alignment_errors(ring):
     refpts = [(hasattr(e, 'ShiftErr') or hasattr(e, 'RotationErr'))
               for e in ring]
@@ -81,99 +85,75 @@ def _apply_alignment_errors(ring):
         if rots is not None:
             rotate_elem(e, tilt=rots[0], pitch=rots[1], yaw=rots[2])
     return ring
-    
-               
-def _apply_field_errors(ring): 
 
+
+def _apply_field_errors(ring):
     def sanitize(e):
-        pola = getattr(e, 'PolynomA')
-        polb = getattr(e, 'PolynomB')
-        lm = max(len(pola), len(polb))
-        pan = numpy.zeros(lm)
-        pbn = numpy.zeros(lm)
-        pan[:len(pola)] = pola
-        pbn[:len(polb)] = polb
-        e.PolynomA = pan
-        e.PolynomB = pbn 
-        e.MaxOrder = lm-1
- 
-    def set_polerr(e, pname):
-        pol = getattr(e, pname)
-        err = getattr(e, pname+'Err')
-        pn = numpy.zeros(max(len(pol), len(err)))
-        if len(pol) > len(err):
-            pn[:len(err)] = numpy.add(pol[:len(err)], err)
-            pn[len(err):] = pol[len(err):]
-        else:
-            pn[:len(pol)] = numpy.add(err[:len(pol)], pol)  
-            pn[len(pol):] = err[len(pol):]
-        setattr(e, pname, pn)
+        mo = max(len(e.PolynomA), len(e.PolynomB))
+        e.PolynomA = numpy.pad(e.PolynomA, mo - len(e.PolynomA))
+        e.PolynomB = numpy.pad(e.PolynomB, mo - len(e.PolynomB))
+        e.MaxOrder = mo - 1
+
+    def get_pol(e, pname):
+        le = sorted((getattr(e, pname), getattr(e, pname + 'Err')), key=len)
+        pn = le[1].copy()
+        pn[:len(le[0])] += le[0]
+        return pn
+
+    def set_polerr(ring, pname):
+        refpts = [hasattr(e, pname) and hasattr(e, pname+'Err') for e in ring]
+        rr = ring.replace(refpts)
+        for e in rr[refpts]:
+            setattr(e, pname, get_pol(e, pname))
+            sanitize(e)
+        return rr
     
-    refpts = [(hasattr(e, 'PolynomBErr') or hasattr(e, 'PolynomAErr'))
-              and hasattr(e, 'PolynomB') and hasattr(e, 'PolynomA')
-              for e in ring]
-    ring = ring.replace(refpts)                            
-    for e in ring[refpts]:
-        if hasattr(e, 'PolynomBErr'):  
-            set_polerr(e, 'PolynomB')
-        if hasattr(e, 'PolynomAErr'):        
-            set_polerr(e, 'PolynomA') 
-        sanitize(e) 
-    return ring      
+    ring = set_polerr(ring, 'PolynomA')
+    ring = set_polerr(ring, 'PolynomB')
+    return ring
 
 
-def _orbit_errors(func) -> Callable:
+def _apply_errors(func) -> Callable:
     @functools.wraps(func)
     def wrapper(ring, *args, **kwargs):
-        ring = _apply_field_errors(ring)     
-        ring = _apply_alignment_errors(ring)     
-        orbit0, orbit = func(ring, *args, **kwargs)
+        ring = _apply_field_errors(ring)
+        ring = _apply_alignment_errors(ring)
         refpts = kwargs.get('refpts', None)
-        if refpts is not None:
-            orbit = _apply_bpm_orbit_error(ring, orbit, refpts)
-        return orbit0, orbit
-    return wrapper
-    
-    
-def _linopt_errors(func) -> Callable:
-    @functools.wraps(func)
-    def wrapper(ring, *args, **kwargs): 
-        ring = _apply_field_errors(ring)     
-        ring = _apply_alignment_errors(ring)       
-        ld0, bd, ld = func(ring, *args, **kwargs)
-        refpts = kwargs.get('refpts', None)
-        if refpts is not None:
-            ld.closed_orbit = \
-                _apply_bpm_orbit_error(ring, ld.closed_orbit, refpts)
-        return ld0, bd, ld
-    return wrapper
-    
-    
-def _track_errors(func) -> Callable:
-    @functools.wraps(func)
-    def wrapper(ring, *args, **kwargs): 
-        ring = _apply_field_errors(ring)     
-        ring = _apply_alignment_errors(ring)       
-        rout = func(ring, *args, **kwargs)
-        refpts = kwargs.get('refpts', None)
-        if refpts is not None:
+        if func is lattice_pass:
+            rout = func(ring, *args, **kwargs)
             rout = _apply_bpm_track_error(ring, rout, refpts)
-        return rout
-    return wrapper    
+            return rout
+        elif func is get_optics:
+            ld0, bd, ld = func(ring, *args, **kwargs)
+            ld.closed_orbit = _apply_bpm_orbit_error(ring, ld.closed_orbit,
+                                                     refpts)
+            return ld0, bd, ld
+        elif func is find_orbit:
+            orbit0, orbit = func(ring, *args, **kwargs)
+            orbit = _apply_bpm_orbit_error(ring, orbit, refpts)
+            return orbit0, orbit
+        else:
+            raise AtError('Error wrapper not available for {}'
+                          .format(func.__name__))
+    return wrapper
     
 
 def get_ring_with_errors(ring):
     ring = _apply_field_errors(ring) 
     ring = _apply_alignment_errors(ring) 
     for e in ring:
-       for a in _ERR_ATTRS:
-           if hasattr(e, a):
-               delattr(e, a)
+        for a in _ERR_ATTRS:
+            if hasattr(e, a):
+                delattr(e, a)
     return ring
 
 
-find_orbit_err = _orbit_errors(find_orbit)
+def get_mean_std_err(ring, key, errattr):
+    elems = ring.get_elements(key)
+
+
+find_orbit_err = _apply_errors(find_orbit)
 Lattice.find_orbit_err = find_orbit_err
-get_optics_err = _linopt_errors(get_optics)
+get_optics_err = _apply_errors(get_optics)
 Lattice.get_optics_err = get_optics_err
-lattice_pass_err = _track_errors(lattice_pass)
+lattice_pass_err = _apply_errors(lattice_pass)
