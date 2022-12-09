47fe48e7f597a1d18df973b374f87232b78dd64c
Simon White <swhite@esrf.fr>
2022-12-09 05:13:56 +0100

    removed pandas

--- pyat/at/corrections/elements.py
+++ pyat/at/corrections/elements.py
@@ -3,105 +3,25 @@ Classes for response matrix observables and variables
 """
 
 from typing import List
-import pandas
 import numpy
 from at.physics import find_orbit, get_optics
 from at.tracking import lattice_pass
 from fnmatch import fnmatch
 
 
-_DATA_NAMES = {'trajectory': ['X', 'XP', 'Y', 'YP', 'DP', 'CT'], 
+_DATA_NAMES = {'trajectory': ['X', 'XP', 'Y', 'YP', 'DP', 'CT'],
                'closed_orbit': ['X', 'XP', 'Y', 'YP', 'DP', 'CT'],
                'beta': ['BETX', 'BETY'],
-               'alpha': ['ALFX', 'ALFY'],
+               'alpha': ['ALFX', 'ALFY'], 
                'mu': ['MUX', 'MUY'],
-               'dispersion': ['DX', 'DPY', 'DY', 'DPY'],
+               'dispersion': ['DX', 'DPX', 'DY', 'DPY'],
                'W': ['WX', 'WY']}
-               
-_VAR_ATTR = ['name', 'refpts', 'attname', 'attindex', 'delta']
-_OBS_ATTR = ['name', 'refpts', 'weight', 'parindex', 'parname', 'funname']
 
 
 def sum_polab(ring, refpts, attrname, index):
     k =  [getattr(ring[r], attrname)[index] * 
           getattr(ring[r], 'Length') for r in refpts]                              
     return numpy.sum(k)
-
-
-def gen_unique_keys(arr):
-    vun, vind = numpy.unique(arr, return_inverse=True, axis=0)
-    for i in range(len(vun)):
-        vr = [ii for ii, vi in enumerate(vind) if vi==i]
-        if len(vr) > 1:
-            for ii, ik in enumerate(vr):
-                arr[ik] = (arr[ik][0], arr[ik][1]+'.'+str(ii))
-    return arr
-
-
-def get_keys(ring, variables, attrkey):
-    keys = []
-    for v in variables:
-        refs = getattr(v, 'refpts', None)
-        if refs is None:
-            keys.append((v.name, 'GLOBAL'))
-        else:
-            r = numpy.atleast_1d(refs)[0]               
-            keys.append((v.name, getattr(ring[r], attrkey)))
-    return pandas.MultiIndex.from_tuples(gen_unique_keys(keys)) 
-
-
-def get_attkey(conf, attr):
-    attrs = [col for col in conf.columns if col not in attr]
-    cref = conf.loc[[r[0] is not None for r in conf.refpts]]
-    c0 = cref.iloc[0]
-    names = [n for n in c0.index if isinstance(numpy.atleast_1d(c0[n])[0], str)]
-    mask = [numpy.atleast_1d(c0[n])[0] in c0.name[1] for n in names]
-    return numpy.array(names)[mask][0], attrs
-
-    
-def  load_confs(rm, ring, oconf, vconf):
-    vattkey, vattrs = get_attkey(vconf, _VAR_ATTR)
-    oattkey, oattrs = get_attkey(oconf, _OBS_ATTR)
-    rm.variables = RMVariables(attrkey=vattkey, attrlist=vattrs)
-    rm.observables = RMObservables(attrkey=oattkey, attrlist=oattrs)    
-    vgroups = numpy.unique(vconf.index.get_level_values(0))
-    ogroups = numpy.unique(oconf.index.get_level_values(0))  
-      
-    for name in vgroups:
-       refpts = vconf.refpts[name]     
-       if numpy.any(refpts.index == 'GLOBAL'):
-           print('No refpts found for {}, ignored.'.format(name))
-           rm.variables.conf.drop(name)
-       else: 
-           if len(refpts) >1: 
-               refpts = numpy.concatenate(refpts.to_numpy(), axis=0).ravel()  
-           else:
-               refpts = numpy.array(refpts)  
-           delta = numpy.unique(vconf.delta[name][0])[0]
-           attname = numpy.unique(vconf.attname[name][0])[0]
-           attindex = numpy.unique(vconf.attindex[name][0])[0]
-           sum_zero =  name+'_SUM' in ogroups
-           rm.add_variables_refpts(ring, name, delta, refpts, attname,
-                                   index=attindex, sum_zero=sum_zero)
-    for name in ogroups:
-       refpts = oconf.refpts[name]
-       if numpy.any(refpts.index == 'GLOBAL'):
-           if name not in rm.observables.conf.index.get_level_values(level=0):
-               print('No refpts found for {}, ignored.'.format(name))
-               rm.observables.conf.drop(name)
-       else: 
-           if len(refpts) >1:
-               refpts = numpy.concatenate(refpts.to_numpy(), axis=0).ravel()
-           else:
-               refpts = numpy.array(refpts)
-           weight = numpy.unique(oconf.weight[name][0])[0]
-           parname = numpy.unique(oconf.parname[name][0])[0]
-           parindex = numpy.unique(oconf.parindex[name][0])[0]
-           rm.add_observables_refpts(ring, parname, refpts, index=parindex,
-                                     weight=weight)
-
-    rm.fullrm = rm.fullrm.reindex(index=rm.observables.conf.index)
-    rm.fullrm = rm.fullrm.reindex(columns=rm.variables.conf.index)
              
       
 class Variable(object):
@@ -176,12 +96,11 @@ class Observable(object):
             assert refpts is not None, \
                 'repfts required for orbit or linopt observable'
             assert name in _DATA_NAMES.keys(),\
-                '{} observable not defined in at.get_optics'.format(name) 
-            self.parname = name           
-            self.name = _DATA_NAMES[name][index]
+                '{} observable not defined in at.get_optics'.format(name)   
+            self.parname = name
+            self.name = _DATA_NAMES[name][index]       
             if self.parname == 'trajectory':
                 fun = lattice_pass
-                self.name += '_T'+str(kwargs.get('nturns', 1))
             elif self.parname == 'closed_orbit':
                 fun = find_orbit
             else:
@@ -216,44 +135,18 @@ class Observable(object):
         
 class RMElements(List):
 
-    def __init__(self, attrs, attrkey='FamName', attrlist=[], elements=[], **kwargs):
-        self.attrkey = attrkey
-        self.attrs = attrs
-        self.attrlist = numpy.unique(attrlist+[attrkey])
+    def __init__(self, elements=[], **kwargs):
         self.kwargs = kwargs
-        self.conf = pandas.DataFrame(columns=numpy.concatenate((attrs,
-                                                                attrlist)))
         super(RMElements, self).__init__(elements)   
         
     def add_elements(self, ring, elements):
-        self += numpy.atleast_1d(elements) 
-        self._update(ring) 
-        
-    def _update(self, ring):
-        keys = get_keys(ring, self, self.attrkey)
-        varsd = {} 
-        for attr in self.attrs:
-            s = [numpy.atleast_1d(getattr(v, attr, None)) for v in self]
-            varsd.update({attr: pandas.Series(s, index=keys)})                 
-        for attr in self.attrlist:
-            s = []
-            for v in self:
-                ref = numpy.atleast_1d(getattr(v, 'refpts', None))
-                if ref[0] is None:
-                    s += [ref[0]]
-                else:
-                    s += [getattr(ring[ref[0]], attr, None)]
-            varsd.update({attr: pandas.Series(s, index=keys)})               
-        df = pandas.DataFrame(varsd) 
-        self.conf = df  
+        self += numpy.atleast_1d(elements)   
         
         
 class RMVariables(RMElements):
 
-    def __init__(self, attrkey='FamName', attrlist=[], variables=[], **kwargs):
-        super(RMVariables, self).__init__(_VAR_ATTR, attrkey=attrkey,
-                                          attrlist=attrlist,
-                                          elements=variables, **kwargs)   
+    def __init__(self, variables=[], **kwargs):
+        super(RMVariables, self).__init__(elements=variables, **kwargs)   
         
     def add_elements_refpts(self, ring, name, delta, refpts, attname, index=None):                          
         attname = numpy.broadcast_to(attname, len(refpts))
@@ -261,8 +154,7 @@ class RMVariables(RMElements):
         delta = numpy.broadcast_to(delta, len(refpts))
         index = numpy.broadcast_to(index, len(refpts))
         self += [Variable(n, d, refpts=r, attname=a, index=i, getsetf=None)
-                 for r, a, n, d, i in zip(refpts, attname, name, delta, index)]   
-        self._update(ring)        
+                 for r, a, n, d, i in zip(refpts, attname, name, delta, index)]          
         
     def get(self, ring, mask=None):
         if mask is None:
@@ -277,10 +169,8 @@ class RMVariables(RMElements):
             
 class RMObservables(RMElements):
 
-    def __init__(self, attrkey='FamName', attrlist=[], observables=[], **kwargs):
-        super(RMObservables, self).__init__(_OBS_ATTR, attrkey=attrkey,
-                                            attrlist=attrlist,
-                                            elements=observables, **kwargs)
+    def __init__(self, observables=[], **kwargs):
+        super(RMObservables, self).__init__(elements=observables, **kwargs)
 
     def add_elements_refpts(self, ring, name, refpts, index=None, weight=1):
         traj = name == 'trajectory'
@@ -297,15 +187,14 @@ class RMObservables(RMElements):
                          for r, n, i, w in zip(refpts, name, index, weight)]
         else:
             self += [Observable(n, r, index=i, weight=w, fun=None, kwargs=self.kwargs)
-                     for r, n, i, w in zip(refpts, name, index, weight)]    
-        self._update(ring)       
+                     for r, n, i, w in zip(refpts, name, index, weight)]          
 
     def values(self, ring, mask=None):
         if mask is None:
             mask = numpy.ones(len(self), dtype=bool)
-        allref = [n[0] for n in self.conf.refpts[mask] if n[0] is not None]
+        allref = [obs.refpts for obs in numpy.array(self)[mask] if obs.refpts is not None]
         allref = numpy.sort(numpy.unique(allref))
-        all_fun = numpy.unique([f[0] for f in self.conf.funname[mask]])     
+        all_fun = numpy.unique([obs.funname for obs in self])     
         trajectory = 'lattice_pass' in all_fun
         linopt = 'get_optics' in all_fun
         orbit = 'find_orbit' in all_fun
@@ -318,7 +207,7 @@ class RMObservables(RMElements):
             o0 = self.kwargs.get('init_coord', numpy.zeros((6, )))
             traj = lattice_pass(ring, o0, nturns=turn, refpts=allref)
         elif linopt:
-            get_w = numpy.any([fnmatch(n[0], 'W[XY]') for n in self.conf.name[mask]])
+            get_w = numpy.any([fnmatch(obs.name, 'W[XY]') for obs in numpy.array(self)[mask]])
             twiss_in = self.kwargs.get('twiss_in', None)
             _, _, valref = get_optics(ring, refpts=allref, get_w=get_w,
                                       twiss_in=twiss_in)
--- pyat/at/corrections/response_matrix.py
+++ pyat/at/corrections/response_matrix.py
@@ -2,10 +2,9 @@
 Classes to compute arbitrary response matrices
 """
 import numpy
-import pandas
 import multiprocessing
 from .elements import RMVariables, RMObservables
-from .elements import sum_polab, Observable, load_confs
+from .elements import sum_polab, Observable
 from functools import partial
 from fnmatch import fnmatch
 import warnings
@@ -21,8 +20,6 @@ class ElementResponseMatrix(object):
         self.variables = RMVariables(**vkw)
         self.observables = RMObservables(**okw)
         self.fullrm = None
-        self.excl_obs = []
-        self.excl_var = []
         super(ElementResponseMatrix, self).__init__()
         
     def add_variables(self, ring, variables):
@@ -50,7 +47,7 @@ class ElementResponseMatrix(object):
                                              weight=weight)
                                   
     @staticmethod
-    def _resp_one(ring, observables, variable, key):
+    def _resp_one(ring, observables, variable):
         if ring is None:
             ring = globring
         if observables is None:
@@ -60,13 +57,10 @@ class ElementResponseMatrix(object):
         op = observables.values(ring)
         variable.set(ring, v0-variable.delta)  
         om = observables.values(ring)
-        do = {key: (oom-oop)/2/variable.delta for key, oom, oop 
-              in zip(observables.conf.index, om, op)}
-        variable.set(ring, v0)
-        return {key: pandas.Series(do)}         
+        do = [(oom-oop)/2/variable.delta for oom, oop in zip(om, op)]
+        return do         
         
     def compute_fullrm(self, ring, use_mp=False, pool_size=None, start_method=None):
-        rv = {}
         if use_mp:
             ctx = multiprocessing.get_context(start_method)
             if pool_size == None:
@@ -76,120 +70,68 @@ class ElementResponseMatrix(object):
                 global globobs
                 globring = ring
                 globobs = self.observables
-                args = [(None, None, var, key) 
-                        for var, key in zip(self.variables, self.variables.conf.index)]
+                args = [(None, None, var) for var in self.variables]
                 with ctx.Pool(pool_size) as pool:
                     results = pool.starmap(partial(self._resp_one), args)
                 globring = None
             else:
-                args = [(ring, self.observables, var, key)
-                        for var, key in zip(self.variables, self.variables.conf.index)]
+                args = [(ring, self.observables, var) for var in self.variables]
                 with ctx.Pool(pool_size) as pool:
                     results = pool.starmap(partial(self._resp_one), args)
-            for r in results:
-                rv.update(r)             
-        else: 
-            for var, key in zip(self.variables, self.variables.conf.index):  
-                rv.update(self._resp_one(ring, self.observables, var, key))
-        self.fullrm = pandas.DataFrame(rv)         
-        
-    def save_fullrm(self, filename):
-        assert self.fullrm is not None,\
-           ' Empty response matrix: please run compute_fullrm() first' 
-        if not filename.endswith('.h5'):
-            filename += '.h5'
-        store = pandas.HDFStore(filename)
-        store['fullrm'] = self.fullrm
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', pandas.errors.PerformanceWarning)
-            store['vconf'] = self.variables.conf
-            store['oconf'] = self.observables.conf
-        store.close()
-        
-    def load_fullrm(self, ring, filename):
-        store = pandas.HDFStore(filename)
-        self.fullrm = store['fullrm']
-        vconf = store['vconf']
-        oconf = store['oconf']
-        store.close()
-        load_confs(self, ring, oconf, vconf)
-        
-    def exclude(self, obsdictlist=None, vardictlist=None):
-    
-        def remove_element(conf, df):
-            cond = False
-            for c in numpy.atleast_1d(conf):
-                cond2 =True
-                for k, v in c.items():
-                    cond2 = cond2 & numpy.array([d[0] in v for d in df[k]], dtype=bool)
-            cond = cond | cond2
-            return df.loc[cond].index   
-        
-        if vardictlist is not None:           
-            self.excl_var = remove_element(vardictlist, self.variables.conf)
-        if obsdictlist is not None:
-            self.excl_obs = remove_element(obsdictlist, self.observables.conf)
+        else:
+            results = [self._resp_one(ring, self.observables, var) for var in self.variables]            
+        self.fullrm = numpy.array(results)         
        
     def get_mat(self):
         assert self.fullrm is not None,\
            ' Empty response matrix: please run compute_fullrm() first'                               
-        return self.fullrm.loc[~self.fullrm.index.isin(self.excl_obs),
-                               ~self.fullrm.columns.isin(self.excl_var)]                           
+        return self.fullrm                          
         
-    def get_vals(self, ring, mat=None):
-        if mat is None:
-            mat = self.get_mat()
-        mask = self.observables.conf.index.isin(mat.index)  
-        obs = numpy.array(self.observables.values(ring, mask))
-        mask = self.variables.conf.index.isin(mat.columns)
-        var = numpy.array(self.variables.get(ring, mask))
+    def get_vals(self, ring, masko=None, maskv=None):           
+        obs = self.observables.values(ring, masko)
+        var = self.variables.get(ring, maskv)
         return obs, var
         
-    def _get_weights(self, mat=None):
-        if mat is None:
-            mat = self.get_mat()
-        mask = self.observables.conf.index.isin(mat.index)
-        return numpy.concatenate(self.observables.conf.weight[mask].to_numpy()) 
+    def _get_weights(self, mask):
+        if mask is None:
+            mask = numpy.ones(len(self.observables), dtype=bool)
+        w = [obs.weight for obs in numpy.array(self.observables)[mask]]
+        return numpy.array(w) 
         
     def svd(self, mat=None):
         if mat is None:
             mat = self.get_mat()
         return numpy.linalg.svd(mat, full_matrices=False)                                                
                 
-    def svd_fit(self, ring, target, mat=None, svd_cut=0, apply_correction=False):
-        if mat is None:
-            mat = self.get_mat()
-        val, var = self.get_vals(ring, mat=mat)
-        weights = self._get_weights(mat=mat)
-        err = (val-target)
-        U, W, V = self.svd(mat=mat.multiply(weights, axis=0))
-        Winv = numpy.linalg.inv(numpy.diag(W))
-        Wtmp = numpy.zeros(Winv.shape)
-        Wtmp[:len(Winv)-svd_cut]=Winv[:len(Winv)-svd_cut]
-        dk = V.T @ Wtmp @ U.T @ (err*weights)
-        exp = mat @ dk
-        corr = {'Vals': pandas.Series(var, mat.columns),
-                'Corr': pandas.Series(dk, mat.columns),
-                'Exp': pandas.Series(var+dk, mat.columns)}
-        obs = {'Vals': pandas.Series(val, mat.index),
-               'Target': pandas.Series(target, mat.index),
-               'Weight': pandas.Series(weights, mat.index),
-               'Err': pandas.Series(err, mat.index),
-               'Exp': pandas.Series(exp, mat.index)}
-        if apply_correction:
-            mask = self.variables.conf.index.isin(mat.columns) 
-            self.variables.set(ring, var+dk, mask)
-        valn, varn = self.get_vals(ring, mat=mat) 
-        corr.update({'New': pandas.Series(varn, mat.columns)})
-        obs.update({'New': pandas.Series(valn, mat.index)})
-        return pandas.DataFrame(corr), pandas.DataFrame(obs)
+    def svd_fit(self, ring, target, svd_cut=0, apply_correction=False, masko=None, maskv=None, niter=1):
+        if masko is None:
+            masko = numpy.ones(len(self.observables), dtype=bool)
+        if maskv is None:
+            maskv = numpy.ones(len(self.variables), dtype=bool)
+        mat = self.get_mat()[maskv,:][:,masko]
+        weights = self._get_weights(masko)
+        wm = (mat*weights).T
+        dks = numpy.zeros(sum(maskv))
+        for i in range(niter):
+            val, var = self.get_vals(ring, masko=masko, maskv=maskv)
+            err = (val-target[masko])
+            U, W, V = self.svd(mat=wm)
+            Winv = numpy.linalg.inv(numpy.diag(W))
+            Wtmp = numpy.zeros(Winv.shape)
+            Wtmp[:len(Winv)-svd_cut]=Winv[:len(Winv)-svd_cut]
+            dk = V.T @ Wtmp @ U.T @ (err*weights)
+            if apply_correction:
+                self.variables.set(ring, var+dk, maskv)
+            dks += dk
+        exp = mat.T @ dks
+        return dks, exp
 
         
 class OrbitResponseMatrix(ElementResponseMatrix):
 
-    _MAT_INDEX = {'X': ['X', 'HST_SUM'],
+    _MAT_OBS = {'X': ['X', 'HST_SUM'],
                   'Y': ['Y', 'VST_SUM']}
-    _MAT_COLS = {'X': ['HST', 'RF'], 'Y': ['VST']}         
+    _MAT_VARS = {'X': ['HST', 'RF'], 'Y': ['VST']}         
 
     def __init__(self, ring, bpms, steerers, cavities=None, sum_zero=True,
                  deltax=1.0e-4, deltay=1.0e-4, **kwargs):
@@ -213,29 +155,23 @@ class OrbitResponseMatrix(ElementResponseMatrix):
             'Inactive cavities used for RM, please turn on your cavities'
         self.add_variables_refpts(ring, 'RF', delta, [refpts],'Frequency')
         
-    def set_excluded_bpms(self, bpmdictlist):
-        self.set_exluded(obsdistlist=bpmdictlist)
-        
-    def set_excluded_steerer(self, steerersdictlist):
-        self.set_exluded(vardistlist=steerersdictlist)
-        
-    def correct(self, ring, mat=None, target=0, plane=['X', 'Y'], svd_cut=0, apply_correction=False):
-        if mat is None:
-            mat = self.get_mat()
+    def correct(self, ring, target=0, plane=['X', 'Y'], svd_cut=0, apply_correction=False, niter=1):
         svd_cut = numpy.broadcast_to(svd_cut, len(plane))       
-        target = numpy.broadcast_to(target, mat.shape[0])
-        corr = pandas.DataFrame()
-        obs = pandas.DataFrame()
+        target = numpy.broadcast_to(target, len(self.observables))
+        dk = {}
+        exp = {}
         for p, s in zip(plane, svd_cut):
             assert fnmatch(p,'[XY]'),\
                'Orbit plane has to be X or Y'
-            mati = mat.loc[mat.index.isin(self._MAT_INDEX[p], level=0),
-                           mat.columns.isin(self._MAT_COLS[p], level=0)]
-            ti = target[mat.index.isin(self._MAT_INDEX[p], level=0)]
-            c, o = self.svd_fit(ring, ti, mat=mati, svd_cut=s, apply_correction=apply_correction)
-            corr = pandas.concat((corr, c))
-            obs = pandas.concat((obs, o))
-        return corr, obs
+            masko = numpy.zeros(len(self.observables), dtype=bool)
+            maskv = numpy.zeros(len(self.variables), dtype=bool)
+            for st in self._MAT_OBS[p]:
+                masko = masko | [fnmatch(obs.name, st) for obs in self.observables]
+            for st in self._MAT_VARS[p]:    
+                maskv = maskv | [fnmatch(var.name, st) for var in self.variables]
+            dk[p], exp[p] = self.svd_fit(ring, target, svd_cut=s, apply_correction=apply_correction,
+                                         masko=masko, maskv=maskv, niter=niter)
+        return dk, exp 
  
         
 class TrajectoryResponseMatrix(OrbitResponseMatrix): 
@@ -248,30 +184,29 @@ class TrajectoryResponseMatrix(OrbitResponseMatrix):
         self.add_observables_refpts(ring, 'trajectory', refpts=refpts, index=2)
         
     def correct(self, ring, mat=None, target=0, plane=['X', 'Y'], svd_cut=0,
-                apply_correction=False, threshold=None):
-        if mat is None:
-            mat = self.get_mat()
-        target = numpy.broadcast_to(target, mat.shape[0])
+                apply_correction=False, threshold=None, niter=1):
+        target = numpy.broadcast_to(target, len(self.observables))
         svd_cut = numpy.broadcast_to(svd_cut, len(plane))
         threshold = numpy.broadcast_to(threshold, len(plane))
-        corr = pandas.DataFrame()
-        obs = pandas.DataFrame()
+        dk = {}
+        exp = {}
         for p, s, th in zip(plane, svd_cut, threshold):
             assert fnmatch(p,'[XY]'),\
                'Trajectory plane has to be X or Y'
-            mask = [fnmatch(idx, p+'*') for idx in mat.index.get_level_values(0)]
-            mati = mat.loc[mask, mat.columns.isin(self._MAT_COLS[p], level=0)]
-            ti = target[mask]
+            masko = numpy.zeros(len(self.observables), dtype=bool)
+            maskv = numpy.zeros(len(self.variables), dtype=bool)
+            for st in self._MAT_OBS[p]:
+                masko = masko | [fnmatch(obs.name, st) for obs in self.observables]
+            for st in self._MAT_VARS[p]:    
+                maskv = maskv | [fnmatch(var.name, st) for var in self.variables]
             if th is not None:
-                v, _ = self.get_vals(ring, mat=mati)
+                v, _ = self.get_vals(ring)
                 idx_values = numpy.where(numpy.absolute(v) >= th)[0]
                 if len(idx_values) > 0:
-                    mati = mati[:idx_values[0]]
-                    ti = ti[:idx_values[0]]
-            c, o = self.svd_fit(ring, ti, mat=mati, svd_cut=s, apply_correction=apply_correction)
-            corr = pandas.concat((corr, c))
-            obs = pandas.concat((obs, o))
-        return corr, obs
+                    masko[:idx_values[0]] = False
+            dk[p], exp[p] = self.svd_fit(ring, target, svd_cut=s, apply_correction=apply_correction,
+                                         masko=masko, maskv=maskv, niter=niter)
+        return dk, exp 
         
 
 class OpticsResponseMatrix(ElementResponseMatrix):
@@ -314,13 +249,11 @@ class OpticsResponseMatrix(ElementResponseMatrix):
         delta = numpy.broadcast_to(delta, len(refpts))
         self.add_variables_refpts(ring, magnames, delta, refpts, magattr, index=magidx)
         
-    def correct(self, ring, target, mat=None, svd_cut=0, apply_correction=False):
-        if mat is None:
-            mat = self.get_mat()      
-        assert len(target) == mat.shape[0], \
-            'Linopt RM: target must be of the same length as the matrix'
-        corr, obs = self.svd_fit(ring, target, mat=mat, svd_cut=svd_cut, apply_correction=apply_correction)
-        return corr, obs
+    def correct(self, ring, target, svd_cut=0, apply_correction=False, niter=1):    
+        assert len(target) == len(self.observables), \
+            'Linopt RM: target must have its length equal to the number of observables'
+        dk, exp = self.svd_fit(ring, target, svd_cut=svd_cut, apply_correction=apply_correction, niter=1)
+        return dk, exp
         
         
 class LinoptResponseMatrix(OpticsResponseMatrix):
