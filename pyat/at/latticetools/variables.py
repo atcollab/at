fb16a0be7d59f08ecd562b47ca0e64174dc959cb
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-02-21 14:53:58 +0100

    Documentation

--- pyat/at/latticetools/variables.py
+++ pyat/at/latticetools/variables.py
@@ -105,29 +105,28 @@ class Variable(object):
         self._history = []
 
     @property
-    def history(self):
+    def history(self) -> list[float]:
         """History of the values of the variable"""
         return self._history
 
     @property
-    def initial_value(self):
+    def initial_value(self) -> float:
+        """Initial value of the variable"""
         if len(self._history) > 0:
             return self._history[0]
         else:
-            mess = "{}: Initial value has not been defined"
-            raise ValueError(mess.format(self.name))
+            raise ValueError(f"{self.name}: Initial value has not been defined")
 
     @property
-    def last_value(self):
+    def last_value(self) -> float:
         """Last value of the variable"""
         if len(self._history) > 0:
             return self._history[-1]
         else:
-            mess = "{}: No value has been set yet"
-            raise ValueError(mess.format(self.name))
+            raise ValueError(f"{self.name}: No value has been set yet")
 
     @property
-    def previous_value(self):
+    def previous_value(self) -> float:
         """Value before the last one"""
         if len(self._history) > 1:
             return self._history[-2]
@@ -135,7 +134,7 @@ class Variable(object):
             mess = "{}: history too short"
             raise ValueError(mess.format(self.name))
 
-    def set(self, ring: Lattice, value: float):
+    def set(self, ring: Lattice, value: float) -> None:
         """Set the variable value
 
         Args:
@@ -163,8 +162,12 @@ class Variable(object):
             h.append(value)
         return value
 
-    def set_previous(self, ring: Lattice):
-        """Set to the value before the last one"""
+    def set_previous(self, ring: Lattice) -> None:
+        """Set to the value before the last one
+
+        Args:
+            ring:   Lattice description
+        """
         try:
             _ = self._history.pop()
             previous = self._history.pop()
@@ -174,24 +177,34 @@ class Variable(object):
         else:
             self.set(ring, previous)
 
-    def set_initial(self, ring: Lattice):
-        """Set to initial"""
+    def set_initial(self, ring: Lattice) -> None:
+        """Set to initial value
+
+        Args:
+            ring:   Lattice description
+        """
         self.set(ring, self.initial_value)
 
-    def increment(self, ring: Lattice, incr: float):
+    def increment(self, ring: Lattice, incr: float) -> None:
+        """Increment the variable value
+
+        Args:
+            ring:   Lattice description
+            incr:   Increment value
+        """
         if len(self._history) == 0:
             self.get(ring, initial=True)
         self.set(ring, self.last_value + incr)
 
-    def _step(self, ring: Lattice, step: float):
+    def _step(self, ring: Lattice, step: float) -> None:
         self.set(ring, self.initial_value + step)
 
-    def step_up(self, ring: Lattice):
-        """Set to initial + delta"""
+    def step_up(self, ring: Lattice) -> None:
+        """Set to initial_value + delta"""
         self._step(ring, self.delta)
 
-    def step_down(self, ring: Lattice):
-        """Set to initial - delta"""
+    def step_down(self, ring: Lattice) -> None:
+        """Set to initial_value - delta"""
         self._step(ring, -self.delta)
 
     @staticmethod
@@ -213,11 +226,11 @@ class Variable(object):
             self.name, vini, vnow, (vnow - vini))
 
     def status(self, ring: Lattice = None):
-        """Return a string describing the current status of the observable
+        """Return a string describing the current status of the variable
 
         Args:
             ring:   Lattice description. If :py:obj:`None`, the last archived
-              value is returned. Otherwise, new values are extracted from
+              value is returned. Otherwise, current values are extracted from
               *ring*
 
         Returns:
@@ -240,7 +253,6 @@ class ElementVariable(Variable):
 
     of one or several :py:class:`.Element`\ (s) of a lattice.
     """
-
     def __init__(self,
                  refpts: Refpts, attrname: str,
                  index: Optional[int] = None,
@@ -265,11 +277,33 @@ class ElementVariable(Variable):
 
 
 class VariableList(list):
+    """Container for :py:class:`Variable` objects
+
+    :py:class:`VariableList` supports all :py:class:`list` methods, like
+    appending, insertion or concatenation with the "+" operator.
+    """
 
     def get(self, ring: Lattice, initial=False) -> Sequence[float]:
+        r"""Get the current :py:class:`Variable\ s' values
+
+        Args:
+            ring:       Lattice description
+            initial:    If :py:obj:`True`, set the :py:class:`Variable\ s'
+              initial value
+
+        Returns:
+            values:     1D array of values of all variables
+
+        """
         return np.array([var.get(ring, initial=initial) for var in self])
 
     def set(self, ring: Lattice, values: Iterable[float]) -> None:
+        """
+
+        Args:
+            ring:       Lattice description
+            values:     Iterable of values
+        """
         for var, val in zip(self, values):
             var.set(ring, val)
 
@@ -279,6 +313,7 @@ class VariableList(list):
 
     # noinspection PyProtectedMember
     def status(self, ring: Lattice = None) -> str:
+        """String description of the variables"""
         values = "\n".join(var._line(ring) for var in self)
         return "\n".join((Variable._header(), values))
 
