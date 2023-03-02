593e891b4623360977ff65c81ddb1b99ccf3f2ef
Simone Liuzzo <simone.liuzzo@esrf.fr>
2023-03-02 13:03:47 +0100

    correction for tests

--- pyat/examples/undulators/test_undulator.py
+++ pyat/examples/undulators/test_undulator.py
@@ -3,11 +3,7 @@ import at.plot
 import matplotlib.pyplot as plt
 from at.lattice.undulator_dipoles import undulator_dipoles, insert_undulator
 
-lattice_file_for_test = '/machfs/liuzzo/EBS/beamdyn/matlab/optics/sr/theory/betamodel.mat'
-lattice_variable_name = 'betamodel'
-
-ring = at.load_mat(lattice_file_for_test, mat_key=lattice_variable_name)
-
+ring = at.load_m('../../../machine_data/esrf.m')
 
 und = undulator_dipoles(0.150*10, 10,
               BendAngle=None,
@@ -27,9 +23,9 @@ def split_element(ring, idx, insert_elem=None):
         ring.insert(idx + 1, insert_elem)
     return ring
 
-insertIDindex = ring.get_refpts('ID17')[0]
-ring = split_element(ring, insertIDindex+1, insert_elem=at.Marker('ID17downstream'))
-insertIDindex = ring.get_refpts('ID17downstream')[0]
+insertIDindex = ring.get_refpts('SDHI')[1]
+ring = split_element(ring, insertIDindex, insert_elem=at.Marker('IDdownstream'))
+insertIDindex = ring.get_refpts('IDdownstream')[0]
 
 ring_w150, ring_w150_unmatched = insert_undulator(ring, und, insertIDindex)
 
@@ -61,13 +57,13 @@ emittance_h_0 = emit0['emitXY'][0]
 emittance_v_0 = emit0['emitXY'][1]
 U0_0 = params.U0
 
-print(f'emittance Hor w/o undulator: {emittance_h_0}')
-print(f'emittance Hor w/  undulator: {emittance_h_u}')
+print(f'emittance Hor w/o undulator: {emittance_h_0*1e9:2.2f} nm')
+print(f'emittance Hor w/  undulator: {emittance_h_u*1e9:2.2f} nm')
 
 
-print(f'U0 w/o undulator: {U0_0}')
-print(f'U0 w/  undulator: {U0_u}')
-print(f'Delta U0 = {U0_u-U0_0} eV')
+print(f'U0 w/o undulator: {U0_0*1e-6:2.2f} MeV')
+print(f'U0 w/  undulator: {U0_u*1e-6:2.2f} MeV')
+print(f'Delta U0 = {(U0_u-U0_0)*1e-6:2.4f} MeV')
 
 if __name__=='__main__':
     pass
