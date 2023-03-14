55002ac5e486b28fa07d855834b6fc2ff90190c6
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-03-14 18:57:14 +0100

    comments

--- atintegrators/exactdrift.c
+++ atintegrators/exactdrift.c
@@ -5,6 +5,9 @@ static double get_pz(double *r6) {
   return sqrt(SQR(1 + r6[4]) - SQR(r6[1]) - SQR(r6[3]));
 }
 
+/* Forest 10.23, exact drift
+   L: length [m]
+*/
 static void exact_drift(double *r6, double L) {
   double NormL = L / get_pz(r6);
   r6[0] += r6[1] * NormL;
