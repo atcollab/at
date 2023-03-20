035023c40c0fa5848f89596e38b2ad1a5640b1ee
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-03-20 14:37:50 +0100

    exact rad kick

--- atintegrators/exactbendfringe.c
+++ atintegrators/exactbendfringe.c
@@ -9,47 +9,55 @@ void bend_fringe(double *r6, double irho, double gK)
 {
     /* Forest 13.13, bend fringe in the hard-edge limit */
 
-    double dpx, dpy, dd, b0, px, py, pz, d, phi, xp, yp, yf, xf, lf, pyf;
+    double b0 = irho;
 
-    b0 = irho;
+    double pz = get_pz(r6);
+    double px = r6[px_];
+    double py = r6[py_];
+    double d  = r6[delta_];
+    double xp = px / pz;
+    double yp = py / pz;
 
-    pz = get_pz(r6);
-    px = r6[px_];
-    py = r6[py_];
-    d  = r6[delta_];
-    xp = px / pz;
-    yp = py / pz;
-
-    phi = -b0 * tan( b0 * gK * (1 +    SQR(xp)*(2 + SQR(yp)))*pz - atan(xp / (1 + SQR(yp))));
+    double phi = -b0 * tan( b0 * gK * (1 + SQR(xp)*(2 + SQR(yp)))*pz - atan(xp / (1 + SQR(yp))));
 
     /* these are the partial derivatives of phi with respect to px, py and delta
        total horror from Mathematica. This could benefit from some mini-TPSA */
 
-    dpx = -((b0*(pow(px,2)*pow(pz,4)*(pow(py,2) - pow(pz,2)) - pow(pz,6)*(pow(py,2) + pow(pz,2)) +
-                 b0*gK*px*(pow(pz,2)*pow(pow(py,2) + pow(pz,2),2)*(2*pow(py,2) + 3*pow(pz,2)) + pow(px,4)*(3*pow(py,2)*pow(pz,2) + 2*pow(pz,4)) +
-                           pow(px,2)*(3*pow(py,6) + 8*pow(py,4)*pow(pz,2) + 9*pow(py,2)*pow(pz,4) + 5*pow(pz,6))))*
-             pow(Sec((b0*gK*(pow(pz,4) + pow(px,2)*(pow(py,2) + 2*pow(pz,2))))/pow(pz,3) - atan((px*pz)/(pow(py,2) + pow(pz,2)))),2))/
-            (pow(pz,5)*(pow(py,4) + pow(px,2)*pow(pz,2) + 2*pow(py,2)*pow(pz,2) + pow(pz,4))));
+    double px2 = px*px;
+    double px4 = px2*px2;
+    double py2 = py*py;
+    double py4 = py2*py2;
+    double py6 = py4*py2;
+    double pz2 = pz*pz;
+    double pz3 = pz2*pz;
+    double pz4 = pz2*pz2;
+    double pz5 = pz4*pz;
+    double pz6 = pz4*pz2;
+    double py2z2 = SQR(py2 + pz2);
+    double powsec = pow(Sec((b0*gK*(pz4 + px2*(py2 + 2*pz2)))/pz3 - atan((px*pz)/(py2 + pz2))),2);
+    double denom = (pz5*(py4 + px2*pz2 + 2*py2*pz2 + pz4));
 
+    double dpx = -(b0*(px2*pz4*(py2 - pz2) - pz6*(py2 + pz2) +
+            b0*gK*px*(pz2*py2z2*(2*py2 + 3*pz2) + px4*(3*py2*pz2 + 2*pz4) +
+                      px2*(3*py6 + 8*py4*pz2 + 9*py2*pz4 + 5*pz6)))*powsec)
+           /denom;
 
-    dpy = -((b0*py*(px*pow(pz,4)*(pow(py,2) + pow(pz,2)) + b0*gK*(-(pow(pz,4)*pow(pow(py,2) + pow(pz,2),2)) +
-                                                                  pow(px,4)*(3*pow(py,2)*pow(pz,2) + 4*pow(pz,4)) +
-                                                                  pow(px,2)*(3*pow(py,6) + 10*pow(py,4)*pow(pz,2) + 11*pow(py,2)*pow(pz,4) + 3*pow(pz,6))))*
-             pow(Sec((b0*gK*(pow(pz,4) + pow(px,2)*(pow(py,2) + 2*pow(pz,2))))/pow(pz,3) - atan((px*pz)/(pow(py,2) + pow(pz,2)))),2))/
-            (pow(pz,5)*(pow(py,4) + pow(px,2)*pow(pz,2) + 2*pow(py,2)*pow(pz,2) + pow(pz,4))));
+    double dpy = -(b0*py*(px*pz4*(py2 + pz2) +
+            b0*gK*(-(pz4*py2z2) + px4*(3*py2*pz2 + 4*pz4) +
+                   px2*(3*py6 + 10*py4*pz2 + 11*py2*pz4 + 3*pz6)))*powsec)
+           /denom;
 
-    dd = (b0*(1 + d)*(px*pow(pz,4)*(pow(py,2) - pow(pz,2)) + b0*gK*
-                      (-(pow(pz,4)*pow(pow(py,2) + pow(pz,2),2)) + pow(px,4)*(3*pow(py,2)*pow(pz,2) + 2*pow(pz,4)) +
-                       pow(px,2)*(3*pow(py,6) + 8*pow(py,4)*pow(pz,2) + 7*pow(py,2)*pow(pz,4) + pow(pz,6))))*
-          pow(Sec((b0*gK*(pow(pz,4) + pow(px,2)*(pow(py,2) + 2*pow(pz,2))))/pow(pz,3) - atan((px*pz)/(pow(py,2) + pow(pz,2)))),2))/
-        (pow(pz,5)*(pow(py,4) + pow(px,2)*pow(pz,2) + 2*pow(py,2)*pow(pz,2) + pow(pz,4)));
+    double dd = (b0*(1 + d)*(px*pz4*(py2 - pz2) + b0*gK*
+                      (-(pz4*py2z2) + px4*(3*py2*pz2 + 2*pz4) +
+                       px2*(3*py6 + 8*py4*pz2 + 7*py2*pz4 + pz6)))*powsec)
+         /denom;
 
     /* solve quadratic equation in yf (Forest fringe_part_I.pdf) */
 
-    yf = (2 * r6[y_]) / (1 + sqrt(1 - 2 * dpy * r6[y_]));
-    xf = r6[x_] + 0.5 * dpx * SQR(yf);
-    lf = r6[ct_] - 0.5 * dd * SQR(yf);
-    pyf = py - phi * yf;
+    double yf = (2 * r6[y_]) / (1 + sqrt(1 - 2 * dpy * r6[y_]));
+    double xf = r6[x_] + 0.5 * dpx * SQR(yf);
+    double lf = r6[ct_] - 0.5 * dd * SQR(yf);
+    double pyf = py - phi * yf;
 
     r6[y_]  = yf;
     r6[x_]  = xf;
