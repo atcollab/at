726b6ed761abd77cae1eee6362128a27c764c02f
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-03-29 09:39:18 +0200

    test ExactRectBendPass

--- atintegrators/exactbend.c
+++ atintegrators/exactbend.c
@@ -5,16 +5,36 @@ static void exact_bend(double *r6, double irho, double L)
 
     double dp1 = 1.0 + r6[delta_];
     double pz = pxyz(dp1, r6[px_], r6[py_]);
+    double pzmx = pz-(1.0+r6[x_]*irho);
     double cs = cos(irho*L);
     double sn = sin(irho*L);
-    double px = r6[px_]*cs + (pz-(1.0+r6[x_]*irho))*sn;
+    double px = r6[px_]*cs + pzmx*sn;
     double d2 = pxyz(dp1, 0.0, r6[py_]);
     double dasin = L + (asin(r6[px_]/d2) - asin(px/d2))/irho;
-    double x = (pxyz(dp1,px,r6[py_]) - (pz-(1.0+r6[x_]*irho))*cs + r6[px_]*sn - 1.0)/irho;
+    double x = (pxyz(dp1,px,r6[py_]) - pzmx*cs + r6[px_]*sn - 1.0)/irho;
     double dy = r6[py_]*dasin;
     double dct = dp1*dasin;
     
     r6[x_] = x;
+    r6[px_] = px;
+    r6[y_] += dy;
+    r6[ct_] += dct;
+}
+
+static void exact_straight_bend(double *r6, double irho, double L)
+{
+    /* Forest 12.39, bend-kick split, map V(L,irho) */
+
+    double dp1 = 1.0 + r6[delta_];
+    double pz = pxyz(dp1, r6[px_], r6[py_]);
+    double px = r6[px_] - irho*L;
+    double d2 = pxyz(dp1, 0.0, r6[py_]);
+    double dasin = (asin(r6[px_]/d2) - asin(px/d2))/irho;
+    double dx = (pxyz(dp1, px, r6[py_]) - pz)/irho;
+    double dy = r6[py_]*dasin;
+    double dct = dp1*dasin;
+
+    r6[x_] += dx;
     r6[px_] = px;
     r6[y_] += dy;
     r6[ct_] += dct;
