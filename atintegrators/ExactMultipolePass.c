67b3065794c91a144ecb270c71a4111b5da8affd
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-09-12 15:23:58 +0200

    Protect NumIntSteps == 0

--- atintegrators/ExactMultipolePass.c
+++ atintegrators/ExactMultipolePass.c
@@ -131,6 +131,10 @@ ExportMode struct elem *trackFunction(const atElem *ElemData, struct elem *Elem,
     double *RApertures = atGetOptionalDoubleArray(ElemData, "RApertures"); check_error();
     double *KickAngle = atGetOptionalDoubleArray(ElemData, "KickAngle"); check_error();
 
+    if (NumIntSteps <= 0) {
+        atError("NumIntSteps must be positive"); check_error();
+    }
+
     Elem = (struct elem *)atMalloc(sizeof(struct elem));
     Elem->Length = Length;
     Elem->PolynomA = PolynomA;
@@ -182,6 +186,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     double *RApertures = atGetOptionalDoubleArray(ElemData, "RApertures"); check_error();
     double *KickAngle = atGetOptionalDoubleArray(ElemData, "KickAngle"); check_error();
 
+    if (NumIntSteps <= 0) {
+        atError("NumIntSteps must be positive"); check_error();
+    }
     /* ALLOCATE memory for the output array of the same size as the input  */
     plhs[0] = mxDuplicateArray(prhs[1]);
     r_in = mxGetDoubles(plhs[0]);
