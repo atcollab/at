e5279ecdf15b8fdbcae50584b4ec88099d249666
Laurent Farvacque <laurent.farvacque@esrf.fr>
2023-09-18 15:09:54 +0200

    Fix ExactSectorBend with bending angle == 0

--- atintegrators/exactbend.c
+++ atintegrators/exactbend.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #ifndef PXYZ
 #define PXYZ
 static double pxyz(double dp1, double px, double py)
@@ -12,20 +14,28 @@ static void exact_bend(double *r6, double irho, double L)
 
     double dp1 = 1.0 + r6[delta_];
     double pz = pxyz(dp1, r6[px_], r6[py_]);
-    double pzmx = pz-(1.0+r6[x_]*irho);
-    double cs = cos(irho*L);
-    double sn = sin(irho*L);
-    double px = r6[px_]*cs + pzmx*sn;
-    double d2 = pxyz(dp1, 0.0, r6[py_]);
-    double dasin = L + (asin(r6[px_]/d2) - asin(px/d2))/irho;
-    double x = (pxyz(dp1,px,r6[py_]) - pzmx*cs + r6[px_]*sn - 1.0)/irho;
-    double dy = r6[py_]*dasin;
-    double dct = dp1*dasin;     /* Absolute path length */
-    
-    r6[x_] = x;
-    r6[px_] = px;
-    r6[y_] += dy;
-    r6[ct_] += dct;
+    if (fabs(irho) < 1.e-6) {
+        double NormL = L / pz;
+        r6[x_] += r6[px_] * NormL;
+        r6[y_] += r6[py_] * NormL;
+        r6[ct_] += NormL * dp1;   /* Absolute path length */
+    }
+    else {
+        double pzmx = pz-(1.0+r6[x_]*irho);
+        double cs = cos(irho*L);
+        double sn = sin(irho*L);
+        double px = r6[px_]*cs + pzmx*sn;
+        double d2 = pxyz(dp1, 0.0, r6[py_]);
+        double dasin = L + (asin(r6[px_]/d2) - asin(px/d2))/irho;
+        double x = (pxyz(dp1,px,r6[py_]) - pzmx*cs + r6[px_]*sn - 1.0)/irho;
+        double dy = r6[py_]*dasin;
+        double dct = dp1*dasin;     /* Absolute path length */
+
+        r6[x_] = x;
+        r6[px_] = px;
+        r6[y_] += dy;
+        r6[ct_] += dct;
+    }
 }
 
 static void exact_straight_bend(double *r6, double irho, double L)
@@ -34,15 +44,23 @@ static void exact_straight_bend(double *r6, double irho, double L)
 
     double dp1 = 1.0 + r6[delta_];
     double pz = pxyz(dp1, r6[px_], r6[py_]);
-    double px = r6[px_] - irho*L;
-    double d2 = pxyz(dp1, 0.0, r6[py_]);
-    double dasin = (asin(r6[px_]/d2) - asin(px/d2))/irho;
-    double dx = (pxyz(dp1, px, r6[py_]) - pz)/irho;
-    double dy = r6[py_]*dasin;
-    double dct = dp1*dasin;     /* Absolute path length */
+    if (fabs(irho) < 1.e-6) {
+        double NormL = L / pz;
+        r6[x_] += r6[px_] * NormL;
+        r6[y_] += r6[py_] * NormL;
+        r6[ct_] += NormL * dp1;   /* Absolute path length */
+    }
+    else {
+        double px = r6[px_] - irho*L;
+        double d2 = pxyz(dp1, 0.0, r6[py_]);
+        double dasin = (asin(r6[px_]/d2) - asin(px/d2))/irho;
+        double dx = (pxyz(dp1, px, r6[py_]) - pz)/irho;
+        double dy = r6[py_]*dasin;
+        double dct = dp1*dasin;     /* Absolute path length */
 
-    r6[x_] += dx;
-    r6[px_] = px;
-    r6[y_] += dy;
-    r6[ct_] += dct;
+        r6[x_] += dx;
+        r6[px_] = px;
+        r6[y_] += dy;
+        r6[ct_] += dct;
+    }
 }
