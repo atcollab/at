87dcf42c95ddc20f3850fce878a94c0787602145
Simon White <swhite@esrf.fr>
2023-10-23 16:20:42 +0200

    docstring of lattice options updated

--- pyat/at/lattice/options.py
+++ pyat/at/lattice/options.py
@@ -68,14 +68,15 @@ DConstant = _Dst()
 Default values for AT algorithms
 
 Attributes:
-    XYStep:             Coordinate step for differentiation
-    DPStep:             Momentum step for dispersion and chromaticity
-    OrbConvergence:     Convergence criterion for orbit
-    OrbMaxIter:         Max. number of iterations for orbit
-    omp_num_threads:    Default number of OpenMP threads
-    patpass_poolsize:   Default size of multiprocessing pool
-    mpi:               :py:obj:`True` if MPI is active
-    openmp:            :py:obj:`True` if OpenMP is active
+    XYStep:              Coordinate step for differentiation
+    DPStep:              Momentum step for dispersion and chromaticity
+    OrbConvergence:      Convergence criterion for orbit
+    OrbMaxIter:          Max. number of iterations for orbit
+    omp_num_threads:     Default number of OpenMP threads
+    patpass_poolsize:    Default size of multiprocessing pool
+    patpass_startmethod: Default start method for the multiprocessing
+    mpi:                 :py:obj:`True` if MPI is active
+    openmp:              :py:obj:`True` if OpenMP is active
 
 Methods:
     reset(attrname):    Reset the attribute to its default value
