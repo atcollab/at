655aad9201779b088d01c3c7a806267e5fea133c
PONS <pons@esrf.fr>
2023-11-08 09:58:13 +0100

    Adjusted constants to the one used in PhysConstant.m

--- atintegrators/atconstants.h
+++ atintegrators/atconstants.h
@@ -22,8 +22,8 @@ const double KICK2 = 1.0 - 2.0 / (2.0 - THIRD_ROOT_2);
 const double C0 = 2.99792458e8;
 
 // Radiation damping, Physics of Electron Storage Ring, M. Sands (4.2)
-#define __RE 2.81794033e-15 // Classical electron radius [m]
-#define __E0 510.99895e-6   // Electron rest energy [GeV]
+#define __RE 2.8179403262e-15 // Classical electron radius [m]
+#define __E0 510.99895e-6     // Electron rest energy [GeV]
 const double CGAMMA = 4.0*M_PI*__RE / (3.0*__E0*__E0*__E0);
 
 #endif //AT_CONSTANTS_H
