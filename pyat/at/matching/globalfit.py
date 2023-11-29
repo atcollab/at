9593643e3a604767ae261590a807cb5b030b407d
Simone Liuzzo <simone.liuzzo@esrf.fr>
2023-11-29 20:53:36 +0100

    KStep

--- pyat/at/matching/globalfit.py
+++ pyat/at/matching/globalfit.py
@@ -77,7 +77,7 @@ def _fit_tune_chrom(ring: Lattice, index: int, func,
 def fit_tune(ring: Lattice, refpts1: Refpts, refpts2: Refpts, newval,
              tol: float = 1.0e-12,
              dp: Optional[float] = 0, niter: int = 3, regex=False, 
-             delta: Optional[float] = None,
+             KStep: Optional[float] = None,
              **kwargs) -> None:
     """Fits the tunes using 2 families
 
@@ -95,7 +95,7 @@ def fit_tune(ring: Lattice, refpts1: Refpts, refpts2: Refpts, newval,
         fit_integer: bool (default=False), use integer tune
         regex:      Using regular expressions for refpt string matching;
                     Default: False
-        delta: gradient variation applied to magnets. Default 1e-6 
+        KStep: gradient variation applied to magnets. Default 1e-5
 
     Typical usage:
     at.fit_tune(ring, refpts1, refpts2, [0.1,0.25])
@@ -104,13 +104,13 @@ def fit_tune(ring: Lattice, refpts1: Refpts, refpts2: Refpts, newval,
     if numpy.any(numpy.floor(newval) != 0.0):
         kwargs['fit_integer'] = True
     _fit_tune_chrom(ring, 1, _get_tune, refpts1, refpts2, newval, tol=tol,
-                    dp=dp, niter=niter, regex=regex, delta=delta, **kwargs)
+                    dp=dp, niter=niter, regex=regex, delta=KStep, **kwargs)
 
 
 def fit_chrom(ring: Lattice, refpts1: Refpts, refpts2: Refpts, newval,
               tol: Optional[float] = 1.0e-12,
               dp: Optional[float] = 0, niter: Optional[int] = 3, regex=False,
-              delta: Optional[float] = None,
+              KStep: Optional[float] = None,
               **kwargs) -> None:
     """Fit the chromaticities using 2 families
 
@@ -126,11 +126,11 @@ def fit_chrom(ring: Lattice, refpts1: Refpts, refpts2: Refpts, newval,
         niter:      Maximum number of iterations. Default 3
         regex:      Using regular expressions for refpt string matching;
                     Default: False
-        delta: gradient variation applied to magnets. Default 1e-6
+        KStep: gradient variation applied to magnets. Default 1e-4
 
     Typical usage:
     at.fit_chrom(ring, refpts1, refpts2, [10,5])
     """
     print('\nFitting Chromaticity...')
     _fit_tune_chrom(ring, 2, _get_chrom, refpts1, refpts2, newval, tol=tol,
-                    dp=dp, niter=niter, regex=regex, delta=delta)
+                    dp=dp, niter=niter, regex=regex, delta=KStep, **kwargs)
