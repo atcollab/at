647c230ca3698fd6e4661997eeef79ba3da1d901
PONS <pons@esrf.fr>
2024-02-05 13:11:56 +0100

    Added ExactDriftPass ExactMultipolePass ExactMultipoleRadPass

--- atgpu/ExactDriftPass.h
+++ atgpu/ExactDriftPass.h
@@ -1,12 +1,19 @@
-//
-// Created by pons on 2/2/24.
-//
-
 #ifndef AT_GPU_EXACTDRIFTPASS_H
 #define AT_GPU_EXACTDRIFTPASS_H
+#include "DriftPass.h"
 
+class ExactDriftPass: public DriftPass {
 
-class ExactDriftPass {
+public:
+    // Construct a drift pass
+    ExactDriftPass() noexcept;
+
+    // Retrieve parameters from upper layer (Python, Matlab)
+    void getParameters(AbstractInterface *param, PassMethodInfo *info) override;
+
+    // Generic code generation
+    static void generateCode(std::string& code, PassMethodInfo *info,SymplecticIntegrator &integrator) noexcept;
+    static void generateUtilsFunction(std::string& code, PassMethodInfo *info) noexcept;
 
 };
 
