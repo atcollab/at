3cf13807462db8560204c586438dc0bca3d880fb
oscarxblanco <oblanco@cells.es>
2024-02-22 18:49:11 +0100

    fix bug in tune above 0.5

--- pyat/at/physics/frequency_maps.py
+++ pyat/at/physics/frequency_maps.py
@@ -280,6 +280,14 @@ def fmap_parallel_track(ring,
     xy_nuxy_lognudiff_array = numpy.delete(xy_nuxy_lognudiff_array, 0)
     # reshape for plots and output files
     xy_nuxy_lognudiff_array = xy_nuxy_lognudiff_array.reshape(-1, 7)
+    # check if initial tune is above 0.5
+    flagabove0p5 = 0.5 < ring.get_tune()
+    for i in range(len(flagabove0p5)):
+        if flagabove0p5[i]:
+            verboseprint('Plane',i,'is above 0.5... fixing freq. data')
+            xy_nuxy_lognudiff_array[:,2+i] = 1-xy_nuxy_lognudiff_array[:,2+i]
+            xy_nuxy_lognudiff_array[:,4+i] =  -xy_nuxy_lognudiff_array[:,4+i]
+
 
     return xy_nuxy_lognudiff_array, loss_map_array
 # the end
