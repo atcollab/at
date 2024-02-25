528ec9d0cfe1fc4b56b693f823fb5b66eea0954d
Laurent Farvacque <laurent.farvacque@esrf.fr>
2024-02-25 16:16:24 +0100

    bug fixes

--- atintegrators/diff_yrot.c
+++ atintegrators/diff_yrot.c
@@ -20,21 +20,21 @@ static yrot_propagate(double *r6, double c, double s, double pz, double p, doubl
   for (int m = 0; m < 6; m++)
     yrotmat[m * 7] = 1.0;
 
-  yrotmat[0] = pz /p;           /* [0,0] */
+  yrotmat[0] = pz / p;           /* [0,0] */
   yrotmat[6] = x_s_pz_p2 * (SQR(px) + SQR(pz));  /* [0, 1] */
   yrotmat[18] = x_s_pz_p2 * px * py;          /* [0, 3] */
-  yrotmat[24] = x_s_pz_p2 * px * dp1;    /* [0, 4] */
+  yrotmat[24] = -x_s_pz_p2 * px * dp1;    /* [0, 4] */
   yrotmat[7] = c - s*px/pz;                       /* [1, 1] */
   yrotmat[19] = -s*py/pz;                        /* [1, 3] */
   yrotmat[25] = s*dp1/pz; /* [1, 4] */
   yrotmat[2] = s*py/p; /* [2, 0] */
-  yrotmat[8] = x_s_pz_p2 * (c*px + s*pz); /* [2, 1] */
+  yrotmat[8] = x_s_pz_p2 * py * (c*px + s*pz); /* [2, 1] */
   yrotmat[20] = x_s_pz_p2 * (c*(SQR(pz)+SQR(py)) - s*px*pz);   /* [2, 3] */
   yrotmat[26] = -x_s_pz_p2 * c*py*dp1;  /* [2, 4] */
   yrotmat[5] = s*dp1/p; /* 5, 0] */
   yrotmat[11] = x_s_pz_p2 * dp1*(c*px + s*pz); /* [5, 1] */
   yrotmat[23] = x_s_pz_p2 * c*dp1*py; /* [5, 3] */
-  yrotmat[29] = x_s_pz_p2 * (c*(SQR(dp1)-SQR(pz)) + s*px*pz); /* 5, 4] */
+  yrotmat[29] = -x_s_pz_p2 * (c*(SQR(px)+SQR(py)) + s*px*pz); /* 5, 4] */
 
   ATsandwichmmt(yrotmat, bdiff);
 }
