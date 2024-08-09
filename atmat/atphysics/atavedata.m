2781b0406a35e5f11c909b43f03a15e0377310be
swhite <swhite@esrf.fr>
2024-08-09 17:41:34 +0200

    remove atlinopt from matlab avedata

--- atmat/atphysics/atavedata.m
+++ atmat/atphysics/atavedata.m
@@ -26,7 +26,11 @@ long=atgetcells(ring,'Length',@(elem,lg) lg>0) & refs(1:end-1); %lr-1
 needed=refs | [false;long]; %lr
 initial=[long(needed(1:end-1));false]; %needed
 final=[false;initial(1:end-1)]; %needed
-[lind,nu,xsi]=atlinopt(ring,dpp,needed,varargin{:}); %needed
+setoption('WarningDp6D',false)
+[ringdata, lind]=atlinopt6(ring,needed,'dp',dpp,'get_chrom',varargin{:}); %needed
+setoption('WarningDp6D',true)
+nu = ringdata.tune;
+xsi = ringdata.chromaticity;
 
 lindata=lind(refs(needed)); %refpts
 avebeta=cat(1,lindata.beta); %refpts
