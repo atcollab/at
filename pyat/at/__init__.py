1c68e2dc0e33fc0c96e346552b40b8932eafcb04
Laurent Farvacque <laurent.farvacque@esrf.fr>
2024-08-11 15:07:47 +0200

    Import at.plot in at

--- pyat/at/__init__.py
+++ pyat/at/__init__.py
@@ -1,6 +1,7 @@
 """Python port of the Accelerator Toolbox"""
 
 from ._version import __version__, __version_tuple__
+
 # Make all functions visible in the at namespace:
 from .lattice import *
 from .tracking import *
@@ -10,3 +11,4 @@ from .load import *
 from .matching import *
 from .acceptance import *
 from .collective import *
+from .plot import *
