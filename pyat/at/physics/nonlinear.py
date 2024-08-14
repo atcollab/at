d3023cbb3def885040eabb2bc1751c44a7d84b74
oscarxblanco <oblanco@cells.es>
2024-08-14 17:31:16 +0200

    solving flake8

--- pyat/at/physics/nonlinear.py
+++ pyat/at/physics/nonlinear.py
@@ -1,6 +1,9 @@
 """
 Non-linear optics
 """
+
+from __future__ import annotations
+
 from typing import Optional, Sequence
 
 import numpy
@@ -256,8 +259,8 @@ def gen_detuning_elem(ring: Lattice, orbit: Optional[Orbit] = None) -> Element:
 def feeddown_polynomba(
     polb: numpy.ndarray = numpy.zeros(0),
     pola: numpy.ndarray = numpy.zeros(0),
-    x0: float = 0,
-    y0: float = 0,
+    xoffset: float = 0,
+    yoffset: float = 0,
     verbose=False,
     debug=False,
 ) -> dict[str, numpy.ndarray]:
@@ -283,16 +286,16 @@ def feeddown_polynomba(
     debugprint(f"polbpad={polbpad}, polapad={polapad}")
 
     verboseprint(f"polb={polb},pola={pola}")
-    verboseprint(f"x0={x0},y0={y0}")
+    verboseprint(f"xoffset={xoffset},yoffset={yoffset}")
     polasum = numpy.zeros(maxord - 1)
     polbsum = numpy.zeros(maxord - 1)
     debugprint(f"ith=1, first order nothing to do")
     for ith in range(2, maxord + 1):
         polbaux_b, polaaux_b = feeddown_from_nth_order(
-            ith, polbpad[ith - 1], x0, y0, magtype="normal"
+            ith, polbpad[ith - 1], xoffset, yoffset, poltype="B"
         )
         polbaux_a, polaaux_a = feeddown_from_nth_order(
-            ith, polapad[ith - 1], x0, y0, magtype="skew"
+            ith, polapad[ith - 1], xoffset, yoffset, poltype="A"
         )
         polbshort = polbaux_b + polbaux_a
         polashort = polaaux_b + polaaux_a
@@ -310,46 +313,71 @@ def feeddown_polynomba(
 
 def feeddown_from_nth_order(
     nthorder: int,
-    nthfieldcomp: float,
-    x0: float,
-    y0: float,
-    magtype: str = "normal",
+    nthpolcomp: float,
+    xoffset: float,
+    yoffset: float,
+    poltype: str = "B",
     debug: bool = False,
-    verbose=False,
-):
+    verbose: bool = False,
+) -> tuple[numpy.ndarray, numpy.ndarray]:
+    """
+    Return the PolynomB and PolynomA from a magnet offset.
+
+    Parameters:
+        nthorder: integer order of the magnet component, e.g. 1 for dipole,
+            2 for quadrupoles, 3 for sextupoles, etc.
+        nthpolcomp: float. nth component of the polynom, i.e. the value of
+            PolynomA/B[nthorder-1]
+        xoffset: float. Horizontal offset
+        yoffset: float. Vertical offset.
+        poltype: Default 'B'. Could be 'B' or 'A', otherwise ignored.
+        debug: print info on the feeddown polynom construction.
+        verbose: print info on input and output parameters.
+
+    Returns:
+        Tuple of two numpy arrays with PolynomB and PolynomA from feeddown.
+    """
     # print debugging output, equivalent to extra verbose
     debugprint = print if debug else lambda *a, **k: None
     verboseprint = print if verbose else lambda *a, **k: None
-    verboseprint(f"nthorder={nthorder},nthfieldcomp={nthfieldcomp},magtype={magtype}")
-    verboseprint(f"x0={x0},y0={y0}")
+    verboseprint(f"nthorder={nthorder},nthpolcomp={nthpolcomp},poltype={poltype}")
+    verboseprint(f"xoffset={xoffset},yoffset={yoffset}")
     fakeimag = {0: 1, 1: 0, 2: -1, 3: 0}
     polbaux = numpy.zeros(nthorder - 1)
     polaaux = numpy.zeros(nthorder - 1)
-    for k in range(1, nthorder):
-        debugprint(f"nthorder={nthorder}, k={k}, nthfieldcomp={nthfieldcomp}")
-        ichoosek = comb(nthorder - 1, k)
+    for kterm in range(1, nthorder):
+        debugprint(f"nthorder={nthorder}, kterm={kterm}, nthpolcomp={nthpolcomp}")
+        ichoosek = comb(nthorder - 1, kterm)
         debugprint(f"ichoosek={ichoosek}")
-        pascalsn = comb(k, numpy.arange(k + 1))
+        pascalsn = comb(kterm, numpy.arange(kterm + 1))
         debugprint(f"pascalsn={pascalsn}")
-        powk = numpy.arange(k + 1)
-        powkflip = numpy.arange(k, -1, -1)
+        powk = numpy.arange(kterm + 1)
+        powkflip = numpy.arange(kterm, -1, -1)
         debugprint(f"powk={powk}")
         debugprint(f"powkflip={powkflip}")
-        recoefs = numpy.array(list(map(lambda x: fakeimag[x], numpy.mod(powk, 4))))
-        imcoefs = numpy.array(list(map(lambda x: fakeimag[x], numpy.mod(powk + 3, 4))))
+        recoefs = numpy.array([fakeimag[numpy.mod(idx, 4)] for idx in powk])
+        imcoefs = numpy.array([fakeimag[numpy.mod(idx + 3, 4)] for idx in powk])
         debugprint(f"recoefs={recoefs}, imcoefs={imcoefs}")
-        commonfactor = nthfieldcomp * ichoosek * (-1) ** k
+        commonfactor = nthpolcomp * ichoosek * (-1) ** kterm
         debugprint(f"commonfactor={commonfactor}")
-        repart = recoefs * commonfactor * (pascalsn * (x0**powkflip * y0**powk))
-        impart = imcoefs * commonfactor * (pascalsn * (x0**powkflip * y0**powk))
+        repart = (
+            recoefs
+            * commonfactor
+            * (pascalsn * (xoffset**powkflip * yoffset**powk))
+        )
+        impart = (
+            imcoefs
+            * commonfactor
+            * (pascalsn * (xoffset**powkflip * yoffset**powk))
+        )
         debugprint(f"repart={repart}")
         debugprint(f"impart={impart}")
-        polbaux[nthorder - k - 1] = polbaux[nthorder - k - 1] + repart.sum()
-        polaaux[nthorder - k - 1] = polaaux[nthorder - k - 1] + impart.sum()
+        polbaux[nthorder - kterm - 1] = polbaux[nthorder - kterm - 1] + repart.sum()
+        polaaux[nthorder - kterm - 1] = polaaux[nthorder - kterm - 1] + impart.sum()
     verboseprint(f"polbaux={polbaux},polaaux={polaaux}")
     polbout, polaout = polbaux, polaaux
     # skew components swap the imaginary and real feed-down and change the sign
     # of the real part, i.e. j*j = -1
-    if magtype == "skew":
+    if poltype == "A":
         polbout, polaout = -1 * polaaux, polbaux
     return polbout, polaout
