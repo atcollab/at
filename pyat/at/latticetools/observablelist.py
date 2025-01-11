a92f70b9065a41c27ff39923075717c363a0d5ba
Laurent Farvacque <laurent.farvacque@esrf.fr>
2025-01-11 17:46:30 +0100

    Check for empty refpts

--- pyat/at/latticetools/observablelist.py
+++ pyat/at/latticetools/observablelist.py
@@ -376,6 +376,17 @@ class ObservableList(list):
         )
         return [obseval(ring, ob) for ob in self]
 
+    def check(self) -> bool:
+        """Check evaluation
+
+        Returns:
+            ok: :py:obj:`True` if evaluation is done, :py:obj:`False` otherwise
+
+        Raises:
+            AtError:    any value is doubtful: evaluation failed, empty value…
+        """
+        return all(obs.check() for obs in self)
+
     # noinspection PyProtectedMember
     def exclude(self, obsname: str, excluded: Refpts):
         """Set the excluded mask on the selected observable."""
