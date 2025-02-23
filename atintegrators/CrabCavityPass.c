f7c5d6a4a851854122fdad393b9ab460bef3e2f9
Laurent Farvacque <laurent.farvacque@esrf.fr>
2025-02-23 14:08:26 +0100

    reorganisation

--- atintegrators/CrabCavityPass.c
+++ atintegrators/CrabCavityPass.c
@@ -18,9 +18,12 @@ struct elem
     double PhaseLag;
     double SigPhi;
     double SigVV;
+    long HarmNumber;
 };
 
-static void track_cavity(double *r_in, double nvx, double nvy, double k, double phi)
+typedef void (*kickfun)(double *r_in, double nvx, double nvy, double k, double betgam0, double phi);
+
+static void relativistic_kick(double *r_in, double nvx, double nvy, double k, double betgam0, double phi)
 {
     double omega_t = k*r_in[5] + phi;
     double cos_ot = cos(omega_t);
@@ -32,25 +35,40 @@ static void track_cavity(double *r_in, double nvx, double nvy, double k, double 
     r_in[4] += ddpp;
 }
 
-static void CrabCavityPass(double *r_in, double le, double nvx, double nvy,
-    double freq, double phi, double sigphi, double sigvv, pcg32_random_t* rng, int num_particles)
+static void non_relativistic_kick(double *r_in, double nvx, double nvy, double k, double betgam0, double phi)
+{
+    double betgami = betgam0*(1.0 + r_in[4]);
+    double betai = betgami/sqrt(1.0 + betgami*betgami);
+    double omega_t = k*r_in[5]/betai + phi;
+    double cos_ot = cos(omega_t);
+    double sin_ot = sin(omega_t);
+    double ddpp = -(nvy*r_in[2]+nvx*r_in[0])*k*cos_ot/(1.0+r_in[4]);
+
+    r_in[1] += nvx*sin_ot;
+    r_in[3] += nvy*sin_ot;
+    r_in[4] += ddpp;
+}
+
+static void CrabCavityPass(
+    double *r_in,
+    double le,
+    double nvx,
+    double nvy,
+    double freq,
+    double betgam0,
+    double phiref,
+    int num_particles)
 {
 	double k = TWOPI*freq/C0;
-
-    if (sigphi > 0.0)
-        phi += atrandn_r(rng, 0.0, sigphi);
-    if (sigvv > 0.0) {
-        nvx *= atrandn_r(rng, 1.0, sigvv);
-        nvy *= atrandn_r(rng, 1.0, sigvv);
-    }
+    kickfun ff = isfinite(betgam0) ? &non_relativistic_kick : &relativistic_kick;
 
 	if (le == 0) {
         #pragma omp parallel for if (num_particles > OMP_PARTICLE_THRESHOLD) \
-            default(none) shared(r_in, num_particles, nvx, nvy, k, phi)
+            default(none) shared(r_in, num_particles, nvx, nvy, k, phiref)
         for (int c = 0; c<num_particles; c++) {
             double *r6 = r_in + c*6;
             if (!atIsNaN(r6[0])) {
-                track_cavity(r6, nvx, nvy, k, phi);
+                ff(r6, nvx, nvy, k, betgam0, phiref);
             }
         }
 	}
@@ -58,12 +76,12 @@ static void CrabCavityPass(double *r_in, double le, double nvx, double nvy,
 	    double halflength = le/2.0;
 
         #pragma omp parallel for if (num_particles > OMP_PARTICLE_THRESHOLD) \
-            default(none) shared(r_in, num_particles, nvx, nvy, k, phi, halflength)
+            default(none) shared(r_in, num_particles, nvx, nvy, k, phiref, halflength)
         for (int c = 0; c<num_particles; c++) {
             double *r6 = r_in + c*6;
             if (!atIsNaN(r6[0])) {
                 drift6(r6, halflength);
-                track_cavity(r6, nvx, nvy, k, phi);
+                ff(r6, nvx, nvy, k, betgam0, phiref);
                 drift6(r6, halflength);
             }
         }
@@ -75,11 +93,13 @@ static void CrabCavityPass(double *r_in, double le, double nvx, double nvy,
 ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
 			      double *r_in, int num_particles, struct parameters *Param)
 {
-    double energy;
-    double lag, t0f, phiref;
+    double energy, beta0, gamma0, betgam0;
+    double t0f, lag, phiref, nvx, nvy;
+    long HarmNumber;
+    double SigPhi, SigVV;
 
     if (!Elem) {
-        double Length, *Voltages, Energy, Frequency, TimeLag, PhaseLag, SigPhi, SigVV;
+        double Length, *Voltages, Energy, Frequency, TimeLag, PhaseLag;
 		Length = atGetDouble(ElemData,"Length"); check_error();
 		Voltages = atGetDoubleArray(ElemData,"Voltages"); check_error();
 		Frequency = atGetDouble(ElemData,"Frequency"); check_error();
@@ -87,6 +107,7 @@ ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
 		Energy = atGetOptionalDouble(ElemData,"Energy",Param->energy); check_error();
         TimeLag=atGetOptionalDouble(ElemData,"TimeLag",0.0); check_error();
         PhaseLag=atGetOptionalDouble(ElemData,"PhaseLag",0.0); check_error();
+        HarmNumber=atGetOptionalLong(ElemData,"HarmNumber", lround(Frequency*Param->T0)); check_error();
 		SigPhi = atGetOptionalDouble(ElemData,"SigPhi",0.0); check_error();
 		SigVV = atGetOptionalDouble(ElemData,"SigVV",0.0); check_error();
 
@@ -101,12 +122,35 @@ ExportMode struct elem *trackFunction(const atElem *ElemData,struct elem *Elem,
         Elem->SigPhi=SigPhi;
         Elem->SigVV=SigVV;
     }
+    else {
+        SigPhi=Elem->SigPhi;
+        SigVV=Elem->SigVV;
+    }
     energy = atEnergy(Param->energy, Elem->Energy);
+    gamma0 = energy/Param->rest_energy;
+
+    if (isfinite(gamma0)) {
+        betgam0 = sqrt(gamma0*gamma0 - 1.0);
+        beta0 = betgam0 / gamma0;
+    }
+    else {
+        betgam0 = gamma0;
+        beta0 = 1.0;
+    }
     t0f = Elem->Frequency * Param->T0;
-    lag = TWOPI*Elem->Frequency*Elem->TimeLag/C0 + Elem->PhaseLag;
-    phiref = TWOPI * (t0f - round(t0f)) * Param->nturn - lag;
-    CrabCavityPass(r_in, Elem->Length, Elem->Vx/energy, Elem->Vy/energy,
-        Elem->Frequency, phiref, Elem->SigPhi, Elem->SigVV, Param->common_rng, num_particles);
+    lag = TWOPI*Elem->Frequency*Elem->TimeLag/beta0/C0 + Elem->PhaseLag;
+    phiref = TWOPI * (t0f - Elem->HarmNumber) * Param->nturn - lag;
+    nvx = Elem->Vx/energy/beta0/beta0;
+    nvy = Elem->Vy/energy/beta0/beta0;
+
+    if (SigPhi > 0.0)
+        phiref += atrandn_r(Param->common_rng, 0.0, SigPhi);
+    if (SigVV > 0.0) {
+        nvx *= atrandn_r(Param->common_rng, 1.0, SigVV);
+        nvy *= atrandn_r(Param->common_rng, 1.0, SigVV);
+    }
+
+    CrabCavityPass(r_in, Elem->Length, nvx, nvy, Elem->Frequency, betgam0, phiref, num_particles);
     return Elem;
 }
 
@@ -118,6 +162,7 @@ MODULE_DEF(CrabCavityPass)        /* Dummy module initialisation */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     if (nrhs >= 2) {
+        double betgam0, beta0, gamma0;
         double lag, phiref;
         double *r_in;
         double rest_energy = 0.0;
@@ -131,18 +176,26 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 		double Energy = atGetOptionalDouble(ElemData,"Energy",0.0); check_error();
 		double TimeLag = atGetOptionalDouble(ElemData,"TimeLag",0.0); check_error();
 		double PhaseLag = atGetOptionalDouble(ElemData,"PhaseLag",0.0); check_error();
-		double SigPhi = atGetOptionalDouble(ElemData,"SigPhi",0.0); check_error();
-		double SigVV = atGetOptionalDouble(ElemData,"SigVV",0.0); check_error();
         if (nrhs > 2) atProperties(prhs[2], &Energy, &rest_energy, &charge);
+        gamma0 = Energy/rest_energy;
 
         if (mxGetM(prhs[1]) != 6) mexErrMsgIdAndTxt("AT:WrongArg","Second argument must be a 6 x N matrix");
         /* ALLOCATE memory for the output array of the same size as the input  */
         plhs[0] = mxDuplicateArray(prhs[1]);
         r_in = mxGetDoubles(plhs[0]);
-        lag = TWOPI*Frequency*TimeLag/C0 + PhaseLag;
+        if (isfinite(gamma0)) {
+            betgam0 = sqrt(gamma0*gamma0 - 1.0);
+            beta0 = betgam0 / gamma0;
+        }
+        else {
+            betgam0 = gamma0;
+            beta0 = 1.0;
+        }
+        lag = TWOPI*Frequency*TimeLag/beta0/C0 + PhaseLag;
         phiref = -lag;
-	    CrabCavityPass(r_in, Length, Voltages[0]/Energy, Voltages[1]/Energy,
-	        Frequency, phiref, SigPhi, SigVV, &pcg32_global, num_particles);
+        normvx = Voltages[0] / Energy / beta0 / beta0;
+        normvy = Voltages[1] / Energy / beta0 / beta0;
+	    CrabCavityPass(r_in, Length, normvx, normvy, Frequency, betgam0, phiref, num_particles);
     }
     else if (nrhs == 0) {
 	    plhs[0] = mxCreateCellMatrix(3,1);
