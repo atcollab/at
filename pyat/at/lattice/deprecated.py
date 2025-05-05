3077cf0817de4edfe6a5f894a72863bfccb357b3
SebastienJoly <SebastienJoly@github.com>
2025-05-05 12:36:30 +0200

    Add old shift_elem() to deprecated.py

--- pyat/at/lattice/deprecated.py
+++ pyat/at/lattice/deprecated.py
@@ -168,6 +168,28 @@ def rotate_elem(elem: Element, tilt: float = 0.0, pitch: float = 0.0,
     elem.R2 = r2
     elem.T1 = t1+t10
     elem.T2 = t2+t20
+    
+def shift_elem(elem: Element, deltax: float = 0.0, deltaz: float = 0.0,
+               relative: bool = False) -> None:
+    r"""Sets the transverse displacement of an :py:class:`.Element`
+
+    The translation vectors are stored in the :pycode:`T1` and :pycode:`T2`
+    attributes.
+
+    Parameters:
+        elem:           Element to be shifted
+        deltax:         Horizontal displacement [m]
+        deltaz:         Vertical displacement [m]
+        relative:       If :py:obj:`True`, the translation is added to the
+          existing one
+    """
+    tr = numpy.array([deltax, 0.0, deltaz, 0.0, 0.0, 0.0])
+    if relative and hasattr(elem, 'T1') and hasattr(elem, 'T2'):
+        elem.T1 -= tr
+        elem.T2 += tr
+    else:
+        elem.T1 = -tr
+        elem.T2 = tr
 
 
 Lattice.uint32_refpts = get_uint32_index
