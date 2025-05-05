68e2e3534e89df55f544e2942467ba6dc88f5263
SebastienJoly <SebastienJoly@github.com>
2025-05-05 09:08:05 +0200

    New implementation of relative argument for shift and rotation

--- pyat/at/lattice/transformation.py
+++ pyat/at/lattice/transformation.py
@@ -18,7 +18,7 @@ def _rotation(rotations):
     beta: Rotation about the Y-axis (yaw).
     gamma: Rotation about the Z-axis (roll/tilt).
     """
-    alpha, beta, gamma = rotations # ZYX intrinsic rotations (pitch, yaw, tilt)
+    alpha, beta, gamma = rotations  # ZYX intrinsic rotations (pitch, yaw, tilt)
     R_x = np.array(
         [
             [1, 0, 0],
@@ -27,8 +27,8 @@ def _rotation(rotations):
         ]
     )
     R_y = np.array(
-        [[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], [-np.sin(beta), 0,
-                                                      np.cos(beta)]]
+        [[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], 
+         [-np.sin(beta), 0, np.cos(beta)]]
     )
     R_z = np.array(
         [
@@ -54,7 +54,6 @@ def _translation_vector(ld, r3d, offsets, X_axis, Y_axis):
     offsets: 3D offsets [m]
     X_axis: X unit axis in rotated frame expressed in the xyz coordinate system
     Y_axis: Y unit axis in rotated frame expressed in the xyz coordinate system
-
     """
     tD0 = np.array([-offsets @ X_axis, 0, -offsets @ Y_axis, 0, 0, 0])
     T0 = np.array(
@@ -68,6 +67,29 @@ def _translation_vector(ld, r3d, offsets, X_axis, Y_axis):
         ]
     )
     return T0 + tD0
+
+
+def _offsets_from_translation_vector(T, ld, r3d, X_axis, Y_axis, Z_axis):
+    """
+    Retrieve the 3D offsets from the T1 translation vector.
+    T: Translation vector (T1)
+    ld: Longitudinal displacement [m]
+    r3d: 3D rotation matrix
+    """
+    T0 = np.array(
+        [
+            ld * r3d[2, 0] / r3d[2, 2],
+            r3d[2, 0],
+            ld * r3d[2, 1] / r3d[2, 2],
+            r3d[2, 1],
+            0,
+            ld / r3d[2, 2],
+        ]
+    )
+    tD0 = T - T0
+    offsets = -tD0[0] * X_axis - tD0[2] * Y_axis + (ld + tD0[5]) * Z_axis
+
+    return offsets
 
 
 def _r_matrix(ld, r3d):
@@ -135,36 +157,26 @@ def _r_matrix(ld, r3d):
         ]
     )
 
+
 def _ld_and_r3d_from_r_matrix(r_matrix):
     """
     Extracts the longitudinal displacement (ld) and 3D rotation matrix (r3d)
-    from the rotation matrix operator (`_r_matrix(ld, r3d)`).
-    
-    Args:
-        r_matrix (np.ndarray): 6x6 rotation matrix operator (R1 or R2).
-        
-    Returns:
-        ld (float): Longitudinal displacement.
-        r3d (np.ndarray): 3x3 rotation matrix.
+    from the R1 rotation matrix operator.
+    r_matrix: 6x6 rotation matrix operator (R1).
     """
-    
     r3d = np.eye(3)
-    
     r3d[0, 0] = r_matrix[1, 1]
     r3d[1, 0] = r_matrix[1, 3]
     r3d[2, 0] = r_matrix[1, 4]
-    
     r3d[0, 1] = r_matrix[3, 1]
     r3d[1, 1] = r_matrix[3, 3]
     r3d[2, 1] = r_matrix[3, 4]
-    
     r3d[0, 2] = -r_matrix[5, 0] * r_matrix[1, 1] / r_matrix[2, 2]
     r3d[1, 2] = -r_matrix[5, 2] * r_matrix[1, 1] / r_matrix[2, 2]
     r3d[2, 2] = r_matrix[1, 1] / r_matrix[2, 2]
-    
-    ld = r_matrix[2, 1] * r_matrix[1, 3] / r_matrix[2, 0]
-    
+    ld = r_matrix[0, 1] * r_matrix[3, 3] / r_matrix[0, 0]
     return ld, r3d
+
 
 class ReferencePoint(Enum):
     """Enum class for reference option"""
@@ -233,8 +245,8 @@ def transform_elem(
     elem_bending_angle = getattr(elem, "BendingAngle", 0)
 
     # Rotated transfer matrix linked to a bending angle
-    RB = _rotation([0, -elem_bending_angle, 0]) # Eq. (12)
-    RB_half = _rotation([0, -elem_bending_angle / 2, 0]) # Eq. (12)
+    RB = _rotation([0, -elem_bending_angle, 0])  # Eq. (12)
+    RB_half = _rotation([0, -elem_bending_angle / 2, 0])  # Eq. (12)
 
     # Define transverse offsets (element translation)
     offsets = np.array([dx, dy, dz])
@@ -244,13 +256,38 @@ def transform_elem(
     z_axis = np.array([0, 0, 1])
 
     # Extract current transformations if relative=True
+    offsets0 = np.array([0.0, 0.0, 0.0])
     tilt0, pitch0, yaw0 = 0.0, 0.0, 0.0
     if relative:
-        if hasattr(elem, "_r3d"):
+        if (
+            hasattr(elem, "R1")
+            and hasattr(elem, "R2")
+            and hasattr(elem, "T1")
+            and hasattr(elem, "T2")
+        ):
+            ld, r3d_tmp = _ld_and_r3d_from_r_matrix(elem.R1)
             if reference is ReferencePoint.CENTRE:
-                r3d = RB_half.T @ elem._r3d @ RB_half
+                r3d = RB_half.T @ r3d_tmp @ RB_half
+
+                X_axis = RB_half.T @ x_axis
+                Y_axis = RB_half.T @ y_axis
+                Z_axis = RB_half.T @ z_axis
+
+                offsets0 = _offsets_from_translation_vector(
+                    elem.T1, ld, r3d, X_axis, Y_axis, Z_axis
+                )
+
             elif reference is ReferencePoint.ENTRANCE:
-                r3d = elem._r3d
+                r3d = r3d_tmp
+
+                X_axis = r3d @ x_axis
+                Y_axis = r3d @ y_axis
+                Z_axis = r3d @ z_axis
+
+                offsets0 = _offsets_from_translation_vector(
+                    elem.T1, ld, r3d, X_axis, Y_axis, Z_axis
+                )
+
             else:
                 raise ValueError(
                     "Unsupported reference, please choose either "
@@ -263,32 +300,41 @@ def transform_elem(
             yaw0 = np.arcsin(r3d[0, 2])
             pitch0 = np.arctan2(-r3d[1, 2], r3d[2, 2])
 
-    # Apply new rotations (XYZ intrinsic order)
+        else:
+            print(
+                "Cannot apply a relative transformation without a prior"
+                "transformation, assume an absolute transformation instead."
+            )
+
+    # Apply new offsets and rotations (XYZ intrinsic order)
+    offsets_total = offsets + offsets0
     tilt_total = tilt0 + tilt
     pitch_total = pitch0 + pitch
     yaw_total = yaw0 + yaw
-    rotations = [pitch_total, yaw_total, tilt_total] # X, Y, Z convention
+    rotations = [pitch_total, yaw_total, tilt_total]  # X, Y, Z convention
 
     if reference is ReferencePoint.CENTRE:
         # Compute entrance rotation matrix in the rotated frame
-        r3d_entrance = RB_half @ _rotation(rotations) @ RB_half.T # Eq. (31)
+        r3d_entrance = RB_half @ _rotation(rotations) @ RB_half.T  # Eq. (31)
 
         if elem_bending_angle:
             Rc = elem_length / elem_bending_angle
             OO0 = Rc * np.sin(elem_bending_angle / 2) * \
-                  RB_half @ z_axis # Eq. (34)
-            P0P = -Rc * np.sin(elem_bending_angle / 2) * \
-                  r3d_entrance @ RB_half @ z_axis # Eq. (36)
+                RB_half @ z_axis  # Eq. (34)
+            P0P = (
+                -Rc * np.sin(elem_bending_angle / 2) * \
+                r3d_entrance @ RB_half @ z_axis
+            )  # Eq. (36)
         else:
-            OO0 = elem_length / 2 * z_axis # Eq. (34)
-            P0P = -elem_length / 2 * r3d_entrance @ z_axis # Eq. (36)
+            OO0 = elem_length / 2 * z_axis  # Eq. (34)
+            P0P = -elem_length / 2 * r3d_entrance @ z_axis  # Eq. (36)
 
         # Transform offset to magnet entrance
-        OP = OO0 + P0P + RB_half @ offsets # Eq. (33)
+        OP = OO0 + P0P + RB_half @ offsets_total  # Eq. (33)
 
     elif reference is ReferencePoint.ENTRANCE:
-        r3d_entrance = _rotation(rotations) # Eq. (3)
-        OP = offsets # Eq. (2)
+        r3d_entrance = _rotation(rotations)  # Eq. (3)
+        OP = offsets_total  # Eq. (2)
     else:
         raise ValueError(
             "Unsupported reference, please choose either "
@@ -302,7 +348,7 @@ def transform_elem(
     Y_axis = r3d_entrance @ y_axis
     Z_axis = r3d_entrance @ z_axis
 
-    ld_entrance = Z_axis @ OP # Eq. (33)
+    ld_entrance = Z_axis @ OP  # Eq. (33)
 
     R1 = _r_matrix(ld_entrance, r3d_entrance)
     T1 = np.linalg.inv(R1) @ _translation_vector(
@@ -315,7 +361,7 @@ def transform_elem(
     Y_axis = RB @ y_axis
     Z_axis = RB @ z_axis
 
-    r3d_exit = RB.T @ r3d_entrance.T @ RB # Eq. (18) or (32)
+    r3d_exit = RB.T @ r3d_entrance.T @ RB  # Eq. (18) or (32)
 
     if elem_bending_angle:
         Rc = elem_length / elem_bending_angle
@@ -325,14 +371,14 @@ def transform_elem(
                 0,
                 elem_length * np.sin(elem_bending_angle) / elem_bending_angle,
             ]
-        ) # Eq. (24)
+        )  # Eq. (24)
     else:
-        OPp = elem_length * z_axis # Eq. (24)
+        OPp = elem_length * z_axis  # Eq. (24)
 
-    OOp = r3d_entrance @ OPp + OP # Eq. (25)
+    OOp = r3d_entrance @ OPp + OP  # Eq. (25)
     OpPp = OPp - OOp
 
-    ld_exit = Z_axis @ OpPp # Eq. (23) or (37)
+    ld_exit = Z_axis @ OpPp  # Eq. (23) or (37)
 
     R2 = _r_matrix(ld_exit, r3d_exit)
     T2 = _translation_vector(ld_exit, r3d_exit, OpPp, X_axis, Y_axis)
