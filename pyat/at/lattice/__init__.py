b50e62417754a574fe0c126993053bd25877d4ab
Laurent Farvacque <laurent.farvacque@esrf.fr>
2025-05-11 14:52:20 +0200

    parameters

--- pyat/at/lattice/__init__.py
+++ pyat/at/lattice/__init__.py
@@ -15,8 +15,7 @@ from .elements import *
 from .rectangular_bend import *
 from .idtable_element import InsertionDeviceKickMap
 from .utils import *
-# from .element_parameterise import *
-from .el_parameterise import *
+from .element_parameterise import *
 from .lattice_object import *
 from .lattice_parameterise import *
 # from .lattice_variables import *
