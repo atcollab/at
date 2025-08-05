05cff7f5ca3b32e07d9c8914673b53d3466f3fcd
oscarxblanco <oblanco@cells.es>
2025-08-05 17:37:45 +0200

    fix h11002

--- pyat/at/physics/rdt.py
+++ pyat/at/physics/rdt.py
@@ -421,7 +421,7 @@ def _computedrivingterms(
             )
             rdts2["h11002"] += ( 1.0 / 16 ) * np.sum([
                 + 1 * bxbx[i] * ( ( b2b2lm[i] - 2 * b3b2lm[i] * etaxm[i] + 4 * b3b3lm[i] * etaxm[i] * etaxm )
-                                    * ( pxm2[i] * pxm2 ) + 2 * b3b2lm[i] * etaxm[i] * ( cpxm2[i] * pxm2 ) )
+                                    * ( pxm2[i] * cpxm2 ) + 2 * b3b2lm[i] * etaxm[i] * ( cpxm2[i] * pxm2 ) )
                 + 2 * rbxbx3o2etax[i] * ( b3b3lm[i] * etaxm[i] - b2b3lm[i] ) * ( pxm[i] * cpxm - cpxm[i] * pxm )
                 for i in range(nelem)]
             )
