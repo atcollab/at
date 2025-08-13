4f9a5dd4599ee91472315dee09c583b3201ca9dc
oscarxblanco <oblanco@cells.es>
2025-08-13 18:50:20 +0200

    modify example

--- atmat/atdemos/IDModeling/trackWithU35KickMap.m
+++ atmat/atdemos/IDModeling/trackWithU35KickMap.m
@@ -1,15 +1,17 @@
 %read in ESRF lattice
 r=atradoff(esrf);
 
-%create element from kick map and plot kick map
-U35elem=atidtable_dat('U35',1,'U35.mat',6.04,'IdTablePass');
+% create element from kick map and plot kick map.
+% sort and transpose are needed for U35.mat
+U35elem=atidtable_dat('U35',1,'U35.mat',6.04,'IdTablePass', ...
+                      'sort',1,'transpose',1);
 xtable=U35elem.xtable;
 ytable=U35elem.ytable;
 xkick2=U35elem.xkick;
 ykick2=U35elem.ykick;
-surf(xtable,ytable,xkick2')
+surf(xtable,ytable,xkick2)
 figure
-surf(xtable,ytable,ykick2')
+surf(xtable,ytable,ykick2)
 
 %add kick map to lattice
 esrf_U35 = add_ID(r,U35elem);
@@ -19,4 +21,5 @@ esrf_U35 = add_ID(r,U35elem);
 [pU35,tU35]=atlinopt(esrf_U35,0,1);
 
 %write out tune change due to undulator
-t0-tU35
+deltatune = t0-tU35;
+fprintf("delta_tune = %.3g, %.3g\n",deltatune(1),deltatune(2));
