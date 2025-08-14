af74614408b48f96be21b35133fc89ce8c85588b
oscarxblanco <oblanco@cells.es>
2025-08-14 14:58:24 +0200

    flake8

--- pyat/test/test_load_save.py
+++ pyat/test/test_load_save.py
@@ -30,12 +30,12 @@ def simple_hmba(hmba_lattice):
             del mult.FringeQuadExit
     # Replace Multipoles by Octupoles
     for id in ring.get_uint32_index("OF*"):
-        q = ring[id]
-        ring[id] = elt.Octupole(q.FamName, q.Length, q.PolynomA, q.PolynomB)
+        _q = ring[id]
+        ring[id] = elt.Octupole(_q.FamName, _q.Length, _q.PolynomA, _q.PolynomB)
     # Disable useless e;ements
     for id in ring.get_uint32_index("SH*"):
-        q = ring[id]
-        ring[id] = elt.Drift(q.FamName, q.Length)
+        _q = ring[id]
+        ring[id] = elt.Drift(_q.FamName, _q.Length)
     return ring
 
 
@@ -52,7 +52,7 @@ def simple_hmba(hmba_lattice):
         (".lte", {"use": "ring"}),
     ),
 )
-def test_m(request, lattice, suffix, options):
+def test_m(request, lattice, suffix, options) -> None:
     ring0 = request.getfixturevalue(lattice)
     fname = mkstemp(suffix=suffix)[1]
 
@@ -63,8 +63,8 @@ def test_m(request, lattice, suffix, options):
     ring1 = Lattice.load(fname, **options)
 
     # Check that we get back the original lattice
-    el1, rg1, _ = ring0.linopt6()
-    el2, rg2, _ = ring1.linopt6()
+    _, rg1, _ = ring0.linopt6()
+    _, rg2, _ = ring1.linopt6()
     assert_allclose(rg1.tune, rg2.tune, atol=1.0e-12)
     assert_allclose(rg1.chromaticity, rg2.chromaticity, atol=1.0e-12)
 
