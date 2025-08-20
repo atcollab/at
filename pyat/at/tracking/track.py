4057b4792675fe98fac62a1283d371cac10c1c83
PONS <pons@esrf.fr>
2025-08-20 10:14:27 +0200

    Updated doc

--- pyat/at/tracking/track.py
+++ pyat/at/tracking/track.py
@@ -233,6 +233,12 @@ def lattice_track(
           calculation or to solve Runtime Errors, however it is considered
           unsafe. Used only when *use_mp* is :py:obj:`True`. It can be globally
           set using the variable *at.lattice.DConstant.patpass_startmethod*
+        start_elem (int):       Start tracking at the specified element. The
+          behavior is different from :py:func:`..lattice.rotate`, refpts are
+          still indexed from the begininig of the ring. Default 0.
+        end_elem (int):         End tracking at the specified element (exclusive).
+          When end_elem is different from the number of lattice elements,
+          :pycode:`nturns=1` is required. Default number of elements.
 
     The following keyword arguments overload the lattice values
 
