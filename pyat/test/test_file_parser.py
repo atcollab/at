e4e0095f318cf5d5b4e3473d574173793fc3edf0
Laurent Farvacque <laurent.farvacque@esrf.fr>
2025-10-04 18:56:18 +0200

    Argument parsing

--- pyat/test/test_file_parser.py
+++ pyat/test/test_file_parser.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose as assert_close
 
-from at import UnorderedParser, MadxParser, Mad8Parser, ElegantParser, set_argparser
+from at import UnorderedParser, MadxParser, Mad8Parser, ElegantParser
 
 __all__ = []
 
@@ -173,30 +173,34 @@ CELL.1: LINE=(DR1, Q1.F, DR2, Q1.D, DR3)
 RING.1: LINE=(CELL.1, DR1, SOL1, DR4, MULT1, &
               HK:1, VK:1, HVK2, DR4, BPM1, DR4, BPM2, CELL.1)
 """
+
+
+class _Command1:
+    "Test command checking its arguments versus the database"
+
+    @staticmethod
+    def argparser(parser, argcount, argstr):
+        return parser._argparser(
+            argcount, argstr, bool_attr={"flag1", "FLAG2"}, str_attr={"title"}
+        )
+
+    def __call__(self, **kwargs):
+        print("command1")
+        assert kwargs["title"] == "test parser"
+        assert kwargs["flag1"] is True
+        assert kwargs["FLAG2"] is False
+        assert kwargs["arg1"] == 42.0
+        assert kwargs["ARG2"] == 12.0
+        assert kwargs["ARG3"] is True
+        return "done"
+
+
 # Global definitions for BaseParser
-
-true = True
-false = False
-
-
-def _command1_parser(parser, argcount, argstr):
-    return parser._argparser(
-        argcount, argstr, bool_attr={"flag1", "FLAG2"}, str_attr={"title"}
-    )
-
-
-@set_argparser(_command1_parser)
-def command1(**kwargs):
-    """Sample command testing that arguments are as expected"""
-    assert kwargs["title"] == "test parser"
-    assert kwargs["flag1"] is True
-    assert kwargs["FLAG2"] is False
-    assert kwargs["arg1"] == 42.0
-    assert kwargs["ARG2"] == 12.0
-    assert kwargs["ARG3"] is True
-    return "done"
-
-
+_parser_env = {
+    "sqrt": sqrt,
+    "command1": _Command1(),
+    "__builtins__": {},
+}
 # End of global definitions
 
 
@@ -211,7 +215,7 @@ def test_unordered_parser(cont, delimit, linecomm, data: str):
         _continuation = cont
         _delimiter = delimit
 
-    parser = _Parser(globals())
+    parser = _Parser(_parser_env, verbose=True)
     parser.parse_lines(data.splitlines())
     assert parser["label"] == "done"  # Make sure that "command1" was executed
 
