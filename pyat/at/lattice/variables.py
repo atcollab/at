802a7fe4d390a7308aa7f943915f6add6457a120
Laurent Farvacque <laurent.farvacque@esrf.fr>
2025-10-17 13:31:05 +0200

    small improvements

--- pyat/at/lattice/variables.py
+++ pyat/at/lattice/variables.py
@@ -84,8 +84,8 @@ keeping their sum constant:
 from __future__ import annotations
 
 __all__ = [
-    "VariableBase",
     "CustomVariable",
+    "VariableBase",
     "VariableList",
 ]
 
@@ -133,7 +133,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
             bounds:     Lower and upper bounds of the variable value
             delta:      Initial variation step
             history_length: Maximum length of the history buffer. :py:obj:`None`
-              means infinite
+              means infinite.
 
         Keyword Args:
             **kwargs:    Keyword arguments passed to the _setfun and _getfun methods
@@ -156,43 +156,46 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
 
     @classmethod
     def _generate_name(cls, name: str) -> str:
-        """Generate unique name for variable"""
+        """Generate unique name for variable."""
         cls._counter += 1
         return name if name else f"{cls._COUNTER_PREFIX}{cls._counter}"
 
     @property
     def bounds(self) -> tuple[float, float]:
-        """Lower and upper bounds of the variable"""
+        """Lower and upper bounds of the variable."""
         vmin, vmax = self._bounds
         return -np.inf if vmin is None else vmin, np.inf if vmax is None else vmax
 
     def check_bounds(self, value: Number) -> None:
-        """Check that a value is within the variable bounds
+        """Check that a value is within the variable bounds.
 
         Raises:
             ValueError: If the value is not within bounds
         """
         min_val, max_val = self._bounds
         if min_val is not None and value < min_val:
-            raise ValueError(f"Value {value} must be larger or equal to {min_val}")
+            msg = f"Value {value} must be larger or equal to {min_val}"
+            raise ValueError(msg)
         if max_val is not None and value > max_val:
-            raise ValueError(f"Value {value} must be smaller or equal to {max_val}")
+            msg = f"Value {value} must be smaller or equal to {max_val}"
+            raise ValueError(msg)
 
     def _setfun(self, value: Number, *args, **kwargs) -> None:
         classname = self.__class__.__name__
-        raise TypeError(f"{classname!r} is read-only")
+        msg = f"{classname!r} is read-only"
+        raise TypeError(msg)
 
     @abc.abstractmethod
     def _getfun(self, *args, **kwargs) -> Number: ...
 
     @property
     def history(self) -> list[Number]:
-        """History of the values of the variable"""
+        """History of the values of the variable."""
         return list(self._history)
 
     @property
     def initial_value(self) -> Number:
-        """Initial value of the variable
+        """Initial value of the variable.
 
         Raises:
             IndexError: If no initial value has been set yet
@@ -200,11 +203,12 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
         if not np.isnan(self._initial):
             return self._initial
         else:
-            raise IndexError(f"{self.name}: No initial value has been set yet")
+            msg = f"{self.name}: No initial value has been set yet"
+            raise IndexError(msg)
 
     @property
     def last_value(self) -> Number:
-        """Last value of the variable
+        """Last value of the variable.
 
         Raises:
             IndexError: If no value has been set yet
@@ -217,7 +221,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
 
     @property
     def previous_value(self) -> Number:
-        """Value before the last one
+        """Value before the last one.
 
         Raises:
             IndexError: If there are fewer than 2 values in the history
@@ -229,7 +233,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
             raise
 
     def set(self, value: Number, **setkw) -> None:
-        """Set the variable value
+        """Set the variable value.
 
         Args:
             value:      New value to be applied on the variable
@@ -251,7 +255,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
     def get(
         self, *, initial: bool = False, check_bounds: bool = False, **getkw
     ) -> Number:
-        """Get the actual variable value
+        """Get the actual variable value.
 
         Args:
             initial:    If :py:obj:`True`, clear the history and set the variable
@@ -288,7 +292,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
             return np.nan
 
     def set_previous(self, **setkw) -> None:
-        """Reset to the value before the last one
+        """Reset to the value before the last one.
 
         Keyword Args:
             ring:       Depending on the variable type, a :py:class:`.Lattice` argument
@@ -304,10 +308,11 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
             previous_value = self._history.pop()  # retrieve the previous value
             self.set(previous_value, **setkw)
         else:
-            raise IndexError(f"{self.name}: history too short (need at least 2 values)")
+            msg = f"{self.name}: history too short (need at least 2 values)"
+            raise IndexError(msg)
 
     def reset(self, **setkw) -> None:
-        """Reset to the initial value and clear the history buffer
+        """Reset to the initial value and clear the history buffer.
 
         Keyword Args:
             ring:       Depending on the variable type, a :py:class:`.Lattice` argument
@@ -323,12 +328,11 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
             self._history = deque([], self.history_length)
             self.set(initial_value, **setkw)
         else:
-            raise IndexError(
-                f"Cannot reset {self.name}: No initial value has been set yet"
-            )
+            msg = f"Cannot reset {self.name}: No initial value has been set yet"
+            raise IndexError(msg)
 
     def increment(self, incr: Number, **setkw) -> None:
-        """Increment the variable value
+        """Increment the variable value.
 
         Args:
             incr:   Increment value
@@ -349,7 +353,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
         self.set(current_value + incr, **setkw)
 
     def _step(self, step: Number, **setkw) -> None:
-        """Apply a step relative to the initial value"""
+        """Apply a step relative to the initial value."""
         try:
             initial_value = self.initial_value
         except IndexError:
@@ -360,7 +364,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
         self.set(initial_value + step, **setkw)
 
     def step_up(self, **setkw) -> None:
-        """Set to initial_value + delta
+        """Set to initial_value + delta.
 
         Keyword Args:
             ring:       Depending on the variable type, a :py:class:`.Lattice` argument
@@ -371,7 +375,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
         self._step(self.delta, **setkw)
 
     def step_down(self, **setkw) -> None:
-        """Set to initial_value - delta
+        """Set to initial_value - delta.
 
         Keyword Args:
             ring:       Depending on the variable type, a :py:class:`.Lattice` argument
@@ -393,7 +397,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
         return f"{self.name:>12s}{vini: 16e}{vnow: 16e}{vnow - vini: 16e}"
 
     def status(self):
-        """Return a string describing the current status of the variable
+        """Return a string describing the current status of the variable.
 
         Returns:
             status: Variable description
@@ -405,7 +409,7 @@ class VariableBase(Operand, Generic[Number], abc.ABC):
 
 
 class CustomVariable(VariableBase[Number]):
-    r"""A Variable with user-defined get and set functions
+    r"""A Variable with user-defined get and set functions.
 
     This is a convenience function allowing user-defined *get* and *set*
     functions. But subclassing :py:class:`.Variable` should always be preferred
@@ -467,7 +471,7 @@ class CustomVariable(VariableBase[Number]):
 
 
 class VariableList(list):
-    """Container for Variable objects
+    """Container for Variable objects.
 
     :py:class:`VariableList` supports all :py:class:`list` methods, like
     appending, insertion or concatenation with the "+" operator.
@@ -480,7 +484,7 @@ class VariableList(list):
             return super().__getitem__(index)
 
     def get(self, ring=None, **kwargs) -> Sequence[float]:
-        r"""Get the current values of Variables
+        r"""Get the current values of Variables.
 
         Args:
             ring:   Depending on the variable type, a :py:class:`.Lattice` argument
@@ -498,7 +502,7 @@ class VariableList(list):
         return np.array([var.get(ring=ring, **kwargs) for var in self])
 
     def set(self, values: Iterable[float], ring=None) -> None:
-        r"""Set the values of Variables
+        r"""Set the values of Variables.
 
         Args:
             values:     Iterable of values
@@ -509,7 +513,7 @@ class VariableList(list):
             var.set(val, ring=ring)
 
     def increment(self, increment: Iterable[float], ring=None) -> None:
-        r"""Increment the values of Variables
+        r"""Increment the values of Variables.
 
         Args:
             increment:  Iterable of values
@@ -531,7 +535,7 @@ class VariableList(list):
 
     # noinspection PyProtectedMember
     def status(self, **kwargs) -> str:
-        """String description of the variables"""
+        """String description of the variables."""
         values = "\n".join(var._line(**kwargs) for var in self)
         return "\n".join((VariableBase._header(), values))
 
@@ -540,7 +544,7 @@ class VariableList(list):
 
     @property
     def deltas(self) -> npt.NDArray[Number]:
-        """delta values of the variables"""
+        """delta values of the variables."""
         return np.array([var.delta for var in self])
 
     @deltas.setter
--- pyat/at/load/__init__.py
+++ pyat/at/load/__init__.py
@@ -3,14 +3,14 @@ Import/export AT lattice from/to different formats:
 - .mat files
 - .m files
 """
-from .rpn import *  # noqa: F401, F403
-from .allfiles import *  # noqa: F401, F403
-from .matfile import *  # noqa: F401, F403
-from .reprfile import *  # noqa: F401, F403
-from .tracy import *  # noqa: F401, F403
-from .elegant import *  # noqa: F401, F403
-from .json import *  # noqa: F401, F403
-from .file_input import *  # noqa: F401, F403
+from .rpn import *
+from .allfiles import *
+from .matfile import *
+from .reprfile import *
+from .tracy import *
+from .elegant import *
+from .json import *
+from .file_input import *
 from .file_output import *
-from .madx import *  # noqa: F401, F403
-from .mad8 import *  # noqa: F401, F403
+from .madx import *
+from .mad8 import *
