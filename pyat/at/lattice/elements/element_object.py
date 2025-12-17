13ce5d55451082ca646090510c30bac67e1823be
swhite <swhite@esrf.fr>
2025-12-17 10:09:44 +0100

    file save load

--- pyat/at/lattice/elements/element_object.py
+++ pyat/at/lattice/elements/element_object.py
@@ -44,6 +44,14 @@ class Element:
     _entrance_fields = ["T1", "R1"]
     _exit_fields = ["T2", "R2"]
     _no_swap = _entrance_fields + _exit_fields
+    _drop_attr = ["T1", "T2", "R1", "R2"]
+    _convert_attr = {"_dx": "dx",
+                     "_dy": "dy",
+                     "_dz": "dz",
+                     "_pitch": "pitch",
+                     "_yaw": "yaw",
+                     "_tilt": "tilt",
+                     }
 
     def __init__(self, family_name: str, **kwargs):
         """
