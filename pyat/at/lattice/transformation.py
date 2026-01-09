e177f8d5e7f8ae0a0dda6147b6170ab706c292b6
swhite2401 <simon.white@esrf.fr>
2026-01-09 09:53:59 +0100

    raise error if reference point is changed in relative mode

--- pyat/at/lattice/transformation.py
+++ pyat/at/lattice/transformation.py
@@ -254,7 +254,7 @@ def transform_elem(
             return ini if val is None else ini + val
         if reference != elem.ReferencePoint:
             msg = "Reference point changed not allowed for relative transformations"
-            raise AtError()
+            raise AtError(msg)
     else:
         def _set(ini, val):
             return ini if val is None else val  
