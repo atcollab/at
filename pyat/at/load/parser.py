e0f9d4b9296662ad927548bbdf8f83faa450d3bf
Laurent Farvacque <laurent.farvacque@esrf.fr>
2026-01-09 17:24:12 +0100

    lattice_object

--- pyat/at/load/parser.py
+++ pyat/at/load/parser.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["StrParameter", "StrParser"]
+__all__ = ["MadParameter", "StrParameter", "StrParser"]
 
 import abc
 from collections.abc import Callable
@@ -162,3 +162,87 @@ class StrParameter(ParamDef):
 
     def fast_value(self) -> Any:
         return self._conversion(self.parser.evaluate(self.expr))
+
+
+class MadParameter:
+    """MAD parameter.
+
+    A MAD parameter is an expression which can be evaluated n the context
+    of a MAD parser
+    """
+
+    def __init__(self, parser: StrParser, expr: str):
+        """Initialise a string parameter.
+
+        Args:
+            parser: StrParser instance defining the context for evaluation
+            expr: Expression to be evaluated
+            conversion: Function to convert the evaluated result to the appropriate type
+
+        The expression may contain parameter names, arithmetic operators and
+        mathematical functions, depending on the capabilities of the parser.
+        """
+        self.expr = expr
+        self.parser = parser
+
+    @classmethod
+    def parameter(cls, parser, expr: str):
+        try:
+            val = parser._check_constant(expr)
+        except NameError:
+            return cls(parser, expr)
+        else:
+            return val
+
+    @property
+    def value(self) -> Any:
+        """Current value of the parameter."""
+        return self.parser.evaluate(self.expr)
+
+    def __str__(self):
+        return self.expr
+
+    def __repr__(self):
+        return repr(self.value)
+
+    def __float__(self):
+        return float(self.value)
+
+    def __int__(self):
+        return int(self.value)
+
+    def __add__(self, other):
+        return float(self) + float(other)
+
+    def __radd__(self, other):
+        return float(other) + float(self)
+
+    def __mul__(self, other):
+        return float(self) * float(other)
+
+    def __rmul__(self, other):
+        return float(other) * float(self)
+
+    def __sub__(self, other):
+        return float(self) - float(other)
+
+    def __rsub__(self, other):
+        return float(other) - float(self)
+
+    def __truediv__(self, other):
+        return float(self) / float(other)
+
+    def __rtruediv__(self, other):
+        return float(other) / float(self)
+
+    def __pow__(self, other):
+        return pow(float(self), other)
+
+    def __rpow__(self, other):
+        return pow(float(other), float(self))
+
+    def __neg__(self):
+        return -float(self)
+
+    def __pos__(self):
+        return +float(self)
