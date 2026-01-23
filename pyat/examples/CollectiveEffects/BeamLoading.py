0c4a00cc656a974b2137a8b373da23ff98dfafb9
lcarver <lee.carver@esrf.fr>
2026-01-23 15:20:59 +0100

    Upgrade and major bug fix to the beam loading module (#1025)
    * Initial stashing to implement mbtrack1 feedback loop into pyat to check uniform bug
    
    * Stash
    
    * stash
    
    * stash
    
    * Attained first working version
    
    * Remove BLMode from tests
    
    * Fix dirty fill pattern computation
    
    * seg fault?
    
    * no
    
    * Remove some hard coded thigns
    
    * test M
    
    * Change mean of phase and amp to mean of real and imaginary to avoid issues
    
    * Change rad on to enable 6d
    
    * Add harmonic_number and fillpattern to ringparams
    
    * whitespace
    
    * Fix mex
    
    * update example and clean up
    
    * modernise
    
    * update test_track_beamloading
    
    * test
    
    * Update documentation
    
    * Update test for windows failure
    
    * Add check_error
    
    * update tests
    
    * green tick
    
    * Fix formatting of doc
    
    * Include ts in beam voltage calulation
    
    * simplrrad pass modifs
    
    * Clean up phase 1
    
    * Test update needed as timelag behaviour modified
    
    * Add example file for Passive Cavity with Feedback
    
    * documentation
    
    * Update beamloading example file
    
    * black
    
    * bug fix for TBL. Include bunch_spos[0] in dt computation for islice=0
    
    * Change M to ring_harmn
    
    * run black on beam_loading.py
    
    * add docstrong for FeedbackMode
    
    * numpy to np
    
    * change fillpattern to (double[1]){1.0}
    
    * doc
    
    * ruff suggestion to remove unused variable. Not bad!
    
    * ruff check
    
    * missed one
    
    * Do all ruff commands
    
    * fix typo
    
    * docs
    
    ---------
    
    Co-authored-by: Lee Carver <carver@cluster-access-03.esrf.fr>
    Co-authored-by: Lee Carver <carver@cluster-access-02.esrf.fr>

--- pyat/examples/CollectiveEffects/BeamLoading.py
+++ pyat/examples/CollectiveEffects/BeamLoading.py
@@ -2,47 +2,49 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmap
 import at
-from at.collective import BeamLoadingElement, add_beamloading, BLMode
+from at.collective import BeamLoadingElement, add_beamloading
+from at.collective import FeedbackMode as FBMode
 from mpi4py import MPI
 from at.tracking.utils import get_bunches_std_mean
 from at.constants import qe
 from at.physics import harmonic_analysis
 
 
-def analytical_qs(ring, I):
+def analytical_qs(ring, I, Qfactor, Rshunt, beta):
 
     E0 = ring.energy * qe
-    alpha_c = at.get_mcf(ring.radiation_off(copy=True))
+    alpha_c = at.get_mcf(ring.disable_6d(copy=True))
     omega_rf = 2 * numpy.pi * ring.get_rf_frequency()
     Vc = ring.get_rf_voltage()
 
-    envel = at.envelope_parameters(ring.radiation_on(copy=True))
+    envel = at.envelope_parameters(ring.enable_6d(copy=True))
     U0 = envel.U0 * qe
 
-    Q0 = bl_elem.Qfactor * (1 + beta)
-    Rsh = bl_elem.Rshunt * (1 + beta)  # Difference in definition
+    Q0 = Qfactor * (1 + beta)
+    Rsh = Rshunt * (1 + beta)  # Difference in definition
 
     phi_s = numpy.pi - numpy.arcsin(U0 / (qe * Vc))  # sync. phase in radian
-    psi = numpy.arctan(-2 * Rsh * I * numpy.cos(numpy.pi - phi_s) /
-                         (Vc * (1 + beta)))  # tuning angle
-                         
-    omega_res = (omega_rf /
-                 (1 - 2 * Rsh * I * (numpy.cos(numpy.pi - phi_s)) /
-                  (2 * Q0 * Vc)))  # resonant freq.
+    psi = numpy.arctan(
+        -2 * Rsh * I * numpy.cos(numpy.pi - phi_s) / (Vc * (1 + beta))
+    )  # tuning angle
+
+    omega_res = omega_rf / (
+        1 - 2 * Rsh * I * (numpy.cos(numpy.pi - phi_s)) / (2 * Q0 * Vc)
+    )  # resonant freq.
 
     h = ring.harmonic_number  # harmonic number
     omega0 = omega_rf / h  # rev. freq.
     T0 = 2 * numpy.pi / omega0  # rev. time
 
-    omega_s0 = numpy.sqrt(qe * Vc * omega_rf * alpha_c * numpy.cos(numpy.pi - phi_s) /
-                          (E0 * T0))  # Synch. freq. for a single particle
+    omega_s0 = numpy.sqrt(
+        qe * Vc * omega_rf * alpha_c * numpy.cos(numpy.pi - phi_s) / (E0 * T0)
+    )  # Synch. freq. for a single particle
 
     K = alpha_c * qe * I / (E0 * T0) * Rsh / (1 + beta)
 
     # Synch. freq. with beam loading
-    tilde_omega_s = numpy.sqrt(omega_s0**2 +
-                               K * omega_rf * numpy.sin(2 * psi) + 1j * 0)
-    return tilde_omega_s / omega0, omega_s0/omega0
+    tilde_omega_s = numpy.sqrt(omega_s0**2 + K * omega_rf * numpy.sin(2 * psi) + 1j * 0)
+    return tilde_omega_s / omega0, omega_s0 / omega0
 
 
 comm = MPI.COMM_WORLD
@@ -51,9 +53,14 @@ rank = comm.Get_rank()
 
 print(size, rank)
 
-ring = at.load_lattice('../../../machine_data/esrf.m')
-ring.enable_6d(cavity_pass='RFCavityPass')
-ring.set_rf_frequency()
+ring = at.load_lattice("../../../machine_data/esrf.m")
+
+ring.enable_6d()
+ring.set_rf_voltage(8.0e6)
+
+Nbunches = 32
+
+current = 150e-3
 
 
 # In fact, the following resonator parameters are for the EBS machine
@@ -62,48 +69,67 @@ beta = 2.8
 q0 = 37500  # unloaded
 qfactor = q0 / (1 + beta)  # loaded
 rshunt = 145 * qfactor * 13  # loaded
-o6, _ = ring.find_orbit6()
 
-ring.set_rf_voltage(8.e6)
 
-Nbunches = 16
-ring.set_fillpattern(Nbunches)
+tunes = ring.get_tune()
+envel = ring.envelope_parameters()
+mcf = at.get_mcf(ring.disable_6d(copy=True))
+tauz = envel.Tau[2] * ring.revolution_frequency
 
-_, fring = at.fast_ring(ring)
-fring.pop(-1)  # drop diffusion element
+simple_ring = at.simple_ring(
+    ring.energy,
+    ring.circumference,
+    ring.harmonic_number,
+    tunes[0],
+    tunes[1],
+    ring.get_rf_voltage(),
+    mcf,
+    U0=envel.U0,
+    tauz=tauz,
+    espread=envel.sigma_e,
+)
 
-# Here we specify whether we want to use PHASOR or WAKE
-# beam loading models.
-mode = 'WAKE'
-if mode == 'WAKE':
-    blm = BLMode.WAKE
-else:
-    blm = BLMode.PHASOR
+simple_ring.set_beam_current(current)
+simple_ring.set_fillpattern(Nbunches)
+simple_ring.pop(2)  # drop diffusion element
+
+simple_ring.set_cavity_phase()
+
 
 # Npart must be at least Nbunches per core
 Npart = Nbunches
 
 # Now we give the fring and convert it
 # into a beam loaded cavity.
-add_beamloading(fring, qfactor, rshunt, Nslice=1,
-                Nturns=50, blmode=blm,
-                VoltGain=0.1, PhaseGain=0.1)
 
-bl_elem = fring[at.get_refpts(fring, BeamLoadingElement)[0]]
+# Be careful with the gains. A voltgain of 1.0
+# might be stable with FBMode.ONETURN, but you
+# will lose the reduction of the coherent tune
+# due to the feedback working too quickly.
+
+add_beamloading(
+    simple_ring,
+    qfactor,
+    rshunt,
+    Nslice=1,
+    VoltGain=0.001,
+    PhaseGain=0.001,
+    buffersize=1000,
+    windowlength=1000,
+    FBMode=FBMode.WINDOW,
+)
+
 
 # Specify some simulation parameters
-kickTurn = 500
+kickTurn = 0
 Nturns = 2**14 + kickTurn
-current = 150e-3
-fring.beam_current = current
 
 part = numpy.zeros((6, Npart))
-part = (part.T + o6).T
 
 if rank == 0:
 
-    print('\n')
-    print('Current:', current)
+    print("\n")
+    print("Current:", current)
 
     z_all = numpy.zeros((Nturns, Nbunches))
     dp_all = numpy.zeros((Nturns, Nbunches))
@@ -115,7 +141,7 @@ if rank == 0:
 # With the former, you should use the BeamMoments element to acquire
 # the means and stds of each bunch turn by turn when using MPI.
 # However, when you want to kick after a certain turn, you have 1
-# of 2 choices. Either you use a BeamMoments element, and split the 
+# of 2 choices. Either you use a BeamMoments element, and split the
 # tracking into 2 (before and after). You can then concatenate the
 # the results. Or you can use the code below to gather all particles
 # yourself with the MPICOMM after each turn.
@@ -123,9 +149,9 @@ if rank == 0:
 for i in numpy.arange(Nturns):
     # Apply a kick to ensure the coherent tune is large
     if i == kickTurn:
-        part[4, :] += 3e-3
+        part[4, :] += 1e-3
 
-    fring.track(part, nturns=1, refpts=None, in_place=True)
+    simple_ring.track(part, nturns=1, refpts=None, in_place=True)
 
     # Gather particles over all cores (compatible with MPI on or off)
     allPartsg = comm.gather(part)
@@ -145,37 +171,51 @@ if rank == 0:
     qscoh = numpy.zeros(Nbunches)
     for ib in numpy.arange(Nbunches):
         dp_dat = dp_all[kickTurn:, ib] - numpy.mean(dp_all[kickTurn:, ib])
-        qs = harmonic_analysis.get_tunes_harmonic(dp_dat,
-                                                  num_harmonics=20,
-                                                  fmin=1e-5, fmax=0.1)
-        qscoh[ib] = qs
+        qs = harmonic_analysis.get_tunes_harmonic(
+            dp_dat, num_harmonics=20, fmin=1e-5, fmax=0.1
+        )
+        qscoh[ib] = qs[0]
 
     qs_mn, qs_std = numpy.array([numpy.mean(qscoh), numpy.std(qscoh)])
 
-    qs_theory, qs_zerocurrent = analytical_qs(ring, current)
+    qs_theory, qs_zerocurrent = analytical_qs(ring, current, qfactor, rshunt, beta)
 
-    print('Analytical:', numpy.real(qs_theory))
-    print('Simulated:', qs_mn, 'pm', qs_std)
+    print("Analytical:", numpy.real(qs_theory))
+    print("Simulated:", qs_mn, "pm", qs_std)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8))
 
     freq = numpy.fft.rfftfreq(Nturns - kickTurn)
     for i in numpy.arange(Nbunches):
         fftdat = numpy.abs(numpy.fft.rfft(dp_all[kickTurn:, i]))
         if i == 0:
-            plt.semilogy(freq, fftdat,
-                         color=cmap.jet(float(i + 1) / Nbunches),
-                         label='Bunch by Bunch FFT')
+            ax2.semilogy(
+                freq,
+                fftdat,
+                color=cmap.jet(float(i + 1) / Nbunches),
+                label="Bunch by Bunch FFT",
+            )
         else:
-            plt.semilogy(freq, fftdat,
-                         color=cmap.jet(float(i + 1) / Nbunches))
+            ax2.semilogy(freq, fftdat, color=cmap.jet(float(i + 1) / Nbunches))
 
-    plt.xlim([0, 8e-3])
-    plt.xlabel('Tune')
-    plt.ylabel('FFT Amp [A.U.]')
-    plt.axvline(numpy.mean(qscoh), label='Coherent Tune',
-                linestyle='dashed', color='k')
-    plt.axvline(numpy.real(qs_theory), label='Analytical Beam Loaded Tune',
-                linestyle='dashed', color='r')
-    plt.axvline(qs_zerocurrent, label='Zero Current Tune',
-                linestyle='dashed', color='b')
-    plt.legend()
+    ax1.plot(1e3 * numpy.mean(z_all, axis=1), color="r")
+    ax1.set_xlabel("Turn")
+    ax1.set_ylabel("<z> [mm]")
+    ax1.set_xlim(0, 1000)
+
+    ax2.set_xlim([0, 8e-3])
+    ax2.set_xlabel("Tune")
+    ax2.set_ylabel("FFT Amp [A.U.]")
+    ax2.axvline(numpy.mean(qscoh), label="Coherent Tune", linestyle="dashed", color="k")
+    ax2.axvline(
+        numpy.real(qs_theory),
+        label="Analytical Beam Loaded Tune",
+        linestyle="dashed",
+        color="r",
+    )
+    ax2.axvline(
+        qs_zerocurrent, label="Zero Current Tune", linestyle="dashed", color="b"
+    )
+    ax2.legend()
+    plt.suptitle("VGain=0.001, PGain=0.001")
     plt.show()
