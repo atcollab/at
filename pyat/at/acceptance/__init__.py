87b82e0648011efa7b3cf20cebdd4d2ac54fcf68
oscarxblanco <oblanco@cells.es>
2026-03-25 13:55:18 +0100

    import all from floodfill_acceptance

--- pyat/at/acceptance/__init__.py
+++ pyat/at/acceptance/__init__.py
@@ -5,4 +5,4 @@ from .boundary import *
 from .acceptance import *
 from .touschek import *
 from .momap_alternative import *
-from .floodfill_acceptance import floodfill
+from .floodfill_acceptance import *
