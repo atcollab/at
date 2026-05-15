51ba4327f41975b0a12575b266b3c0322784cd15
Laurent Farvacque <laurent.farvacque@esrf.fr>
2026-05-15 15:58:21 +0200

    rbend_tune inactive if no multipole

--- pyat/at/lattice/elements/rectangular_bend.py
+++ pyat/at/lattice/elements/rectangular_bend.py
@@ -1,4 +1,4 @@
-"""Additional method for rectangular bending magnets"""
+"""Additional method for rectangular bending magnets."""
 
 from math import sin, cos
 
@@ -12,7 +12,7 @@ __all__ = []
 
 def rbendtune(self: Dipole) -> None:
     # noinspection PyUnresolvedReferences
-    r"""Set *X0ref* and *RefDZ* for rectangular bending magnets
+    r"""Set *X0ref* and *RefDZ* for rectangular bending magnets.
 
     This method must be called after creating a rectangular bending magnet
     or after setting its *PolynomA/B* attributes. It will set the correct *X0ref*
@@ -31,33 +31,30 @@ def rbendtune(self: Dipole) -> None:
     """
 
     def cross(x0r: float):
-        """Return the horizontal exit angle of the reference particle"""
+        """Return the horizontal exit angle of the reference particle."""
         elem.X0ref = x0r
         out = elem.track(np.zeros(6))
         return out[1]
-
-    def checkmul(el):
-        """Check if there are multipoles"""
-        for order in range(el.MaxOrder + 1):
-            if el.PolynomB[order] != 0.0:
-                return True
-        return False
 
     passmethod = self.PassMethod.replace("RadPass", "Pass")
     if passmethod in {
         "BndStrMPoleSymplectic4Pass",
         "ExactRectangularBendPass",
         "ExactRectBendPass",
-    } and checkmul(self):
-        elem = self.copy()
-        elem.PassMethod = passmethod
-        theta = elem.BendingAngle
+    }:
+        # check if there are multipoles
+        if any(self.PolynomB[order] != 0.0 for order in range(self.MaxOrder + 1)):
+            elem = self.copy()
+            elem.PassMethod = passmethod
+            tta = elem.BendingAngle
 
-        # Analytical estimate
-        x0ref = elem.Length * ((cos(0.5 * theta) - 1.0) / theta + sin(0.5 * theta) / 12)
+            # Analytical estimate
+            x0ref = elem.Length * ((cos(0.5 * tta) - 1.0) / tta + sin(0.5 * tta) / 12)
 
-        # cancel output angle
-        x0ref = float(fsolve(cross, x0ref))
+            # cancel output angle
+            x0ref = float(fsolve(cross, x0ref))
+        else:
+            x0ref = 0.0
 
         self.X0ref = x0ref
         rout = self.track(np.zeros(6))
