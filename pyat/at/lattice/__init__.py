ebdd4676dbd9563b4ea9a1780700f45eef55e1f9
Laurent Farvacque <laurent.farvacque@esrf.fr>
2026-05-18 21:01:02 +0200

    new matching

--- pyat/at/lattice/__init__.py
+++ pyat/at/lattice/__init__.py
@@ -19,7 +19,7 @@ from .geometry import *
 from .transformation import *
 from .lattice_object import *
 from .lattice_parameterise import *
-# from .lattice_variables import *
+from .lattice_variables import *
 from .cavity_access import *
 from .deprecated import *
 # Define the module "lattice.constants" for backward compatibility
