285921524898b84dc2a6b515208876416e287276
Simon White <swhite@esrf.fr>
2026-05-19 15:59:31 +0200

    docstring added

--- pyat/at/physics/new_fastring.py
+++ pyat/at/physics/new_fastring.py
@@ -4,7 +4,7 @@ Functions relating to fast_ring
 
 from __future__ import annotations
 
-__all__ = ["fast_ring", "simple_ring"]
+__all__ = ["fast_ring_new"]
 
 from collections.abc import Sequence
 
@@ -60,15 +60,47 @@ def _rearrange(ring: Lattice, split_inds: Refpts = None) -> tuple:
     return newring, all_rings, all_cavs
 
 
-def fast_ring(ring, split_inds: Refpts = None,
+def fast_ring_new(ring, split_inds: Refpts = None,
               qpx: Sequence[float] = None,
               qpy: Sequence[float] = None,
               detuning_coeff: Sequence[float] = None) -> Lattice:
+    """
+    A fast ring consisting in:
+
+    * a RF cavity per distinct frequency,
+    * a 6x6 linear transfer map,
+    * a detuning and chromaticity element,
+    * a quantum diffusion element active when radiations are enabled.
+
+    A new lattices is returned with the same attributes (energy, particle,
+    circumference, periodicity,…) as the initial one. The radiation state is
+    also concerved. Radiations can be turned on or of using ring.disable_6d()
+    or ring.enable_6d().
+
+    It is possible to split the original ring in multiple "fastrings"
+    using the ``split_inds`` argument.
+
+    Parameters:
+        ring:       Lattice description
+        split_inds: List of indexes where to split the ring
+        qpx:            horizontal chromatic detuning coefficients. Default None.
+          If specified qpy should also be provided, if not first order term computed
+          automatically from ring.
+        qpy:            vertical chromatic detuning coefficients. Default None.
+          If specified qpy should also be provided, if not first order term computed
+          automatically from ring.
+        detuning_coeff: First order amplitude detuning coefficients
+          [dQx/dJx, dQx/dJy, dQy/dJy]. Default None: coefficient computer from ring.
+
+    Returns:
+        fastring (Lattice):    Fast ring lattice object
+    """
     new_ring, all_rings, all_cavs = _rearrange(ring, split_inds)
     fastring=[]
     _, o4 = new_ring.disable_6d(copy=True).find_orbit(refpts='xsplit')
     _, o6 = new_ring.enable_6d(copy=True).find_orbit(refpts='xsplit')
-    for r, cav, o4b, o4e, o6b, o6e in zip(all_rings, all_cavs, o4[:-1], o4[1:], o6[:-1], o6[1:]):
+    for r, cav, o4b, o4e, o6b, o6e in zip(all_rings, all_cavs, o4[:-1],
+                                          o4[1:], o6[:-1], o6[1:]):
         lin_elem = gen_m66_elem(r.disable_6d(copy=True), o4b, o4e,
                                 r.enable_6d(copy=True), o6b, o6e)
         fastring = fastring + list(np.atleast_1d(cav)) + [lin_elem]
