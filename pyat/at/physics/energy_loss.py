fd37bbb6d194c9eb483b3d9efc0e7fdf0757beea
Simon White <swhite@esrf.fr>
2026-06-09 13:57:50 +0200

    black

--- pyat/at/physics/energy_loss.py
+++ pyat/at/physics/energy_loss.py
@@ -91,16 +91,18 @@ def get_energy_loss(
             o6l, *_ = ring.disable_6d(RFCavity, copy=True).track(o6)
             delta = np.squeeze(o6l)[4] - o6[4]
         except:
-            msg = ("Closed orbit not found, falling back to enerly loss "
-                   "calculation excluding orbit effects")
+            msg = (
+                "Closed orbit not found, falling back to enerly loss "
+                "calculation excluding orbit effects"
+            )
             warn(AtWarning(msg))
             for e in ring:
-                if e.PassMethod == 'SimpleRadiationRadPass':
-                    delta -= e.U0 / energy #Needed to prevent mixing with rad. damping
-                elif e.PassMethod.endswith('RadPass'):
+                if e.PassMethod == "SimpleRadiationRadPass":
+                    delta -= e.U0 / energy  # Needed to prevent mixing with rad. damping
+                elif e.PassMethod.endswith("RadPass"):
                     ot = e.track(np.zeros(6), energy=energy, particle=particle)
                     delta += ot[4]
-        return -delta*energy
+        return -delta * energy
 
     if isinstance(method, str):
         method = ELossMethod[method.upper()]
