68f6c29b3ececdcaa80deada9cc0faf550d93651
oscarxblanco <oblanco@cells.es>
2026-07-07 16:38:39 +0200

    change 2nd order sign implementation

--- pyat/at/physics/rdt.py
+++ pyat/at/physics/rdt.py
@@ -260,8 +260,11 @@ def _computedrivingterms(
                 }
             )
             # fmt: off
-            bsign = np.array([np.sign(sm[i] - sm) * 1j * b3lm[i] * b3lm
-                              for i in range(nelem)])
+            # The variable imag_and_sign multiplies by -1 the expression in¶
+            # ANL/APS/LS-330 March 10, 2012. Chun-xi Wang. Eq (46)¶
+            # in order to follow AT sign convention.¶
+            imag_and_sign = 1j * (np.tri(nelem, nelem, -1) - 1 + np.tri(nelem))
+            bsign = np.array([imag_and_sign[i] * b3lm[i] * b3lm for i in range(nelem)])
             rbbx = np.array([bsign[i] * rbbetaxm[i] * rbbetaxm
                              for i in range(nelem)])
             rbxy = np.array([bsign[i] * rbetaxm[i] * rbetaxm * betaym[i]
