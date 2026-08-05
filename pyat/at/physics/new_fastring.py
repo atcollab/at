d17af6dc792542357b56a5aa99500665bc416b2b
Simon White <swhite@esrf.fr>
2026-08-05 14:10:10 +0200

    inclued keep_cavities option

--- pyat/at/physics/new_fastring.py
+++ pyat/at/physics/new_fastring.py
@@ -8,7 +8,7 @@ __all__ = ["fast_ring_new"]
 
 from collections.abc import Sequence
 import numpy as np
-from ..lattice import Lattice, Refpts, End
+from ..lattice import Lattice, Refpts, AtError
 from ..lattice import Drift, RFCavity, Element, Marker, SimpleQuantDiff
 from ..physics import gen_m66_elem, gen_detuning_elem, gen_quantdiff_elem
 from ..physics import ELossMethod
@@ -44,11 +44,16 @@ def _rearrange(ring) -> tuple:
     mcavs = _merge_cavs(ring[cav_idx])
     ring[cav_idx] = [_replace_cav(ring[i]) for i in cav_idx]
     ring += mcavs
+    ring += [Marker("xsplit")]
 
 
-def _split_ring(ring: Lattice, split_inds: Refpts | None = None) -> Sequence:
+def _split_ring(ring: Lattice, split_inds: Refpts | None = None, keep_cavities: bool = False) -> Sequence:
     inds = ring.get_bool_index(split_inds, endpoint=True)
     inds[[0, -1]] = True
+    if keep_cavities:
+        cavsb = ring.get_uint32_index(RFCavity)
+        cavse = cavsb + 1
+        inds = inds | ring.get_bool_index(cavsb) | ring.get_bool_index(cavse)
     inds = ring.get_uint32_index(inds)
     split_ring = []
     for b, e in zip(inds[:-1], inds[1:], strict=True):
@@ -56,7 +61,8 @@ def _split_ring(ring: Lattice, split_inds: Refpts | None = None) -> Sequence:
         split_ring += ring[int(b) : int(e)] 
     split_ring += [Marker("xsplit")]
     split_ring = Lattice(split_ring, **vars(ring)) 
-    _rearrange(split_ring)
+    if not keep_cavities:
+        _rearrange(split_ring)
     return split_ring, split_ring.get_uint32_index("xsplit")
 
 
@@ -70,6 +76,7 @@ def fast_ring_new(
     emitx: float = None,
     emity: float = None,
     espread: float = None,
+    keep_cavities: bool = False,
 ) -> Lattice:
     """
     A fast ring consisting in the following elements.
@@ -87,6 +94,13 @@ def fast_ring_new(
 
     It is possible to split the original ring in multiple "fastrings"
     using the ``split_inds`` argument.
+    
+    By default all the cavities are merged ans placed at the end of the ring this
+    allows to minimize the number of segments and therefore lattice elements.
+    Howevever, the longitudinal dynamics is modified by this action. In order to
+    preserve the original RF configuration the ``keep_cavities`` argument should
+    be set to true. In this configuration the tracking is slower as more segment are
+    introduced.
 
     Parameters:
         ring:       Lattice description
@@ -107,34 +121,44 @@ def fast_ring_new(
         emitx: Equilibrium horizontal emittance. Default is the one given by ring
         emity: Equilibrium vertical emittance. Default is the one given by ring
         espread: Equilibrium energy spread. Default is the one given by ring
+        keep_cavities: The ring will be sliced at each cavities. This allows to
+          match longitudinal dynamics of the original ring bu t slows down the
+          tracking as more segments are generated
 
     Returns:
         fastring (Lattice):    Fast ring lattice object
     """
-    split_ring, split_inds = _split_ring(ring, split_inds)
-    _, o4 = split_ring.disable_6d(copy=True).find_orbit(refpts=split_inds)
+    split_ring, split_inds = _split_ring(ring, split_inds, keep_cavities)
+    _, o4 = split_ring.disable_6d(copy=True).find_orbit(refpts=split_inds)  
     _, o6 = split_ring.enable_6d(copy=True).find_orbit(refpts=split_inds)
-    _, o4f = split_ring.disable_6d(copy=True).find_orbit(refpts=End)
-    _, o6f = split_ring.enable_6d(copy=True).find_orbit(refpts=End)
+    
     fastring = []
     for o4b, o4e, o6b, o6e, sib, sie in zip(o4[:-1], o4[1:], o6[:-1], o6[1:], split_inds[:-1], split_inds[1:]):
-        r = split_ring[sib:sie]
-        lin_elem = gen_m66_elem(
-            r.disable_6d(copy=True), o4b, o4e, r.enable_6d(copy=True), o6b, o6e
-        ) 
-        fastring = [*fastring, lin_elem]
-    fastring = [*fastring, *list(split_ring[split_inds[-1]+1:])]
+        r = split_ring[sib+1:sie]
+        cond1 = np.all([isinstance(elem, RFCavity) for elem in r])
+        cond2 = np.all([not isinstance(elem, RFCavity) for elem in r])
+        if cond1:
+            fastring = [*fastring, *list(r)]
+        elif cond2:
+            lin_elem = gen_m66_elem(
+                r.disable_6d(copy=True), o4b, o4e, r.enable_6d(copy=True), o6b, o6e
+            ) 
+            fastring = [*fastring, lin_elem]
+        else:
+            msg = ("One fast ring segment contains a combination of magnets and cavity."
+                   "They need to be separated")
+            raise AtError(msg)
     detuning_elem = gen_detuning_elem(
         split_ring,
         qpx=qpx,
         qpy=qpy,
         detuning_coeff=detuning_coeff,
         alphac=alphac,
-        orbit=o4f[-1],
-        orbit6=o6f[-1],
+        orbit=o4[-1],
+        orbit6=o6[-1],
     )
     if emity is None and emitx is None and espread is None:
-        qd_elem = gen_quantdiff_elem(split_ring.enable_6d(copy=True), orbit=o6f[-1])
+        qd_elem = gen_quantdiff_elem(split_ring.enable_6d(copy=True), orbit=o6[-1])
     else:
         params = ring.enable_6d(copy=True).envelope_parameters()
         taux, tauy, tauz = params.Tau
