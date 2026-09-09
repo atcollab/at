7dcbdf8bbf99174d13496f99a8966b1d6c8736fb
oscarxblanco <oblanco@cells.es>
2026-09-09 19:16:29 +0200

    use atsearch.c

--- atintegrators/ImpedanceTablePass.c
+++ atintegrators/ImpedanceTablePass.c
@@ -1,4 +1,5 @@
 #include "atelem.c"
+#include "atsearch.c"
 #include <math.h>
 #include <float.h>
 
@@ -32,25 +33,6 @@ double getWake(double *waketable,double *waketableT,double distance,int index){
         return 0;
     }else{
         return w;
-    };
-};
-
-
-int binarySearch(double *array,double value,int upper,int lower,int nStep){
-    int pivot = (int)(lower+upper)/2;
-    if ((upper-lower)<=1){
-        return lower;
-    };
-    if (value < array[pivot]){
-        upper = pivot;
-        nStep+=1;
-        return binarySearch(array,value,upper,lower,nStep);
-    } else if (value > array[pivot]){
-        lower = pivot;
-        nStep+=1;
-        return binarySearch(array,value,upper,lower,nStep);
-    }else{
-        return pivot;
     };
 };
 
