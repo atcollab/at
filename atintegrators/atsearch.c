3017c9e98dfb9254ce78d38030b697b4a33791e4
oscarxblanco <oblanco@cells.es>
2026-09-09 19:16:15 +0200

    add atsearch

---
+++ atintegrators/atsearch.c
@@ -0,0 +1,23 @@
+/* Implementation of search algorithms for AT. */
+
+//#include "atelem.c"
+
+
+int binarySearch(double *array,double value,int upper,int lower,int nStep){
+    int pivot = (int)(lower+upper)/2;
+    if ((upper-lower)<=1){
+        return lower;
+    };
+    if (value < array[pivot]){
+        upper = pivot;
+        nStep+=1;
+        return binarySearch(array,value,upper,lower,nStep);
+    } else if (value > array[pivot]){
+        lower = pivot;
+        nStep+=1;
+        return binarySearch(array,value,upper,lower,nStep);
+    }else{
+        return pivot;
+    };
+};
+
