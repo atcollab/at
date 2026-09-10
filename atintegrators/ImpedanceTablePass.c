1e74200774844fa81fcc650c37ebdac9c6e131aa
oscarxblanco <oblanco@cells.es>
2026-09-10 15:26:35 +0200

    remove getWake

--- atintegrators/ImpedanceTablePass.c
+++ atintegrators/ImpedanceTablePass.c
@@ -26,17 +26,6 @@ struct elem
 };
 
 
-double getWake(double *waketable,double *waketableT,double distance,int index){
-    double w = waketable[index] + (distance-waketableT[index])*(waketable[index+1]-waketable[index])/
-          (waketableT[index+1]-waketableT[index]);
-    if(atIsNaN(w)){
-        return 0;
-    }else{
-        return w;
-    };
-};
-
-
 void impedance_tablePass(double *r_in,int num_particles, struct elem *Elem){
     
     /*
@@ -192,11 +181,11 @@ void impedance_tablePass(double *r_in,int num_particles, struct elem *Elem){
                 register double dx = xpos[ii];
                 register double dy = ypos[ii];
                 int index = binarySearch(waketableT,-ds,nelem,0,0);              
-                double fieldx = getWake(waketableDX,waketableT,-ds,index);
-                double fieldy = getWake(waketableDY,waketableT,-ds,index);
-                double fieldx2 = getWake(waketableQX,waketableT,-ds,index);
-                double fieldy2 = getWake(waketableQY,waketableT,-ds,index);
-                double fieldz = getWake(waketableZ,waketableT,-ds,index);
+                double fieldx = interpolTable(waketableDX,waketableT,-ds,index);
+                double fieldy = interpolTable(waketableDY,waketableT,-ds,index);
+                double fieldx2 = interpolTable(waketableQX,waketableT,-ds,index);
+                double fieldy2 = interpolTable(waketableQY,waketableT,-ds,index);
+                double fieldz = interpolTable(waketableZ,waketableT,-ds,index);
                 kx[i] += fx*wi*fieldx*dx;
                 ky[i] += fy*wi*fieldy*dy;
                 kx2[i] += fqx*wi*fieldx2;
