9e66d2ce17e1cfa7010496858275e7d8ae8ec9d1
oscarxblanco <oblanco@cells.es>
2026-09-15 19:03:11 +0200

    printf

--- atintegrators/interpolate.c
+++ atintegrators/interpolate.c
@@ -17,7 +17,7 @@
 int binarySearch(double *array,double value,int upper,int lower,int nStep){
     int pivot = (int)(lower+upper)/2;
     if (nStep > 200){
-       printf("Warning: binarySearch could not converge, -1 returned.");
+       printf("Warning: binarySearch could not converge, -1 returned.\n");
        return -1;
     };
     if ((upper-lower)<=1){
